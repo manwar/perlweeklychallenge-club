c70540c41e15aadbed7cf3df04a6a982f94059c1
Ailbhe Tweedie <atweedie@protonmail.ch>
2019-04-02 11:20:25 +0200

    002-p5-01: add initial solution

---
+++ challenge-002/ailbhe-tweedie/perl5/ch-01.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+#
+# Solution to challenge #1 of the Perl Weekly Challenge 002.
+# 
+# TODO: I would like to add some automated testing to this solution.
+
+$number = '001';
+
+$number =~ s/^0+//;
+
+print "$number\n";
