2fb6c2e357732c020209ea130df2b96290a36844
Ailbhe Tweedie <atweedie@protonmail.ch>
2019-04-02 11:29:31 +0200

    002-p5-01: read from STDIN

--- challenge-002/ailbhe-tweedie/perl5/ch-01.pl
+++ challenge-002/ailbhe-tweedie/perl5/ch-01.pl
@@ -1,11 +1,9 @@
 #!/usr/bin/env perl
 #
 # Solution to challenge #1 of the Perl Weekly Challenge 002.
-# 
-# TODO: I would like to add some automated testing to this solution.
 
-$number = '001';
-
-$number =~ s/^0+//;
-
-print "$number\n";
+while (<>) {
+	chomp;
+	$_ =~ s/^0+//;
+	print "$_\n";
+}
