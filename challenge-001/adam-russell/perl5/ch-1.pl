e658f5c0aa5f41c468d60e1a62779b3cbb96412e
Adam Russell <adam.russell@optum.com>
2019-04-03 15:15:25 -0400

    Added solution for challenge-001.

---
+++ challenge-001/adam-russell/perl5/ch-1.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings; 
+##
+# Challenge #1
+# Write a script to replace the character 'e' with 'E' in the string 'Perl Weekly Challenge'. 
+# Also print the number of times the character 'e' is found in the string.
+##
+my $challenge_string="Perl Weekly Challenge";
+my $number=do{
+    $challenge_string=~tr/e/E/
+};
+print "$number $challenge_string\n"; 
