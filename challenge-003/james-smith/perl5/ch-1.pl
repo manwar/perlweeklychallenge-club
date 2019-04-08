7d63a391e750a31d5a73f7f985e78f2751008533
bagheera-sands <git@sandsscouts.org.uk>
2019-04-08 12:29:16 +0100

    fixed minor code bug

--- challenge-003/james-smith/perl5/ch-1.pl
+++ challenge-003/james-smith/perl5/ch-1.pl
@@ -1,21 +1,22 @@
 use strict;
 use feature ':5.10';
 
-my @hammings = (1);
+my @hammings = ();
 
 sub next_hamming {
-  my $lowest;
+  my $lowest = 1;
   local $_;
-  foreach my $n (2,3,5) {
-    foreach (@hammings) {
-      next if $_*$n <= $hammings[-1];
-      $lowest = $_*$n unless $lowest && $lowest < $_*$n;
-      last;
-    }
+  ## Find first multiple of 2,3,5 > max hamming no in array
+  my $n = 5; my @other = (3,2);
+  foreach (@hammings) {
+    next if $_*$n <= $hammings[-1];
+    $lowest = $_*$n if $lowest == 1 || $lowest > $_*$n;
+    last unless @other;
+    $n = shift @other;
+    redo;
   }
   push @hammings, $lowest;
   return $lowest;
 }
 
-say 1;
 say next_hamming() foreach 1..shift;
