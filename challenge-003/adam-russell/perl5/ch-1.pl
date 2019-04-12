8b74c2038a052927add114b4a69f4b7c214e1ad0
Adam Russell <adam.russell@optum.com>
2019-04-12 15:43:43 -0400

    initial commit for challenge-003

---
+++ challenge-003/adam-russell/perl5/ch-1.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+##
+# Create a script to generate 5-smooth numbers, whose prime divisors are less or equal to 5. 
+# They are also called Hamming/Regular/Ugly numbers.  
+##
+ 
---
+++ challenge-003/adam-russell/perl5/ch-2.pl
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+##
+# Create a script that generates Pascal Triangle. 
+# Accept number of rows from the command line. 
+# The Pascal Triangle should have at least 3 rows.
+## 
+
