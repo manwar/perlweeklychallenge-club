216c92d674b9f61e082ed55431a310d2b6819d7a
k-mx <cppfero@gmail.com>
2019-04-21 14:40:52 +0500

    Comment about B::Deparse - musthave feature for perl developer

--- challenge-004/maxim-kolodyazhny/perl5/ch-2.pl
+++ challenge-004/maxim-kolodyazhny/perl5/ch-2.pl
@@ -3,7 +3,11 @@
 use strict;
 use warnings;
 
+# usage:
 # cat words | perl -p ./ch-2.pl foobar
+#
+# you can figure out whats actually going on this way:
+# perl -MO=Deparse -p ./ch-2.pl
 
 our $letters;
 
