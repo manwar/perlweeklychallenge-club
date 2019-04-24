0527cda37094b391eb39858f2a5ebfde09d86f11
John Barrett <john@jbrt.org>
2019-04-24 10:14:43 +0100

    Extract sort / lc op to function

--- challenge-005/john-barrett/perl5/ch-1.pl
+++ challenge-005/john-barrett/perl5/ch-1.pl
@@ -4,13 +4,15 @@ use strict;
 use warnings;
 use autodie;
 
-my $word = join '', sort { $a cmp $b } split '', lc $ARGV[0];
+my $word = sortedlc( $ARGV[0] );
 my $wordlength = length $word;
+
+sub sortedlc { join '', sort { $a cmp $b } split '', lc $_[0] }
 
 sub is_anagram {
     my ( $dictword ) = @_;
     return 0 if length $dictword != $wordlength;
-    $word eq join '', sort { $a cmp $b } split '', lc $dictword;
+    $word eq sortedlc $dictword;
 }
 
 open my $fh, '<:encoding(UTF-8)', '/usr/share/dict/words';
