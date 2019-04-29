f5603bb8366797ae519bb8f1e7a3a4bbb257c15a
Joelle Maslak <jmaslak@antelope.net>
2019-04-29 00:09:57 -0600

    Perl 5 solution to Week 6, Problem 2

---
+++ challenge-006/joelle-maslak/perl5/ch-2.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use v5.26;
+use strict;
+use warnings;
+
+use bignum;
+
+#
+# Copyright © 2019 Joelle Maslak
+# All Rights Reserved - See License
+#
+# I am assuming that we really wanted Ramanujan's Constant, and not the
+# linked to Wiki page on the Landau-Ramanujan Constant which is
+# something else entirely!
+#
+# Checked against http://oeis.org/A060295
+
+# We'll give *33* digits of precison, not 32.  ;)
+say substr(640320**3 + 744 - 196844/(640320**3 + 744), 0, 34);
+
