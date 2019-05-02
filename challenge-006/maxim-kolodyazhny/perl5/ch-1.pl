deae72c45b317c9c99566cddc40c59ba1032b073
k-mx <cppfero@gmail.com>
2019-05-02 11:49:17 +0500

    ch-1.pl solution by Maxim Kolodyazhny

---
+++ challenge-006/maxim-kolodyazhny/perl5/ch-1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl -p
+
+# echo 1,2,3,4,42,43,45 | ch-1.pl
+
+use 5.028.1;
+use List::Util qw(sum);
+
+s{
+    (\b \d+)
+    (,?\d+,)+
+    (\d+ \b)
+    (??{
+        # this block is treated as a pattern
+        # != will return '' (always match) or 1 (fail because of previous \b)
+        ($3-$1+1)*($1+$3)/2 != sum split ',', $&;
+    })
+}
+{$1-$3}xg;
