f672631668e142626182cffbd82d83898593b565
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-06-13 19:20:54 -0600

    Create ch-1.pl

---
+++ challenge-012/jaime/perl5/ch-1.pl
@@ -0,0 +1,8 @@
+# Solution by Jaime Corchado, (@tortsnare)[https://twitter.com/tortsnare].
+#
+# Challenge #1
+#
+# The numbers formed by adding one to the products of the smallest
+# primes are called the Euclid Numbers (see wiki). Write a script that
+# finds the smallest Euclid Number that is not prime.
+
