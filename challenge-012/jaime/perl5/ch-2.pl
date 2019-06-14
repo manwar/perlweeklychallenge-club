c40f4a63c65ee34e3ec96197559b8c90e783a1e6
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-06-13 19:20:58 -0600

    Create ch-2.pl

---
+++ challenge-012/jaime/perl5/ch-2.pl
@@ -0,0 +1,17 @@
+# Solution by Jaime Corchado, (@tortsnare)[https://twitter.com/tortsnare].
+#
+# Challenge #2
+#
+# Write a script that finds the common directory path, given a
+# collection of paths and directory separator. For example, if the
+# following paths are supplied.
+#
+# ```
+# /a/b/c/d
+# /a/b/cd
+# /a/b/cc
+# /a/b/c/d/e
+# ```
+#
+# and the path separator is `/`. Your script should return `/a/b` as common
+# directory path.
