cbf44639456e37d33be3d533a111ce8ed54094b7
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-06-26 11:48:31 -0600

    Create ch-2.pl
    Using only the official postal abbreviations for the 50 U.S. states, write a script to find the longest English word you can spell.

---
+++ challenge-014/jaime/perl5/ch-2.pl
@@ -0,0 +1,7 @@
+# Solution by Jaime Corchado, (@tortsnare)[https://twitter.com/tortsnare].
+# 
+# Challenge #2
+# 
+# Using only the official postal abbreviations for the 50 U.S. states,
+# write a script to find the longest English word you can spell.
+
