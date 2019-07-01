f1461f387051469a5169827a29f1a74b8e3e806d
Steven Wilson <steven1170@zoho.eu>
2019-07-01 16:05:19 +0100

    add handling of command-line arguments and usage

--- challenge-015/steven-wilson/perl5/ch-2.pl
+++ challenge-015/steven-wilson/perl5/ch-2.pl
@@ -7,6 +7,9 @@
 # Write a script to implement Vigenère cipher. The script should be able
 # encode and decode. Checkout wiki page for more information.
 # https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+#   Usage:  $ perl ch-2.pl OPERATION KEYWORD MESSAGE
+#           $ perl ch-2.pl -e LEMONS ATTACKATDAWN  # encrypt a message
+#           $ perl ch-2.pl -d LEMONS LXFOPCLXPOJF  # decrypt a message
 
 use strict;
 use warnings;
@@ -14,13 +17,28 @@ use feature qw / say /;
 use Test::More;
 
 my @alphabet = ("A".."Z");
-my $plaintext;
-my $keyword;
-my $cyphertext;
+my ($operation, $keyword, $in_text) = ($ARGV[0], $ARGV[1], $ARGV[2]);
+my $out_text;
+my %operations = (
+    '-e' => 'encrypted',
+    '-d' => 'decrypted',
+);
 
 ok( encode( "ATTACKATDAWN", "LEMON" ) eq "LXFOPVEFRNHR", "encode working" );
 ok( decode( "LXFOPVEFRNHR", "LEMON" ) eq "ATTACKATDAWN", "decode working" );
 done_testing();
+
+if ($operation eq "-e"){
+    $out_text = encode($in_text, $keyword);
+}
+elsif ($operation eq "-d"){
+    $out_text = decode($in_text, $keyword);
+}
+else {
+    die "$operation is not a valid operation";
+}
+
+say "Message $operations{$operation} to '$out_text'";
 
 sub encode {
     my ( $plaintext, $keyword ) = @_;
