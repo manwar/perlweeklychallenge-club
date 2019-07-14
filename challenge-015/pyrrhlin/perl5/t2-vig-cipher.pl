c391c81f1858e84d2f2303be25e948bd1a2b7f0d
Michael Hamlin <1197072+myrrhlin@users.noreply.github.com>
2019-07-14 09:28:21 -0400

    add help to cipher test script

--- challenge-015/pyrrhlin/perl5/t2-vig-cipher.pl
+++ challenge-015/pyrrhlin/perl5/t2-vig-cipher.pl
@@ -9,7 +9,9 @@ my %opt;
 GetOptions(\%opt,
     'key|k=s@',
     'msg|m=s',
+    'help|h|?',
 );
+helpout() if $opt{help};
 
 my $plaintext = $opt{msg} || 'attack at dawn';
 my @keys = $opt{key} ? @{ $opt{key} } : 'lemon';
@@ -20,3 +22,14 @@ my $decoded = vdecode($cipher, reverse @keys);
 print "ciphertext: $cipher\n";
 print "decoded:    $decoded\n";
 
+sub helpout {
+    print <<EOHELP;
+This script demonstrates the Vigenère cipher.  Options:
+  --msg|m MSG  specify the text to be encoded
+  --key|k KEY  specify the text to use a key
+(Default values are used for demonstration purposes.)
+You may specify multiple keys (by giving multiple --key options), in 
+which case multiple rounds of encoding take place.
+EOHELP
+    exit(0);
+}
