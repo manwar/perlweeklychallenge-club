f9a095fa25cbdadf0548b73a4a8df0f89ee5e79b
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-07-14 07:10:07 +0100

    - Added solutions by Michael Hamlin.

---
+++ challenge-015/michael-hamlin/perl5/GetPrime.pm
@@ -0,0 +1,48 @@
+#! /usr/bin/env perl
+
+#  find primes (sort-of) efficiently (without a sieve)
+#  for an exercise without using cpan...
+
+package GetPrime;
+
+use 5.18.0;
+use parent 'Exporter';
+our @EXPORT = qw<getprime>;
+
+use integer;
+
+my @primes = (2, 3, 5);
+sub getprime ($) {
+    my $n = shift || die "must give an ordinal number to getprime";
+    push @primes, _nextprime() while @primes < $n;
+    return $primes[$n - 1];
+}
+
+sub _nextprime {
+    my $n;
+    for ($n = $primes[-1] + 2; _hasfactor($n); $n += 2) { }
+    return $n;
+}
+sub _hasfactor {
+    my ($n) = @_;
+    # dont bother testing when ending in 5...
+    return 5 if $n =~ /5$/;
+    # we only get odds, so we dont need...
+    # return 2 if $n =~ /[02468]$/;
+
+    my $maxfactor = sqrt($n);
+    foreach my $p (@primes) {
+        return $p unless $n % $p;
+        last if $p >= $maxfactor;
+    }
+    return 0;
+}
+
+1;
+
+__END__
+#foreach (1..20) {
+#    printf "%2u: %u\n", $_, getprime($_);
+#}
+say 'the hundredth prime is ', getprime(100);
+
---
+++ challenge-015/michael-hamlin/perl5/ch-2.pl
@@ -0,0 +1,23 @@
+#! /usr/bin/env perl
+
+use 5.18.0;
+use Vigenere;
+
+use Getopt::Long;
+
+my %opt;
+GetOptions(\%opt,
+    'key|k=s@',
+    'msg|m=s',
+);
+
+my $plaintext = $opt{msg} || 'attack at dawn';
+my @keys = $opt{key} ? @{ $opt{key} } : 'lemon';
+
+my $cipher  = vencode($plaintext, @keys);
+my $decoded = vdecode($cipher, reverse @key
+s);
+
+print "ciphertext: $cipher\n";
+print "decoded:    $decoded\n";
+
