8730a72d03faaed9e6c4f419e9b06805fb3171f2
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-07-26 19:04:16 -0600

    Create ch-1.pl

---
+++ challenge-018/jaime/perl5/ch-1.pl
@@ -0,0 +1,8 @@
+#
+# # Challenge #1
+#
+# Write a script that takes 2 or more strings as command line parameters
+# and print the longest common substring. For example, the longest
+# common substring of the strings “ABABC”, “BABCA” and “ABCBA” is string
+# “ABC” of length 3. Other common substrings are “A”, “AB”, “B”, “BA”,
+# “BC” and “C”.
