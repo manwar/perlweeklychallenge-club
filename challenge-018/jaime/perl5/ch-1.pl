81caa1238297c1e7aba934168200c314265bd735
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-07-26 23:51:27 -0600

    Update ch-1.pl
    Iterate ordered substrings to find largest common substring.

--- challenge-018/jaime/perl5/ch-1.pl
+++ challenge-018/jaime/perl5/ch-1.pl
@@ -2,7 +2,23 @@
 # # Challenge #1
 #
 # Write a script that takes 2 or more strings as command line parameters
-# and print the longest common substring. For example, the longest
-# common substring of the strings “ABABC”, “BABCA” and “ABCBA” is string
-# “ABC” of length 3. Other common substrings are “A”, “AB”, “B”, “BA”,
-# “BC” and “C”.
+# and print the longest common substring.
+
+sub of_size {
+    my ($n,$s) = @_;
+    my @subs = ();
+    for (my $i = 0; ($i+$n) <= length $s; $i++) {
+	push @subs, substr($s,$i,$n);
+    }
+    return @subs; #substrings of $s of length $n.
+}
+
+my $head = shift;
+for my $n (reverse 1..(length $head)) {
+    for my $s (of_size($n,$head)) {
+	if (@ARGV == grep(/$s/,@ARGV)) {
+	    print "$s\n";
+	    exit;
+	}
+    }
+}
