6e475c2d984d2396ed72b73919d0e03e8aca8e09
Jaime <42359730+bracteatus@users.noreply.github.com>
2019-07-27 17:16:50 -0600

    Update ch-2.pl
    Snappy implementation of priority queue.

--- challenge-018/jaime/perl5/ch-2.pl
+++ challenge-018/jaime/perl5/ch-2.pl
@@ -14,3 +14,21 @@
 # 3. pull_highest_priority_element: remove the element from the queue
 # that has the highest priority, and return it. If two elements have the
 # same priority, then return element added first.
+
+my $queue = {};
+
+sub is_empty {
+    return not map { @{$_} } values $queue;
+}
+
+sub insert_with_priority {
+    my ($priority,$body) = @_;
+    $queue->{$priority} = [] unless $queue->{$priority};
+    push @{$queue->{$priority}}, $body;
+}
+
+sub pull_highest_priority_element {
+    for my $priority (reverse sort keys %$queue) {
+	return shift @{$queue->{$priority}} if @{$queue->{$priority}};
+    }
+}
