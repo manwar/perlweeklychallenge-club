1c5eece32e0d56b33649bd570968a607174d27d9
Roger Bell_West <roger@firedrake.org>
2019-08-05 10:03:48 +0100

    Remove premature optimisation

--- challenge-020/roger-bell-west/perl5/ch-2.pl
+++ challenge-020/roger-bell-west/perl5/ch-2.pl
@@ -11,17 +11,11 @@ my $a=Math::BigInt->new(1);
 while (1) {
   $a++;
   my @a=grep {$_ != $a} divisors($a);
-  unless (@a) {
-    next;
-  }
   my $b=sum(@a);
   if ($b <= $a) {
     next;
   }
   my @b=grep {$_ != $b} divisors($b);
-  unless (@b) {
-    next;
-  }
   my $aa=sum(@b);
   if ($aa == $a) {
     print "$a, $b\n";
