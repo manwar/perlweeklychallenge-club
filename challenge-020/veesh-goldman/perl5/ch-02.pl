090af051b19abda6d8c10b7cafe0eb8a119bd9ec
LapVeesh <rabbiveesh@gmail.com>
2019-08-11 23:46:33 +0300

    Added answers to challenge 20. Many regexes

---
+++ challenge-020/veesh-goldman/perl5/ch-02.pl
@@ -0,0 +1,24 @@
+#! /usr/bin/env perl
+use v5.22;
+
+sub sum_of_proper_divisors {
+  #start with one, because that is always a divisor
+  my $sum = 1;
+  (1 x shift) =~ /^ (..+) \g1+ $ (?{ $sum += length $1 }) ./xg;
+  return $sum
+}
+
+sub has_amicable {
+  my $start = shift;
+  #sometimes the sum of the divisors IS the number. But that's not amicable
+  return 0 if sum_of_proper_divisors($start) == $start;
+  return $start == sum_of_proper_divisors( sum_of_proper_divisors($start) )
+}
+
+
+my $num;
+while (1) {
+  last if has_amicable(++$num);
+}
+
+say "$num, ${\sum_of_proper_divisors($num)} is your pair"
