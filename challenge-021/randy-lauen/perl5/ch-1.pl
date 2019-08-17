6f1606aa134f39ea5bec4938723ecc551025394d
Randy Lauen <randy.lauen@gmail.com>
2019-08-17 12:56:10 -0500

    calculate e

--- challenge-021/randy-lauen/perl5/ch-1.pl
+++ challenge-021/randy-lauen/perl5/ch-1.pl
@@ -6,10 +6,13 @@ use warnings;
 
 # Write a script to calculate the value of e, also known as Euler’s number and Napier’s constant.
 
-# Instead of implementing it myself, I took a tour through
-# some of the CPAN modules that do it for you.
+# In addition to calculating it myself, I took a tour through
+# some of the CPAN modules that do it for you. Some modules,
+# like Math::Pari, have a function called Euler that returns
+# Euler's constant, which is not the same as Euler's number.
 
 use Math::Big ();
+use Math::BigFloat ();
 use Math::NumberCruncher ();
 use Math::Symbolic ();
 use Math::AnyNum ();
@@ -24,6 +27,8 @@ my @eulers = (
     [ 'Math::AnyNum::euler',           sub { Math::AnyNum->e } ],
     [ 'Sidef Number.e',                sub { Sidef->new()->execute_code("Number.e()") } ],
     [ 'bigrat::e',                     sub { require bigrat; bigrat::e() } ],
+    # My calculated version of e starts to differ around the 35th decimal.
+    [ 'Calculated e',                  sub { substr( e(), 0, 48 ) . '...'  } ],
 );
 
 my @table = [ 'Module', q{Euler's Number} ];
@@ -32,4 +37,22 @@ foreach my $euler ( @eulers ) {
 }
 
 say Text::Table::Tiny::generate_table( header_row => 1, rows => \@table );
+
+exit 0;
+
+
+sub e {
+    my $e = Math::BigFloat->bzero;
+    for my $n ( 0 .. 50 ) {
+        $e += Math::BigFloat->bone / Math::BigFloat->new( factorial( $n ) );
+    }
+    return $e;
+}
+
+
+sub factorial {
+    my $n = shift;
+    return 1 if $n == 0;
+    return $n * factorial( $n - 1 );
+}
 
