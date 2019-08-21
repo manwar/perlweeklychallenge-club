2a43422f090e458b8c36478cea30fbd75bc5f2fa
Steven Wilson <steven1170@zoho.eu>
2019-08-21 22:49:59 +0100

    push primes earlier

--- challenge-022/steven-wilson/perl5/ch-1.pl
+++ challenge-022/steven-wilson/perl5/ch-1.pl
@@ -22,11 +22,11 @@ my @sexy_prime_pairs;
 my $next_prime = 2;
 
 while ( scalar @sexy_prime_pairs < 10 ) {
+    push @primes, $next_prime;
     if ( grep { $_ == ( $next_prime - 6 ) } @primes ) {
         push @sexy_prime_pairs, [ ( $next_prime - 6 ), $next_prime ];
     }
     $next_prime = next_prime($next_prime);
-    push @primes, $next_prime;
 }
 
 say "First 10 Sexy Prime Pairs are:";
