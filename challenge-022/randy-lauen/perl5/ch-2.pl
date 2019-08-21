7aa9479fd3773951c93c70004d7e8336402954a6
Randy Lauen <randy.lauen@gmail.com>
2019-08-21 17:11:19 -0500

    encode function

---
+++ challenge-022/randy-lauen/perl5/ch-2.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+# Task:
+# Write a script to implement Lempel–Ziv–Welch (LZW) compression algorithm. 
+# The script should have method to encode/decode algorithm.
+
+# This is a port of LZW-B found at https://marknelson.us/posts/2011/11/08/lzw-revisited.html
+
+encode( $ARGV[0] );
+
+sub encode {
+    my $file = shift;
+
+    open my $in, '<', $file or die $!;
+    open my $out, '>', "$file.lzw" or die $!;
+
+    my %codes = map { chr($_) => $_ } 0 .. 255;
+    my $next_code = 256;
+    my $current_string = '';
+
+    while ( my $line = <$in> ) {
+        foreach my $char ( split //, $line ) {
+            $current_string .= $char;
+            if ( !exists $codes{ $current_string } ) {
+                $codes{ $current_string } = $next_code++;
+                $current_string = substr( $current_string, 0, -1 );
+                print $out pack( 'S', $codes{ $current_string} );
+                $current_string = $char;
+            }
+        }
+
+        if ( length $current_string ) {
+            print $out pack( 'S', $codes{ $current_string} );
+        }
+    }
+
+    close $in;
+    close $out;
+
+    return;
+}
+
