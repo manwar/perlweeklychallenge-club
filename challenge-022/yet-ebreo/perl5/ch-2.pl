827e7bd922a9817cd31eb32d4af80cb467293fa2
Doomtrain14 <yet.ebreo@gmail.com>
2019-08-22 21:06:47 +0800

    Updated code for compressing

--- challenge-022/yet-ebreo/perl5/ch-2.pl
+++ challenge-022/yet-ebreo/perl5/ch-2.pl
@@ -85,7 +85,7 @@ sub decode {
     return $ret;
 }
 
-my $string = "TOBEORNOTTOBEORTOBEORNOT"; #"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+my $string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 print ("Original String has length ". (length $string)."\n");
 print "$string\n\n";
 
