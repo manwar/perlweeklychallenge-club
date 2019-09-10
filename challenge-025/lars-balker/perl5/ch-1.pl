b9918afa6541dd8fb9b4080c1670a4d2f7fc5fbc
Lars Balker <Lars.Balker@teliacompany.com>
2019-09-10 17:11:12 +0200

    25 ch1

---
+++ challenge-025/lars-balker/perl5/ch-1.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+use v5.20;
+use strict;
+use warnings;
+
+# Generate a longest sequence of the following “English Pokemon” names
+# where each name starts with the last letter of previous name.
+
+my @names = qw/
+    audino bagon baltoy banette bidoof braviary bronzor carracosta
+    charmeleon cresselia croagunk darmanitan deino emboar emolga
+    exeggcute gabite girafarig gulpin haxorus heatmor heatran ivysaur
+    jellicent jumpluff kangaskhan kricketune landorus ledyba loudred
+    lumineon lunatone machamp magnezone mamoswine nosepass petilil
+    pidgeotto pikachu pinsir poliwrath poochyena porygon2 porygonz
+    registeel relicanth remoraid rufflet sableye scolipede scrafty
+    seaking sealeo silcoon simisear snivy snorlax spoink starly
+    tirtouga trapinch treecko tyrogue vigoroth vulpix wailord
+    wartortle whismur wingull yamask
+/;
+
+my $longest = [];
+
+try([], "", @names);
+
+say "@$longest";
+
+# naive O(n^2) recursion
+sub try {
+    my ($picks, $prefix, @rest) = @_;
+
+    $longest = $picks if @$picks > @$longest;
+
+    for (my $i = 0; $i < @rest; ++$i) {
+        next unless $rest[$i] =~ /^$prefix/;
+        try([@$picks, $rest[$i]],
+            substr($rest[$i], -1),
+            @rest[0..$i-1, $i+1..$#rest]);
+    }
+}
---
+++ challenge-025/lars-balker/perl5/ch-2.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+use v5.20;
+use strict;
+use warnings;
+
+# Create script to implement Chaocipher. Please checkout
+# https://en.wikipedia.org/wiki/Chaocipher for more information.
+
