23ae84ea0cdc089535a857109c8f38619d4ffcb9
Adam Russell <ac.russell@live.com>
2019-09-29 15:58:21 -0400

    solution for challenge 027

--- challenge-027/adam-russell/perl5/Audit.pm
+++ challenge-027/adam-russell/perl5/Audit.pm
@@ -1,30 +1,37 @@
 package Audit{
+    use Data::Dump q/pp/;
     use Filter::Simple;
 
     our %Log;
 
     sub audit{
         my($name, $value) = @_; 
-        $value = "undef" if !$value; 
+        $value = "undef" if !defined($value); 
         if($Log{$name}){
             push @{$Log{$name}}, $value;
         }
         else{
             $Log{$name} = [$value]; 
         } 
-        #print "A " . $name . " " . $value . "\n"; 
     }  
+
+    sub clear_log{
+        %Log = (); 
+    } 
 
     sub print_log{
         for my $key (keys %Log){
             print "$key: " . join(", ", @{$Log{$key}}) . "\n";  
         } 
     } 
+    
+    sub pretty_print_log{
+        for my $key (keys %Log){
+            print "$key: " . join(", ", map{ pp $_ } @{$Log{$key}}) . "\n";  
+        } 
+    } 
 
     FILTER_ONLY code => sub{
-       s|((\$[[:alpha:]]*).*;)|$1 Audit::audit('$2', $2);|g;
-       #print $1 . "\n";  
-       #print $2 . "\n";  
-       #print;
+       s|((\$[[:alnum:]_]*).*;)|$1 Audit::audit('$2', $2);|g;
     }  
 }
