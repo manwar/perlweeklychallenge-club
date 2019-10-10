60e88f0338ef44fe2ff9bca2bf09ffcaa59e256e
Duane Powell <duane.r.powell@gmail.com>
2019-10-10 07:55:10 -0500

    Properly escape usage() example

--- challenge-029/duane-powell/perl5/ch-1.pl
+++ challenge-029/duane-powell/perl5/ch-1.pl
@@ -16,7 +16,7 @@ Note: 'brace-expansion' must be quoted to surpress bash brace expansion, ironic.
 Examples:
         $0 'Perl {Daily,Weekly,Monthly,Yearly} Challenge'       # PWC 29
         $0 'It{{em,alic}iz,erat}e{d,}'                          # test from RosettaCode
-        $0 '{,{,gotta have{ ,\, again\, }}more }cowbell!'       # ''
+        $0 '{,{,gotta have{ ,\\, again\\, }}more }cowbell!'       # ''
         $0 'mkdir -p ~/project/{src,docs,test,foo}'             # practical usage
         $0 'apt-get install lib{file-find-rule,test-more}-perl' # ''
 EOU
