20542c10408562598107d35245eb339c80df3aae
k-mx <cppfero@gmail.com>
2019-10-19 19:30:03 +0500

    challenge-030 by Maxim Kolodyazhny

---
+++ challenge-030/maxim-kolodyazhny/ch-2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use List::Util qw( sum0 any );
+
+use constant {
+    NUM_COUNT    => 3,
+    REQUIRED_SUM => 12,
+};
+
+sub n {
+    my $depth = shift // 1;
+    my @nums  = @_;
+
+    my $sum = sum0 @nums;
+
+    if ( $depth > NUM_COUNT || REQUIRED_SUM < $sum ) {
+
+        if ( $sum == REQUIRED_SUM && any { $_ % 2 == 0 } @nums ) {
+            print "@nums\n";
+        }
+
+        return;
+    }
+    n( $depth + 1, @nums, $_ ) for 0 .. REQUIRED_SUM;
+}
+
+n();
