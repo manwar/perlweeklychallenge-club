a94f0198629782810670ea660469ee84ff7f52e3
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2019-10-20 17:11:56 +0500

    Challenge 30

---
+++ challenge-030/svetlana-nesterova/perl5/ch-1.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+use DateTime;
+use DateTime::Format::HTTP;
+
+use constant SUNDAY => 7;
+
+
+=Write a script to list dates for Sunday Christmas between 2019 and 2100.
+ For example, 25 Dec 2022 is Sunday.
+=cut
+
+
+sub PrintSundayChristmases() {
+    for my $year (2019 .. 2100) {
+        my $christmas = DateTime->new(year => $year, month => 12, day => 25);
+        if ($christmas->day_of_week == SUNDAY) {
+            print DateTime::Format::HTTP->format_datetime($christmas), "\n";
+        }
+    }
+}
+
+print PrintSundayChristmases();
