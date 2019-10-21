2a09423282d4cc5ccf78189eca89961b872dec40
Ysmael Ebreo <Ysmael.Ebreo@latticesemi.com>
2019-10-21 15:50:22 +0800

    Added perl5 solution ch#31-1

---
+++ challenge-031/yet-ebreo/perl5/ch-1.pl
@@ -0,0 +1,17 @@
+#Create a function to check divide by zero error 
+#without checking if the denominator is zero.
+use strict;
+use warnings;
+use feature 'say';
+
+sub div_zero_check {
+    my @div =  @_;
+    my $r   =  eval "$_[0]/$_[1]";
+    $@      =~ /z/ ? -1 :$r ;
+} 
+my $r;
+say ( (($r = div_zero_check(112,0)) < 0) ? "Division by zero detected" : $r );
+say ( (($r = div_zero_check(0,0))   < 0) ? "Division by zero detected" : $r );
+say ( (($r = div_zero_check(0,1))   < 0) ? "Division by zero detected" : $r );
+say ( (($r = div_zero_check(32,12)) < 0) ? "Division by zero detected" : $r );
+
