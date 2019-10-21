368712a38adde5d366c0d8c7496a96aeee80ed24
k-mx <cppfero@gmail.com>
2019-10-21 20:27:23 +0500

    improved challenge-031.1 by Maxim Kolodyazhny

--- challenge-031/maxim-kolodyazhny/perl5/ch-1.pl
+++ challenge-031/maxim-kolodyazhny/perl5/ch-1.pl
@@ -3,8 +3,12 @@
 use strict;
 use warnings;
 
-sub div {
+use constant ANSWER2EVERYTHING => 42;
+
+sub harmless_division {
     my ($x, $y ) = @_;
 
-    42^$y^42 ? $x / $y : undef;
+    return if $y == int $y && ( ANSWER2EVERYTHING ^ $y ) == ANSWER2EVERYTHING;
+
+    $x / $y;
 }
