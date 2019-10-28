86cd1a128101c8552a6e9043b79d20e9adfc4d35
Joelle Maslak <jmaslak@antelope.net>
2019-10-28 09:54:46 -0500

    Joelle's solutions for 32.1 in Raku and Perl 5

---
+++ challenge-032/joelle-maslak/perl5/ch-1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use v5.14;
+use strict;
+use warnings;
+
+# Turn on method signatures
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+my $csv;
+if (@ARGV and $ARGV[0] eq '--csv') {
+    $csv = 1;
+    shift @ARGV;
+}
+push @ARGV, "example.txt" unless @ARGV;
+
+MAIN: {
+    my %words;
+    while (<<>>) {
+        chomp;
+        $words{$_}++;
+    }
+
+    my @sorted = reverse sort { $words{$a} <=> $words{$b} } keys %words;
+    for my $word (@sorted) {
+        if ($csv) {
+            say "$word," . $words{$word};
+        } else {
+            say "$word\t" . $words{$word};
+        }
+    }
+}
+
