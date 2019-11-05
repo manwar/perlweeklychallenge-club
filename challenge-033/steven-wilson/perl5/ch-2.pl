1ba911085aac1551507c53f8d658cdcfc487e768
Steven Wilson <steven1170@zoho.eu>
2019-11-05 15:15:57 +0000

    tidy

--- challenge-033/steven-wilson/perl5/ch-2.pl
+++ challenge-033/steven-wilson/perl5/ch-2.pl
@@ -21,21 +21,20 @@
 #  10|                                     100 110
 #  11|                                         121
 
-
 use strict;
 use warnings;
 
-printf("  x| %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d\n", 1..11);
+printf( "  x| %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d %3d\n", 1 .. 11 );
 printf("---+--------------------------------------------\n");
 
-for (1..11) {
-  my @row = get_row($_);
-  print sprintf("%3d|", $_);
-  map { $_ == 0 ? print "    " : printf(" %3d", $_)} @row;
-  print "\n";
+for ( 1 .. 11 ) {
+    my @row = get_row($_);
+    printf( "%3d|", $_ );
+    map { $_ == 0 ? print "    " : printf( " %3d", $_ ) } @row;
+    print "\n";
 }
 
 sub get_row {
-  my $row = shift;
-  my @row = map {$_ < $row ? 0 : $_ * $row } 1..11;
+    my $row = shift;
+    my @row = map { $_ < $row ? 0 : $_ * $row } 1 .. 11;
 }
