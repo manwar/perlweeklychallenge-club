5cd650edf4d85702e2f3d90622437a6bdd518130
Adam Russell <ac.russell@live.com>
2019-11-10 02:05:55 -0500

    initial commit

---
+++ challenge-033/adam-russell/cxx/ch-1.cxx
@@ -0,0 +1,32 @@
+#include <map>
+#include <cctype>
+#include <vector>
+#include <iostream> 
+
+int main(int argc, char** argv){
+    std::string input; 
+    std::vector<std::pair<char, int>> v; 
+    std::map<char, int> letter_counts; 
+    do{ 
+        getline(std::cin, input);
+        if(!input.empty()){
+            for(char letter: input){
+                letter = tolower(letter); 
+                if(letter != ' ' && letter != '\t' && letter != '\n' && letter != '.'){
+                    if(letter_counts.find(letter) != letter_counts.end()){
+                        letter_counts[letter] += 1;
+                    }
+                    else{
+                        letter_counts[letter] = 1; 
+                    } 
+                }  
+            } 
+        }  
+    }while(!input.empty());   
+    std::copy(letter_counts.begin(), letter_counts.end(), std::back_inserter<std::vector<std::pair<char, int>>>(v));
+    std::vector<std::pair<char, int>>::iterator iter = v.begin();
+    while(iter != v.end()){
+        std::cout << iter->first << ": " << iter->second << std::endl; 
+        iter++; 
+    }  
+}
---
+++ challenge-033/adam-russell/raku/ch-2.p6
@@ -0,0 +1,25 @@
+##
+# Write a script to print 11x11 multiplication
+# table, only the top half triangle.
+##
+use Form; 
+sub print_table11 {
+    my ($x,$x1,$x2,$x3,$x4,$x5,$x6,$x7,$x8,$x9,$x10,$x11);
+    my $header = form
+        '    x|   1   2   3   4   5   6   7   8   9   10   11',
+        '  ---+----------------------------------------------';
+    print $header;
+    for 1 .. 11 -> $x {
+        my @a;
+        @a = (0) xx ($x -1) if $x > 1;
+        @a.append($x .. 11);
+        my @b = map({$_ ==  0 ?? "" !! $_}, map({ $x * $_ }, @a));
+        print sprintf '%5s|', $x;
+        my $s = sprintf '%4s%4s%4s%4s%4s%4s%4s%4s%4s%5s%5s', @b;
+say $s;
+    }
+}
+
+sub MAIN {
+    print_table11;
+}
