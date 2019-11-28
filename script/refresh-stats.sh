fc08cb38596b2134618b32904957974d4d72cb76
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2019-11-28 12:05:50 +0000

    - Updated script/refresh-stats.sh script.

--- script/refresh-stats.sh
+++ script/refresh-stats.sh
@@ -67,6 +67,8 @@ mv pwc-current.json stats/pwc-challenge-033.json
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-034 --current
 mv pwc-current.json stats/pwc-challenge-034.json
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-035 --current
+mv pwc-current.json stats/pwc-challenge-035.json
+fetch-pwc-stats --members members.json --guests guests.json --source challenge-036 --current
 mv pwc-current.json stats/
 
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-001 --summary
@@ -103,6 +105,7 @@ fetch-pwc-stats --members members.json --guests guests.json --source challenge-0
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-032 --master pwc-summary.json --update
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-033 --master pwc-summary.json --update
 fetch-pwc-stats --members members.json --guests guests.json --source challenge-034 --master pwc-summary.json --update
+fetch-pwc-stats --members members.json --guests guests.json --source challenge-035 --master pwc-summary.json --update
 mv pwc-summary.json stats/pwc-master-stats.json
 
 echo #####################################
