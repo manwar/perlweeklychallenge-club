ce39059319051b6945a0fa6fe3dd1c82da69e35a
Ruben Westerberg <drclaw@mac.com>
2019-12-17 22:20:18 +1000

    added ch-1.p6 ch-2.p6

--- challenge-039/ruben-westerberg/perl/ch-2.pl
+++ challenge-039/ruben-westerberg/perl/ch-2.pl
@@ -5,7 +5,7 @@ my @stack;
 $expression=~s/ +/ /g;
 print "Input expression: $expression\n";
 for (split " ", $expression) {
-	die "Error in input expression" if !/[\-+]?\d+(\.\d+)*/ && !/[+\-*\/]/;
+	die "Error in input expression" if !/^[\-+]?\d+(\.\d+)*$/ && !/^[+\-*\/]$/;
 	for ($_) {
 		if ($_ eq "+") {
 			push @stack, pop(@stack)+ pop(@stack);
