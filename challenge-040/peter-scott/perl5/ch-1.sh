2d953a0e89ef07f7950e7ddadfe53230fdc44bd5
Peter Scott <Peter@PSDT.com>
2019-12-22 21:10:53 -0800

    use @F

--- challenge-040/peter-scott/perl5/ch-1.sh
+++ challenge-040/peter-scott/perl5/ch-1.sh
@@ -4,4 +4,4 @@ I L O V E Y O U
 2 4 0 3 2 0 1 9
 ! ? £ $ % ^ & *
 EOF
-perl -lane '$i=0; push @{$x[$i++]}, $_ for split; END{print "@$_" for @x}' /tmp/arrays
+perl -lane '$i=0; push @{$x[$i++]}, $_ for @F; END{print "@$_" for @x}' /tmp/arrays
