a46b1736f6246158883e012b0fc37ba01a69dc1f
andrezgz <andrezgz@gmail.com>
2020-01-24 07:45:40 -0300

    challenge-044 ch-1 add spaces for legibility

--- challenge-044/andrezgz/perl/ch-1.pl
+++ challenge-044/andrezgz/perl/ch-1.pl
@@ -13,7 +13,7 @@ use warnings;
 my @digits = split //, '123456789';
 my $initial = shift @digits;
 
-my %values = ( 0 => '', 1 => '+', 2 => '-' );
+my @values = ( '' , ' + ' , ' - ' );
 
 # A number in base 3 is used to get a particular combination
 #  for each of the 8 positions between the 9 digits
@@ -26,7 +26,7 @@ my %values = ( 0 => '', 1 => '+', 2 => '-' );
 foreach my $n (0 .. (3**8)-1) {
     my $equation = $initial;
     my @combination = split //, sprintf "%08d", to_base3($n);
-    $equation .= $values{ $combination[$_] } . $digits[$_] for (0 .. @digits - 1);
+    $equation .= $values[ $combination[$_] ] . $digits[$_] for (0 .. @digits - 1);
     print "$equation\n" if (eval $equation == 100);
 }
 
@@ -47,14 +47,15 @@ sub to_base3 {
 __END__
 
 ./ch-1.pl
-123+45-67+8-9
-123+4-5+67-89
-123-45-67+89
-123-4-5-6-7+8-9
-12+3+4+5-6-7+89
-12+3-4+5+67+8+9
-12-3-4+5-6+7+89
-1+23-4+56+7+8+9
-1+23-4+5+6+78-9
-1+2+34-5+67-8+9
-1+2+3-4+5+6+78+9
+123 + 45 - 67 + 8 - 9
+123 + 4 - 5 + 67 - 89
+123 - 45 - 67 + 89
+123 - 4 - 5 - 6 - 7 + 8 - 9
+12 + 3 + 4 + 5 - 6 - 7 + 89
+12 + 3 - 4 + 5 + 67 + 8 + 9
+12 - 3 - 4 + 5 - 6 + 7 + 89
+1 + 23 - 4 + 56 + 7 + 8 + 9
+1 + 23 - 4 + 5 + 6 + 78 - 9
+1 + 2 + 34 - 5 + 67 - 8 + 9
+1 + 2 + 3 - 4 + 5 + 6 + 78 + 9
+
