20b65d9c8cdd978fe7206b2d100c414375c7cbfd
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-02-18 11:15:36 +0000

    - Added solutions by E. Choroba.

--- challenge-048/e-choroba/perl/ch-1a.pl
+++ challenge-048/e-choroba/perl/ch-1a.pl
@@ -3,11 +3,19 @@ use warnings;
 use strict;
 use feature qw{ say };
 
-my @people = 1 .. 50;
+my @people = (1) x 50;
 my $sword = 0;
-while (@people > 1) {
-    $sword = (1 + $sword) % @people;
-    splice @people, $sword, 1;
-    $sword %= @people;
+my $next = sub {
+    do { $sword = (1 + $sword) % @people } until $people[$sword];
+};
+while (1 < (my $living = grep $_, @people)) {
+    # print 1 + $sword, ' kills ';
+    $next->();
+    # print 1 + $sword, $living == 2
+    #     ? ' and the survivor is '
+    #     : ' and passes the sword to ';
+    $people[$sword] = 0;
+    $next->();
+    # say 1 + $sword;
 }
-say $people[0];
+say 1 + $sword;
