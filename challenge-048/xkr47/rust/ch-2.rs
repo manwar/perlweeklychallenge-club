a61cbfd37e263dd12ade63155d5d79468d2cf7c8
Jonas Berlin <xkr47@outerspace.dyndns.org>
2020-02-21 22:44:47 +0200

    With help of ##raku/freende I improved it a bit :)

--- challenge-048/xkr47/rust/ch-2.rs
+++ challenge-048/xkr47/rust/ch-2.rs
@@ -5,13 +5,13 @@ fn main() {
     let dates: Vec<String> = (2000..=2999)
         .map(|y| {
             let ys = y.to_string();
-            let (m, d): (Vec<(usize, char)>, Vec<(usize, char)>) = ys.chars().rev().enumerate().partition(|(i, _)| *i < 2usize);
-            let m = String::from_iter(m.iter().map(|(_, val)| val)).parse::<u32>().unwrap();
-            let d = String::from_iter(d.iter().map(|(_, val)| val)).parse::<u32>().unwrap();
+            let revchars: Vec<char> = ys.chars().rev().collect();
+            let m = String::from_iter(&revchars[0..2]).parse::<u32>().unwrap();
+            let d = String::from_iter(&revchars[2..4]).parse::<u32>().unwrap();
             chrono::Utc.ymd_opt(y, m, d)
         })
         .flat_map(|date| date.earliest())
-        .map(|date| format!("{}", date.format("%m%d%y")))
+        .map(|date| format!("{}", date.format("%m%d%Y")))
         .collect();
 
     println!("{:?}", dates);
