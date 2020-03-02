4021ea96c7f0085477f4276d002923c3f15ad686
sol-demuth <sledmouth@gmail.com>
2020-03-02 14:25:20 -0600

    challenge 050 - sol demuth ch-1.pl = merge intervals ch-2.pl = noble integer

---
+++ challenge-050/sol-demuth/perl/ch-1.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @sets = (
+    [2,7], [3,9], [10,12], [15,19], [18,22],
+);
+
+print "Intervals:\n". dumpSets(@sets);
+
+@sets = sort {
+    # pre-sort by beginning then end of intervals
+    # allows maintaining single array for merge tracking
+    (
+        $a->[0] <=> $b->[0]
+    ) || (
+        $a->[1] <=> $b->[1]
+    )
+} @sets;
+
+my $cur = undef;
+my $nxt = undef;
+my $i   = 0; # cleaner than for loop, IMHO
+
+foreach my $r (@sets) {
+    unless ($cur) {
+        # get started
+        $cur = $r;
+    } else {
+        $nxt = $r;
+
+        if ($cur->[1] < $nxt->[0]) {
+            # no overlap, increment
+            $cur = $nxt;
+        } else {
+            # partial overlap where next interval
+            # ends after current
+            if ($cur->[1] < $nxt->[1]) {
+                # current takes end of next
+                $cur->[1] = $nxt->[1];
+            }
+            # consume next
+            splice @sets, $i, 1;
+        }
+    }
+
+    $i++;
+}
+
+print "Merged:\n" . dumpSets(@sets);
+
+sub dumpSets { #convenienct for printing sets of intervals
+    return join(', ',
+        map { '[' . $_->[0] . ', ' . $_->[1] . ']' } @_
+    ) . "\n";
+}
