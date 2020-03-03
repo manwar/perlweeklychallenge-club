d9f7c52b8defec3074f4241db552d2897696b15c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-03-03 16:57:13 +0000

    - Fixed order interval bug.

--- challenge-050/mohammad-anwar/perl/ch-1.pl
+++ challenge-050/mohammad-anwar/perl/ch-1.pl
@@ -3,14 +3,19 @@
 use strict;
 use warnings;
 
-use Test::More tests => 2;
+use Test::More tests => 5;
 use Test::Deep;
 
-cmp_deeply( merge_intervals( [ [1, 12], [7, 8], [12, 14], [15, 19] ]),
+cmp_deeply( merge_intervals( [ [1, 12], [7, 8], [12, 14], [15, 19] ] ),
             [ [1, 14], [15, 19] ] );
-cmp_deeply( merge_intervals( [ [2,7], [3,9], [10,12], [15,19], [18,22] ]),
+cmp_deeply( merge_intervals( [ [2,7], [3,9], [10,12], [15,19], [18,22] ] ),
             [ [2, 9], [10, 12], [15, 22] ] );
-
+cmp_deeply( merge_intervals( [ [1,2], [5,6], [3,4] ] ),
+            [ [1,2], [3,4], [5,6] ] );
+cmp_deeply( merge_intervals( [ [1,2], [5,6], [2,5] ] ),
+            [ [1,6] ] );
+cmp_deeply( merge_intervals( [ [1,1], [2,2], [3,3] ] ),
+            [ [1,1], [2,2], [3,3] ] );
 #
 #
 # METHODS
@@ -22,12 +27,18 @@ sub merge_intervals {
     my $l;
     my $m;
 
+    $intervals = _order_intervals($intervals);
+
     foreach my $interval (@$intervals) {
         my $i = $interval->[0];
 
-        next if (defined $l &&
-                 defined $k &&
-                 (($i <= $l) || ($k == $i)));
+        if (defined $l && defined $k) {
+            if (($i <= $l) || ($k == $i)) {
+                $k = $interval->[1];
+                $m->[-1]->[1] = $k;
+                next;
+            }
+        }
 
         my ($j, $_k) = _merge_intervals($interval->[1], $intervals);
         push @$m, [$i, $j];
@@ -37,6 +48,17 @@ sub merge_intervals {
     }
 
     return $m;
+}
+
+sub _order_intervals {
+    my ($intervals) = @_;
+
+    my $ordered = {};
+    foreach my $i (@$intervals) {
+        $ordered->{$i->[0]} = $i;
+    }
+
+    return [  map { $ordered->{$_} } sort { $a <=> $b } keys %$ordered ];
 }
 
 sub _merge_intervals {
