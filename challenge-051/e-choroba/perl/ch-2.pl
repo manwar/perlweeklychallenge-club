5bdc4f2a7dc275c0247c728a7403da54f095589b
E. Choroba <choroba@matfyz.cz>
2020-03-15 23:39:52 +0100

    Add blog post by E. Choroba about 051 (3Sum and Colourful Numbers)
    Also, simplify the solution of the second task: we don't need to pad
    the array.

--- challenge-051/e-choroba/perl/ch-2.pl
+++ challenge-051/e-choroba/perl/ch-2.pl
@@ -12,9 +12,7 @@ sub is_colorful_number {
     my $count = 0;
     for my $length (1 .. $max_length) {
         for my $pos (0 .. $max_length - $length) {
-            my @pattern = (
-                (0) x $pos, (1) x $length, (0) x ($max_length - $pos - $length)
-            );
+            my @pattern = ((0) x $pos, (1) x $length);
             undef $uniq{
                 product((split //, $n)[ grep $pattern[$_], 0 .. $#pattern ])
             };
