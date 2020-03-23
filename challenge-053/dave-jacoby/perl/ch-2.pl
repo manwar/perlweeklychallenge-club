7bb009b65541c03e71468b4917b9a098f1fbbba7
Dave Jacoby <jacoby.david@gmail.com>
2020-03-23 19:41:11 -0400

    I solved task 2 first and got confused

--- challenge-053/dave-jacoby/perl/ch-2.pl
+++ challenge-053/dave-jacoby/perl/ch-2.pl
@@ -5,82 +5,41 @@ use warnings;
 use feature qw{ say postderef signatures state };
 no warnings qw{ experimental::postderef experimental::signatures };
 
-use JSON;
-my $json = JSON->new;
+my $l = 2;
+if ( scalar @ARGV && int $ARGV[0] > 0 ) { $l = int $ARGV[0] }
 
-my $array = [ [ 1 .. 3 ], [ 4 .. 6 ], [ 7 .. 9 ] ];
+my @strings = vowel_strings($l);
+say join "\n", @strings;
 
-my $a90   = rotate_90($array);
-my $a180a = rotate_90($a90);
-my $a270a = rotate_90($a180a);
-my $a360a = rotate_90($a270a);
-
-my $a180b = rotate_180($array);
-my $a270b = rotate_270($array);
-
-say $json->encode($array);
-say '';
-say $json->encode($a90);
-say '';
-say $json->encode($a180a);
-say $json->encode($a180b);
-say '';
-say $json->encode($a270a);
-say $json->encode($a270b);
-say '';
-say $json->encode($a360a);
-
-sub rotate_90( $array ) {
-    my $x      = -1 + scalar $array->@*;
-    my $y      = -1 + scalar $array->[0]->@*;
-    my $output = [];
-    for my $i ( 0 .. $x ) {
-        my $jj = $i;
-        for my $j ( 0 .. $y ) {
-            my $ii = $y - $j;
-            $output->[$i][$j] = int $array->[$ii][$jj];
-        }
+sub vowel_strings ( $l, $string = '' ) {
+    if ( length $string == $l ) {
+        return $string;
     }
-    return $output;
-}
-
-sub rotate_180( $array ) {
-    my $x      = -1 + scalar $array->@*;
-    my $y      = -1 + scalar $array->[0]->@*;
-    my $output = [];
-    for my $i ( 0 .. $x ) {
-        my $jj = $x - $i;
-        for my $j ( 0 .. $y ) {
-            my $ii = $y - $j;
-            $output->[$i][$j] = int $array->[$ii][$jj];
-        }
+    my @next;
+    my $m = length $string == 0 ? '' : substr $string, -1;
+    if ( $string eq '' ) {
+        @next = qw{ a e i o u};
     }
-    return $output;
-}
-
-sub rotate_270($array) {
-    my $x      = -1 + scalar $array->@*;
-    my $y      = -1 + scalar $array->[0]->@*;
-    my $output = [];
-    for my $i ( 0 .. $x ) {
-        my $jj = $x - $i;
-        for my $j ( 0 .. $y ) {
-            my $ii = $j;
-            $output->[$i][$j] = int $array->[$ii][$jj];
-        }
+    elsif ( $m eq 'a' ) {
+        @next = qw{ e i };
     }
-    return $output;
+    elsif ( $m eq 'e' ) {
+        @next = qw{ i };
+    }
+    elsif ( $m eq 'i' ) {
+        @next = qw{ a o u e };
+    }
+    elsif ( $m eq 'o' ) {
+        @next = qw{ a u };
+    }
+    elsif ( $m eq 'u' ) {
+        @next = qw{ o e };
+    }
+
+    my @output;
+    for my $n (@next) {
+        push @output, vowel_strings( $l, $string . $n );
+    }
+    return @output;
+
 }
-
-__DATA__
-
-1 2 3
-4 5 6
-7 8 9
-
-7 4 1
-8 5 2
-9 6 3 
-
-so 0,0 = 2,0
-   0,1 = 3,0
