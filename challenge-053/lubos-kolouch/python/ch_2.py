f10c5f37aa721630216911f71471f775c91cda8b
Lubos Kolouch <lubos@kolouch.net>
2020-03-27 11:15:21 +0100

    Style cleanups

--- challenge-053/lubos-kolouch/python/ch_2.py
+++ challenge-053/lubos-kolouch/python/ch_2.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python
+""" PerlWeeklyChallenge 053"""
 
 from itertools import permutations
 
 
 class VowelString:
+    """ Produce all valid permutations"""
 
-    def check_valid(self, permutation):
+    def __init__(self):
+        self.permutation = ''
+
+    def check_valid(self):
+        """ Check if given permutation is valid"""
 
         ok_rules = ['ae', 'ai', 'ei', 'ia', 'ie',
                     'io', 'iu', 'oa', 'ou', 'uo', 'ue']
 
-        for x, item in enumerate(permutation[:-1]):
-            if item + permutation[x+1] not in ok_rules:
+        for item_count, item in enumerate(self.permutation[:-1]):
+            if item + self.permutation[item_count+1] not in ok_rules:
                 return 0
 
             return 1
 
-    def get_permutations(self,  count: int):
+    def get_permutations(self, count: int):
+        """ process all possible permutations"""
 
         wovels = ['a', 'e', 'i', 'o', 'u']
 
         result = []
 
         for perm in permutations(wovels, count):
-            if self.check_valid(perm):
+            self.permutation = perm
+            if self.check_valid():
                 result.append(perm)
 
-        return(result)
+        return result
 
 
 class TestVowelString:
+    """Test for the VowelString method"""
 
     def __init__(self):
         self.vow = VowelString()
 
     def do_tests(self):
+        """Run the tests"""
         assert len(self.vow.get_permutations(1)) == 0
         assert len(self.vow.get_permutations(2)) == 11
 
 
-if __name__ == "__main__":
+def main():
+    """Main method"""
+
     vowcomb = VowelString()
 
     print(vowcomb.get_permutations(2))
 
     vowtester = TestVowelString()
     vowtester.do_tests()
+
+
+if __name__ == "__main__":
+    main()
