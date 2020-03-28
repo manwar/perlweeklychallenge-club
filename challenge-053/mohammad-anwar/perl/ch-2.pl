48f1c6f8ca8133b8e461c03004ca9fd3977ac6b0
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-03-28 16:40:48 +0000

    - Rollback changes.

--- challenge-053/mohammad-anwar/perl/ch-2.pl
+++ challenge-053/mohammad-anwar/perl/ch-2.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Algorithm::Combinatorics qw(variations_with_repetition);
+use Algorithm::Combinatorics qw(combinations);
 
 my $count = $ARGV[0] // 2;
 die "ERROR: Invalid count $count.\n"
@@ -17,7 +17,7 @@ if ($count == 1) {
     exit;
 }
 
-my $iter = variations_with_repetition($chars, $count);
+my $iter = combinations($chars, $count);
 
 my $char_sets = [];
 while (my $char = $iter->next) {
