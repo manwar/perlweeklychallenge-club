2709bdea964d2f0a1c93a05bd8d9b575e2e93663
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2020-03-29 16:32:56 +0800

    Add files via upload

---
+++ xy.pm
@@ -0,0 +1,15 @@
+package xy;
+use strict;
+
+sub new {
+    my ($class) = @_;
+    bless{
+        _value=> $_[1],
+        _x=>$_[2],
+        _y=>$_[3],
+      }, $class;
+}
+
+sub x{ $_[0]->{_x}}
+sub y{ $_[0]->{_y}}
+sub value{ $_[0]->{_value} }
