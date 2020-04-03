ef87311a4af09169b6e49cbb9fcaf259947bb1cf
Lubos Kolouch <lubos@kolouch.net>
2020-04-03 14:01:32 +0200

    Perl solutions Challenge 054

---
+++ challenge-054/lubos-kolouch/perl/ch-1.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature qw/say/;
+use Algorithm::Permute;
+
+sub get_nth_permutations {
+
+    my ( $n, $k ) = @_;
+
+    # reverse because Algorithm::Permute returns the sequence backwards
+    my @numbers = reverse ( 1 .. $n );
+
+    my $p = Algorithm::Permute->new(\@numbers, $n);
+
+    my @all_perms;
+    while (my @res = $p->next) {
+        push @all_perms, join("", @res);
+    }
+    
+    my @sorted_perms = sort @all_perms;
+    return $sorted_perms[$k-1];
+}
+
+say(get_nth_permutations( 3, 4 ));
+
+use Test::More;
+
+is(get_nth_permutations(3, 4),'231');
+done_testing();
+
