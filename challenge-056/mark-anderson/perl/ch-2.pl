8d8efca3cf48774e45ec7164b74858cdfe3aab9a
Mark Anderson <mark@frontrangerunner.com>
2020-04-14 05:13:33 -0600

    ch-2.pl

--- challenge-056/mark-anderson/perl/ch-2.pl
+++ challenge-056/mark-anderson/perl/ch-2.pl
@@ -9,7 +9,7 @@ my $lol = [ [ [ [7], [2], 11 ], 4 ], [ [13], [ [1], 9 ], 8 ], 5 ];
 
 my $tree = Tree::DAG_Node->lol_to_tree($lol);
 
-$tree->walk_down ({callback => \&traverse});
+$tree->walk_down({callback => \&traverse});
 
 say for $tree->draw_ascii_tree->@*;
 
