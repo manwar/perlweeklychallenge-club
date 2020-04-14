6c219b95222dc4ee35b5b2e70fbc54f63408f21c
Mark Anderson <mark@frontrangerunner.com>
2020-04-14 05:00:18 -0600

    ch-2.pl

--- challenge-056/mark-anderson/perl/ch-2.pl
+++ challenge-056/mark-anderson/perl/ch-2.pl
@@ -2,6 +2,8 @@
 use Modern::Perl;
 use List::Util 'sum';
 use Tree::DAG_Node;
+use feature qw(signatures);
+no warnings qw(experimental::signatures);
 
 my $lol = [ [ [ [7], [2], 11 ], 4 ], [ [13], [ [1], 9 ], 8 ], 5 ];
 
@@ -11,9 +13,7 @@ $tree->walk_down ({callback => \&traverse});
 
 say for $tree->draw_ascii_tree->@*;
 
-sub traverse {
-    my $node = shift;
- 
+sub traverse($node, $options) {
     unless ($node->daughters) {
         my @nodes = reverse ($node->name, (map { $_->name } $node->ancestors));
         say join(" + ", @nodes), " == 22" if (sum @nodes) == 22;
