06e323f78c6aab067bb0c7f59764f19f422fd4c5
E. Choroba <choroba@matfyz.cz>
2020-04-25 22:36:18 +0200

    Keep only two solutions to Invert Tree (and the bonus)

--- challenge-057/e-choroba/perl/ch-1b.pl
+++ challenge-057/e-choroba/perl/ch-1b.pl
@@ -3,12 +3,6 @@ use warnings;
 use strict;
 use feature qw{ say };
 
-use ARGV::OrDATA;
-
-use FindBin;
-use lib $FindBin::Bin;
-use Tree qw{ from_structure };
-
 sub invert {
     my ($node, $tree) = @_;
     return $node unless exists $tree->{$node};
@@ -17,9 +11,16 @@ sub invert {
     return $node . '(' . join(',', map invert($_, $tree), @ch) . ')'
 }
 
-chomp( my $structure = <> );
-my ($root, $tree) = from_structure($structure);
-say '(', invert($root, $tree), ')';
+chomp( my $structure = <DATA> );
+
+my %tree;
+while ($structure =~ s/([0-9]+) \( ([0-9]+) , ([0-9]+) \) /$1/x) {
+    my ($parent, $left, $right) = ($1, $2, $3);
+    $tree{$parent} = [$left, $right];
+}
+
+my ($root) = $structure =~ /[0-9]+/;
+say '(', invert($root, \%tree), ')';
 
 __DATA__
 (1(2(4,5),3(6,7)))
