0fcb0c88ec1263d030bc4df66f98ddaea13f7566
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-05-26 18:44:17 +0100

    - Added Perl solution to the "Sort Email Addresses" task.

---
+++ challenge-062/mohammad-anwar/perl/ch-1.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $FILE = $ARGV[0];
+my $FLAG = $ARGV[1];
+
+my $sorted = sort_data($FILE, $FLAG);
+print join "\n", @$sorted, "\n";
+
+sub sort_data {
+    my ($file, $flag) = @_;
+
+    die "ERROR: Missing data file.\n"
+        unless defined $file;
+
+    die "ERROR: Invalid flag [$flag].\n"
+        if (defined $flag && $flag !~ /^\-u$/);
+
+    open (my $F, "<:encoding(utf8)", $file)
+        or die "ERROR: Can't open $file: $!\n";
+
+    my @source = ();
+    my $source = ();
+    while (my $row = <$F>) {
+        chomp $row;
+        my ($mailbox, $domain) = split /\@/, $row, 2;
+        push @source, [$mailbox, $domain];
+        if ($flag) {
+            $source->{$mailbox} = lc $domain;
+        }
+    }
+
+    close($F);
+
+    my $sorted = [];
+    if ($flag) {
+        foreach (sort { $source->{$a} cmp $source->{$b} } sort keys %$source) {
+            push @$sorted, sprintf("%s@%s", $_, $source->{$_});
+        }
+    }
+    else {
+        foreach (reverse sort { lc $a->[0] cmp lc $b->[0] } @source) {
+            push @$sorted, sprintf("%s@%s", $_->[0], $_->[1]);
+        }
+    }
+
+    return $sorted;
+}
---
+++ challenge-062/mohammad-anwar/perl/ch-1a.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Deep;
+
+is_deeply ( sort_data('data.txt'),
+            [ 'user@alpha.example.org',
+              'rjt@CPAN.org',
+              'rjt@cpan.org',
+              'Name@example.org',
+              'name@example.org',
+            ],
+            'regular sort' );
+
+is_deeply ( sort_data('data.txt', '-u'),
+            [ 'user@alpha.example.org',
+              'rjt@cpan.org',
+              'Name@example.org',
+              'name@example.org',
+            ],
+            'unique sort' );
+
+done_testing;
+
+sub sort_data {
+    my ($file, $flag) = @_;
+
+    die "ERROR: Missing data file.\n"
+        unless defined $file;
+
+    die "ERROR: Invalid flag [$flag].\n"
+        if (defined $flag && $flag !~ /^\-u$/);
+
+    open (my $F, "<:encoding(utf8)", $file)
+        or die "ERROR: Can't open $file: $!\n";
+
+    my @source = ();
+    my $source = ();
+    while (my $row = <$F>) {
+        chomp $row;
+        my ($mailbox, $domain) = split /\@/, $row, 2;
+        push @source, [$mailbox, $domain];
+        if ($flag) {
+            $source->{$mailbox} = lc $domain;
+        }
+    }
+
+    close($F);
+
+    my $sorted = [];
+    if ($flag) {
+        foreach (sort { $source->{$a} cmp $source->{$b} } sort keys %$source) {
+            push @$sorted, sprintf("%s@%s", $_, $source->{$_});
+        }
+    }
+    else {
+        foreach (reverse sort { lc $a->[0] cmp lc $b->[0] } @source) {
+            push @$sorted, sprintf("%s@%s", $_->[0], $_->[1]);
+        }
+    }
+
+    return $sorted;
+}
