7ce3c10c21e237198d35d0e72462f80d570ae099
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-07-01 14:42:48 +0100

    - Added Number Combinations task in Perl.

---
+++ challenge-067/mohammad-anwar/perl/ch-1a.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Deep;
+use List::Util qw(uniq);
+
+is_deeply( [ comb(5, 2) ],
+           [ '1, 2', '1, 3', '1, 4', '1, 5',
+             '2, 3', '2, 4', '2, 5',
+             '3, 4', '3, 5',
+             '4, 5' ],
+             'testing m=5; n=2' );
+is_deeply( [ comb(5, 3) ],
+           [ '1, 2, 3', '1, 2, 4', '1, 2, 5', '1, 3, 4', '1, 3, 5', '1, 4, 5',
+             '2, 3, 4', '2, 3, 5', '2, 4, 5',
+             '3, 4, 5' ],
+             'testing m=5; n=3' );
+is_deeply( [ comb(5, 4) ],
+           [ '1, 2, 3, 4', '1, 2, 3, 5', '1, 2, 4, 5', '1, 3, 4, 5',
+             '2, 3, 4, 5' ],
+             'testing m=5; n=4' );
+is_deeply( [ comb(5, 5) ],
+           [ '1, 2, 3, 4, 5' ],
+             'testing m=5; n=5' );
+
+done_testing;
+
+sub comb {
+    my ($m, $n) = @_;
+
+    die "ERROR: Missing 'm'.\n"
+        unless defined $m;
+    die "ERROR: Missing 'n'.\n"
+        unless defined $n;
+    die "ERROR: Invalid m=$m (m>0).\n"
+        unless ($m > 0);
+    die "ERROR: Invalid n=$n (n>0).\n"
+        unless ($n > 0);
+    die "ERROR: Invalid n=$n (n<=m).\n"
+        unless ($n <= $m);
+
+    # prepare data set
+    my @data = ();
+    push @data, [1..$m] foreach (1..$n);
+
+    # generate all possible combinations
+    my @combinations = ('');
+    foreach my $members (@data) {
+        @combinations = map {
+            my  $member = $_;
+            map $member.$_, @$members
+        } @combinations;
+    }
+
+    # filter combinations
+    my %filter;
+    foreach my $member (@combinations) {
+        my @uniq = uniq split //, $member;
+        next unless (@uniq == $n);
+        $filter{ join ', ', sort @uniq } = 1;
+    }
+
+    # return sorted result
+    return (sort keys %filter);
+}
