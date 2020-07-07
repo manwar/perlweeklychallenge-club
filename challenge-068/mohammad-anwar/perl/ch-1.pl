6e494938d4b4391fb29ced8bb4d337cf142d9994
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-07-07 06:59:13 +0100

    - Added Perl solutions to Zero Matrix task.

---
+++ challenge-068/mohammad-anwar/perl/ch-1.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+#
+# Perl Weekly Challenge - 068
+#
+# Task #1: Zero Matrix
+#
+# https://perlweeklychallenge.org/blog/perl-weekly-challenge-068/
+#
+
+use strict;
+use warnings;
+
+my $R = $ARGV[0];
+my $C = $ARGV[1];
+
+my $matrix;
+if (defined $R && defined $C) {
+    die "ERROR: Invalid rows [$R].\n" unless ($R =~ /^\d+$/ && $R >=2 );
+    die "ERROR: Invalid cols [$C].\n" unless ($C =~ /^\d+$/ && $C >=2 );
+
+    $matrix = get_matrix(--$R, --$C);
+}
+else {
+    $matrix = [
+        [1, 0, 1],
+        [1, 1, 1],
+        [1, 0, 1],
+    ];
+}
+
+my $zero_m = make_zero_matrix($matrix);
+
+print matrix($matrix), "\n";
+print matrix($zero_m), "\n";
+
+#
+#
+# METHODS
+
+sub make_zero_matrix {
+    my ($matrix) = @_;
+
+    my $rows = $#$matrix;
+    my $cols = $#{$matrix->[0]};
+
+    my $zero_matrix = [];
+    foreach my $r (0..$rows) {
+        foreach my $c (0..$cols) {
+            if ($matrix->[$r][$c] == 0) {
+                # make zero row
+                $zero_matrix->[$r][$_] = 0 foreach (0..$cols);
+                # make zero col
+                $zero_matrix->[$_][$c] = 0 foreach (0..$rows);
+            }
+        }
+    }
+
+    # fill empty cells, if any.
+    foreach my $r (0..$rows) {
+        foreach my $c (0..$cols) {
+            $zero_matrix->[$r][$c] = 1 unless
+                defined $zero_matrix->[$r][$c];
+        }
+    }
+
+    return $zero_matrix;
+}
+
+sub matrix {
+    my ($matrix) = @_;
+
+    my $rows = $#$matrix;
+    my $cols = $#{$matrix->[0]};
+    foreach my $r (0..$rows) {
+        print sprintf("[ %s ]\n", join ', ', @{$matrix->[$r]});
+    }
+}
+
+sub get_matrix {
+    my ($rows, $cols) = @_;
+
+    my $minimum = 0;
+    my $maximum = 2;
+    my $matrix  = [];
+    foreach my $r (0..$rows) {
+        foreach my $c (0..$cols) {
+            $matrix->[$r][$c] = int($minimum + rand($maximum - $minimum));
+        }
+    }
+
+    return $matrix;
+}
