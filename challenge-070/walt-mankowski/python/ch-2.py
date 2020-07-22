aa197c2dabf76dc064e5f4b8a39abede35e7b3e6
Walt Mankowski <waltman@pobox.com>
2020-07-22 19:18:41 -0400

    simplified the logic for task 2

--- challenge-070/walt-mankowski/python/ch-2.py
+++ challenge-070/walt-mankowski/python/ch-2.py
@@ -4,9 +4,7 @@ n = int(argv[1])
 S = [0, 1, 3, 2]
 
 def gray_code(n, S):
-    fmt = f"0{n-1}b"
-    S2 = [eval(f"0b1{x}") for x in [f"{x:{fmt}}" for x in S][::-1]]
-    return S + S2
+    return S + [2**(n-1) + x for x in S][::-1]
 
 for i in range(3, n+1):
     S = gray_code(i, S)
