58200fb2aee94857fdba10965ce71ea4e8a287b8
Walt Mankowski <waltman@pobox.com>
2020-07-27 20:46:38 -0400

    perl code for challenge 71 task 1

---
+++ challenge-071/walt-mankowski/perl/ch-1.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature qw(:5.32);
+use experimental qw(signatures);
+
+# TASK #1 › Peak Element
+# Submitted by: Mohammad S Anwar
+#
+# You are given positive integer $N (>1).
+#
+# Write a script to create an array of size $N with random unique
+# elements between 1 and 50.
+#
+# In the end it should print peak elements in the array, if found.
+#
+#     An array element is called peak if it is bigger than it’s
+#     neighbour.
+#
+# Example 1
+# Array: [ 18, 45, 38, 25, 10, 7, 21, 6, 28, 48 ]
+# Peak: [ 48, 45, 21 ]
+# Example 2
+# Array: [ 47, 11, 32, 8, 1, 9, 39, 14, 36, 23 ]
+# Peak: [ 47, 32, 39, 36 ]
+
+my $N = shift @ARGV;
+
+my @a = make_array($N);
+my @p = peaks(@a);
+
+$" = ", ";
+say "Array: [@a[1..$#a-1]]";
+say "Peak:  [@p]";
+
+sub make_array($N) {
+    my @a = (0);
+    push @a, map { int(rand(50)) + 1} 1..$N;
+    push @a, 0;
+
+    return @a;
+}
+
+sub peaks(@a) {
+    my @p;
+    for my $i (1..$#a-1) {
+        push @p, $a[$i] if $a[$i-1] < $a[$i] > $a[$i+1];
+    }
+
+    return @p;
+}
