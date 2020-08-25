3560a64821237620d87e517eaab67ca2dd94bb17
E7-87-83 <fungcheokyin@gmail.com>
2020-08-25 23:18:14 +0800

    edit Python codes; thanks data-scientist Clare

--- challenge-075/cheok-yin-fung/python/ch-2.py
+++ challenge-075/cheok-yin-fung/python/ch-2.py
@@ -1,74 +1,57 @@
 # Python3
 # Perl Weekly Challenge #075 Task 2 Largest Rectangle Histogram , Python script
+# test cases:
+# items = [3, 2, 3, 5, 7, 5] --> 15
+# items = [2, 1, 4, 3, 5, 7] --> 12
+# items = [1, 2, 3, 4, 5] --> 9
 
-# A = [3, 2, 3, 5, 7, 5]
-# A = [2, 1, 4, 3, 5, 7]
-# A = [1, 2, 3, 4, 5]
+def largest_rectangle_histogram(items):
+    max_item = max(items)
 
+    subtract1 = lambda n: n-1 if n > 0 else 0
 
-length = int(input("Enter number of items of the histogram: \n"))
-print("Enter items of the histogram with line breaks")
-A = []
-for i in range(length):  
-    A.append(int(input()))
+    temp = items
+    temp = temp + [0]  # put a zero at the end for ease
 
-MAX_ = max(A)
-#length = len(A)
+    histogram = []
+    pair = []
+    areas = []
 
-def subtract1(n):
-    if n > 0:
-        r = n-1
-    else:
-        r = 0
-    return r
+    for i in range(max_item):
+        histogram.append(temp)
+        temp = list(map(subtract1, temp))
 
-temp = A
-temp.append(0)  # put a zero at the end for ease
-
-histogram = []
-pair = []
-areas = []
-
-for i in range(MAX_):
-    histogram.append(temp)
-    temp = list(map(subtract1, temp))
-
-histogram = histogram[::-1]
+    histogram = histogram[::-1]
 
 
 
-for i in range(MAX_):
-    j = 0
-    while j < length:
-        if histogram[i][j] != 0:
-            h = j
-            k = j
-            while True:
-                t = k
-                j = j+1
-                k = k+1
-                if histogram[i][k] == 0:
-                    break
-            if not([h,t] in pair):
-                areas.append( (t-h+1)*(MAX_-i) )
-                pair.append([h,t])
-        j = j+1
-    histogram[i].pop(-1)   #pop out the assisting zero
+    for i in range(max_item):
+        j = 0
+        while j < length:
+            if histogram[i][j] != 0:
+                h = j
+                k = j
+                while True:
+                    t = k
+                    j = j+1
+                    k = k+1
+                    if histogram[i][k] == 0:
+                        break
+                if not([h,t] in pair):
+                    areas.append( (t-h+1)*(max_item-i) )
+                    pair.append([h,t])
+            j = j+1
+        histogram[i].pop(-1)   #pop out the assisting zero
 
 
-print("Answer:")
-print(max(areas))
-print()
+    print("Answer:")
+    print(max(areas))
+    print()
 
-def zeroornumsign(x):
-    if x > 0:
-        return "#"
-    else:
-        return " "
 
-def print_A():
-    for i in range( MAX_ ):
-        print(MAX_-i, end=' ')
+    zeroornumsign = lambda x: "#" if x > 0 else " " 
+    for i in range( max_item ):
+        print(max_item-i, end=' ')
         templine = list(map(zeroornumsign, histogram[i]));
         for j in range(length):
             print(templine[j], end=' ')        
@@ -77,10 +60,17 @@ def print_A():
     print("_ " * length)
     print('  ', end='')
     for i in range(length):
-        print(str(A[i]), end=' ')
+        print(str(items[i]), end=' ')
     print()
 
-print_A();
+
+if __name__ == "__main__":
+    length = int(input("Enter number of items of the histogram: \n"))
+    print("Enter items of the histogram with line breaks")
+    items = []
+    for i in range(length):  
+        items.append(int(input()))
+    largest_rectangle_histogram(items)
 
 #references:
 #https://www.csestack.org/unique-elements-from-list-in-python/
