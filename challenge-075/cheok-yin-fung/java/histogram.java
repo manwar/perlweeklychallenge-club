af2739515a0a2fb09b3a6601b539c80bc1a10914
E7-87-83 <fungcheokyin@gmail.com>
2020-08-27 03:50:04 +0800

    edit ch-1.py by advice from friend; complete histogram.java

--- challenge-075/cheok-yin-fung/java/histogram.java
+++ challenge-075/cheok-yin-fung/java/histogram.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-// not yet produce the "image" of histogram, 25th Aug
-
 public class histogram {
     static public void main(String[] args) {
         int maxarea = 0;
+        int max_element = 0;
     //    int[] A = new int[] {3, 2, 3, 5, 7, 5};
       //  int[] A = new int[] {2, 1, 4, 5, 3, 7};
      //   int[] A = new int[] {1, 2, 3, 4, 5};
@@ -15,6 +14,9 @@ public class histogram {
         System.out.println("Input items of the histogram:");
         for (int k = 0; k < size; k++) {
             A[k] = scn.nextInt();
+            if (A[k] > max_element) {
+                max_element = A[k];
+            }
         }
         for (int winsize = 1; winsize <= A.length; winsize++) {
             for (int i = 0; i+winsize-1 < A.length; i++) {
@@ -36,12 +38,41 @@ public class histogram {
                 }
             }
         }
-        System.out.println(maxarea);
+        
+        System.out.println("\n"+maxarea + "\n");
+        
+        // print the histogram
+
+        for (int r = max_element; r > 0; r--) {
+            System.out.print(r+" ");
+            for (int k = 0; k < size; k++) {
+                char temp;
+                if (A[k] >= r) {
+                    temp = '#';
+                } else {
+                    temp = ' ';
+                }
+                System.out.print(temp+" ");
+            }
+
+            System.out.println();
+        }
+        System.out.print("_ ");
+        for (int s = 0; s < size; s++) {
+            System.out.print("_ ");
+        }
+        System.out.println();
+        System.out.print("  ");
+        for (int k = 0; k < size; k++) {
+            System.out.print(A[k]+" ");
+        }
+        System.out.println();
 
     }
 }
 
 
 // references:
+// textbook
 // https://stackoverflow.com/questions/2795350/how-to-put-a-scanner-input-into-an-array-for-example-a-couple-of-numbers
 // https://stackoverflow.com/questions/1484347/finding-the-max-min-value-in-an-array-of-primitives-using-java
--- challenge-075/cheok-yin-fung/python/ch-1.py
+++ challenge-075/cheok-yin-fung/python/ch-1.py
@@ -10,15 +10,14 @@
 # --> len(arr_for_dp[total]) == 7
 
 def coins_sum(uinput):
-    userinput = list(uinput).copy()
+    userinput = uinput
     total = userinput.pop(0)
     coins = userinput
     dp_range = []
     arr_for_dp = [ [] ]
 
-    for i_ in range(total):
-        dp_range.append(i_ + 1)
-
+    dp_range = list((range(1,total+1)))
+    print(dp_range)
 
     j = 0
     for i in dp_range:
@@ -34,23 +33,22 @@ def coins_sum(uinput):
             if (i-coins[k] > 0):
                 for p in range(len(arr_for_dp[i-coins[k]])):
                     partition = arr_for_dp[i-coins[k]][p]
-                    partition_p = list(partition).copy()
+                    partition_p = partition.copy()
                     partition_p.append(coins[k])
                     partition_p = sorted(partition_p)
                     if not(partition_p in arr_for_dp[i]):
-                        arr_for_dp[i].append(list(partition_p).copy())
+                        arr_for_dp[i].append(partition_p.copy())
 
     return arr_for_dp[total]
 
 
 if __name__ == "__main__":
+    urinput = [];
     target = int(input("Enter the sum: \n"))
-    urinput = [target]
-    coins_sum_len = int(input("Enter number of varieties of coins:\n")) 
-    print("Enter the value of coins with line breaks")
-    for i in range(coins_sum_len):
-        urinput.append(int(input()))
+    set_of_coins = [int(x) for x in input("Enter the value of coins with spaces seperated:\n").split()]
+    urinput = [target] + set_of_coins;
 
+    print(urinput)
     ans = coins_sum( urinput )
     print("===================")
     for item in ans:
