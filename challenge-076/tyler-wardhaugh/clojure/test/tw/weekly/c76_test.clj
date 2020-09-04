61693aedc19be0752aca365702514f4b325b921f
Tyler Wardhaugh <tyler.wardhaugh@entercom.com>
2020-09-04 13:43:16 -0700

    Task #2

--- challenge-076/tyler-wardhaugh/clojure/test/tw/weekly/c76_test.clj
+++ challenge-076/tyler-wardhaugh/clojure/test/tw/weekly/c76_test.clj
@@ -1,7 +1,8 @@
 (ns tw.weekly.c76-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
             [tw.weekly.ch-1 :refer [find-min-primes-to-sum find-min-primes-to-sum-brute-force]]
-            [tw.weekly.ch-2 :refer []]))
+            [tw.weekly.ch-2 :refer [parse-dict-file parse-grid-file word-search]]))
 
 (deftest ch-1
   (testing "Task 1"
@@ -10,4 +11,7 @@
 
 (deftest ch-2
   (testing "Task 2"
-    ))
+    (let [grid (parse-grid-file (io/resource "grid.txt"))
+          dict (parse-dict-file (io/resource "dict.txt"))
+          words (word-search grid dict)]
+      (is (= (into #{} dict) (into #{} words))))))
