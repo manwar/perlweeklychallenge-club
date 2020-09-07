9bff867e95b2e505122617b098fde4fa6362ac6d
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2020-09-07 09:47:35 -0700

    prepare files for Challenge #077

---
+++ challenge-077/tyler-wardhaugh/clojure/test/tw/weekly/c77_test.clj
@@ -0,0 +1,13 @@
+(ns tw.weekly.c77-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [tw.weekly.ch-1 :refer []]
+            [tw.weekly.ch-2 :refer []]))
+
+(deftest ch-1
+  (testing "Task 1"
+    ))
+
+(deftest ch-2
+  (testing "Task 2"
+    ))
