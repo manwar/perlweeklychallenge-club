fca5d7df3072bc6d574b2dc71c2a80ee5a82359b
Myoungjin JEON <jeongoon@gmail.com>
2020-09-15 23:47:27 +1000

    [ch-078/jeongoon] add Haskell Solution

---
+++ challenge-078/jeongoon/haskell/ch-2.hs
@@ -0,0 +1,16 @@
+import System.Environment
+import Data.List
+import Data.Char (isNumber)
+import Data.Maybe (catMaybes)
+
+-- tested with: runhaskell 7 4 2 6 3 / 1 3 4
+-- use any non-digit value as a separator between @A and @B
+
+unsafe_mapLeftRotate ls rotates =
+  scanl (\ls' n -> (drop n ls') ++ (take n ls')) ls rotates
+
+parseNumbers = catMaybes.map (\nStr -> if (all isNumber nStr)
+                                        then Just(read nStr:: Int) else Nothing)
+main = do (mapM_ print.tail.
+           uncurry unsafe_mapLeftRotate.(\(a,b) -> (a, parseNumbers b)))
+           =<< (span (all isNumber)) `fmap` getArgs
