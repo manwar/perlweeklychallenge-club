ffbd8368ce571dab7b941dd8aa8e50f2f5678b5b
Abigail <abigail@abigail.be>
2020-09-15 14:30:29 +0200

    Modify the test program so it tests Javascript solutions as well.

--- challenge-078/abigail/test.pl
+++ challenge-078/abigail/test.pl
@@ -17,12 +17,25 @@ use experimental 'signatures';
 
 use Test::More;
 
+my %languages = (
+    Perl        =>   ["/opt/perl/bin/perl"  => 'pl'],
+    JavaScript  =>   ["/usr/local/bin/node" => 'js'],
+);
+
+
 my @inputs = <input*>;
-foreach my $input (@inputs) {
-    my $output_exp = ($input =~ s/input/output/r) . ".exp";
-    my $exp        = `cat $output_exp`;
-    my $got        = `./solution.pl < $input`;
-    is $got, $exp, $input;
+
+foreach my $language (sort keys %languages) {
+    my ($exe, $ext) = @{$languages {$language}};
+    next unless -r "solution.$ext";
+    subtest $language => sub {
+        foreach my $input (@inputs) {
+            my $output_exp = ($input =~ s/input/output/r) . ".exp";
+            my $exp        = `cat $output_exp`;
+            my $got        = `$exe ./solution.$ext < $input`;
+            is $got, $exp, $input;
+        }
+    }
 }
 
 done_testing;
