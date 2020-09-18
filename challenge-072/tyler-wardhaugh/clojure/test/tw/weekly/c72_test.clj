061716eb0bcbdd2c7a89b95f4a9f89f89551190c
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2020-09-18 12:17:25 -0700

    Task 2

--- challenge-072/tyler-wardhaugh/clojure/test/tw/weekly/c72_test.clj
+++ challenge-072/tyler-wardhaugh/clojure/test/tw/weekly/c72_test.clj
@@ -1,7 +1,8 @@
 (ns tw.weekly.c72-test
   (:require [clojure.test :refer [are deftest is testing]]
+            [clojure.java.io :as io]
             [tw.weekly.c72.t1 :refer [fact trailing-zeros]]
-            [tw.weekly.c72.t2 :refer []]))
+            [tw.weekly.c72.t2 :refer [lines-from]]))
 
 (deftest task-1
   (testing "Task 1, Trailing Zeroes"
@@ -12,4 +13,4 @@
 
 (deftest task-2
   (testing "Task 2, Lines Range"
-    ))
+    (is (= (list "L1" "L2" "L3") (lines-from (io/resource "input.txt") 1 3)))))
