d6ee3a6749c2fe6f837ff4021e559cde8b34a0f6
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-09-21 23:58:29 +0100

    - Added clean up directory script.

---
+++ clean-up-dir.sh
@@ -0,0 +1,19 @@
+find . -name "*.t" -delete
+find . -name "*.pl" -delete
+find . -name "*.p6" -delete
+find . -name "*.py" -delete
+find . -name "*.js" -delete
+find . -name "*.hs" -delete
+find . -name "*.lsp" -delete
+find . -name "*.txt" -delete
+find . -name "*.cpp" -delete
+find . -name "*.clj" -delete
+find . -name "*.lua" -delete
+find . -name "*.raku" -delete
+find . -name "*.swift" -delete
+find . -name "*.aplf" -delete
+rm -rf abigail/Part1
+rm -rf abigail/Part2
+rm -rf arne-sommer/perl/*
+rm -rf arne-sommer/raku/*
+rm -rf tyler-wardhaugh/clojure/src/tw/weekly/*
