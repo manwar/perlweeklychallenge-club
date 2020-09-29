3d582d8fd55cef900675a2aa5885d18ab7b8ed82
drbaggy <js5@sanger.ac.uk>
2020-09-29 08:41:04 +0100

    re-written with test-more tests and added additional tests to ensure that the ch-2.pl was working correctly as the examples in the blog could be solved with code which terminated prematurely [you need to repeat the application of rule 2 until you add no new candies] - and looking at some of the solutions this is the case

--- challenge-080/james-smith/perl/ch-2.pl
+++ challenge-080/james-smith/perl/ch-2.pl
@@ -5,20 +5,24 @@ use warnings;
 
 use feature qw(say);
 
-say candies( qw(1 2 2) );
-say candies( qw(1 4 3 2) );
+use Test::More;
+
+is( candies( qw(1 2 2) ),          4 );
+is( candies( qw(1 4 3 2) ),        7 );
+is( candies( qw(5 4 3 2 1) ),     15 );
+is( candies( qw(2 1 2 1 2 1 2) ), 11 );
+
+done_testing;
 
 sub candies {
   my @ranks = @_;
-  my $prev_count = @candies = map { 1 } @ranks; ## First pass we set everything to 1!
+  my $prev_count = my @candies = map { 1 } @ranks; ## First pass we set everything to 1!
   my $flag;
   do {
     my $count = 0;
     foreach( 0..(@ranks-2) ) {         ## Loop through comparing element to next one - increase as approprite
-
-      $candies[$_]   = $candies[$_+1]+1 if ($ranks[$_] > $ranks[$_+1]) && ($candies[$_] <= $candies[$_+1]);
-      $candies[$_+1] = $candies[$_  ]+1 if ($ranks[$_] < $ranks[$_+1]) && ($candies[$_] => $candies[$_+1]);
-
+      $candies[$_+1] = $candies[$_  ]+1 if $ranks[$_] < $ranks[$_+1] && $candies[$_] >= $candies[$_+1];
+      $candies[$_]   = $candies[$_+1]+1 if $ranks[$_] > $ranks[$_+1] && $candies[$_] <= $candies[$_+1];
       $count        += $candies[$_  ]; ## by the time we get here we would have done both comparisons that
                                        ## cause this entry to be updated..
     }
