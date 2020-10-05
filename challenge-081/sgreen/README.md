cef248ba491398a30061ba49fbc2a824116ae996
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2020-10-05 05:09:37 +0100

    - Added template for Challenge 81.

---
+++ challenge-081/sgreen/README.md
@@ -0,0 +1,3 @@
+# The Weekly Challenge 080
+
+Solution by Simon Green. [Blog](https://dev.to/simongreennet/the-weekly-challenge-080-2if0)
--- clean-up-dir.sh
+++ clean-up-dir.sh
@@ -7,20 +7,26 @@ find . -name "*.p6" -delete
 find . -name "*.py" -delete
 find . -name "*.js" -delete
 find . -name "*.hs" -delete
+find . -name "*.go" -delete
 find . -name "*.rs" -delete
+find . -name "*.rb" -delete
 find . -name "*.sh" -delete
 find . -name "*.awk" -delete
+find . -name "*.kts" -delete
 find . -name "*.exp" -delete
+find . -name "*.elm" -delete
 find . -name "*.lsp" -delete
 find . -name "*.txt" -delete
 find . -name "*.cpp" -delete
 find . -name "*.clj" -delete
 find . -name "*.lua" -delete
+find . -name "*.tcl" -delete
 find . -name "*.raku" -delete
 find . -name "*.swift" -delete
 find . -name "*.aplf" -delete
 find . -name "*.lock" -delete
 find . -name "*.toml" -delete
+find . -name "*.json" -delete
 rm -rf abigail/Part1
 rm -rf abigail/Part2
 rm -rf abigail/input-*
