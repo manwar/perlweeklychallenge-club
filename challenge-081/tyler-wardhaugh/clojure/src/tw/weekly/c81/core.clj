01d318f579ad3fdae1b796d57e83daccbe2c4198
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2020-10-06 08:56:51 -0700

    Ch81: Task 2

--- challenge-081/tyler-wardhaugh/clojure/src/tw/weekly/c81/core.clj
+++ challenge-081/tyler-wardhaugh/clojure/src/tw/weekly/c81/core.clj
@@ -1,9 +1,12 @@
 (ns tw.weekly.c81.core
  (:require [tw.weekly.c81.t1 :as t1])
+ (:require [tw.weekly.c81.t2 :as t2])
  (:gen-class))
 
 (defn -main
   "Run all tasks"
   [& _]
   (println "Task #1")
-  (t1/-main))
+  (t1/-main)
+  (println "Task #2")
+  (t2/-main))
