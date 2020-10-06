5a7b6deedb64b69ddad17271be3714f5d0f1334c
Dave Jacoby <jacoby.david@gmail.com>
2020-10-06 17:53:24 -0400

    Challenge 81

---
+++ challenge-081/dave-jacoby/perl/ch-1.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw{ say signatures state };
+no warnings qw{ experimental };
+
+my $comment = <<'DOC';
+You are given 2 strings, $A and $B.
+
+Write a script to find out common base strings in $A and $B.
+
+A substring of a string $S is called base string if repeated concatenation of the substring results in the string.
+
+Example 1:
+Input:
+    $A = "abcdabcd"
+    $B = "abcdabcdabcdabcd"
+
+Output:
+    ("abcd", "abcdabcd")
+Example 2:
+Input:
+    $A = "aaa"
+    $B = "aa"
+
+Output:
+    ("a")
+DOC
+
+common_base( "abcdabcd", "abcdabcdabcdabcd" );
+common_base( "aaa",      "aa" );
+
+sub common_base ( @words ) {
+    my ( $aa, $bb ) = sort { length $a <=> length $b } @words;
+    my %output;
+
+    for my $i ( 0 .. length $aa ) {
+        for my $j ( $i + 1 .. length $aa ) {
+            my $aaa = $aa;
+            my $bbb = $bb;
+            my $sub = substr( $aa, $i, $j );
+            $aaa =~ s/$sub//gmix;
+            $bbb =~ s/$sub//gmix;
+            $output{$sub} = 1 if $aaa eq '' && $bbb eq '';
+        }
+    }
+    say join ', ', keys %output;
+}
+
