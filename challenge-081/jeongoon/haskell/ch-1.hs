5ee41daf6b0576204c21643270b8d68bde353517
Myoungjin JEON <jeongoon@gmail.com>
2020-10-08 15:06:29 +1100

    [ch-081/jeongoon] Haskell solution modified

--- challenge-081/jeongoon/haskell/ch-1.hs
+++ challenge-081/jeongoon/haskell/ch-1.hs
@@ -1,6 +1,10 @@
 import System.Environment
 import System.Exit
 
+{- comment:
+   I tried with Data.Text but it was faster without Data.Text
+   but ch-2.hs is much faster when using it -}
+
 --commonDivisors :: (Integral a) => [a] -> [a]
 commonDivisors [] = []
 commonDivisors xs = filter (\cd -> all ((==0).(flip rem) cd) xs) [1..(gcd')]
@@ -25,4 +29,4 @@ main = do
   strs <- getArgs
   if length strs  < 2
     then die $ "usage: runhaskell ch-1.hs <string> <string>"
-    else putStrLn $ (unwords.commonBaseStrings) strs
+    else (putStrLn.unwords.commonBaseStrings) strs
