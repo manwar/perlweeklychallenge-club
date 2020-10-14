d597407285896782e8654f36f1c2f1bad2f7fdeb
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2020-10-14 16:35:06 -0700

    Ch82 (Lua): Task 2

--- challenge-082/tyler-wardhaugh/lua/test.lua
+++ challenge-082/tyler-wardhaugh/lua/test.lua
@@ -9,3 +9,12 @@ describe("Task 1, Common Factors", function()
     assert.are.same(t1.common_factors(18, 23), {1})
   end)
 end)
+
+describe("Task 2, Interleave Strings", function()
+  local t2 = require'ch-2'
+  it("produces correct results for the examples", function()
+    assert.are.same(t2.interleave_string("XY", "X", "XXY"), true)
+    assert.are.same(t2.interleave_string("XXY", "XXZ", "XXXXZY"), true)
+    assert.are.same(t2.interleave_string("YX", "X", "XXY"), false)
+  end)
+end)
