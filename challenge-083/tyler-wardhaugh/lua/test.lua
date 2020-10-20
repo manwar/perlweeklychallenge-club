392152e581a9176e9013e786b2d8f94e0ebb3b57
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2020-10-20 12:36:44 -0700

    Ch83 (Lua): Task 2

--- challenge-083/tyler-wardhaugh/lua/test.lua
+++ challenge-083/tyler-wardhaugh/lua/test.lua
@@ -10,3 +10,11 @@ describe("Task 1, Words Length", function()
     assert.are.same(t1.inner_words_length"Zero when-no-inner-words-exist!", 0)
   end)
 end)
+
+describe("Task 2, Flip Array", function()
+  local t2 = require'ch-2'
+  it("produces correct results for the examples", function()
+    assert.are.same(t2.flip_array({3, 10, 8}), 1)
+    assert.are.same(t2.flip_array({12, 2, 10}), 1)
+  end)
+end)
