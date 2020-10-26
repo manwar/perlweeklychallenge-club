4a795c91db71b2eaeb560caade54e63c08a1b858
Daniel Mita <mienaikage@gmail.com>
2020-10-26 14:18:12 +0000

    Fix incorrect implementation of combinations for challenge-083-2-perl

--- challenge-083/daniel-mita/perl/ch-2.pl
+++ challenge-083/daniel-mita/perl/ch-2.pl
@@ -13,10 +13,48 @@ if ( @ARGV < 2 || grep { !looks_like_number($_) || $_ < 1 } @ARGV ) {
 }
 
 my @combinations;
-for ( my $i = 0; $i < @ARGV; $i++ ) {
-    for ( my $j = $i; $j < @ARGV; $j++ ) {
-        push @combinations, [ @ARGV[ $i .. $j ] ]
-            if sum( @ARGV[ $i .. $j ] ) <= sum(@ARGV) / 2;
+my @idxs = (0);
+
+TOP: while (1) {
+
+    # Add a combination to our array if the sum of that combination
+    # is ≤ half the sum of args i.e. we can make them all negative
+    push @combinations, [ @ARGV[@idxs] ]
+        if sum( @ARGV[@idxs] ) <= sum(@ARGV) / 2;
+
+    # Nothing left to do if all indexes are covered
+    last if @idxs == @ARGV;
+
+    # Increase last index if possible
+    if ( $idxs[-1] < $#ARGV ) {
+        $idxs[-1]++;
+    }
+
+    # If we've maxed out the last index
+    else {
+        # Loop through all the previous indexes
+        for ( my $i = $#idxs; $i > 0; $i-- ) {
+
+            # If the difference between an index
+            # and the one before is more than 1
+            if ( $idxs[$i] - $idxs[ $i - 1 ] > 1 ) {
+
+                # Increase that previous index
+                my $j = ++$idxs[ $i - 1 ];
+
+                # Reset all the subsequent indexes (+1 of previous index)
+                for ( @idxs[ $i .. $#idxs ] ) {
+                    $_ = ++$j;
+                }
+
+                # Go back to the start
+                next TOP;
+            }
+        }
+
+        # Reset all indexes and add an additional element
+        # if we couldn't increase any further
+        @idxs = ( 0 .. @idxs );
     }
 }
 
