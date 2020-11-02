45c48ef959298df6e2380eb0414f5c7ce11eea53
Kai Burgdorf <k@iburgdorf.de>
2020-11-02 12:51:32 +0100

    fix: now check wether result fits in int

--- challenge-084/kai-burgdorf/perl/ch-1.pl
+++ challenge-084/kai-burgdorf/perl/ch-1.pl
@@ -6,13 +6,16 @@ use warnings;
 use Data::Dumper;
 
 my $N = $ARGV[0] // 1234;
+my $output;
+my @numbers = split( "", $N );
 
-if ($N >= 2147483647) {
-  print "0";
+while ( scalar @numbers != 0 ) {
+    $output .= pop @numbers;
+}
+if ( $output >= 2147483647 ) {
+    print "0\n";
 }
 else {
-  my @numbers = split("", $N);
-  while(scalar @numbers != 0) {
-    print pop @numbers;
-  }
+    print $output . "\n";
 }
+
