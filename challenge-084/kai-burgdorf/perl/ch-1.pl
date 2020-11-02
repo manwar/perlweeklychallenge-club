9414aee35a3f6411ccaa086d57d871e2862fdd39
Kai Burgdorf <k@iburgdorf.de>
2020-11-02 12:52:26 +0100

    fixed reverse integer challenge for negative numbers

--- challenge-084/kai-burgdorf/perl/ch-1.pl
+++ challenge-084/kai-burgdorf/perl/ch-1.pl
@@ -9,10 +9,18 @@ my $N = $ARGV[0] // 1234;
 my $output;
 my @numbers = split( "", $N );
 
+my $sign = "";
+if ( $numbers[0] eq "-" ) {
+    $sign = shift @numbers;
+}
+
 while ( scalar @numbers != 0 ) {
     $output .= pop @numbers;
 }
-if ( $output >= 2147483647 ) {
+
+$output = $sign . $output;
+
+if ( $output > 2147483647 || $output < -2147483648 ) {
     print "0\n";
 }
 else {
