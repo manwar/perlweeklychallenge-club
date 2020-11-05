283260cb8265a966b89dd5f7c425966332ce4fd0
juliodcs <julio.dcs@gmail.com>
2020-11-05 17:43:11 +0100

    Simplify solution and add more tests

--- challenge-085/juliodcs/perl/ch-2.pl
+++ challenge-085/juliodcs/perl/ch-2.pl
@@ -2,29 +2,30 @@ use strict;
 use warnings;
 use feature 'say';
 use experimental 'signatures';
-use Math::BigFloat;
+use List::Util 'first';
 
-sub power_of_2_ints($number) {
-    my $num = Math::BigFloat->new($number);
-    foreach my $x (2 .. $num / 2) {
-        if ($num % $x == 0) {
-            my $log = log($num)/log($x);
-            return $log->bround(2) == int $log;
-        }
-    }
-    return 0;
+sub power_of_ints($n) {
+    return 0 unless my $base = first { $n % $_ == 0 } (2 .. $n / 2);
+    return $n == $base ** int(0.5 + log($n)/log($base))
 }
 
 if (@ARGV > 0) {
-    say power_of_2_ints(+shift);
+    say power_of_ints(+shift);
     exit 0;
 }
 
 use Test::More;
 
-ok  power_of_2_ints(4), '2^2 = 4';
-ok  power_of_2_ints(8), 'Test case 1';
-ok !power_of_2_ints(15), 'Test case 2';
-ok  power_of_2_ints(125), 'Test case 3';
+ok !power_of_ints(1), 'Number 1';
+
+ok  power_of_ints(4), '2^2 = 4';
+ok  power_of_ints(8), 'Test case 1';
+ok !power_of_ints(15), 'Test case 2';
+ok  power_of_ints(125), 'Test case 3';
+ok  power_of_ints(64), 'Test case 3';
+ok  power_of_ints(2197), '13^3';
+ok !power_of_ints(2198), '13^3 + 1';
+ok !power_of_ints(2196), '13^3 - 1';
+ok !power_of_ints(4394), '13^3 * 2';
 
 done_testing;
