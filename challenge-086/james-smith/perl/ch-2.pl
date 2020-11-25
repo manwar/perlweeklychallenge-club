783f4bed20188b8713c558e0e501d037a90f58af
drbaggy <js5@sanger.ac.uk>
2020-11-25 08:58:43 +0000

    pushing changes

---
+++ challenge-086/james-smith/perl/ch-2.pl
@@ -0,0 +1,17 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+use warnings;
+use feature qw(say);
+use Test::More;
+
+# is( my_function(), 1 );
+
+done_testing();
+
+sub my_function() {
+
+  return;
+}
+
