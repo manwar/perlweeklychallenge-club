b3fc767b2ae0a07ff45f6f0a754fd2bf7d27bd5b
Abigail <abigail@abigail.be>
2020-12-14 00:03:46 +0100

    Comments

--- challenge-090/abigail/perl/ch-2.pl
+++ challenge-090/abigail/perl/ch-2.pl
@@ -16,13 +16,25 @@ binmode STDOUT, ":utf8";
 while (<>) {
     use integer;
     my ($A, $B) = /([0-9]+) \s+ ([0-9]+)/x or next;
+    #
+    # Max width of the left and right columns.
+    #
     my $w1 = length $A;
     my $w2 = length (my $product = $A * $B);
     while ($A) {
+        #
+        # Print a line, add a tick mark if the left column is odd
+        #
         printf "%${w1}d  %${w2}d %s\n" => $A, $B, $A % 2 ? $TICK : "";
+        #
+        # Divide and double
+        #
         $A /= 2;
         $B *= 2;
     }
+    #
+    # Print the sum
+    #
     say " " x $w1, "  ", "-" x $w2, " +";
     say " " x $w1, "  ", $product;
 }
