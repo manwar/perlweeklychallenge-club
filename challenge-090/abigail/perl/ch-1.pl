95fb080ab7e86d8bca779a3a0d448d54ee676a3a
Abigail <abigail@abigail.be>
2020-12-13 21:06:46 +0100

    Comments

--- challenge-090/abigail/perl/ch-1.pl
+++ challenge-090/abigail/perl/ch-1.pl
@@ -9,6 +9,12 @@ no  warnings 'syntax';
 use experimental 'signatures';
 use experimental 'lexical_subs';
 
+#
+# y/// does the required replacements, and returns the number
+# of characters replaced -- which turns out to be the length
+# of the sequence. So, we first print the length of the sequence,
+# followed by the modified sequence.
+#
 print y/TAGC/ATCG/, "\n", $_ while <>;
 
 __END__
