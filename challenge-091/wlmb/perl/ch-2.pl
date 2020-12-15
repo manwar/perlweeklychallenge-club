abf0b8a71295c0c00fcee0659fd795ff48d45248
Luis Mochan <mochan@fis.unam.mx>
2020-12-15 12:13:27 -0600

    Use normal order. Remove previous solution.

--- challenge-091/wlmb/perl/ch-2.pl
+++ challenge-091/wlmb/perl/ch-2.pl
@@ -7,19 +7,19 @@
 use strict;
 use warnings;
 use v5.10;
-use List::Util qw(all any);
+use List::Util qw(all);
 use Scalar::Util::Numeric qw(isint);
 
 die "Usage: ./ch-2.pl s0 s1 s2...\n\t with sn maximum number of steps from stone n"
     unless @ARGV;
 die "Only non-negative numbers allowed" unless all {isint $_ == 1} @ARGV;
-my @stones=reverse @ARGV;
+my @stones=@ARGV;
 
 my @stepping_stones;
-push @stepping_stones, 0;
-foreach(1..$#stones){
-    unshift @stepping_stones, $_ if $stepping_stones[0] >= $_-$stones[$_];
+push @stepping_stones, $#stones;
+foreach(reverse (0..$#stones-1)){ # add stepping stones from left to right
+    unshift @stepping_stones, $_ if $stepping_stones[0] <= $_+$stones[$_];
 }
-say $stepping_stones[0]==$#stones # Notice I numbered stones from right to left
+say $stepping_stones[0]==0
     ?"1 Success\nPath: " . join "->", @stepping_stones # Notice I n
     :"0 Failure";
