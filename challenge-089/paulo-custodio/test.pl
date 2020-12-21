fa3f9df97e5e46ef32a5d7207e7b578cfebd2158
Paulo Custodio <pauloscustodio@gmail.com>
2020-12-21 00:08:40 +0000

    Add FreeBASIC solutions

--- challenge-089/paulo-custodio/test.pl
+++ challenge-089/paulo-custodio/test.pl
@@ -6,24 +6,39 @@ use Test::More;
 
 # task 1
 
-is `perl perl/ch-1.pl 3`, "3\n";
-is `perl perl/ch-1.pl 4`, "7\n";
-
-is `gforth -e 3 forth/ch-1.fs`, "3 \n";
-is `gforth -e 4 forth/ch-1.fs`, "7 \n";
+ok system("fbc basic/ch-1.bas") == 0;
+for ([3,3], [4,7]) {
+	my($in, $out) = @$_;
+	is capture("basic/ch-1 $in"), " $out\n";
+	is capture("perl perl/ch-1.pl $in"), "$out\n";
+	is capture("gforth -e $in forth/ch-1.fs"), "$out\n";
+}
 
 # task 2
 
-is `perl perl/ch-2.pl`, <<END;
+ok system("fbc basic/ch-2.bas") == 0;
+
+is capture("perl perl/ch-2.pl"), <<END;
 [ 2 9 4 ]
 [ 7 5 3 ]
 [ 6 1 8 ]
 END
 
-is `gforth forth/ch-2.fs`, <<END;
+for ("basic/ch-2",
+	 "gforth forth/ch-2.fs") {
+	is capture($_), <<END;
 [ 2 7 6 ]
 [ 9 5 1 ]
 [ 4 3 8 ]
 END
-
+}
+	 
 done_testing;
+
+# capture output, normalize eol
+sub capture {
+	my($cmd) = @_;
+	local $_ = `$cmd`;
+	s/[ \t\r]*\n/\n/g;
+	return $_;
+}
