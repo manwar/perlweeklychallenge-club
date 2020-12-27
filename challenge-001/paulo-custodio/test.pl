fdf1a0e3be22cd785e59d729cdb5cff48623de88
Paulo Custodio <pauloscustodio@gmail.com>
2020-12-27 21:14:45 +0000

    Add Perl solution to challenge 001

---
+++ challenge-001/paulo-custodio/test.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Test::More;
+use 5.030;
+
+is capture("perl perl/ch-1.pl"), 	"5 PErl WEEkly ChallEngE\n";
+
+is capture("perl perl/ch-2.pl"), <<END;
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+16
+17
+fizz
+19
+buzz
+END
+
+
+done_testing;
+
+sub capture {
+	my($cmd) = @_;
+	my $out = `$cmd`;
+	$out =~ s/\r//g;
+	return $out;
+}
