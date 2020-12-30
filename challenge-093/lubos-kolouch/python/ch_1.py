1259ff77d0b88fc14ccbb0e8d4fccd946c77cec9
Lubos Kolouch <lubos@kolouch.net>
2020-12-30 23:43:36 +0100

    Python solutions LK

---
+++ challenge-093/lubos-kolouch/python/ch_1.py
@@ -0,0 +1,44 @@
+#!/bin/env python
+# ===============================================================================
+#
+#         FILE: ch_2.py
+#
+#        USAGE: ./ch_2.py
+#
+#  DESCRIPTION: Perl Weekly Challenge #093
+#               https://perlweeklychallenge.org/blog/perl-weekly-challenge-093/
+#               Max Points
+#
+#       AUTHOR: Lubos Kolouch
+#      CREATED: 12/30/2020 02:39:16 PM
+# ===============================================================================
+from collections import defaultdict
+
+
+def get_max_lines(inp):
+    """ Get the slopes """
+
+    max_count = 0
+
+    for point in inp:
+
+        slopes = defaultdict(int)
+
+        for point2 in inp:
+            if point == point2:
+                continue
+
+            if point[0] == point2[0]:
+                slope = 'inf'
+            else:
+                slope = (point[1] - point2[1]) / (point[0] - point2[0])
+
+            slopes[slope] += 1
+
+        max_count = max(max_count, max(slopes.values()))
+
+    return max_count + 1
+
+
+assert get_max_lines([[1, 1], [2, 2], [3, 3]]) == 3
+assert get_max_lines([[1, 1], [2, 2], [3, 1], [1, 3], [5, 3]]) == 3
