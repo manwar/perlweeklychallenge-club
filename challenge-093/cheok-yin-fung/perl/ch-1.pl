7dffb30e4223d44138bd122ca9018013f979f2bf
Fung Cheok Yin <61836418+E7-87-83@users.noreply.github.com>
2021-01-04 07:44:39 +0800

    Update ch-1.pl

--- challenge-093/cheok-yin-fung/perl/ch-1.pl
+++ challenge-093/cheok-yin-fung/perl/ch-1.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/perl
 # The Weekly Challenge 093
 # Task 1 Max Points
+# I choose to interpret the task as only integer 
+# coordinates are allowed and the "lines" are only the 
+# diagonals, horizontals or verticals because
+# I find it fun to show it can inplement as an O(n) solution,
+# instead of O(n^2) solution of BF for a more general case.
+# ref: https://stackoverflow.com/questions/4179581/what-is-the
+# -most-efficient-algorithm-to-find-a-straight-line-that-goes-through-m
 use strict;
 use warnings;
 use List::Util qw/max/;
