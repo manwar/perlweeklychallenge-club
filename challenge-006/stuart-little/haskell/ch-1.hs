b9b1a86a5a6d8486d08b635e351888367b7f562b
chirvasitua <stuart-little@users.noreply.github.com>
2021-01-09 09:44:17 -0500

    sort input in ch-1

--- challenge-006/stuart-little/haskell/ch-1.hs
+++ challenge-006/stuart-little/haskell/ch-1.hs
@@ -4,6 +4,7 @@
 
 import System.Environment (getArgs,)
 import Data.List.Extra (groupOn,)
+import Data.List (sort,)
 
 showInterval :: Show a => [a] -> String
 showInterval xs
@@ -14,4 +15,4 @@ showInterval xs
 
 main = do
   args <- getArgs
-  putStrLn $ unwords $ map showInterval $ (map.map) snd $ groupOn fst $ zipWith (\x y -> (y-x,y)) [0..] $ map (read::String->Int) args
+  putStrLn $ unwords $ map showInterval $ (map.map) snd $ groupOn fst $ zipWith (\x y -> (y-x,y)) [0..] $ sort $ map (read::String->Int) args
