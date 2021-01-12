9af685c4d60cc554406827da104fc2df5078ae99
Abigail <abigail@abigail.be>
2021-01-12 16:58:05 +0100

    Handle Unicode for Python solution, week 95/part 1.
    No script runs though.

--- challenge-095/abigail/python/ch-1.py
+++ challenge-095/abigail/python/ch-1.py
@@ -2,8 +2,17 @@ import fileinput
 import re
 
 for line in fileinput . input ():
-    line = line . rstrip ()
-    if (re . match (r'^\d+(\.\d+)?$', line) and line == line [::-1]):
+    #
+    # Make sure the string is in UTF-8, and remove any newlines
+    #
+    line = line . decode ("utf-8") . strip ()
+
+    #
+    # Should look like an unsigned number (possible decimal)
+    # and be a palindrome.
+    #
+    if (re . match (r'^\d+(\.\d+)?$', line, re . UNICODE) and
+        line == line [::-1]):
         print 1
     else:
         print 0
