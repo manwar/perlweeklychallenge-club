b6cf3f4d1dc865882332c297616553d4a4fe4581
Adam Russell <ac.russell@live.com>
2021-01-17 11:35:02 -0500

    initial commit

---
+++ challenge-095/adam-russell/perl/ch-2.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+##
+# Write a script to demonstrate Stack operations.
+##
+use Stack;
+
+use Data::Dump q/pp/;
+
+my $stack = new Stack();
+$stack->push(2);
+$stack->push(-1);
+$stack->push(0);
+$stack->pop;       
+print $stack->top . "\n"; 
+$stack->push(0);
+print $stack->min . "\n"; 
