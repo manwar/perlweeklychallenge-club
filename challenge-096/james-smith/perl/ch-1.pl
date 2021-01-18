f509d1459ce594e00206b63dd90692ede6c9e95e
James Smith <js5@sanger.ac.uk>
2021-01-18 11:19:52 +0000

    Added note

--- challenge-096/james-smith/perl/ch-1.pl
+++ challenge-096/james-smith/perl/ch-1.pl
@@ -6,12 +6,20 @@ use warnings;
 use feature qw(say);
 use Test::More;
 
-is( rev_words('The Weekly Challenge'), 'Challenge Weekly The' );
-is( rev_words('    Perl and   Raku are  part of the same family  '), 'family same the of part are Raku and Perl' );
+is( rev_words('The Weekly Challenge'),
+    'Challenge Weekly The' );
+is( rev_words('    Perl and   Raku are  part of the same family  '),
+    'family same the of part are Raku and Perl' );
 
 done_testing();
 
+## Nothing much to say here...
+## split the string into words
+## remove any "empty" strings {the one at the start!}
+## reverse the order
+## and stitch it back together....
+
 sub rev_words {
-  return join q( ), grep {$_} reverse split m{\s+}, $_[0];
+  return join q( ), reverse grep {$_} split m{\s+}, $_[0];
 }
 
