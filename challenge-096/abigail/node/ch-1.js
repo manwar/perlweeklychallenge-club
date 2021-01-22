d3b7070aceb66209460753b95438b7e53f07bd61
Abigail <abigail@abigail.be>
2021-01-22 20:28:00 +0100

    Use the 'readline' module instead of the 'fs' module to read lines.

--- challenge-096/abigail/node/ch-1.js
+++ challenge-096/abigail/node/ch-1.js
@@ -8,15 +8,10 @@
 // Run as: node ch-1.js < input-file
 //
 
-  require      ("fs")
-. readFileSync (0)               // Read all.
-. toString     ()                // Turn it into a string.
-. split        ("\n")            // Split on newlines.
-. filter       (_ => _ . length) // Filter out empty lines.
-. map          (_ => console . log (_ . trim    ()        // Remove leading
-                                                          // and trailing
-                                                          // whitespace.
-                                      . split   (/\s+/)   // split ...
-                                      . reverse ()        // reverse ...
-                                      . join    (" ")))   // and recombine.
-;
+require ('readline')
+. createInterface ({input: process . stdin})
+. on ('line', _ =>
+       console . log (_ . trim    ()        // Remove leading/trailing spaces
+                        . split   (/\s+/)   // Split on white space
+                        . reverse ()        // Reverse the words
+                        . join    (" ")))   // And join them again.
