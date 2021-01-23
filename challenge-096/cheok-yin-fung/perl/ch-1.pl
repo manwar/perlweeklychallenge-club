0070c847995a4ed84f1563d1873a07255ee73bf0
E7-87-83 <fungcheokyin@gmail.com>
2021-01-23 10:48:47 +0800

    2 Perl scripts

---
+++ challenge-096/cheok-yin-fung/perl/ch-1.pl
@@ -0,0 +1,10 @@
+# The Weekly Challenge 096
+# Usage:
+# $ perl ch-1.pl "the weekly challenge"
+# challenge weekly the
+
+$_ = $ARGV[0];
+s/(\s)+/ /g ; 
+@a = reverse split / /, $_; 
+print join " ", @a; 
+print "\n"
