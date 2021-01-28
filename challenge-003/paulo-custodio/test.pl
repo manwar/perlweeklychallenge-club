e233d3a019dda275d8d790076d25ec92e47b4ffe
Paulo Custodio <pauloscustodio@gmail.com>
2021-01-28 00:33:56 +0000

    Add Basic, C, C++, Forth, Lua and Python to challenge 003

--- challenge-003/paulo-custodio/test.pl
+++ challenge-003/paulo-custodio/test.pl
@@ -5,7 +5,13 @@ use warnings;
 use Test::More;
 use 5.030;
 
-is capture("perl perl/ch-1.pl 20"), <<END;
+my $LUA = ($^O eq 'msys') ? "lua.exe" : "lua";
+
+run("gcc     c/ch-1.c   -o     c/ch-1");
+run("g++   cpp/ch-1.cpp -o   cpp/ch-1");
+run("fbc basic/ch-1.bas -o basic/ch-1");
+
+for ([20 => <<END]) {
 1
 2
 3
@@ -27,20 +33,43 @@ is capture("perl perl/ch-1.pl 20"), <<END;
 32
 36
 END
+    my($in, $out) = @$_;
 
-is capture("perl perl/ch-2.pl 10"), <<END;
-          1
-         1 1
-        1 2 1
-       1 3 3 1
-      1 4 6 4 1
-     1 5 10 10 5 1
-    1 6 15 20 15 6 1
-   1 7 21 35 35 21 7 1
-  1 8 28 56 70 56 28 8 1
- 1 9 36 84 126 126 84 36 9 1
+    is capture(     "$LUA lua/ch-1.lua $in"), $out;
+    is capture(    "perl perl/ch-1.pl  $in"), $out;
+    is capture( "gforth forth/ch-1.fs  $in"), $out;
+    is capture("python python/ch-1.py  $in"), $out;
+    is capture(            "c/ch-1     $in"), $out;
+    is capture(          "cpp/ch-1     $in"), $out;
+    is capture(        "basic/ch-1     $in"), $out;
+}
+
+run("gcc     c/ch-2.c   -o     c/ch-2");
+run("g++   cpp/ch-2.cpp -o   cpp/ch-2");
+run("fbc basic/ch-2.bas -o basic/ch-2");
+
+for ([10 => <<END]) {
+         1
+        1 1
+       1 2 1
+      1 3 3 1
+     1 4 6 4 1
+    1 5 10 10 5 1
+   1 6 15 20 15 6 1
+  1 7 21 35 35 21 7 1
+ 1 8 28 56 70 56 28 8 1
+1 9 36 84 126 126 84 36 9 1
 END
+    my($in, $out) = @$_;
 
+    is capture(      "$LUA lua/ch-2.lua    $in "), $out;
+    is capture(     "perl perl/ch-2.pl     $in "), $out;
+    is capture(  "gforth forth/ch-2.fs     $in "), $out;
+    is capture( "python python/ch-2.py     $in "), $out;
+    is capture(             "c/ch-2        $in "), $out;
+    is capture(           "cpp/ch-2        $in "), $out;
+    is capture(         "basic/ch-2        $in "), $out;
+}
 
 done_testing;
 
@@ -49,4 +78,9 @@ sub capture {
     my $out = `$cmd`;
     $out =~ s/[ \t\v\f\r]*\n/\n/g;
     return $out;
+}
+
+sub run {
+    my($cmd) = @_;
+    ok 0==system($cmd), $cmd;
 }
