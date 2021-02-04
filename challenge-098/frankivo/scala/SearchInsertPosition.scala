5325d798c533ee12e814ef1b666962bef002f29c
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-02-04 14:27:22 +0100

    Search index

--- challenge-098/frankivo/scala/SearchInsertPosition.scala
+++ challenge-098/frankivo/scala/SearchInsertPosition.scala
@@ -9,22 +9,11 @@ object SearchInsertPosition {
         Example(Seq(11, 13, 15, 17), 19, 4)
     )
 
-    def searchExisting(A: Seq[Int], N: Int) : Int = A.indexOf(N)
-
-    def searchInsert(A: Seq[Int], N: Int) : Int = (A :+ N).indexWhere(_ >= N)
-
-    def check(exp: Example) : Boolean = {
-        Seq(
-            searchExisting(exp.A, exp.N),
-            searchInsert(exp.A, exp.N)
-        )
-            .filterNot(_ == -1)
-            .head == exp.E
-    }
+    def searchIndex(A: Seq[Int], N: Int) : Int = (A :+ N).indexWhere(_ >= N)
 
     def main(args: Array[String]): Unit = {
         examples
-        .map(check)
+        .map(e => searchIndex(e.A, e.N) == e.E)
         .foreach(println)
     }
 }
