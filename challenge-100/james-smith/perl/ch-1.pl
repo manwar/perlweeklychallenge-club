344e484c4eeebf978a065fa456480b842c7304ca
drbaggy <js5@sanger.ac.uk>
2021-02-15 08:44:02 +0000

    solutions to challenge 100 - nice to give me a one liner <g>!; Blog link is there - will write it later!

---
+++ challenge-100/james-smith/perl/ch-1.pl
@@ -0,0 +1,67 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+use warnings;
+use feature qw(say);
+use Test::More;
+
+is( fun_time( '05:15pm' ), '17:15' );
+is( fun_time( '05:15 pm' ), '17:15' );
+is( fun_time( '17:15' ), '05:15pm' );
+is( fun_time( '05:15am' ), '05:15' );
+is( fun_time( '05:15 am' ), '05:15' );
+is( fun_time( '05:15' ), '05:15am' );
+is( fun_time( '00:00' ), '12:00am' );
+is( fun_time( '12:00' ), '12:00pm' );
+is( fun_time( '24:00' ), '12:00pm' );
+is( fun_time( '12:00 am' ), '00:00' );
+is( fun_time( '12:00 pm' ), '12:00' );
+
+is( fun_time_readable( '05:15pm' ), '17:15' );
+is( fun_time_readable( '05:15 pm' ), '17:15' );
+is( fun_time_readable( '17:15' ), '05:15pm' );
+is( fun_time_readable( '05:15am' ), '05:15' );
+is( fun_time_readable( '05:15 am' ), '05:15' );
+is( fun_time_readable( '05:15' ), '05:15am' );
+is( fun_time_readable( '00:00' ), '12:00am' );
+is( fun_time_readable( '12:00' ), '12:00pm' );
+is( fun_time_readable( '24:00' ), '12:00pm' );
+is( fun_time_readable( '12:00 am' ), '00:00' );
+is( fun_time_readable( '12:00 pm' ), '12:00' );
+
+done_testing();
+
+## 72 chars ############################################################
+sub fun_time {
+  $_[0]=~s{\A(\d+):(\d+)\s*([ap]?m?)\Z}{sprintf'%02d:%02d%s',
+    ($1%12||($3?0:12))+('pm'eq$3?12:0),$2,$3?'':$1>11?'pm':'am'}regx;
+}
+
+sub fun_time_readable {
+  return $_[0] =~
+    s{
+      \A (\d+) : (\d+) \s* ( [ap]?m? ) \Z
+        ## Split into 3 parts - we cheat a bit with the
+        ## third capture ([ap]m)? may have been better
+        ## but then $3 isn't always defined...
+        ## could do ((?:[ap]m)?) but that would have
+        ## been longer....
+    }
+    {
+      sprintf '%02d:%02d%s',
+
+        ( $1%12 || ($3?0:12) ) + ( 'pm' eq $3 ? 12 : 0 ),
+          ## Get hour modulo 12..
+          ## If were in 24 hour clock we need to convert
+          ## 00:?? to 12:??
+          ## If it is pm we then need to add 12...
+        $2,
+          ## The minutes is the easy bit just copy..
+        $3 ? '' : $1>11 ? 'pm' : 'am'
+          ## If we are converting from 12hr clock the third
+          ## string is empty - if not and the time is <12
+          ## we return am o/w return pm
+    }regx;
+}
+
---
+++ challenge-100/james-smith/perl/ch-2.pl
@@ -0,0 +1,34 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+use warnings;
+use feature qw(say);
+use Test::More;
+
+is( triangle_sum( [1],[2,4],[6,4,9],[5,1,7,2] ), 8 );
+is( triangle_sum( [3],[3,1],[5,2,3],[4,3,1,3] ), 7 );
+
+done_testing();
+
+## 72 chars ############################################################
+
+## There are two directions you can go with this problem (up or down)
+## Down may appear initially the best solution, but looking at it
+## from the other angle - up is easier. We take off the last row of
+## the triangle, and "merge it into" the line before - replacing
+## the entry in the last row with the minimum of it's two "children"
+## repeating until the triangle only has a single cell the answer..
+
+sub triangle_sum {
+  my @tri = @_;
+  while(@tri>1) {
+    ## Strip off base of triangle...
+    my $base = pop @tri;
+    ## Update new last line by adding smallest of it's "children"
+    $tri[-1][$_] += $base->[ $base->[$_] < $base->[$_+1] ? $_ : $_ + 1 ]
+      foreach 0..@tri-1;
+  }
+  return $tri[0][0];
+}
+
