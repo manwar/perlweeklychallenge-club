b7f712c394905b7dc401767d2f723802e79cd5d2
Luis Mochan <mochan@fis.unam.mx>
2021-03-02 19:53:46 -0600

    Add solutions to challenge 102 1, 1a and 2

---
+++ challenge-102/wlmb/perl/ch-2.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+# Perl weekly challenge 102
+# Task 2: Hash-counting string
+#
+# See https://wlmb.github.io/2021/03/01/PWC102/#task-2-hash-counting-string
+use strict;
+use warnings;
+use v5.12;
+
+foreach my $length(@ARGV){
+    my $remaining=$length;
+    my @pieces;
+    while($remaining>0){
+	unshift @pieces, hash_string($remaining);
+	$remaining-=length $pieces[0];
+    }
+    say "Length: $length\tString: ", join '', @pieces;
+}
+sub hash_string {
+    my $N=shift;
+    return $N==1? "#": "$N#";
+}
