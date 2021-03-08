2fb98d85f3b55202fa7fc177258001a576d973d2
drbaggy <js5@sanger.ac.uk>
2021-03-08 22:50:41 +0000

    added documentation

--- challenge-103/james-smith/perl/ch-2.pl
+++ challenge-103/james-smith/perl/ch-2.pl
@@ -7,25 +7,24 @@ use feature qw(say);
 use Test::More;
 use Data::Dumper qw(Dumper);
 
-is( position( 1606134123, 1614591276, 'filelist.csv'), 'Les Miserables Episode 1: The Bishop (broadcast date: 1937-07-23) - 00:10:24' );
+is( position( 1606134123, 1614591276, 'filelist.csv'), 'Les Miserables Episode 1: The Bishop (broadcast date: 1937-07-23) @ 00:10:24' );
 
 done_testing();
 
 sub position {
   my ($start, $now, $filename ) =@_;
 
-  ## Get the durations and titles of episode..
-
-  open my $fh, q(<), $filename;
-  my @episodes = map { [split m{,?"}] } <$fh>;
-  close $fh;
-
-  ## Get the total duration of all the episodes
-  ## We need this to work out how far we have
-  ## wrapped around the time slots ....
+  ## Slurp in the durations and title's of the episodes,
+  ## and while doing this - computer the total length of
+  ## all the episodes
 
   my $tot_duration  = 0;
-     $tot_duration += $_->[0] foreach @episodes;
+  open my $fh, q(<), $filename;
+  $tot_duration += $_->[0] foreach my @episodes = map { [split m{,?"}] } <$fh>;
+  close $fh;
+
+  ## We need the total duration of episodes to work out
+  ## how far we have wrapped around the time slots ....
 
   ## Get the position through the current cycle...
 
@@ -33,15 +32,21 @@ sub position {
 
   ## Now find which episode that is at that time...
 
-  my $episode_start = 0;
   foreach( @episodes )  {
-    ## Return the output.... (in this case we add the duration through episode at the end of the episode name)
-    return sprintf '%s - %02d:%02d:%02d',
+
+    ## If the $position variable is less than the length of the episode
+    ## then this is the one we want to return....
+    ## $position value is the time after the start we want to return...
+    return sprintf '%s @ %02d:%02d:%02d',
       $_->[1],
-      int( ($position-$episode_start) / 3600000 )     ,
-      int( ($position-$episode_start) /   60000 ) % 60,
-      int( ($position-$episode_start) /    1000 ) % 60  if $episode_start + $_->[0] > $position;
-    $episode_start += $_->[0];
+      int( $position / 3600000 )     ,
+      int( $position /   60000 ) % 60,
+      int( $position /    1000 ) % 60  if $position < $_->[0];
+
+    ## OK - so we now need to reduce position by the length of the
+    ## episode and try again....
+
+    $position -= $_->[0];
   }
 }
 
