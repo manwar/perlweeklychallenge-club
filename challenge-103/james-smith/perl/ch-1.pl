1a2ae0c10cfa6ec2a8a750deb0efc0f6160cbb06
drbaggy <js5@sanger.ac.uk>
2021-03-09 09:02:58 +0000

    tidied up layout

--- challenge-103/james-smith/perl/ch-1.pl
+++ challenge-103/james-smith/perl/ch-1.pl
@@ -13,11 +13,12 @@ is( year_name( 1938 ), 'Yang Earth Tiger' );
 
 done_testing();
 
-## Choose the order based on what would happen in years "0","60" - this is "Yang Metal Monkey",
-## so we make them the first entry in the list - saves us doing a "shift" to get the right
-## modulus key....
+## Choose the order based on what would happen in years "0","60" -
+## this is "Yang Metal Monkey", so we make them the first entry in
+## the list - saves us doing a "shift" to get the right modulus key.
 
-## We have added in the last "classifier" - Yang/Yin which split the element entry in two.
+## We have added in the last "classifier" - Yang/Yin which split the
+## element entry in two.
 
 sub year_name {
   return join q( ),
