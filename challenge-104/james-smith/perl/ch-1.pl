9e86d8945acde5d75a3fbc69ad4f222a6d290a3b
drbaggy <js5@sanger.ac.uk>
2021-03-15 10:02:40 +0000

    push ch104

---
+++ challenge-104/james-smith/perl/ch-1.pl
@@ -0,0 +1,42 @@
+#!/usr/local/bin/perl
+
+use strict;
+
+use warnings;
+use feature qw(say state);
+use Test::More;
+
+my @known = (0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, 2, 5, 3, 4, 1, 5, 4, 7, 3, 8, 5, 7, 2, 7, 5, 8, 3, 7, 4, 5, 1, 6, 5, 9, 4, 11, 7, 10, 3, 11, 8, 13, 5, 12, 7, 9, 2, 9, 7, 12, 5, 13, 8, 11, 3, 10, 7, 11, 4, 9, 5, 6, 1, 7, 6, 11, 5, 14, 9, 13, 4, 15, 11, 18, 7, 17, 10, 13, 3, 14, 11, 19, 8, 21, 13, 18, 5, 17, 12, 19);
+
+subtest 'fusc_seq' => sub {
+  is( join(' ',fusc_seq($_)), join ' ',@known[0..$_-1] ) foreach 0..@known-1;
+  done_testing();
+};
+
+subtest 'fusc' => sub {
+  is( fusc($_),               $known[$_-1]             ) foreach 1..@known-1;
+  done_testing();
+};
+
+done_testing();
+say join ' ',fusc_seq(50);
+
+sub fusc {
+  my $n = shift;
+  (fusc_seq($n))[$n-1];
+}
+
+sub fusc_seq {
+  my $n = shift;
+  return 0..$n-1 if $n<2;
+  my @seq = (0,1,1); ## We need to use one more seed value as the 5th entry
+                     ## when we pushed it needs the 3rd one before it can
+                     ## push as we are pushing two at a time...
+
+  ## We can simplify the code by pushing two entries onto the list each time...
+  push @seq, $seq[$_]+$seq[$_+1], $seq[$_+1] foreach 1..$n/2-1;
+
+  ## and if odd we just remove the last one we added...
+  pop @seq unless $n%2;
+  return @seq;
+}
