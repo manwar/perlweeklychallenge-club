cbb128243542bc7a9c2a6b3e7d8161d16a693f9a
Jörg Sommrey <28217714+jo-37@users.noreply.github.com>
2021-03-21 17:27:12 +0100

    Calculate fusc from the Calkin-Wilf sequence

--- challenge-104/jo-37/perl/ch-1.pl
+++ challenge-104/jo-37/perl/ch-1.pl
@@ -1,18 +1,19 @@
 #!/usr/bin/perl -s
 
 use v5.16;
-use Test2::V0;
+use Test2::V0 '!float';
 use Math::Prime::Util 'binomial';
-use Math::Utils 'ceil';
 use List::Util 'reduce';
+use Math::ContinuedFraction;
+use PDL; # Just for ceil and rle
 use experimental 'signatures';
 
-our ($help, $tests, $verbose, $fusc);
+our ($help, $tests, $verbose, $single, $cws);
 
 run_tests() if $tests;	# does not return
 
 die <<EOS if $help;
-usage: $0 [-help] [-tests] [-verbose] [-fusc] [N]
+usage: $0 [-help] [-tests] [-verbose] [-single] [-cws] [N]
 
 -help
     print this help text
@@ -23,11 +24,11 @@ usage: $0 [-help] [-tests] [-verbose] [-fusc] [N]
 -verbose
     print argument and result
 
--fusc
-    calculate fusc(N) only
+-single
+    calculate fusc(N) or fusc_from_cws(N) only
 
 N
-    calculate fusc(k) for 0 <= k < N
+    calculate fusc(k) or fusc_from_cws(N) for 0 <= k < N
     Default: 50
 
 EOS
@@ -36,22 +37,59 @@ EOS
 ### Input and Output
 
 my $n = shift // 50;
-if ($fusc) {
-    say "fusc($n)=" x !!$verbose, fusc($n);
+if ($single) {
+    if ($cws) {
+        say "fusc_from_cws($n)=" x !!$verbose, fusc_from_cws($n);
+    } else {
+        say "fusc($n)=" x !!$verbose, fusc($n);
+    }
 } else {
-    say "fusc($_)=" x !!$verbose, fusc($_) for 0 .. $n - 1;
+    if ($cws) {
+        say "fusc_from_cws($_)=" x !!$verbose, fusc_from_cws($_)
+            for 0 .. $n - 1;
+    } else {
+        say "fusc($_)=" x !!$verbose, fusc($_) for 0 .. $n - 1;
+    }
 }
 
 
 ### Implementation
 
 # Non-recursive implementation of fusc according to
-# http://oeis.org/A002487
+# http://oeis.org/A002487 as the number of odd elements in the diagonal
+# of Pascal's triangle.  Drawback of this implementations: rather large
+# numbers are involved.
 sub fusc ($n) {
     # Interestingly, without the modulus this would produce the
     # respective Fibonacci number.
     reduce {$a += $b % 2} 0, map binomial($_, $n - $_ - 1),
         ceil(($n - 1) / 2) .. $n - 1;
+}
+
+# An alternative non-recursive implementation:
+# Compute fusc(n) from the Calkin-Wilf sequence.  The n-th element of
+# the Calkin-Wilf sequence is the fraction fusc(n)/fusc(n+1).  This
+# element can be calculated by taking the run-length encoding of the
+# binary representation of n as the coefficients of a continued
+# fraction.
+# See https://en.wikipedia.org/wiki/Calkin%E2%80%93Wilf_tree
+sub fusc_from_cws ($n) {
+    # This doesn't work for zero.
+    return 0 unless $n;
+    # Get the rle of the binary representation.  Using PDL here as I
+    # didn't find an easier way.
+    my @rle = grep $_,
+        (rle(byte reverse split //, sprintf '%b', $n))[0]->list;
+    # Prepend a zero if the binary number ends in zero.  See the example
+    # in Wikipedia.
+    unshift @rle, 0 unless $n % 2;
+    # A bit tricky: Math::ContinuedFraction seems to require a trailing
+    # zero.
+    push @rle, 0;
+
+    # Return the numerator of the normalization of the continued
+    # fraction.
+    (Math::ContinuedFraction->new(\@rle)->convergent)[0];
 }
 
 
@@ -60,13 +98,17 @@ sub fusc ($n) {
 sub run_tests {
 
     my $n = 0;
-    for (0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, 2, 5, 3, 4, 1, 5, 4, 7,
-        3, 8, 5, 7, 2, 7, 5, 8, 3, 7, 4, 5, 1, 6, 5, 9, 4, 11, 7,
-        10, 3, 11, 8, 13, 5, 12, 7, 9, 2, 9, 7, 12, 5, 13, 8, 11, 3,
-        10, 7, 11, 4, 9, 5, 6, 1, 7, 6, 11, 5, 14, 9, 13, 4, 15, 11,
-        18, 7, 17, 10, 13, 3, 14, 11, 19, 8, 21, 13, 18, 5, 17, 12,
-        19) {
+    my @target = (0, 1, 1, 2, 1, 3, 2, 3, 1, 4, 3, 5, 2, 5, 3, 4, 1, 5,
+        4, 7, 3, 8, 5, 7, 2, 7, 5, 8, 3, 7, 4, 5, 1, 6, 5, 9, 4, 11, 7,
+        10, 3, 11, 8, 13, 5, 12, 7, 9, 2, 9, 7, 12, 5, 13, 8, 11, 3, 10,
+        7, 11, 4, 9, 5, 6, 1, 7, 6, 11, 5, 14, 9, 13, 4, 15, 11, 18, 7,
+        17, 10, 13, 3, 14, 11, 19, 8, 21, 13, 18, 5, 17, 12, 19);
+    for (@target) {
         is fusc($n++), $_;
+    }
+    $n = 0;
+    for (@target) {
+        is fusc_from_cws($n++), $_;
     }
 
     done_testing;
