a3af256668c8a6d723e5596ec1f7c6d15c87904f
drbaggy <js5@sanger.ac.uk>
2021-04-05 14:13:23 +0100

    Comments...

--- challenge-107/james-smith/perl/ch-2.pl
+++ challenge-107/james-smith/perl/ch-2.pl
@@ -8,5 +8,9 @@ use feature qw(say);
 use lib '.';
 use Calc;
 
+## %{class}:: contains a list of the methods for the class,
+## so we can just dump these (to make it deterministic) we
+## sort first....
+
 say join "\n", sort keys %Calc::;
 
