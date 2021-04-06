2b79dbff795727e5ee8227de995c0cc27a92db34
drbaggy <js5@sanger.ac.uk>
2021-04-06 09:24:02 +0100

    Documentation, some more golfing on ch-2 and an example of the difference between use & require.

--- challenge-107/james-smith/perl/ch-2.pl
+++ challenge-107/james-smith/perl/ch-2.pl
@@ -6,11 +6,19 @@ use warnings;
 use feature qw(say);
 #use Test::More;
 use lib '.';
-use Calc;
+require Calc_Require;
+use     Calc_Use;
 
 ## %{class}:: contains a list of the methods for the class,
 ## so we can just dump these (to make it deterministic) we
 ## sort first....
+##
+## We "import" the classes in two ways... use & require.
+##
+## By "require"ing the class - we don't get the additional
+## import method added to the class... (and so get the same
+## list of fns as in the question)
 
-say join "\n", sort keys %Calc::;
+say "Calc_Require:\n",join "\n  ", sort keys %Calc_Require::;
+say "Calc_Use:\n",join "\n  ", sort keys %Calc_Use::;
 
