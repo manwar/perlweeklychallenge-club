39caa8e6b7bcad824bcb8c00fbedd707fd2da998
Abigail <abigail@abigail.be>
2021-04-06 13:14:30 +0200

    Go, Java, m4 and OCaml solutions for week 107, part 1

---
+++ challenge-107/abigail/ocaml/ch-1.ml
@@ -0,0 +1,9 @@
+(*                  *)
+(* See ../README.md *)
+(*                  *)
+
+(*                       *)
+(* Run as: ocaml ch-1.ml *)
+(*                       *)
+
+print_endline "1210, 2020, 21200";
