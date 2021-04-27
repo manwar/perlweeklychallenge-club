70914a59ac8330255d08ac70e5fdfe3617533f67
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-04-27 17:45:23 +0100

    - Removed comments.

--- challenge-110/mohammad-anwar/perl/ch-1.pl
+++ challenge-110/mohammad-anwar/perl/ch-1.pl
@@ -12,17 +12,6 @@ open(my $fh, '<:encoding(utf8)', $FILE)
 while (my $row = <$fh>) {
     chomp $row;
 
-    #
-    # I got the following regex works fine against the test data
-    # when used on regex101.com
-    #
-    # /^\+?|(\()?\d{2}\d{2}?\1?\s\d{10}$/
-    #
-    # But For some strange reason, it didn't work here.
-    #
-    # So I had to get the following instead.
-    #
-
     if ( ($row =~ /\+\d{2}\s\d{10}/)   ||
          ($row =~ /\d{4}\s\d{10}/)     ||
          ($row =~ /\(\d{2}\)\s\d{10}/)
