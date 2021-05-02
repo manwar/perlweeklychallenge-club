2d2f56894d29cf5dfa7d4dcdda2ff56131793aae
E7-87-83 <fungcheokyin@gmail.com>
2021-05-02 22:18:42 +0800

    edit ch-1.pl

--- challenge-110/cheok-yin-fung/perl/ch-1.pl
+++ challenge-110/cheok-yin-fung/perl/ch-1.pl
@@ -8,7 +8,9 @@ use warnings;
 my $s;
 while ($s = <STDIN>) {
     chomp($s);
-    if (substr($s, -11) =~ / \d{10}/ &&
+    if ( length $s == 15 &&
+        substr($s, 4, 1) eq " " && 
+        substr($s, -11) =~ / \d{10}/ &&
         (substr($s,0,4) =~ /\d{4}/ ||
          substr($s,0,4) =~ /\+\d{2}/ ||
          substr($s,0,4) =~ /\(\d{2}\)/)
