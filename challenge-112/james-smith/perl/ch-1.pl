d202d9104bf9fb8f332005545dfe22b344da6b68
drbaggy <js5@sanger.ac.uk>
2021-05-11 09:34:02 +0100

    remove need for double nested loop - by implementing backtracking

--- challenge-112/james-smith/perl/ch-1.pl
+++ challenge-112/james-smith/perl/ch-1.pl
@@ -14,18 +14,14 @@ is( can_path('/a/b/../c/..'), '/a' );
 is( can_path('/a/../b/../c/..'), '/' );
 is( can_path('/a/../b/../c/..'), '/' );
 is( can_path('/a/../b/../c/../../..'), '/' );
+is( can_path('/a/../b/../c/../../..'), '/' );
+is( can_path('/../../../a/'), '/a' );
 
 done_testing();
 
 sub can_path {
-  my $l = my @parts = grep { $_ ne '' && $_ ne '.' } split m{/}, $_[0];
-  while(--$l>0) {
-    if($parts[$l] eq '..' && $parts[$l-1] ne '..' ) {
-      splice @parts, $l-1,2;
-      $l = @parts;# = @parts;
-    }
-  }
-  shift @parts while @parts && $parts[0] eq '..'; ## Remove any ##
+  my($p,@parts) = (1,grep { $_ ne '' && $_ ne '.' } split m{/}, $_[0]);
+  $parts[$p] ne '..' ? $p++ : $p>0 ? splice @parts,--$p,2 : shift @parts while $p < @parts;
   return '/'.join '/',@parts;
 }
 
