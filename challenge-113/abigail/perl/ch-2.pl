56e0ba6fe2b2ef91ca13fb7b2c212b4d396244a6
Abigail <abigail@abigail.be>
2021-05-23 22:11:14 +0200

    Adjust comments

--- challenge-113/abigail/perl/ch-2.pl
+++ challenge-113/abigail/perl/ch-2.pl
@@ -18,17 +18,25 @@ use experimental 'lexical_subs';
 #
 
 #
-# Note that the "binary tree" part of the challenge is a red herring.
-# We just have a bunch of numbers, and we want to replace each number
-# with the sum of all the numbers, minus the number itself.
-#
-# We decide to complete ignore the binary tree part. Just like in a
-# previous challenge, it's absolutely not stated what the format of
-# binary tree is -- and the maker of the challenge gave a simple
-# example of a 7 node tree, one which would immediately break down
-# if we added an 8th node without adding an additional layer.
-#
-# So, we will just take a row of numbers as input.
+# I have two major issues with the "binary tree" part of the problem.
+# 
+# First, and we have seen this problem before, the author of the problem
+# couldn't be bothered to state how the binary tree is given. Worse, the
+# author gave an utterly useless example, carefully crafted to work exactly
+# for the given input. But what if we were to make a tiny change? Suppose
+# the 7 was a right child of 2? Then what? How would that tree look like?
+# 
+# Second, what does having the input as a binary tree actually contribute
+# to the problem? We have a set of numbers, and we need to replace each number
+# with the difference of the sum of all the numbers, and said number. It
+# does not matter one iota whether the numbers are in a tree (let alone how
+# they are distributed in a tree), or in a list, or in an array. We'd still
+# get the set of numbers as output.
+# 
+# We therefore decide to completely ignore the binary tree aspect of the
+# problem. We take sets of numbers: each line of standard input is taken as
+# a different set of whitespace separated numbers, and we write the result
+# to standard output, one set on each line.
 #
 
 use List::Util qw [sum];
