15d2cfb15c51b4299d7ee632c149721a6b5293ef
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-05-25 13:49:44 +0100

    - Added Perl solution to task 2 of week 114.

--- challenge-114/mohammad-anwar/perl/ch-1.pl
+++ challenge-114/mohammad-anwar/perl/ch-1.pl
@@ -4,8 +4,7 @@ use strict;
 use warnings;
 
 my $N = $ARGV[0] || 1234;
-my $T = sprintf("%b", $N) =~ tr/1//;
 
 do { $N++ }
-until ($N == reverse split //,$N) && ((sprintf("%b", $N) =~ tr/1//) == $T+1);
-print sprintf("%d => %b\n", $N, $N);
+until ($N == reverse split //,$N);
+print "$N\n";
