aa64a9da8e325855e9d3e72bd571b16b90bbf78a
Luis Mochan <mochan@fis.unam.mx>
2021-06-05 00:08:16 -0500

    Add comment

--- challenge-115/wlmb/perl/ch-1.pl
+++ challenge-115/wlmb/perl/ch-1.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # Perl weekly challenge 115
-# Task 1: String Chain
+# Task 1: String Chain. Connectivity matrix solution.
 #
 # See https://wlmb.github.io/2021/06/01/PWC115/#task-1-string-chain
 use strict;
@@ -14,7 +14,7 @@ die "Usage ./ch-1.pl string1 [string2...]" unless @strings;
 my $C=zeroes(long,scalar(@strings), scalar(@strings)); #connectivity matrix
 map {my $f=$_;map {$C->slice("$f,$_").=follows($f, $_)}
     (0..@strings-1)}(0..@strings-1);
-my $R=reduce {map{$_->diagonal(0,1).=0}($a,$b); $b x $a;}
+my $R=reduce {map{$_->diagonal(0,1).=0}($a, $b); $b x $a;}
              ($C) x @strings;
 say "Input: ", join " ", @strings;
 say "Output: ", all($R->diagonal(0,1)>0);
