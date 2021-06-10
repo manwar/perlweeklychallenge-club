2ba8fa25d8bdaf1da13742db10a10a775e47083e
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-06-10 10:05:14 -0700

    Ch116 (Clojure): Task 1

--- challenge-116/tyler-wardhaugh/clojure/src/tw/weekly/c116/t1.clj
+++ challenge-116/tyler-wardhaugh/clojure/src/tw/weekly/c116/t1.clj
@@ -1,14 +1,47 @@
 (ns tw.weekly.c116.t1
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.pprint :refer [cl-format]]))
 
 ;;;
 ; Task description for TASK #1 › Number Sequence
 ;;;
 (def DEFAULT-INPUT [1234])
+(def NORESULT-FMT "~a as it is impossible to split satisfying the conditions.")
+
+(defn seq-to-int
+  [coll]
+  (->> coll (reduce str "") (#(Integer/parseInt %))))
+
+(defn -number-sequence
+  [n]
+  (let [nseq (-> n str seq)
+        nlen (count nseq)]
+    (->> (range 1 (quot nlen 2))
+         (keep (fn [len]
+                 (let [start (->> nseq (take len) seq-to-int)]
+                   (reduce
+                     (fn [acc end]
+                       (let [accint (seq-to-int acc)]
+                         (if (>= (-> accint str count) nlen)
+                           (reduced [accint start end])
+                           (conj acc end))))
+                     [start]
+                     (iterate inc (inc start))))))
+         (keep (fn [[n-candidate start end]]
+                 (when (= n-candidate n) [start end])))
+         first)))
+
+(defn number-sequence
+  "Treating the number as a string, split the number into elements such that
+  the difference is 1 and each element > 0 (and no leading zeros are used)"
+  [n]
+  (if-let [[start end] (-number-sequence n)]
+    (cl-format nil "~{~a~^,~}" (range start end))
+    (cl-format nil NORESULT-FMT n)))
 
 (defn -main
   "Run Task 1 with a given input N, defaulting to the first example from the
   task description."
   [& args]
   (let [[N] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+    (println (number-sequence N))))
--- challenge-116/tyler-wardhaugh/clojure/test/tw/weekly/c116_test.clj
+++ challenge-116/tyler-wardhaugh/clojure/test/tw/weekly/c116_test.clj
@@ -1,13 +1,14 @@
 (ns tw.weekly.c116-test
   (:require [clojure.test :refer [deftest is testing]]
-            #_[tw.weekly.c116.t1 :refer []]
+            [tw.weekly.c116.t1 :refer [number-sequence]]
             #_[tw.weekly.c116.t2 :refer []]))
 
-#_(deftest task-1
+(deftest task-1
   (testing "Task 1, Number Sequence"
-    (is )
-    (is )
-    (is )))
+    (is (= "1,2,3,4" (number-sequence 1234)))
+    (is (= "9,10,11" (number-sequence 91011)))
+    (is (= "10203 as it is impossible to split satisfying the conditions."
+           (number-sequence 10203)))))
 
 #_(deftest task-2
   (testing "Task 2, Sum of Squares"
