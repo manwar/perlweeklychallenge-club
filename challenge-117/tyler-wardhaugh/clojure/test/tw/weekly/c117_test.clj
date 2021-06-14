3d37b2d24933141599134e34af2a9fa3e9ee1c58
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-06-14 14:54:56 -0700

    Ch117 (Clojure): Task 1

--- challenge-117/tyler-wardhaugh/clojure/test/tw/weekly/c117_test.clj
+++ challenge-117/tyler-wardhaugh/clojure/test/tw/weekly/c117_test.clj
@@ -1,11 +1,20 @@
 (ns tw.weekly.c117-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c117.t1 :refer [missing-row]]
-            [tw.weekly.c117.t2 :refer [find-possible-paths]]))
+            [clojure.java.io :as io]
+            [tw.weekly.c117.t1 :as t1]
+            #_[tw.weekly.c117.t2 :refer [find-possible-paths]]))
 
 (deftest task-1
   (testing "Task 1, Missing Row"
-    ))
+    (let [example (first t1/DEFAULT-INPUT)
+          m-first (io/resource "input-missing-first.txt")
+          m-last (io/resource "input-missing-last.txt")]
+      (is (= 12 (t1/missing-row-by-incrementing example)))
+      (is (= 12 (t1/missing-row-by-set-difference example)))
+      (is (= 1 (t1/missing-row-by-incrementing m-first)))
+      (is (= 1 (t1/missing-row-by-set-difference m-first)))
+      (is (= 15 (t1/missing-row-by-incrementing m-last)))
+      (is (= 15 (t1/missing-row-by-set-difference m-last))))))
 
 (deftest task-2
   (testing "Task 2, Find Possible Paths"
