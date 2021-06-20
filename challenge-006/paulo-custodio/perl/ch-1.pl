0ed815ce97a445c5a43d6eb496b29970d1d89bc7
Paulo Custodio <pauloscustodio@gmail.com>
2021-06-20 01:34:18 +0100

    Add C solution to challenge 006

--- challenge-006/paulo-custodio/perl/ch-1.pl
+++ challenge-006/paulo-custodio/perl/ch-1.pl
@@ -3,7 +3,9 @@
 # Challenge 006
 #
 # Challenge #1
-# Create a script which takes a list of numbers from command line and print the same in the compact form. For example, if you pass “1,2,3,4,9,10,14,15,16” then it should print the compact form like “1-4,9,10,14-16”.
+# Create a script which takes a list of numbers from command line and print the
+# same in the compact form. For example, if you pass "1,2,3,4,9,10,14,15,16" 
+# then it should print the compact form like "1-4,9,10,14-16".
 
 use strict;
 use warnings;
--- challenge-006/paulo-custodio/perl/ch-2.pl
+++ challenge-006/paulo-custodio/perl/ch-2.pl
@@ -3,9 +3,11 @@
 # Challenge 006
 #
 # Challenge #2
-# Create a script to calculate Ramanujan’s constant with at least 32 digits of precision. Find out more about it here.
+# Create a script to calculate Ramanujan’s constant with at least 32 digits of 
+# precision. Find out more about it here.
 #
-# The standard IEEE 754 double-precision binary floating-point format: binary64 gives only 15 to 17 significant decimal digits
+# The standard IEEE 754 double-precision binary floating-point format: binary64
+# gives only 15 to 17 significant decimal digits
 # Therefore must use the bignum library
 
 use strict;
@@ -13,8 +15,8 @@ use warnings;
 use 5.030;
 use Math::BigFloat;
 
-my $accuracy = 64;
+my $accuracy = 128;
 
 my $e = Math::BigFloat->bpi($accuracy) * Math::BigFloat->bsqrt(163, $accuracy);
 my $k = Math::BigFloat->bexp($e, $accuracy);
-say $k;
+say $k->bround(30);
