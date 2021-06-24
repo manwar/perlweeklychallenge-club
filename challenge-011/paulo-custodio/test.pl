3f3d45004b145f2b8bc15617e0cbce80142ccf5b
Paulo Custodio <pauloscustodio@gmail.com>
2021-06-24 20:07:39 +0100

    Add bc solution to challenge 011

--- challenge-011/paulo-custodio/test.pl
+++ challenge-011/paulo-custodio/test.pl
@@ -1,46 +1,4 @@
-#!/usr/bin/perl
-
-use strict;
-use warnings;
-use 5.030;
+#!/usr/bin/env perl
+use Modern::Perl;
 use Test::More;
-
-is capture("perl perl/ch-1.pl"),    "-40\n";
-
-is capture("perl perl/ch-2.pl 1"), <<END;
-[[1]]
-END
-
-is capture("perl perl/ch-2.pl 2"), <<END;
-[[1, 0]
- [0, 1]]
-END
-
-is capture("perl perl/ch-2.pl 4"), <<END;
-[[1, 0, 0, 0]
- [0, 1, 0, 0]
- [0, 0, 1, 0]
- [0, 0, 0, 1]]
-END
-
-is capture("perl perl/ch-2.pl 10"), <<END;
-[[1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
- [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
- [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
- [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
- [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
- [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
- [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
- [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
- [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
- [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]
-END
-
-done_testing;
-
-sub capture {
-    my($cmd) = @_;
-    my $out = `$cmd`;
-    $out =~ s/[ \r\t]*\n/\n/g;
-    return $out;
-}
+require '../../challenge-001/paulo-custodio/test.pl';
