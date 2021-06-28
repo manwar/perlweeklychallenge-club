ccc5210b036d6b877b4ecb92bb8167f5bf1b86f1
drbaggy <js5@sanger.ac.uk>
2021-06-28 14:30:11 +0100

    add object solution

--- challenge-119/james-smith/perl/ch-2.pl
+++ challenge-119/james-smith/perl/ch-2.pl
@@ -13,9 +13,11 @@ my @TESTS = ( [5,13],[10,32],[60,2223] );
 is( no_11_filter($_->[0]), $_->[1] ) foreach @TESTS;
 is( no_11_filter_regex($_->[0]), $_->[1] ) foreach @TESTS;
 is( no_11_array( $_->[0]), $_->[1] ) foreach @TESTS;
+is( no_11_object( $_->[0]), $_->[1] ) foreach @TESTS;
 
 cmpthese(10_000,{
   'filter' => sub { no_11_filter($_->[0]) foreach @TESTS; },
+  'object' => sub { no_11_object($_->[0]) foreach @TESTS; },
   'regex' => sub { no_11_filter_regex($_->[0]) foreach @TESTS; },
   'array'  => sub { no_11_array( $_->[0]) foreach @TESTS; },
 });
@@ -24,13 +26,13 @@ done_testing();
 
 sub no_11_array {
   my( $n, $ptr, @v ) = ( shift, undef, 0 );
-  for( my $i = 0; $i < $n; $i++ ) {
+  for( my $i = 0; $i < $n; ) {
     my $ptr;
     for( $ptr = $#v; $ptr>-1 && ++$v[$ptr]>3; $ptr--) {
       $v[$ptr]=1;
     }
     unshift @v,1 if $ptr < 0;
-    $i-- if "@v"=~m{1 1};
+    $i++ unless "@v"=~m{1 1};
   }
   return join q(),@v;
 }
@@ -60,3 +62,45 @@ sub no_11_filter_regex {
     $n-- if $v!~m{(?:[0456789]|11)};
   }
 }
+
+sub no_11_object {
+  my( $n, $v ) = ( shift, Three->new );
+  for( my $i = 0; $i < $n; $v++, $i++ ) {
+    $v++ while $v->has_double_one;
+  }
+  return ''.$v;
+}
+
+package Three;
+
+use overload '++' => 'incr';
+use overload '""' => 'str';
+
+sub new {
+  my $x = [0];
+  bless $x, 'Three';
+  return $x;
+}
+
+sub has_double_one {
+  my($f,@v) = @{$_[0]};
+  while(@v) {
+    return 1 if ($f == 1) && $v[0] == 1;
+    $f = shift @v;
+  }
+  return 0;
+}
+sub incr {
+  my $v = shift;
+  my $ptr;
+  for( $ptr = @{$v}-1; $ptr>-1 && ++$v->[$ptr]>3; $ptr--) {
+    $v->[$ptr]=1;
+  }
+  unshift @{$v},1 if $ptr < 0;
+}
+
+sub str {
+  return join '',@{$_[0]};
+}
+
+1;
