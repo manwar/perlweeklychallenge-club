0324a00222492d9728627ed63289d8f8fc33d848
drbaggy <js5@sanger.ac.uk>
2021-06-28 12:06:34 +0100

    changes to 2

--- challenge-119/james-smith/perl/ch-2.pl
+++ challenge-119/james-smith/perl/ch-2.pl
@@ -10,18 +10,24 @@ use Data::Dumper qw(Dumper);
 
 my @TESTS = ( [5,13],[10,32],[60,2223] );
 
-is( no_11($_->[0]), $_->[1] ) foreach @TESTS;
+is( no_11_filter($_->[0]), $_->[1] ) foreach @TESTS;
+is( no_11_filter_regex($_->[0]), $_->[1] ) foreach @TESTS;
+is( no_11_array( $_->[0]), $_->[1] ) foreach @TESTS;
+
+cmpthese(10_000,{
+  'filter' => sub { no_11_filter($_->[0]) foreach @TESTS; },
+  'regex' => sub { no_11_filter_regex($_->[0]) foreach @TESTS; },
+  'array'  => sub { no_11_array( $_->[0]) foreach @TESTS; },
+});
 
 done_testing();
 
-sub no_11 {
+sub no_11_array {
   my $n = shift;
   my @v = (0);
   for( my $i = 0; $i < $n; $i++ ) {
     my $ptr;
-    for( $ptr =$#v; $ptr>-1; $ptr--) {
-      $v[$ptr]++;
-      last if $v[$ptr]<4;
+    for( $ptr = $#v; $ptr>-1 && ++$v[$ptr]>3; $ptr--) {
       $v[$ptr]=1;
     }
     unshift @v,1 if $ptr < 0;
@@ -30,3 +36,28 @@ sub no_11 {
   return join q(),@v;
 }
 
+sub no_11_filter {
+  my $n = shift;
+  my $v = 0;
+  while(1){
+    return $v unless $n;
+    $v++;
+    $n-- if 0 > index( $v,'4')
+         && 0 > index( $v,'0')
+         && 0 > index( $v,'5')
+         && 0 > index( $v,'6')
+         && 0 > index( $v,'7')
+         && 0 > index( $v,'8')
+         && 0 > index( $v,'9')
+         && 0 > index( $v,'11');
+  }
+}
+sub no_11_filter_regex {
+  my $n = shift;
+  my $v = 0;
+  while(1){
+    return $v unless $n;
+    $v++;
+    $n-- if $v!~m{(?:[0456789]|11)};
+  }
+}
