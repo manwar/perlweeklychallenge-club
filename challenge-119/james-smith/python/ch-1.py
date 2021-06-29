ad31f28cca546ea4c8d872b1021c7119b9051c9a
drbaggy <js5@sanger.ac.uk>
2021-06-29 10:46:12 +0100

    python - what an ****y language

---
+++ challenge-119/james-smith/python/ch-1.py
@@ -0,0 +1,8 @@
+TESTS=[[15,240],[165,90]]
+def swap_nibbles(_):
+  return (_>>4)|((_&15)<<4)
+for t in TESTS:
+  print( 'OK' if swap_nibbles(t[0])==t[1] else 'Not OK' )
+for t in TESTS:
+  print( 'OK' if swap_nibbles(t[1])==t[0] else 'Not OK' )
+
