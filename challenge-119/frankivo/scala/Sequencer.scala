039206dc90cc538bc78500f9105a997389c0fcd3
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-07-01 14:49:30 +0200

    Better Sequencer

--- challenge-119/frankivo/scala/Sequencer.scala
+++ challenge-119/frankivo/scala/Sequencer.scala
@@ -1,8 +1,8 @@
 object Sequencer {
   def sequencer(n: Int): Int = {
-    (0 to (n / 3))
-      .map(x => (1 to 3).map(y => y + x * 10))
-      .flatten
-      .toSeq(n)
+    ((1 to n * n.toString.length * 30)
+      .filterNot(_.toString exists (((4 to 9).toSeq :+ 0).map(_.toString) contains _.toString))
+      .filterNot(_.toString contains "11")
+      ) (n - 1)
   }
 }
