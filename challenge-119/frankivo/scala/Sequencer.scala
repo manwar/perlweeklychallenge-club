bea3fdaad02054bc86e585baf38ee6a63a99553f
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-07-01 15:05:58 +0200

    Limitless Sequencer

--- challenge-119/frankivo/scala/Sequencer.scala
+++ challenge-119/frankivo/scala/Sequencer.scala
@@ -1,8 +1,13 @@
 object Sequencer {
   def sequencer(n: Int): Int = {
-    ((1 to n * n.toString.length * 30)
-      .filterNot(_.toString exists (((4 to 9).toSeq :+ 0).map(_.toString) contains _.toString))
-      .filterNot(_.toString contains "11")
-      ) (n - 1)
+    var i = 1
+    var ok = 1
+    while (ok < n) {
+      if (!i.toString.exists(((4 to 9).toSeq :+ 0).map(_.toString) contains _.toString))
+        if (!i.toString.contains("11"))
+          ok = ok + 1
+      i = i + 1
+    }
+    i
   }
 }
