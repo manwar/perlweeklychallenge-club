bbf90db88eacfac6840509f26ef358b73e5bdeeb
Jaldhar H. Vyas <jaldhar@braincells.com>
2021-07-04 16:47:28 -0400

    Challenge 119 by Jaldhar H. Vyas.

---
+++ challenge-119/jaldhar-h-vyas/perl/ch-1.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use 5.020;
+use warnings;
+
+my $N = shift // die "Must specify an integer.\n";
+
+my $n = sprintf "%b", $N;
+if ((length $n) % 8 != 0) {
+    $n = 0 x (8 - (length $n) % 8) . $n;
+}
+
+say oct '0b' . join q{}, reverse (unpack '(A4)*', $n);
+
