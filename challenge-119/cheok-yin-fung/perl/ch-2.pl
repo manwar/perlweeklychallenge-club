cdfef3f9404041b4c7936a26532757e63fd4fcef
E7-87-83 <fungcheokyin@gmail.com>
2021-07-04 22:17:19 +0800

    wk 119

--- challenge-119/cheok-yin-fung/perl/ch-2.pl
+++ challenge-119/cheok-yin-fung/perl/ch-2.pl
@@ -1,25 +1,33 @@
 #!/usr/bin/perl
 # The Weekly Challenge - 119
 # Task 2 Sequence without 1-on-1
+# Usage: $ ch-2.pl N 
 use strict;
 use warnings;
+use Test::More tests => 3;
 
-my $N = $ARGV[0];
+my $N = $ARGV[0] || 0;
+print noo($N), "\n";
 
-my @arr = (0,1,2,3);
+sub noo {
+    my $p = $_[0];
+    my @arr = (0,1,2,3);
 
-my $x = 0, my $y;
+    my $x = 0, my $y;
 
-while ($#arr < $N) {
-    $y = $#arr;
-    for my $here ($x+1..$y) {
-        push @arr, $arr[$here]."1" 
-            if substr($arr[$here], -1, 1) ne "1";
-        push @arr, $arr[$here]."2";
-        push @arr, $arr[$here]."3";
+    while ($#arr < $p) {
+        $y = $#arr;
+        for my $here ($x+1..$y) {
+            push @arr, $arr[$here]."1" 
+                if substr($arr[$here], -1, 1) ne "1";
+            push @arr, $arr[$here]."2";
+            push @arr, $arr[$here]."3";
+        }
+        $x = $y;
     }
-    $x = $y;
+    return $arr[$p];
 }
-   
 
-print $arr[$N], "\n";
+ok (noo(5) == 13, "Example 1");
+ok (noo(10) == 32, "Example 2");
+ok (noo(60) == 2223, "Example 3");
