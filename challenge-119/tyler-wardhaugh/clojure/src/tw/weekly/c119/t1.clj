d5474cfb849c49abe27d3214293971282c69dd36
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-07-03 18:15:20 -0700

    Ch119 (Clojure): Task 1

--- challenge-119/tyler-wardhaugh/clojure/src/tw/weekly/c119/t1.clj
+++ challenge-119/tyler-wardhaugh/clojure/src/tw/weekly/c119/t1.clj
@@ -7,8 +7,12 @@
 (def DEFAULT-INPUT [101])
 
 (defn swap-nibbles
-  ""
-  [n])
+  "Swap the two nibbles of the binary representation of n."
+  [n]
+  (bit-or
+    (bit-and n (bit-not 2r11111111))
+    (unsigned-bit-shift-right (bit-and n 2r11110000) 4)
+    (bit-shift-left (bit-and n 2r00001111) 4)))
 
 (defn -main
   "Run Task 1 with a given input N, defaulting to the first example from the
