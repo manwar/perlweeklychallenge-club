b09c37a6a9ed4cf2f1fe19862649744284436066
Luis Mochan <mochan@fis.unam.mx>
2021-07-07 08:17:53 -0500

    Add solutions to PWC220

---
+++ challenge-120/wlmb/perl/ch-1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# Perl weekly challenge 120
+# Task 1: Swap odd/even bits
+#
+# See https://wlmb.github.io/2021/07/07/PWC220/#task-2-swap-even-odd-bits
+use strict;
+use warnings;
+use v5.12;
+use List::Util qw(all);
+my $odd=170; # 10101010
+my $even=85; # 01010101
+
+say "Input: $_, Output: ",
+     0<=$_<=255?((($_&$odd)>>1) |(($_&$even)<<1)):"Invalid"
+     for @ARGV;
---
+++ challenge-120/wlmb/perl/ch-2.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+# Perl weekly challenge 120
+# Task 2: Clock angle
+#
+# See https://wlmb.github.io/2021/07/07/PWC220/#task-2-clock-angle
+use strict;
+use warnings;
+use v5.12;
+
+for(@ARGV){
+    say "Wrong format: $_", next unless $_=~m/^(\d?\d):(\d\d)$/;
+    my $angle=(30*($1+$2/60)-$2*6)%360;
+    $angle=360-$angle if $angle>180;
+    say "Input: $_, Output: $angle";
+}
