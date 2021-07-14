bbf09073ce0c20a456e3134885d24e2c4a4b28d3
Mohammad S Anwar <Mohammad.Anwar@yahoo.com>
2021-07-14 20:17:04 +0100

    Update ch-1.pl

--- challenge-121/mohammad-anwar/perl/ch-1.pl
+++ challenge-121/mohammad-anwar/perl/ch-1.pl
@@ -1,5 +1,29 @@
 #!/usr/bin/perl
 
+=head1 Task #1: Invert Bit
+
+You are given integers 0 <= $m <= 255 and 1 <= $n <= 8.
+
+Write a script to invert $n bit from the end of the binary representation of $m and print the decimal representation of the new binary number.
+
+=head1 Example
+
+    Input: $m = 12, $n = 3
+    Output: 8
+
+    Binary representation of $m = 00001100
+    Invert 3rd bit from the end = 00001000
+    Decimal equivalent of 00001000 = 8
+
+    Input $m = 18, $n = 4
+    Output: 26
+
+    Binary representation of $m = 00010010
+    Invert 4th bit from the end = 00011010
+    Decimal equivalent of 00011010 = 26
+
+=cut
+
 use strict;
 use warnings;
 use Test::More;
