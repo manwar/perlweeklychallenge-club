580f84362298c4c14b3b41aa124dd3bb99d448f6
drbaggy <js5@sanger.ac.uk>
2021-07-19 13:09:15 +0100

    2nd pass with comments

--- challenge-122/james-smith/perl/ch-1.pl
+++ challenge-122/james-smith/perl/ch-1.pl
@@ -8,13 +8,24 @@ use Test::More;
 use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 
-my @stream = map {$_*10} 1..50;
+stream( map {$_*10} 1..50 );           ## Push values into stream...
+eval {say stream_average();} until $@; ## Use eval/$@ to repeat until stream dies.
 
-say stream_average($_) foreach @stream;
-
-sub stream_average {
-  state($n,$t);
-  $n++; $t+=shift;
-  return $t/$n;
+sub stream {
+  state(@stream);
+     @_      ? (push @stream,@_)       ## Parameters passed - push to stream
+   : @stream ? shift @stream           ## We have entry in stream return it
+   :           die;                    ## exhausted stream die....
 }
 
+sub stream_average {
+  ## Use state variables for the total & count;
+  state($n,$t);
+
+  ## Take next element and add to total
+  ## Increment the count, and return the ratio of the true values
+  ## Note we need to do pre-increment rather than
+  ## post increment so the incremement is done before use.
+
+  return ($t+=stream)/++$n;
+}
