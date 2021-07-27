5f22259b0ad9138417e7a89f698d619f21230dfb
Niels van Dijke <perlboy@cpan.org>
2021-07-27 10:25:17 +0000

    Task 2 Overlooked rotated squares earlier. Fixed.

--- challenge-123/perlboy1967/perl/ch-2.pl
+++ challenge-123/perlboy1967/perl/ch-2.pl
@@ -11,12 +11,14 @@ use v5.16;
 use strict;
 use warnings;
 
-use List::MoreUtils qw(frequency);
+use List::MoreUtils qw(frequency arrayify);
+use Data::Printer;
 
 use Test::More;
 
 # Prototype(s)
 sub areSquarePoints(\@);
+sub dist(\@\@);
 
 
 my $tests = [ 
@@ -24,6 +26,7 @@ my $tests = [
    [ [12,24],[16,10],[20,12],[18,16], 0 ],
    [ [10,24],[12,24],[10,12],[18,16], 0 ],
    [ [20,20],[20,10],[10,10],[10,20], 1 ],
+   [ [ 0, 1],[ 1, 2],[ 2, 1],[ 1, 0], 1 ],
 ];
 
 foreach my $t (@$tests) {
@@ -37,15 +40,28 @@ done_testing();
 sub areSquarePoints(\@) {
   my ($ar) = @_;
 
-  my %xF = frequency map {$_->[0]} @$ar;
-  my %yF = frequency map {$_->[1]} @$ar;
+  my @dP;
 
-  my @xV = sort { $b <=> $a } keys %xF;
-  my @yV = sort { $b <=> $a } keys %yF;
+  # If all points given are coordinates of a square
+  # then the distance between one point and the other three
+  # will give 2 identical and on other
+  foreach my $i (0 .. 3) {
+    my @d;
+    foreach my $j (0 .. 3) {
+      next if ($i == $j);
+      push(@d,sqrt(($ar->[$i][0]-$ar->[$j][0])**2 +
+                   ($ar->[$i][1]-$ar->[$j][1])**2));
+    }
+    my %f = frequency @d;
+    push(@dP,[keys %f]);
+  }
 
-  return 1 if (scalar(@xV) == 2 and
-               scalar(@yV) == 2 and
-               $xV[0]-$xV[1] == $yV[0]-$yV[1]);
+  # If all four points give same distance 'frequencies'
+  # then we have a square
+  my %f = frequency arrayify @dP;
+
+  return 1 if (scalar keys %f == 2);
 
   return 0;
 }
+
