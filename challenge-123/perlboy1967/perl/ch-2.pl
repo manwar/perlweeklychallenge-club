5746a8d5a6437903f87f33b2202eddc7988a7f72
Niels van Dijke <perlboy@cpan.org>
2021-07-27 11:19:03 +0000

    Ditch the arrayify usage

--- challenge-123/perlboy1967/perl/ch-2.pl
+++ challenge-123/perlboy1967/perl/ch-2.pl
@@ -11,14 +11,12 @@ use v5.16;
 use strict;
 use warnings;
 
-use List::MoreUtils qw(frequency arrayify);
-use Data::Printer;
+use List::MoreUtils qw(frequency);
 
 use Test::More;
 
 # Prototype(s)
 sub areSquarePoints(\@);
-sub dist(\@\@);
 
 
 my $tests = [ 
@@ -28,6 +26,7 @@ my $tests = [
    [ [20,20],[20,10],[10,10],[10,20], 1 ],
    [ [ 0, 1],[ 1, 2],[ 2, 1],[ 1, 0], 1 ],
    [ [-5, 0],[ 5, 0],[ 0, 1],[ 0,-1], 0 ],
+   [ [-1, 0],[ 5, 0],[ 0, 1],[ 0,-1], 0 ],
 ];
 
 foreach my $t (@$tests) {
@@ -54,12 +53,12 @@ sub areSquarePoints(\@) {
                    ($ar->[$i][1]-$ar->[$j][1])**2));
     }
     my %f = frequency @d;
-    push(@dP,[keys %f]);
+    push(@dP,keys %f);
   }
 
   # If all four points give same distance 'frequencies'
   # then we have a square
-  my %f = frequency arrayify @dP;
+  my %f = frequency @dP;
 
   return 1 if (scalar keys %f == 2);
 
