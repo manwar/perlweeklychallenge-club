3bb7e5b432f856cf6d2bcbfc930e4c3dd6983c21
Adam Russell <ac.russell@live.com>
2021-08-01 02:10:38 -0400

    initial commit

---
+++ challenge-123/adam-russell/cxx/ch-1.cxx
@@ -0,0 +1,53 @@
+#include <vector>
+#include <iostream>
+
+class Ugly{
+    private:
+        std::vector<int> factor(int);   
+        bool is_ugly(int);
+    public:
+        int nth_ugly(int); 
+};     
+
+std::vector<int> Ugly::factor(int n){
+    std::vector<int> factors;  
+    for (long int i=2; i*i<=n; i++){
+        if (n%i==0){
+            if(i*i!=n){  
+                factors.push_back(i);
+                factors.push_back(n/i);
+            }
+            else
+                factors.push_back(i);
+        }
+    }
+    return factors;  
+}
+
+bool Ugly::is_ugly(int n){
+    std::vector<int> factors = this->factor(n);
+    for(int i = 0; i < factors.size(); i++){
+        if(factors[i] != 2 && factors[i] != 3 && factors[i] != 5)
+            return false; 
+    }    
+    return true;   
+} 
+
+int Ugly::nth_ugly(int n){
+    int ugly_count = 1; 
+    int i = 2; 
+    if(n == 1)
+        return 1; 
+    do{
+        if(is_ugly(i))
+            ugly_count++;  
+        i++;  
+    }while(ugly_count != n); 
+    return i; 
+} 
+
+int main(int argc, char** argv){
+    Ugly ugly;
+    int u = ugly.nth_ugly(10);   
+    std::cout << u << std::endl;
+}
---
+++ challenge-123/adam-russell/perl/ch-1.pl
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+##
+#   
+##
+use boolean;
+
+sub prime_factor{
+    my $x = shift(@_); 
+    my @factors;    
+    for (my $y = 2; $y <= $x; $y++){
+        next if $x % $y;
+        $x /= $y;
+        push @factors, $y;
+        redo;
+    }
+    return @factors;  
+}
+
+sub is_ugly{
+    my($x) = @_; 
+    for my $factor (prime_factor($x)){
+        return false if $factor != 2 && $factor != 3 && $factor !=5; 
+    }
+    return true; 
+}
+
+sub nth_ugly{
+    my($n) = @_;  
+    return 1 if $n == 1; 
+    my $ugly_count = 1; 
+    my $i = 1;  
+    do{
+        $i++;
+        $ugly_count++ if is_ugly($i);   
+    }while($ugly_count != $n);
+    return $i;    
+} 
+
+MAIN:{
+    my($N);
+    $N = 7; 
+    print nth_ugly($N) . "\n"; 
+    $N = 10; 
+    print nth_ugly($N) . "\n"; 
+}   
