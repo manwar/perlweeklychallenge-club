588be19d6c9893bd6fa8e63e44f72147141e7705
drbaggy <js5@sanger.ac.uk>
2021-08-16 11:08:00 +0100

    tidied up layout to make readable - even if long lines

--- challenge-126/james-smith/perl/ch-2.pl
+++ challenge-126/james-smith/perl/ch-2.pl
@@ -10,27 +10,25 @@ use Data::Dumper qw(Dumper);
 
 my @grid = qw(x***x*xxxx *********x ****x*x*x* ***xx***** x***x****x);
 
-solve(@grid);
+say join "\n",'',@grid,'';
+print solve(@grid);
+say '';
 
 sub solve {
   my @res = ('');
   ## Map input of strings into an array of 1s (bombs) + 0s (spaces)
   my @in = map { [ map { $_ eq 'x' ? 1:0 } split //, $_ ] } @_;
-  ## Get max x value....
-  my($h,$w) = ($#_,-1 + length $_[0]);
-  foreach my $y (0..$h) {
+
+  ## Get index of last row or column...
+  my( $h, $w ) = ( $#_, -1 + length $_[0] );
+
+  foreach my $y ( 0 .. $h ) {
     $res[-1] .= $in[$y][$_] ? 'x' :
-     ( $y ? ( ($_?$in[$y-1][$_-1]:0)
-             +$in[$y-1][$_]
-             +($_<$w?$in[$y-1][$_+1]:0) ) : 0 ) +
-              ($_?$in[$y][$_-1]:0)
-             +$in[$y][$_]
-             +($_<$w?$in[$y][$_+1]:0) +
-     ( $y<$h ? ( ($_?$in[$y+1][$_-1]:0)
-               +$in[$y+1][$_]
-               +($_<$w?$in[$y+1][$_+1]:0) ) : 0 ) foreach 0..$w;
+     ( $y      ? ( $_ ? $in[$y-1][$_-1] : 0 ) + $in[$y-1][$_] + ( $_ < $w ? $in[$y-1][$_+1] : 0 ) : 0 ) +
+                 ( $_ ? $in[$y  ][$_-1] : 0 ) + $in[$y  ][$_] + ( $_ < $w ? $in[$y  ][$_+1] : 0 )       +
+     ( $y < $h ? ( $_ ? $in[$y+1][$_-1] : 0 ) + $in[$y+1][$_] + ( $_ < $w ? $in[$y+1][$_+1] : 0 ) : 0 ) foreach 0 .. $w;
     push @res, '';
   }
-  print join "\n", @res;
+  return join "\n", @res;
 }
 
