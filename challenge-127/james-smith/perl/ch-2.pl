3b1d2fb1f71090c802838ca05c612143e2a46e21
drbaggy <js5@sanger.ac.uk>
2021-08-23 15:32:32 +0100

    tidied up fns, and rewriten print_res to be usable in tests

--- challenge-127/james-smith/perl/ch-2.pl
+++ challenge-127/james-smith/perl/ch-2.pl
@@ -9,38 +9,39 @@ use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 
 my @TESTS = (
-  [ [1,4], [3,5], [6,8], [12, 13], [3,20] ],
-  [ [3,4], [5,7], [6,9], [10, 12], [13,15] ],
+  [ [ [1,4], [3,5], [6,8], [12, 13], [3,20] ],  '[ (3,5), (3,20) ]' ],
+  [ [ [3,4], [5,7], [6,9], [10, 12], [13,15] ], '[ (6,9) ]'         ]
 );
 
-#is( my_function($_->[0]), $_->[1] ) foreach @TESTS;
-#done_testing();
+is( print_res( conflict_intervals( $_->[0] ) ), $_->[1] ) foreach @TESTS;
+is( print_res( conflict_intervals_compact( $_->[0] ) ), $_->[1] ) foreach @TESTS;
+done_testing();
 
-print_res( conflict_intervals( $_ ) ) foreach @TESTS;
-print_res( conflict_intervals_compact( $_ ) ) foreach @TESTS;
+say print_res( conflict_intervals(         $_->[0] ) ) foreach @TESTS;
+say print_res( conflict_intervals_compact( $_->[0] ) ) foreach @TESTS;
 
 sub print_res {
-  printf "[ %s ]\n", join ', ', map { "($_->[0],$_->[1])" } @{$_[0]};
+  return sprintf '[ %s ]', join ', ', map { "($_->[0],$_->[1])" } @{$_[0]};
 }
 
 sub conflict_intervals {
   my @in = @{ $_[0] };
-  my @conf;
+  my @out;
   while( my $int = pop @in ) {
     foreach(@in) {
       next unless $int->[1] < $_->[0] || $int->[0] < $_->[1];
-      unshift @conf, $int;
+      unshift @out, $int;
       last;
     }
   }
-  return \@conf;
+  return \@out;
 }
 
 sub conflict_intervals_compact {
-  my(@in,@conf) = @{$_[0]};
-  while(my $int = pop @in) {
-    ($int->[1] < $_->[0] || $int->[0] < $_->[1]) && (unshift @conf, $int) && last foreach @in;
+  my(@i,@o) = @{$_[0]};
+  while(my $r = pop @i) {
+    ($r->[1] < $_->[0] || $r->[0] < $_->[1]) && (unshift @o, $r) && last foreach @i;
   }
-  return \@conf;
+  return \@o;
 }
 
