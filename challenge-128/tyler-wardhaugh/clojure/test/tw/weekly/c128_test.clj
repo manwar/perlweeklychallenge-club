94c38b608d8978e6add0b39857520aba562d8a85
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-09-04 21:37:07 -0700

    Ch128 (Clojure): prep for challenge

---
+++ challenge-128/tyler-wardhaugh/clojure/test/tw/weekly/c128_test.clj
@@ -0,0 +1,12 @@
+(ns tw.weekly.c128-test
+  (:require [clojure.test :refer [deftest is testing]]
+            #_[tw.weekly.c128.t1 :refer []]
+            #_[tw.weekly.c128.t2 :as t2]))
+
+(deftest task-1
+  (testing "Task 1, Maximum Sub-Matrix"
+    ))
+
+(deftest task-2
+  (testing "Task 2, Minimum Platforms"
+    ))
