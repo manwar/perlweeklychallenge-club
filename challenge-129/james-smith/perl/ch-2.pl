40b09fbdf34bf6b04f82423874b0c7522ce46737
drbaggy <js5@sanger.ac.uk>
2021-09-12 09:56:57 +0100

    remove unneeded list

--- challenge-129/james-smith/perl/ch-2.pl
+++ challenge-129/james-smith/perl/ch-2.pl
@@ -14,13 +14,13 @@ my $ch1 = LL->new( 1 )->add( 3 )->add( 2 );
 my $ch2 = LL->new( 3 )->add( 1 )->add( 2 );
 
 my $ch3 = LL->new( $ch1->val + $ch2->val );
-my ( $p1, $p2, $p3 ) = ( $ch1, $ch2, $ch3 );
+my ( $p1, $p2 ) = ( $ch1, $ch2 );
 
 while( 1 ) {
   $p1 = $p1->next;
   last unless $p1;
   $p2 = $p2->next;
-  $p3 = $p3->add( $p1->val + $p2->val );
+  $ch3->add( $p1->val + $p2->val );
 }
 
 say join "   ", $ch3->flatten;
