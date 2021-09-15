5788734e6c4c03df20a99c4db406e150871d635e
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-09-15 19:04:57 +0100

    - Tidied up Perl solution to task 1 of week 130.

--- challenge-130/mohammad-anwar/perl/ch-1.pl
+++ challenge-130/mohammad-anwar/perl/ch-1.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 
+=head1
+
+Week 130:
+
+    https://theweeklychallenge.org/blog/perl-weekly-challenge-130/
+
+Task #1: Odd Number
+
+    You are given an array of positive integers, such that all the
+    numbers appear even number of times except one number.
+
+    Write a script to find that integer.
+
+=cut
+
 use strict;
 use warnings;
 
@@ -7,6 +22,8 @@ use Test::More;
 
 is(odd_number(2, 5, 4, 4, 5, 5, 2), 5);
 is(odd_number(1, 2, 3, 4, 3, 2, 1, 4, 4), 4);
+
+done_testing;
 
 sub odd_number {
     my @numbers = @_;
@@ -18,6 +35,3 @@ sub odd_number {
         return $number if $count_of{$number} % 2 == 1;;
     }
 }
-
-
-done_testing;
