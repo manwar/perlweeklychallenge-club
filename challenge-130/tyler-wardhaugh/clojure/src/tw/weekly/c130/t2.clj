477100312e3271aedfb7150b57f9c61ca747a287
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-09-16 08:29:42 -0700

    Ch130 (Clojure): Task 2

--- challenge-130/tyler-wardhaugh/clojure/src/tw/weekly/c130/t2.clj
+++ challenge-130/tyler-wardhaugh/clojure/src/tw/weekly/c130/t2.clj
@@ -7,7 +7,13 @@
 (def DEFAULT-INPUT '[(8 (5 (4) (6)) (9))])
 
 (defn bst?
-  [tree])
+  ([tree] (bst? tree ##-Inf ##Inf))
+  ([[v l r :as tree] min max]
+   (cond
+     (nil? tree) true
+     (or (< v min) (> v max)) false
+     :else (and (bst? l min (dec v))
+                (bst? r (inc v) max)))))
 
 (defn -main
   "Run Task 1 with a given input T, defaulting to the first example from the
