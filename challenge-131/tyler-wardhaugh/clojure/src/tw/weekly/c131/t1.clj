fc7f505df32a9933d96f64997bc391fe84c07526
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-09-21 09:36:22 -0700

    Ch131 (Clojure): Task 1

--- challenge-131/tyler-wardhaugh/clojure/src/tw/weekly/c131/t1.clj
+++ challenge-131/tyler-wardhaugh/clojure/src/tw/weekly/c131/t1.clj
@@ -1,14 +1,31 @@
 (ns tw.weekly.c131.t1
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.pprint :refer [cl-format]]))
 
 ;;;
-; Task description for TASK #1 ›
+; Task description for TASK #1 › Consecutive Arrays
 ;;;
-(def DEFAULT-INPUT [])
+(def DEFAULT-INPUT [[1 2 3 6 7 8 9]])
+
+(defn partition-by-prev
+  "Similar to partition-by, partition coll when f returns a new value, where f expects the previous
+  element and the current one."
+  [f coll]
+  (let [source (->> (map f coll (rest coll))
+                    (reductions not= true)
+                    (map list coll))
+        xf (comp (partition-by second)
+                 (map (partial map first)))]
+    (sequence xf source)))
+
+(defn split-consecutive
+  "Partition coll into contiguous groups of integers increasing by 1."
+  [coll]
+  (partition-by-prev #(not= %1 (dec %2)) coll))
 
 (defn -main
   "Run Task 1 with a given input N, defaulting to the first example from the
   task description."
   [& args]
   (let [[N] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    N))
+    (cl-format true "(~{[~{~a~^, ~}]~^, ~})~%" (split-consecutive N))))
--- challenge-131/tyler-wardhaugh/clojure/test/tw/weekly/c131_test.clj
+++ challenge-131/tyler-wardhaugh/clojure/test/tw/weekly/c131_test.clj
@@ -1,10 +1,14 @@
 (ns tw.weekly.c131-test
   (:require [clojure.test :refer [deftest is testing]]
-            #_[tw.weekly.c131.t1 :refer []]
+            [tw.weekly.c131.t1 :refer [split-consecutive]]
             #_[tw.weekly.c131.t2 :refer []]))
 
 (deftest task-1
-  )
+  (testing "Task 1, Consecutive Arrays"
+    (is (= '((1 2 3) (6 7 8 9)) (split-consecutive '(1, 2, 3, 6, 7, 8, 9))))
+    (is (= '((11 12) (14) (17 18 19)) (split-consecutive '(11, 12, 14, 17, 18, 19))))
+    (is (= '((2) (4) (6) (8)) (split-consecutive '(2, 4, 6, 8))))
+    (is (= '((1 2 3 4 5)) (split-consecutive '(1, 2, 3, 4, 5))))))
 
 (deftest task-2
   )
