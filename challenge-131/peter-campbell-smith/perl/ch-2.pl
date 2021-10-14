93b3e9f803c45403f40a0dbb6018706de540e9d2
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-10-14 03:20:28 +0100

    - Updated headers as suggested by Colin.

--- challenge-131/peter-campbell-smith/perl/ch-2.pl
+++ challenge-131/peter-campbell-smith/perl/ch-2.pl
@@ -3,32 +3,34 @@ use strict;
 use warnings;
 use v5.10;
 
-# PWC 131 task 1 - Peter Campbell Smith - 2021-09-20
+# PWC 131 task 2 - Peter Campbell Smith - 2021-09-20
 
-# You are given a sorted list of unique positive integers.
-# Write a script to return list of arrays where the arrays are consecutive integers.
+# You are given a string of delimiter pairs and a string to search.
+
+# Write a script to return two strings, the first with any characters matching
+# the “opening character” set, the second with any matching the “closing character” set.
 
 my $input;
 
 # loop over input examples
 until (eof(DATA)) {
-	$input = <DATA>;
-	chop $input;
-	do_task();
+    $input = <DATA>;
+    chop $input;
+    do_task();
 }
 
 sub do_task {
-	
-	my ($prev, $output);
-	
-	# loop over list
-	$prev = -1;
-	while ($input =~ m|(\d+)|g) {          # consecutive  # non-consecutive
-		$output .= ($1 == ($prev + 1)) ?   ", $1" :       "], [$1";
-		$prev = $1;
-	}
-	$output =~ s|...|(|;   # tidy the start of $output
-	say "\nInput:  ($input)\nOutput: $output])";
+
+    my ($prev, $output);
+
+    # loop over list
+    $prev = -1;
+    while ($input =~ m|(\d+)|g) {          # consecutive  # non-consecutive
+    	$output .= ($1 == ($prev + 1)) ?   ", $1" :       "], [$1";
+    	$prev = $1;
+    }
+    $output =~ s|...|(|;   # tidy the start of $output
+    say "\nInput:  ($input)\nOutput: $output])";
 }
 
 __DATA__
