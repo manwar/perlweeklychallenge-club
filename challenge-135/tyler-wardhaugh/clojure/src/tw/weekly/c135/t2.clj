79db9f54e32c0f4394c6133f0526180bde54a3e7
Tyler Wardhaugh <twardhaugh@cap-rx.com>
2021-10-22 23:39:28 -0700

    Ch135 (Clojure): Task 2

--- challenge-135/tyler-wardhaugh/clojure/src/tw/weekly/c135/t2.clj
+++ challenge-135/tyler-wardhaugh/clojure/src/tw/weekly/c135/t2.clj
@@ -1,14 +1,31 @@
-(ns tw.weekly.c135.t2
-  (:require [clojure.edn :as edn]))
+(ns tw.weekly.c135.t2)
 
 ;;;
 ; Task description for TASK #2 › Validate SEDOL
 ;;;
-(def DEFAULT-INPUT [2936921])
+(def DEFAULT-INPUT ["2936921"])
+
+(let [weights [1 3 1 7 3 9]
+      c->n (->> "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                (into {} (map-indexed #(vector %2 %1))))]
+  (defn check-digit
+    [coll]
+    (let [source (map vector coll weights)
+          xf (map (fn [[c w]] (* (c->n c) w)))
+          sum (transduce xf + source)]
+      (mod (- 10 (mod sum 10)) 10))))
+
+(defn sedol?
+  [s]
+  {:pre [(>= 7 (count s))]}
+  (let [candidate (subs s 0 6)
+        modulus (-> s (nth 6) (Character/digit 10))]
+    (when (re-matches #"^[\d\p{Upper}&&[^AEIOU]]{6}\d" s)
+      (= modulus (check-digit candidate)))))
 
 (defn -main
-  "Run Task 1 with a given input M and N, defaulting to the first example from
+  "Run Task 2 with a given input SEDOL, defaulting to the first example from
   the task description."
   [& args]
-  (let [[SEDOL] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+  (let [[SEDOL] (or args DEFAULT-INPUT)]
+    (println (if (sedol? SEDOL) 1 0))))
--- challenge-135/tyler-wardhaugh/clojure/test/tw/weekly/c135/t2_test.clj
+++ challenge-135/tyler-wardhaugh/clojure/test/tw/weekly/c135/t2_test.clj
@@ -1,7 +1,9 @@
 (ns tw.weekly.c135.t2-test
   (:require [clojure.test :refer [deftest is testing]]
-            #_[tw.weekly.c135.t2 :refer []]))
+            [tw.weekly.c135.t2 :refer [sedol?]]))
 
 (deftest examples
   (testing "Examples from description"
-    ))
+    (is (true? (sedol? "2936921")))
+    (is (false? (sedol? "1234567")))
+    (is (true? (sedol? "B0YBKL9")))))
