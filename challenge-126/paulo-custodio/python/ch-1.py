3055f452eeb5554380fed44cc228f72e0236bae2
Paulo Custodio <pauloscustodio@gmail.com>
2021-10-27 12:37:02 +0100

    Add Python solution to challenge 126

---
+++ challenge-126/paulo-custodio/python/ch-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# TASK #1 > Count Numbers
+# Submitted by: Mohammad S Anwar
+# You are given a positive integer $N.
+#
+# Write a script to print count of numbers from 1 to $N that don't contain digit 1.
+#
+# Example
+# Input: $N = 15
+# Output: 8
+#
+#     There are 8 numbers between 1 and 15 that don't contain digit 1.
+#     2, 3, 4, 5, 6, 7, 8, 9.
+#
+# Input: $N = 25
+# Output: 13
+#
+#     There are 13 numbers between 1 and 25 that don't contain digit 1.
+#     2, 3, 4, 5, 6, 7, 8, 9, 20, 22, 23, 24, 25.
+
+import sys
+import re
+
+N = int(sys.argv[1])
+count = 0
+for n in range(1, N+1):
+    if not re.search(r"1", str(n)):
+        count += 1
+print(count)
