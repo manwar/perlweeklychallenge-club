0e0421f1df534125181a91bb6fe51093d90a7f94
Abigail <abigail@abigail.be>
2021-10-27 20:01:33 +0200

    Implement caching for week 136/part 2.
    For those language for which it was easy to make use of an associative
    array/hash/mapping: AWK, Bash, Go, Java, Lua, Node.js, Perl, Python,
    Ruby and Tcl.
    
    Implementations left without caching: bc, C, Pascal, R, and Scheme.

--- challenge-136/abigail/lua/ch-2.lua
+++ challenge-136/abigail/lua/ch-2.lua
@@ -8,11 +8,19 @@
 -- Run as: lua ch-2.lua < input-file
 --
 
+local cache = {}
+
 function _count (target, this_fib, prev_fib)
-    if target <  this_fib then return 0 end
-    if target == this_fib then return 1 end
-    return (_count (target - this_fib, this_fib + prev_fib, this_fib) +
-            _count (target,            this_fib + prev_fib, this_fib))
+    local key = target .. ";" .. this_fib
+    if cache [key] == nil then
+            if target <  this_fib then cache [key] = 0
+        elseif target == this_fib then cache [key] = 1
+        else                           cache [key] =
+              _count (target - this_fib, this_fib + prev_fib, this_fib) +
+              _count (target,            this_fib + prev_fib, this_fib)
+        end
+    end
+    return cache [key]
 end
 
 function count (target)
--- challenge-136/abigail/node/ch-2.js
+++ challenge-136/abigail/node/ch-2.js
@@ -8,13 +8,19 @@
 // Run as: node ch-2.js < input-file
 //
 
+let cache = {}
+
 function count (target, this_fib, prev_fib) {
     if (!this_fib) {this_fib = 1}
     if (!prev_fib) {prev_fib = 1}
-    return target <  this_fib ? 0
-         : target == this_fib ? 1
-         : count (target - this_fib, this_fib + prev_fib, this_fib) +
-           count (target,            this_fib + prev_fib, this_fib)
+    let key = target + ";" + this_fib
+    if (!(key in cache)) {
+        cache [key] = target <  this_fib ? 0
+                    : target == this_fib ? 1
+                    : count (target - this_fib, this_fib + prev_fib, this_fib) +
+                      count (target,            this_fib + prev_fib, this_fib)
+    }
+    return cache [key]
 }
 
 
