950090fb8ed1665038477f469306b468dbb7dfb8
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-10-28 10:47:40 -0700

    Ch136 (Clojure): Task 2

--- challenge-136/tyler-wardhaugh/clojure/test/tw/weekly/c136/t2_test.clj
+++ challenge-136/tyler-wardhaugh/clojure/test/tw/weekly/c136/t2_test.clj
@@ -1,7 +1,9 @@
 (ns tw.weekly.c136.t2-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c136.t2 :refer []]))
+            [tw.weekly.c136.t2 :refer [fibo-sum-count]]))
 
 (deftest examples
   (testing "Examples from description"
-    ))
+    (is (= 4 (fibo-sum-count 16)))
+    (is (= 2 (fibo-sum-count 9)))
+    (is (= 2 (fibo-sum-count 15)))))
