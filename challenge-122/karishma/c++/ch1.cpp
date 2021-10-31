eda45dacb7a71958f1be936d8f98d217699484a1
karishmarajput <karrajput3948@gmail.com>
2021-10-31 14:12:03 +0530

    clean code ch1.cpp

--- challenge-122/karishma/c++/ch1.cpp
+++ challenge-122/karishma/c++/ch1.cpp
@@ -4,22 +4,6 @@ using namespace std;
 int main() {
     int n=5;
     int ptr[]={10,20,30,40,50};
-   //int* ptr;
-    // cout<<"Enter total number of inputs to be entered: ";
-    // cin>>n;
-    // ptr = (int*)calloc(n, sizeof(int));
-    // if (ptr == NULL) {
-    //     cout<<"Memory not allocated.\n";
-    //     exit(0);
-    // }
-    // else {
-    //     for (int i = 1; i <= n; i++) {
-    //         int temp=0;
-    //         scanf("%d",&temp);
-    //         ptr[i] = temp;
-    //     }
-    //     cout<<endl;
-    // }
     int sum = 0;
     for (int j = 1; j <= n; j++) {
         sum += ptr[j-1];
