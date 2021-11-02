4d91ca6d56ca464c41c704d14094ac4e7bf3b0d5
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-02 16:39:20 -0700

    Ch137 (Clojure): prep for challenge

---
+++ challenge-137/tyler-wardhaugh/clojure/test/tw/weekly/c137/t2_test.clj
@@ -0,0 +1,6 @@
+(ns tw.weekly.c137.t2-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [tw.weekly.c137.t2 :refer []]))
+
+(deftest examples
+  (testing "Examples from description"))
