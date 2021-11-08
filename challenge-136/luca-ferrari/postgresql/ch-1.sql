43a8879240a187a1ab1c90688463afff9ad33f36
Luca Ferrari <fluca1978@gmail.com>
2021-11-08 14:41:50 +0100

    PWC 136 merge fix

--- challenge-136/luca-ferrari/postgresql/ch-1.sql
+++ challenge-136/luca-ferrari/postgresql/ch-1.sql
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-CREATE OR REPLACE FUNCTION task1( a int, b int )
-  RETURNS int
-AS $CODE$
-  SELECT gcd( a, b ) % 2;
-  $CODE$
-    LANGUAGE SQL;
-
-/*
-  Example of invocation:
-
-  testdb=> SELECT task1( 8, 24 ) as a8_24, task1( 26, 39 ) as a26_39, task1( 4, 10 ) as a4_10;
-  -[ RECORD 1 ]
-  a8_24  | 0
-  a26_39 | 1
-  a4_10  | 0
-*/
-=======
 /*
 testdb=> SELECT FRIENDLY( 26, 39 );
 friendly
@@ -40,4 +22,4 @@ AS $CODE$
         END;
 $CODE$
 LANGUAGE SQL;
->>>>>>> dfcf558b2190d45a5ebf3c486a7c316dbc3e197e
+
--- challenge-136/luca-ferrari/postgresql/ch-2.sql
+++ challenge-136/luca-ferrari/postgresql/ch-2.sql
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-
-CREATE OR REPLACE FUNCTION task2( l int default 16)
-  RETURNS SETOF text
-  AS $CODE$
-
-WITH RECURSIVE
-  fib( p, n ) AS (
-    SELECT 1 as p, 1 as n
-    UNION
-    SELECT n, p + n FROM fib WHERE n < l
-  )
-    , permutations AS (
-      SELECT n::text as perm_n, n as perm_sum, array[ 1 ] as pivot
-        FROM fib
-      UNION ALL
-      SELECT permutations.perm_n || ',' || fib.n, perm_sum + fib.n, array_append( pivot, fib.n )
-      FROM permutations, fib
-       WHERE fib.n < l
-       AND position( fib.n::text in perm_n ) = 0
-            
-    )
-  , results as (
-    SELECT array( SELECT perm_n FROM permutations )
-  )
-  SELECT *
-  FROM results;
-
-  $CODE$
-    LANGUAGE sql;
-=======
 CREATE OR REPLACE FUNCTION fibonacci_sum( l int DEFAULT 16 )
 RETURNS bigint
 AS $CODE$
@@ -62,4 +31,4 @@ WHERE total_sum = l
 
 $CODE$
 LANGUAGE SQL;
->>>>>>> dfcf558b2190d45a5ebf3c486a7c316dbc3e197e
+
