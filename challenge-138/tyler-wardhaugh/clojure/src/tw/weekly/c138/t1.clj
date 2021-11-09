0814a5fbb2b360dc72ef19246ae752f2efa96d67
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-08 16:02:34 -0800

    Ch138 (Clojure): Task 1

--- challenge-138/tyler-wardhaugh/clojure/src/tw/weekly/c138/t1.clj
+++ challenge-138/tyler-wardhaugh/clojure/src/tw/weekly/c138/t1.clj
@@ -1,14 +1,23 @@
 (ns tw.weekly.c138.t1
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [java-time :as j]))
 
 ;;;
 ; Task description for TASK #1 › Workdays
 ;;;
 (def DEFAULT-INPUT [2021])
 
+(defn count-workdays
+  [year]
+  (let [start (j/local-date year 1 1)
+        end (j/plus start (j/years 1))
+        source (j/iterate j/adjust start :next-working-day)
+        xf (take-while #(j/before? % end))]
+    (count (sequence xf source))))
+
 (defn -main
   "Run Task 2 with a given input N, defaulting to the first example from the
   task description."
   [& args]
   (let [[N] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+    (println (count-workdays N))))
--- challenge-138/tyler-wardhaugh/clojure/test/tw/weekly/c138/t1_test.clj
+++ challenge-138/tyler-wardhaugh/clojure/test/tw/weekly/c138/t1_test.clj
@@ -1,7 +1,8 @@
 (ns tw.weekly.c138.t1-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c138.t1 :refer []]))
+            [tw.weekly.c138.t1 :refer [count-workdays]]))
 
 (deftest examples
   (testing "Examples from description"
-    ))
+    (is (= 261 (count-workdays 2021)))
+    (is (= 262 (count-workdays 2020)))))
