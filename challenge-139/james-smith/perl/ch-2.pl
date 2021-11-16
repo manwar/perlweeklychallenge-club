45c65f5a0c94cb0d4f8b4e4e423ba7a417ef871b
drbaggy <js5@sanger.ac.uk>
2021-11-16 14:44:49 +0000

    save a line!

--- challenge-139/james-smith/perl/ch-2.pl
+++ challenge-139/james-smith/perl/ch-2.pl
@@ -7,7 +7,7 @@ use feature qw(say);
 my( $N, @primes, @long_primes ) = ( $ARGV[0]||5, 2 );
 
 O: for( my $p=3; @long_primes<$N; $p+=2 ) {
-  ($p % $_) || (next O) for @primes; ## next if !prime
+  ($p % $_) || (next O) for @primes;
   push @long_primes, $p if $p - rec_len($p) == 1;
   push @primes,      $p;
 }
@@ -17,6 +17,5 @@ say $_ for @long_primes;
 sub rec_len {
   my( $D, $N, $s ) = ( shift, 1, '' );
   ($s,$N) = ( $s.int($N/$D), ($N%$D).0 ) for 0 .. 2*$D;
-  $s =~ m{(\d+?)\1+$};
-  length $1;
+  $s =~ /(\d+?)\1+$/ ? length $1 : 0;
 }
