f68b891f303a0cb30ae091a8ed3d76fda1e4266f
Paulo Custodio <pauloscustodio@gmail.com>
2021-11-19 09:13:19 +0000

    Add Python solution to challenge 14

--- challenge-014/paulo-custodio/perl/ch-2.pl
+++ challenge-014/paulo-custodio/perl/ch-2.pl
@@ -15,10 +15,61 @@
 # California + Louisiana + Massachusetts + Rhode Island = Calamari
 
 use Modern::Perl;
-use Locale::US;
 
-my $u = Locale::US->new;
-my @codes = $u->all_state_codes;        # all states two letter codes
+my %us_states = (
+    AL => 'Alabama',
+    AK => 'Alaska',
+    AZ => 'Arizona',
+    AR => 'Arkansas',
+    CA => 'California',
+    CO => 'Colorado',
+    CT => 'Connecticut',
+    DE => 'Delaware',
+    FL => 'Florida',
+    GA => 'Georgia',
+    HI => 'Hawaii',
+    ID => 'Idaho',
+    IL => 'Illinois',
+    IN => 'Indiana',
+    IA => 'Iowa',
+    KS => 'Kansas',
+    KY => 'Kentucky',
+    LA => 'Louisiana',
+    ME => 'Maine',
+    MD => 'Maryland',
+    MA => 'Massachusetts',
+    MI => 'Michigan',
+    MN => 'Minnesota',
+    MS => 'Mississippi',
+    MO => 'Missouri',
+    MT => 'Montana',
+    NE => 'Nebraska',
+    NV => 'Nevada',
+    NH => 'New Hampshire',
+    NJ => 'New Jersey',
+    NM => 'New Mexico',
+    NY => 'New York',
+    NC => 'North Carolina',
+    ND => 'North Dakota',
+    OH => 'Ohio',
+    OK => 'Oklahoma',
+    OR => 'Oregon',
+    PA => 'Pennsylvania',
+    RI => 'Rhode Island',
+    SC => 'South Carolina',
+    SD => 'South Dakota',
+    TN => 'Tennessee',
+    TX => 'Texas',
+    UT => 'Utah',
+    VT => 'Vermont',
+    VA => 'Virginia',
+    WA => 'Washington',
+    WV => 'West Virginia',
+    WI => 'Wisconsin',
+    WY => 'Wyoming',
+);
+
+my @codes = keys %us_states;            # all states two letter codes
 my $codes = join("|", @codes);          # regex to match any codes
 my $regex = qr/^($codes)+$/i;           # regex to match word composed of codes
 
@@ -38,6 +89,6 @@ while (<$fh>) {
 
 # show longest words in form: word = state + state + ...
 for my $word (@longest) {
-    my @states = map {$_ = $u->{code2state}{uc($_)}} grep {$_} split /(..)/, $word;
+    my @states = map {$_ = $us_states{uc($_)}} grep {$_} split /(..)/, $word;
     say $word, " = ", join(" + ", @states);
 }
