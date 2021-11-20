543f988df5e6a221a3c990eec574dd7085debd40
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-19 22:25:31 -0800

    Ch139 (Clojure): Task 2

--- challenge-139/tyler-wardhaugh/clojure/test/tw/weekly/c139/t2_test.clj
+++ challenge-139/tyler-wardhaugh/clojure/test/tw/weekly/c139/t2_test.clj
@@ -1,3 +1,7 @@
 (ns tw.weekly.c139.t2-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c139.t2 :refer []]))
+            [tw.weekly.c139.t2 :refer [long-primes]]))
+
+(deftest examples
+  (testing "Examples from description"
+    (is (= [7 17 19 23 29] (long-primes)))))
