d4af8e63ed8bb83070c498dc3b8fd874ad19e438
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-19 16:32:32 -0800

    Ch139 (Clojure): Task 1

--- challenge-139/tyler-wardhaugh/clojure/src/tw/weekly/c139/t1.clj
+++ challenge-139/tyler-wardhaugh/clojure/src/tw/weekly/c139/t1.clj
@@ -6,9 +6,20 @@
 ;;;
 (def DEFAULT-INPUT [[1 2 3 4 5]])
 
+(defn ordered?
+  [coll]
+  (or (empty? coll) (apply <= coll)))
+
+(defn shortcircuiting-ordered?
+  [coll]
+  (or (empty? coll)
+      (-> (when-let [[x & xs] coll]
+            (reduce (fn [p c] (if (<= p c) c (reduced false))) x xs))
+          boolean)))
+
 (defn -main
   "Run Task 3 with a given input COLL, defaulting to the first example from the
   task description."
   [& args]
   (let [[COLL] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+    (println (if (shortcircuiting-ordered? COLL) 1 0))))
--- challenge-139/tyler-wardhaugh/clojure/test/tw/weekly/c139/t1_test.clj
+++ challenge-139/tyler-wardhaugh/clojure/test/tw/weekly/c139/t1_test.clj
@@ -1,3 +1,15 @@
 (ns tw.weekly.c139.t1-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c139.t1 :refer []]))
+            [tw.weekly.c139.t1 :refer [ordered? shortcircuiting-ordered?]]))
+
+(def both (juxt ordered? shortcircuiting-ordered?))
+
+(deftest examples
+  (testing "Examples from description"
+    (is (every? true? (both [1 2 3 4 5])))
+    (is (every? false? (both [1 3 2 4 5])))))
+
+(deftest more
+  (testing "More interesting tests"
+    (is (every? true? (both [1])))
+    (is (every? true? (both [])))))
