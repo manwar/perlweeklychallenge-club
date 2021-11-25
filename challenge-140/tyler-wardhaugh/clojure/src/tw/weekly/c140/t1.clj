5a37dbd484586b60ad2509e98e7282eb439cd58c
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-25 12:06:43 -0800

    Ch140 (Clojure): Task 1

--- challenge-140/tyler-wardhaugh/clojure/src/tw/weekly/c140/t1.clj
+++ challenge-140/tyler-wardhaugh/clojure/src/tw/weekly/c140/t1.clj
@@ -3,11 +3,25 @@
 ;;;
 ; Task description for TASK #1 › Add Binary
 ;;;
-(def DEFAULT-INPUT [11 1])
+(def DEFAULT-INPUT ["11" "1"])
+
+(defn binstr->num
+  [s]
+  (Long/parseLong s 2))
+
+(defn num->binstr
+  [n]
+  (Long/toString n 2))
+
+(defn add-binary
+  [& binstrs]
+  (->> binstrs
+       (transduce (map binstr->num) +)
+       num->binstr))
 
 (defn -main
   "Run Task 1 with a given input A B, defaulting to the first example from the
   task description."
   [& args]
   (let [[A B] (or args DEFAULT-INPUT)]
-    ))
+    (println (add-binary A B))))
