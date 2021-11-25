47c7c038c04618222b67439d9bbe7e482ef3b3f2
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2021-11-25 12:20:21 -0800

    Ch140 (Clojure): Task 2

--- challenge-140/tyler-wardhaugh/clojure/src/tw/weekly/c140/t2.clj
+++ challenge-140/tyler-wardhaugh/clojure/src/tw/weekly/c140/t2.clj
@@ -6,9 +6,17 @@
 ;;;
 (def DEFAULT-INPUT [2 3 4])
 
+(defn get-kth-in-multi
+  [i j k]
+  (-> (for [i (range 1 (inc i))
+            j (range 1 (inc j))]
+        (* i j))
+      sort
+      (nth (dec k))))
+
 (defn -main
   "Run Task 2 with a given input I J K, defaulting to the first example from
   the task description."
   [& args]
   (let [[I J K] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+    (println (get-kth-in-multi I J K))))
--- challenge-140/tyler-wardhaugh/clojure/test/tw/weekly/c140/t2_test.clj
+++ challenge-140/tyler-wardhaugh/clojure/test/tw/weekly/c140/t2_test.clj
@@ -1,7 +1,8 @@
 (ns tw.weekly.c140.t2-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tw.weekly.c140.t2 :refer []]))
+            [tw.weekly.c140.t2 :refer [get-kth-in-multi]]))
 
 (deftest examples
   (testing "Examples from description"
-    ))
+    (is (= 3 (get-kth-in-multi 2 3 4)))
+    (is (= 4 (get-kth-in-multi 3 3 6)))))
