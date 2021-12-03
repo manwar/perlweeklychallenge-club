776c785fa7ed7a77a9ff8fc84d06307cba246482
Alexander Pankoff <ccntrq@screenri.de>
2021-12-03 09:11:43 +0100

    Add debug/explain mode to challenge 141 task 02

--- challenge-141/alexander-pankoff/perl/ch-2.pl
+++ challenge-141/alexander-pankoff/perl/ch-2.pl
@@ -15,7 +15,20 @@ sub run() {
 
     my @possible_integers = numbers_from_digits($m);
     my @divisible_by_n    = grep { $_ % $n == 0 } @possible_integers;
+
+    explain( $m, $n, \@possible_integers, \@divisible_by_n ) if DEBUG;
     say scalar @divisible_by_n;
+}
+
+sub explain ( $m, $n, $possible_integers, $divisible_by_n ) {
+
+    say "Possible integers created using the digits of $m are:";
+    say join( ', ', sort { $a <=> $b } @$possible_integers );
+
+    say "There are "
+      . scalar(@$divisible_by_n)
+      . " integers divisible by $n such as:";
+    say join( ', ', sort { $a <=> $b } @$divisible_by_n );
 }
 
 sub numbers_from_digits($x) {
