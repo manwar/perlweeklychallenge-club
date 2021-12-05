d61d35a6f11e83deba64e6a7008e0432962aacd6
Adam Russell <ac.russell@live.com>
2021-12-05 16:12:14 -0500

    Solutions and blog for Challenge 141.

--- challenge-141/adam-russell/cxx/ch-2.cxx
+++ challenge-141/adam-russell/cxx/ch-2.cxx
@@ -1,3 +1,4 @@
+#include<cmath>
 #include<vector>
 #include<iostream>
 #include<algorithm>
@@ -6,6 +7,7 @@ class LikeNumbers{
     public:
         std::vector<int> like_numbers(int, int);  
     private:
+        int vector2int(std::vector<int>);
         std::vector<int> int2digits(int);
 };
 
@@ -16,19 +18,50 @@ std::vector<int> LikeNumbers::int2digits(int i){
         digits.push_back(digit);
         i = i / 10;  
     }while(i != 0); 
-    std::reverse(digits.begin(), digits.end()); 
     return digits;  
+}
+
+int LikeNumbers::vector2int(std::vector<int> v){
+    int x = 1; 
+    int r = 0;
+    for(int i: v){
+        r = r + (i * x);
+        x = x * 10;   
+    }
+    return r; 
 }
 
 std::vector<int> LikeNumbers::like_numbers(int i, int j){
     std::vector<int> digits = this->int2digits(i);  
-    for(int digit : digits)
-        std::cout << digit << " "; 
-    std::cout << std::endl; 
-    return digits;
+    std::vector<int> like;
+    int power_set_size = std::pow(2, digits.size()); 
+    for(int k = 1; k < power_set_size - 1; k++){
+        std::vector<int> included_digits;
+        for(int l = 0; l < digits.size(); l++){
+            if( k >> l & 1) 
+                included_digits.push_back(digits.at(l));
+        }
+        int number = this->vector2int(included_digits);  
+        if(number % j == 0) 
+            like.push_back(number); 
+    }
+    std::sort(like.begin(), like.end()); 
+    return like;
 }
 
 int main(int argc, char** argv){
     LikeNumbers ln;
-    ln.like_numbers(1234, 2);  
+    std::vector<int> like;
+    like = ln.like_numbers(1234, 2);  
+    std::cout << like.size() << ": ";
+    for(int i : like){
+        std::cout << i << " ";
+    } 
+    std::cout << std::endl;     
+    like = ln.like_numbers(768, 4);
+    std::cout << like.size() << ": ";
+    for(int i : like){
+        std::cout << i << " ";
+    } 
+    std::cout << std::endl;     
 } 
