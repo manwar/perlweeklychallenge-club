7172edadbb0b471cdc3570098fe3f9e3160699e6
Alexander Pankoff <ccntrq@screenri.de>
2021-12-14 10:49:21 +0100

    Remove duplicate char from match group

--- challenge-143/alexander-pankoff/perl/ch-1.pl
+++ challenge-143/alexander-pankoff/perl/ch-1.pl
@@ -3,5 +3,5 @@ use strict;
 use warnings;
 
 my $expr = $ARGV[0];
-die "invalid expr\n" unless $expr && $expr =~ m/^[\s\d\.\+\-\*\*\(\)]*$/;
+die "invalid expr\n" unless $expr && $expr =~ m/^[\s\d\.\+\-\*\(\)]*$/;
 print eval($expr) . "\n";
