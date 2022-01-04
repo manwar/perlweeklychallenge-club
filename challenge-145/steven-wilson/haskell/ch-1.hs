9030d158bb89a81480aa91022f2ef030404dc704
Steven Wilson <steven1170@zoho.eu>
2022-01-04 09:50:11 +0000

    inprove haskell solution

--- challenge-145/steven-wilson/haskell/ch-1.hs
+++ challenge-145/steven-wilson/haskell/ch-1.hs
@@ -1,3 +1,3 @@
-dot_product :: [Int] -> [Int] -> Int
-dot_product [x] [y]       = x * y
+dot_product :: Num a => [a] -> [a] -> a
+dot_product [] []         = 0
 dot_product (x:xs) (y:ys) = (x * y) + dot_product (xs) (ys)
