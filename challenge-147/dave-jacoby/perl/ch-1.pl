f5f9c8a7c5b5e6e640c6e52579878e0ed5cc97fa
Dave Jacoby <jacoby.david@gmail.com>
2022-01-10 19:44:04 -0500

    147 - 3 * 7 * 7

---
+++ challenge-147/dave-jacoby/perl/ch-1.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw{ say postderef signatures state };
+no warnings qw{ experimental };
+
+my %primes;
+my %trunc;
+my $c = 1;
+my $n = 2;
+
+while (1) {
+    if ( is_prime($n) ) {
+        $primes{$n}++;
+        my $copy = $n;
+        while ( length $copy > 0 ) {
+            last unless $primes{$copy};
+            substr( $copy, 0, 1 ) = '';
+            if ( $copy eq '' ) {
+                $trunc{$n}++ if $copy eq '';
+                last;
+            }
+        }
+        last if scalar keys %trunc > 20;
+    }
+    $n++;
+}
+
+say join ', ', sort { $a <=> $b } keys %trunc;
+
+sub is_prime ($n) {
+    for ( 2 .. sqrt $n ) { return unless $n % $_ }
+    return 1;
+}
