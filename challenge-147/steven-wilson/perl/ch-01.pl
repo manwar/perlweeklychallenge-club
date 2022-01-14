54c5f09449b5e3780ed5cac7171ca48d4f99238e
Steven Wilson <steven1170@zoho.eu>
2022-01-14 20:58:01 +0000

    remove unnecessary bobbins

--- challenge-147/steven-wilson/perl/ch-01.pl
+++ challenge-147/steven-wilson/perl/ch-01.pl
@@ -20,19 +20,18 @@ while ( scalar @ltp < 20 ) {
 
 sub is_truncatable_prime {
     my $prime_number   = shift;
-    my $prime_number_m = $prime_number;
     my $truncatable    = 1;
-    while ( length $prime_number_m > 0 ) {
-        if ( $prime_number_m =~ /0/ ) {    # contains no 0
+    while ( length $prime_number > 0 ) {
+        if ( $prime_number =~ /0/ ) {    # contains no 0
             $truncatable = 0;
             last;
         }
-        if ( not is_prime($prime_number_m) ) {
+        if ( not is_prime($prime_number) ) {
             $truncatable = 0;
             last;
         }
-        $prime_number_m =~ /^[0-9]{1}([0-9]*)/;
-        $prime_number_m = $1;
+        $prime_number =~ /^[0-9]{1}([0-9]*)/;
+        $prime_number = $1;
     }
     return $truncatable;
 }
