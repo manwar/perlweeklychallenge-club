6476972d82647f40e1c68ec2d6434ba05147a7bb
Abigail <abigail@abigail.freedom.nl>
2022-01-16 19:39:48 +0100

    Week 147: Hello, world solutions

---
+++ challenge-147/abigail/sql/ch-2.sql
@@ -0,0 +1,9 @@
+--
+-- See https://theweeklychallenge.org/blog/perl-weekly-challenge-147
+--
+
+-- 
+-- Run as: sqlite3 < ch-2.sql
+--
+
+SELECT "1560090 7042750";
---
+++ challenge-147/abigail/tcl/ch-2.tcl
@@ -0,0 +1,11 @@
+#!/usr/local/opt/tcl-tk/bin/tclsh
+
+#
+# See https://theweeklychallenge.org/blog/perl-weekly-challenge-147
+#
+
+#
+# Run as: tclsh ch-2.tcl
+#
+
+puts "1560090 7042750"
