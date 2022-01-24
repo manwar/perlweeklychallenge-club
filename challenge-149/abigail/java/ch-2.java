158e2b79cc16f52dd755457be2a39eaa669de421
Abigail <abigail@abigail.freedom.nl>
2022-01-24 21:17:26 +0100

    Week 149, part 2: Solutions in 16 languages.

---
+++ challenge-149/abigail/java/ch-2.java
@@ -0,0 +1,49 @@
+//
+// See https://theweeklychallenge.org/blog/perl-weekly-challenge-149
+//
+
+//
+// Run as: ln ch-2.java ch2.java; javac ch2.java; java ch2 < input-file
+//
+
+import java.util.*;
+
+public class ch2 {
+    public static void main (String [] args) {
+        String [] A287298 = new String [23];
+        A287298 [ 0] =                       "";
+        A287298 [ 1] =                       "";
+        A287298 [ 2] =                      "1";
+        A287298 [ 3] =                      "1";
+        A287298 [ 4] =                   "3201";
+        A287298 [ 5] =                   "4301";
+        A287298 [ 6] =                 "452013";
+        A287298 [ 7] =                "6250341";
+        A287298 [ 8] =               "47302651";
+        A287298 [ 9] =              "823146570";
+        A287298 [10] =             "9814072356";
+        A287298 [11] =            "A8701245369";
+        A287298 [12] =           "B8750A649321";
+        A287298 [13] =           "CBA504216873";
+        A287298 [14] =         "DC71B30685A924";
+        A287298 [15] =        "EDAC93B24658701";
+        A287298 [16] =       "FED5B39A42706C81";
+        A287298 [17] =       "GFED5A31C6B79802";
+        A287298 [18] =     "HGF80ADC53712EB649";
+        A287298 [19] =    "IHGFD3408C6E715A2B9";
+        A287298 [20] =   "JIHG03DAC457BFE96281";
+        A287298 [21] =                       "";
+        A287298 [22] = "LKJIG5D14B9032FHAC867E";
+    
+        Scanner scanner = new Scanner (System . in);
+        while (scanner . hasNextInt ()) {
+            int n = scanner . nextInt ();
+            if (n > 22 || A287298 [n] . length () == 0) {
+                System . out . println ("Too hard to calculate");
+            }
+            else {
+                System . out . printf ("%s\n", A287298 [n]);
+            }
+        }
+    }
+}
