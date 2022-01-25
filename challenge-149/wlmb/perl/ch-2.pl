f901aa10700e2c14bd614f3bcda2a524bb30d6cd
Luis Mochan <mochan@fis.unam.mx>
2022-01-25 10:21:37 -0600

    Add new solution

--- challenge-149/wlmb/perl/ch-2.pl
+++ challenge-149/wlmb/perl/ch-2.pl
@@ -6,19 +6,21 @@
 use v5.12;
 use warnings;
 use List::MoreUtils qw(duplicates);
-use bigint;
+use POSIX qw(floor);
 my @digits=(0..9,'A'..'Z');
+my $largest_base=15; # might overflow above that
 die "Usage: ./ch-2.pl N [P] [Q]... " .
     "to obtain largest square in base N P Q...\n"
     unless @ARGV;
 foreach my $base(@ARGV){
-    my $root=$base**($base/2);
+    say("Sorry: $base is too large"), next if $base>$largest_base;
+    my $root=$base**floor($base/2);
     --$root while duplicates(convert($root*$root, $base));
     say "f($base)=",convert($root*$root, $base);
 }
 sub convert{
     my ($N, $base)=@_; # convert $N to $base
     my @d=();
-    do {push @d, $N%$base} while $N/=$base;
+    do {push @d, $N%$base} while $N=floor($N/$base);
     return reverse map {$digits[$_]} @d;
 }
