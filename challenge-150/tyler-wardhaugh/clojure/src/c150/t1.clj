1ab77707311e24b86ffcfe82d29e54b4b4f57928
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2022-02-04 17:15:18 -0800

    Ch150 (Clojure): Task 1

--- challenge-150/tyler-wardhaugh/clojure/src/c150/t1.clj
+++ challenge-150/tyler-wardhaugh/clojure/src/c150/t1.clj
@@ -1,6 +1,22 @@
-(ns c150.t1
-  (:require [clojure.pprint :refer [cl-format]]
-            [clojure.edn :as edn]))
+(ns c150.t1)
+
+(def DEFAULT-INPUT ["1234" "5678"])
+(def SIZE 51)
+
+(defn fibo-words
+  [a b]
+  (let [p (promise)]
+    @(doto p
+       (deliver (list* a b (lazy-seq (map str @p (rest @p))))))))
+
+(defn solve
+  [source]
+  (let [xf (comp (drop-while #(< (count %) SIZE)) (take 1))
+        f (completing (fn [_ w] (nth w (dec SIZE))))]
+    (->> source
+         (transduce xf f nil))))
 
 (defn -main
-  [& _])
+  [& args]
+  (let [[A B] (or args DEFAULT-INPUT)]
+    (println (solve (fibo-words A B)))))
