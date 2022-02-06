40cd181ea9567e8fed9fef944e9a8546a325937e
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2022-02-05 22:43:11 -0800

    Ch150 (Clojure): Task 2

--- challenge-150/tyler-wardhaugh/clojure/src/c150/t2.clj
+++ challenge-150/tyler-wardhaugh/clojure/src/c150/t2.clj
@@ -2,7 +2,21 @@
   (:require [clojure.math :as m]
             [clojure.pprint :refer [cl-format]]))
 
-;(set! *warn-on-reflection* true)
+(def TARGET 500)
+
+(defn prime-squares-upto
+  [ceiling]
+  (let [source (iterate #(.nextProbablePrime %) BigInteger/ONE)
+        xf (comp (drop 1) (take-while #(< % ceiling)) (map #(m/pow % 2)))]
+    (into [] xf source)))
+
+(defn square-free-ints
+  [n]
+  (let [prime-squares (prime-squares-upto (m/sqrt n))
+        square-free? (fn [x] (not-any? #(zero? (rem x %)) prime-squares))]
+    (->> (range 1 (inc n))
+         (filter square-free?))))
 
 (defn -main
-  [& _])
+  [& _]
+  (cl-format true "~{~<~%~1,78:;~A~;~^,~> ~}" (square-free-ints TARGET)))
