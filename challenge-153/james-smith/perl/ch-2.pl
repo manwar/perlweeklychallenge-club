f18aa90001a4e642af2eb49083f1145ae6b32f5d
drbaggy <js5@sanger.ac.uk>
2022-02-21 13:14:43 +0000

    pushing changes

--- challenge-153/james-smith/perl/ch-2.pl
+++ challenge-153/james-smith/perl/ch-2.pl
@@ -12,12 +12,12 @@ my @FACT = (1);
 push @FACT, $_*$FACT[-1] foreach 1..9;
 
 my @TESTS = ( [ 145, 1 ], [ 125, 0 ], );
-is( factorion($_->[0]), $_->[1] ) foreach @TESTS;
+is( is_factorion($_->[0]), $_->[1] ) foreach @TESTS;
 done_testing();
 
-factorion($_) && say for 1 .. 2_177_282;
+is_factorion($_) && say for 1 .. 2_177_282;
 
-sub factorion {
+sub is_factorion {
   my $t=0;
   $t+=$FACT[$_] for split //,$_[0];
   $t==$_[0];
