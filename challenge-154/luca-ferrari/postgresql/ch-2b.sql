35c155aa6b02155197de6a4f653789b47fc48b5e
Luca Ferrari <fluca1978@gmail.com>
2022-03-01 12:54:09 +0100

    Prime check!

--- challenge-154/luca-ferrari/postgresql/ch-2b.sql
+++ challenge-154/luca-ferrari/postgresql/ch-2b.sql
@@ -14,6 +14,18 @@ $CODE$
 LANGUAGE plperl;
 
 CREATE OR REPLACE FUNCTION
+pwc154.plperl_is_prime( int )
+RETURNS bool
+AS $CODE$
+   for my $i ( 2 .. ( $_[0] - 1 ) ) {
+      return 0 if $_[0] % $i == 0;
+   }
+
+  return 1;
+$CODE$
+LANGUAGE plperl;
+
+CREATE OR REPLACE FUNCTION
 pwc154.padovans_up_to( int )
 RETURNS SETOF int
 AS $CODE$
@@ -29,6 +41,7 @@ LANGUAGE plperl;
 
 
 SELECT distinct( p.p )
-FROM pwc154.padovans_up_to( 20 ) p
+FROM pwc154.padovans_up_to( 50 ) p
+WHERE pwc154.plperl_is_prime( p.p ) = true
 ORDER BY 1
 LIMIT 10;
