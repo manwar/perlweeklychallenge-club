7c42d50214a94e3b58340120d183c749c32e3d58
drbaggy <js5@sanger.ac.uk>
2022-03-01 04:05:04 +0000

    added comments and tidied up

--- challenge-154/james-smith/perl/ch-1.pl
+++ challenge-154/james-smith/perl/ch-1.pl
@@ -15,32 +15,45 @@ my @words = qw(
   LEPR LRPE LREP
 );
 
-## We enumerate the combinations in "alphabetic" order
-## Rather than returning an array - which uses extra
-## memory we effectively have a generator giving the
-## next combination...
+# We enumerate the combinations in "alphabetic" order
+# Rather than returning an array - which uses extra
+# memory we effectively have a generator giving the
+# next combination... In this case permutation/combination
+# are equivalent as all letters are unique...
+
+# This is a standard algorithm - so I won't include
+# the explanation as it can be looked up, basically will
+# generate
+#        ELRP EPLR EPRL ERLP ERPL
+#   LEPR LERP LPER LPRE LREP LRPE
+#   PELR PERL PLER PLRE PREL PRLE
+#   RELP REPL RLEP RLPE RPEL RPLE
 
 sub next_comb {
   my( $i, $j );
   ( $s[$_] lt $s[$_+1] ) && ( $i = $_ ) for 0    .. @s-2;
   return unless defined $i;
-  ( $s[$i] lt $s[$_]   ) && ( $j = $_ ) for $i+1 .. @s-1;
+  ( $s[$i] lt $s[$_  ] ) && ( $j = $_ ) for $i+1 .. @s-1;
   @s[ $i, $j       ] = @s[ $j, $i               ];
   @s[ $i+1 .. @s-1 ] = @s[ reverse $i+1 .. @s-1 ];
   return join '',@s;
 }
 
-## "Proper" solution...
+# "Proper" solution...
 say '';
 say 'Solution - with no assumptions about uniqueness/# missing:';
 say '';
 
-@s        = sort split //, 'PERL';
+my $w = $_ = join '', @s = sort split //, 'PERL';
 my %check = map { $_=>1 } @words;
-$_ = join '', @s;
-do { exists $check{$_} || say "  * $_" } while $_ = next_comb;
 
-## Hack solution
+# We have to use "do/while" here rather than just "while" to
+# make sure we include the first term, o/w we would miss it
+# out with the update $_ = next_comb happening first
+
+do { exists $check{$w} || say "  * $w" } while $w = next_comb;
+
+## "Hack" solution
 
 say '';
 say 'Solution - assuming uniqueness and one missing!';
@@ -49,6 +62,13 @@ say '';
 my $r ='';
 $r^=$_ for @words;
 say "  * $r";
+
+# You might ask how this works! Every time you XOR a word. It
+# flips all the bits in that character. Now each word should
+# appear 6 times - but the missing word only occurs 5 times.
+# So the 6 times a letter appears are cancelled out as
+# A^B^B = A, so B^B^B^B^B^B = ''
+# The ones appearing only 5 times are not as B^B^B^B^B = B.
 
 say '';
 
--- challenge-154/james-smith/perl/ch-2.pl
+++ challenge-154/james-smith/perl/ch-2.pl
@@ -6,21 +6,42 @@ use warnings;
 use feature qw(say);
 use Time::HiRes qw(time);
 
-my @primes = (2,3);
-my $p = 3;
+my($count,$t0,$pr,@primes) = (10,time,3,2,3);
+my $pad0 = my $pad1 = my $pad2 = 1;
 
-my $p0 = my $p1 = my $p2 = 1;
-my $c = 10;
-my $t0 = time;
-T: while($c) {
-  ($p0,$p1,$p2)=($p1,$p2,$p0+$p1);
-  next if $p2 == $p1;
-  O: for( ;$primes[-1]*$primes[-1]<$p2;$p+=2) {
-    $_*$_ > $p2 ? last : $p%$_ || next O for @primes;
-    push @primes, $p;
+## Minor "buglet" in the loop is the first two values 2 & 3
+## do not appear in the output (this is due to them being less
+## than the square of the maximum value of the primes array,
+## so we just output them...
+
+$count--, say sprintf '%11.6f %20d', -$t0+($t0=time), $_ for @primes;
+
+T: while($count) {
+
+  ## Update padovan number...
+
+  ( $pad0, $pad1, $pad2 ) = ( $pad1, $pad2, $pad0+$pad1 );
+
+  ## Skip early duplicates...
+
+  next if $pad2 == $pad1;
+
+  ## Extend our prime array if required
+
+  O: for( ; $primes[-1]*$primes[-1] < $pad2; $pr += 2) {
+    $_*$_ > $pad2 ? last : $pr % $_ || next O for @primes;
+    push @primes, $pr;
   }
-  $p2%$_ || $_>=$p2 || next T for @primes;
-  say sprintf '%11.6f %20d', -$t0+($t0=time), $p2;
-  $c--;
+
+  ## Now check for prime factors, if we find one we bomb
+  ## out! It doesn't matter than we compute more primes
+  ## than we need for composite numbers as we will need
+  ## them for the next padovan prime....
+
+  $pad2 % $_ || next T for @primes;
+
+  say sprintf '%11.6f %20d', -$t0+($t0=time), $pad2;
+
+  $count--;
 }
 
