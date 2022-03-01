7f3677bc7eb3b23f44ad5bf6bea79f44f1c15d42
E7-87-83 <fungcheokyin@gmail.com>
2022-03-01 16:40:25 +0800

    task 1 with unit tests

--- challenge-154/cheok-yin-fung/perl/ch-1.pl
+++ challenge-154/cheok-yin-fung/perl/ch-1.pl
@@ -3,7 +3,7 @@
 # Task 1 Missing Permutation
 # Usage: 
 # $ ch-1.pl [permutations of alphabets; 
-#            each alpahabet appears once]
+#            each alpahabet appears excatly once in a permutation]
 
 use v5.22.0;
 use warnings;
@@ -11,40 +11,51 @@ use Algorithm::Permute qw/permute/;
 use List::Util qw/uniqstr/;
 use Data::Dumper;
 
-my $word = $ARGV[0];
-my @collection = @ARGV;
 
-my @sw = split "", $word;
+sub look_for {
+    my @collection = @_;
+    my $word = $collection[0];
 
-die "Duplicate alphabet in the word input.\n" 
-    if scalar @sw != scalar uniqstr @sw;
+    my @sw = split "", $word;
+
+    die "Duplicate alphabet in the word input.\n" 
+        if scalar @sw != scalar uniqstr @sw;
 
 
 
-my $iter = Algorithm::Permute->new(\@sw, length($word));
-my @ws;
-while (my @res = $iter->next) {
-    push @ws, join "", @res;
-}
-
-my %bucket = map {$_ => 0} @ws;
-
-for (@collection) {
-    if (defined($bucket{$_})) {
-        $bucket{$_}++;
+    my $iter = Algorithm::Permute->new(\@sw, length($word));
+    my @ws;
+    while (my @res = $iter->next) {
+        push @ws, join "", @res;
     }
-    else {
-        die "Inconsistency: \"$_\" is not a permutation of \"$word\".\n";
+
+    my %bucket = map {$_ => 0} @ws;
+
+    for (@collection) {
+        if (defined($bucket{$_})) {
+            $bucket{$_}++;
+        }
+        else {
+            die "Inconsistency: \"$_\" is not a permutation of \"$word\".\n";
+        }
     }
+
+    return [ grep {!$bucket{$_}} @ws ];
 }
 
 
 
-for (@ws) {
-    say $_ if !($bucket{$_});
+if (defined($ARGV[0])) {
+    my @r = look_for(@ARGV)->@*; 
+    say join "\n", @r;
 }
 
 
 
-# $ ch-1.pl PELR PREL PERL PRLE PLER PLRE EPRL EPLR ERPL ERLP ELPR ELRP RPEL RPLE REPL RELP RLPE RLEP LPER LPRE LEPR LRPE LREP
-# output of example: LERP
+use Test::More tests => 3;
+ok look_for(qw/PELR PREL PERL PRLE PLER
+             PLRE EPRL EPLR ERPL ERLP ELPR ELRP 
+            RPEL RPLE REPL RELP RLPE RLEP LPER 
+            LPRE LEPR LRPE LREP/)->[0] eq "LERP";
+ok look_for("ab")->[0] eq "ba";
+ok look_for(qw/123 132 321 312 213 123/)->[0] eq "231"; 
