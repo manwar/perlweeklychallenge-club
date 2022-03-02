f7ba96ee875e8c2a1d2b630c15ab599e0af6a337
E7-87-83 <fungcheokyin@gmail.com>
2022-03-03 02:54:33 +0800

    Wk 154 improvements

--- challenge-154/cheok-yin-fung/perl/ch-1.pl
+++ challenge-154/cheok-yin-fung/perl/ch-1.pl
@@ -2,46 +2,12 @@
 # The Weekly Challenge 154
 # Task 1 Missing Permutation
 # Usage: 
-# $ ch-1.pl [permutations of alphabets; 
-#            each alpahabet appears excatly once in a permutation]
+# $ ch-1.pl [permutations of alphabets] 
 
 use v5.22.0;
 use warnings;
 use Algorithm::Permute qw/permute/;
 use List::Util qw/uniqstr/;
-use Data::Dumper;
-
-
-sub look_for {
-    my @collection = @_;
-    my $word = $collection[0];
-
-    my @sw = split "", $word;
-
-    die "Duplicate alphabet in the word input.\n" 
-        if scalar @sw != scalar uniqstr @sw;
-
-
-
-    my $iter = Algorithm::Permute->new(\@sw, length($word));
-    my @ws;
-    while (my @res = $iter->next) {
-        push @ws, join "", @res;
-    }
-
-    my %bucket = map {$_ => 0} @ws;
-
-    for (@collection) {
-        if (defined($bucket{$_})) {
-            $bucket{$_}++;
-        }
-        else {
-            die "Inconsistency: \"$_\" is not a permutation of \"$word\".\n";
-        }
-    }
-
-    return [ grep {!$bucket{$_}} @ws ];
-}
 
 
 
@@ -52,10 +18,41 @@ if (defined($ARGV[0])) {
 
 
 
-use Test::More tests => 3;
+sub look_for {
+    my @collection = @_;
+    my $word = $collection[0];
+
+    my @sw = split "", $word;
+
+    my $iter = Algorithm::Permute->new(\@sw, length($word));
+    my @words;
+    while (my @res = $iter->next) {
+        push @words, join "", @res;
+    }
+    @words = uniqstr @words;
+
+    my %bucket = map {$_ => 0} @words;
+
+    for (@collection) {
+        if (defined($bucket{$_})) {
+            $bucket{$_}++;
+        }
+        else {
+            die "Inconsistency: \"$_\" is not a permutation of \"$word\".\n";
+        }
+    }
+
+    return [ grep {!$bucket{$_}} @words ];
+}
+
+
+
+use Test::More tests => 5;
 ok look_for(qw/PELR PREL PERL PRLE PLER
-             PLRE EPRL EPLR ERPL ERLP ELPR ELRP 
-            RPEL RPLE REPL RELP RLPE RLEP LPER 
-            LPRE LEPR LRPE LREP/)->[0] eq "LERP";
+               PLRE EPRL EPLR ERPL ERLP ELPR ELRP 
+               RPEL RPLE REPL RELP RLPE RLEP LPER 
+               LPRE LEPR LRPE LREP/)->[0] eq "LERP";
 ok look_for("ab")->[0] eq "ba";
 ok look_for(qw/123 132 321 312 213 123/)->[0] eq "231"; 
+ok look_for(qw/ell lel/)->[0] eq "lle"; 
+ok look_for(qw/eell leel llee elel elle/)->[0] eq "lele"; 
--- challenge-154/cheok-yin-fung/perl/ch-2.pl
+++ challenge-154/cheok-yin-fung/perl/ch-2.pl
@@ -2,9 +2,6 @@
 # The Weekly Challenge 154
 # Task 2 Padovan Prime
 
-# Todo: quit $NAX_NUM_OF_SEARCHES
-
-
 use v5.22.0;
 use warnings;
 use Math::BigInt;
@@ -15,21 +12,16 @@ prime_precalc(10_000_000);
 
 
 my $NUM_OF_TERMS = $ARGV[0] || 10;
-my $MAX_NUM_OF_SEARCHES = $ARGV[1] || $NUM_OF_TERMS*10;
-
-die "The search parameter is too small.\n"
-    if $MAX_NUM_OF_SEARCHES < $NUM_OF_TERMS;
-
 
 my @ans = ();
-my $pdvstate = [];
+my $pdvstate = [Math::BigInt->new(1), Math::BigInt->new(1),
+                Math::BigInt->new(1), Math::BigInt->new(2)];
 
 core();
 
 
+
 sub pdv_iter {
-    $pdvstate = [Math::BigInt->new(1), Math::BigInt->new(1), Math::BigInt->new(1), Math::BigInt->new(2)]
-      unless defined $pdvstate->[0];
     my ($a, $b, $c, $d) = @{$pdvstate};
     ($a, $b, $c, $d) = ($b, $c, $d, $b+$c);
     @$pdvstate = ($a, $b, $c, $d);
@@ -39,15 +31,11 @@ sub pdv_iter {
 
 
 sub core {
-    my $ind = 3;
-    while (1) {
-        if (!$pdvstate) {
-            pdv_n();
-            next;
-        }
+    my $ind = 3;  # the index is set to be consistent with OEIS:A112882
+    while (scalar @ans < $NUM_OF_TERMS) {
         my $Pk = pdv_iter();
         if (is_prob_prime($Pk)) {
-            update_ans($Pk);
+            say update_ans($Pk);
         }
         $ind++;
     }
@@ -57,11 +45,8 @@ sub core {
 
 sub update_ans {
     my $candidate = $_[0];
-    push @ans, $candidate if   defined($ans[-1]) 
-                            && $candidate != $ans[-1];
+    push @ans, $candidate if    defined($ans[-1]) 
+                             && $candidate != $ans[-1];
     push @ans, $candidate if !defined($ans[-1]);
-    if (scalar @ans >= $NUM_OF_TERMS) {
-        say join "\n", @ans;
-        exit;
-    }
+    return $ans[-1];
 }
