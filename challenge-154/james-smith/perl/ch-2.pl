c6a4ac485deb19dc74928fb571321af11bfa8f63
drbaggy <js5@sanger.ac.uk>
2022-03-04 09:58:31 +0000

    added another solution and fixed code!

--- challenge-154/james-smith/perl/ch-2.pl
+++ challenge-154/james-smith/perl/ch-2.pl
@@ -4,14 +4,11 @@ use strict;
 
 use warnings;
 use feature qw(say);
-use Time::HiRes qw(time);
 use Math::Prime::Util qw(is_prime);
 
 # We only keep the last 3 Padovan numbers as that is all we need
 # to generate the next one... We use the variables $p,$q,$r for
 # this..
-
-
 # We use a little used perl construct here - `redo` which restarts
 # the loop everytime we have a Padovan number which is composite
 # with updating "$_" - so we get 10 results.
@@ -19,16 +16,16 @@ use Math::Prime::Util qw(is_prime);
 # "1-liner" solution...
 
 my$p=my$q=my$r=1;
-($p,$q,$r)=($q,$r,$p+$q),$r!=$q&&is_prime($r)?say$r:redo for 1..10;
+($p,$q,$r)=($q,$r,$p+$q),$r!=$q&&is_prime($r)?say$r:redo for 0..9;
 
 # "expanded" solution...
 
 $p=$q=$r=1;
 
-for (1..10) {
+for (0..9) {
   ($p,$q,$r) = ($q,$r,$p+$q);
   redo if     $r == $q;       ## skip (redo loop) if same as previous value
   redo unless is_prime($r);   ## skip (redo loop) if not prime
-  say $r;                     ## output if we get here!
+  say $r;                     ## output if we get here, $_ will get updated
 }
 
