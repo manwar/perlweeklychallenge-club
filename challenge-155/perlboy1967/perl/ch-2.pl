ccdf5795580553b4b7df84b6b408814554afa518
Niels van Dijke <perlboy@cpan.org>
2022-03-09 07:53:05 +0000

    Minor update to program flow

--- challenge-155/perlboy1967/perl/ch-2.pl
+++ challenge-155/perlboy1967/perl/ch-2.pl
@@ -40,25 +40,22 @@ use bigint;
 # Prototype(s)
 sub fib($);
 
-my @fibMod;
+my (@fibMod, @fModLh, @fModUh);
 
 my $N = shift // 3;
 
 my $i = 0;
-while(1) {
-  foreach (0,1) {
-    push(@fibMod, fib($i++) % $N);
-  }
+do {
+  push(@fibMod, fib($i++) % $N, fib($i++) % $N);
+
   # Split fibMod in two halves
-  my @fModLh = @fibMod[0 .. int($i/2)-1];
-  my @fModUh = @fibMod[int($i/2) .. $i-1];
+  @fModLh = @fibMod[0 .. int($i/2)-1];
+  @fModUh = @fibMod[int($i/2) .. $i-1];
 
   # Compare lower and upper half
-  if (join('',@fModLh) eq join('',@fModUh)) {
-    printf "%dth Pisano Period: %d (%s)\n", $N, scalar @fModLh, join(',',@fModLh);
-    exit;
-  }
-}
+} until (join('',@fModLh) eq join('',@fModUh));
+
+printf "%dth Pisano Period: %d (%s)\n", $N, scalar @fModLh, join(',',@fModLh);
 
 
 sub fib($) {
