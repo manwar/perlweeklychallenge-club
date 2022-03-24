c1acd6517651e5c03edc98fafe7546d0ef016afb
drbaggy <js5@sanger.ac.uk>
2022-03-24 11:30:59 +0000

    tidied up some code {first to test difference between scalable version of #1 & to add more tests

--- challenge-157/james-smith/perl/ch-2.pl
+++ challenge-157/james-smith/perl/ch-2.pl
@@ -8,20 +8,23 @@ use Test::More;
 use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 
-my @TESTS = (
-  [ 7, 1 ],
-  [ 6, 0 ],
-  [ 8, 1 ],
+## List of brazillian numbers... for testing...
+my %T = map { $_ => 1 } qw(
+                     7  8    10    12 13 14 15 16    18     20
+  21 22    24    26 27 28    30 31 32 33 34 35 36    38 39  40
+     42 43 44 45 46    48    50 51 52    54 55 56 57 58     60
+     62 63 64 65 66    68 69 70    72 73 74 75 76 77 78     80
+  81 82    84 85 86 87 88    90 91 92 93 94 95 96    98 99 100
 );
 
-is( is_brazilian($_->[0]), $_->[1] ) foreach @TESTS;
+is( is_brazilian( $_ ), $T{$_}||0 ) for 1..100;
 
 done_testing();
 
 sub is_brazilian {
-  O: for my $b (2.. $_[0]/2-1) {
-    my $last = (my $n=$_[0])%$b;
-    $n%$b==$last || next O while $n=int($n/$b);
+  O: for my $b ( 2 .. $_[0]/2 - 1 ) {
+    my $l = (my $n=$_[0]) % $b;
+    $n % $b == $l || next O while $n = int($n/$b);
     return 1;
   }
   0;
