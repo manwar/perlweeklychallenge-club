9ebc0762c7616e64393ec9bb8fca6cb86561d195
drbaggy <js5@sanger.ac.uk>
2022-03-29 08:55:13 +0100

    tidied up code

--- challenge-158/james-smith/perl/ch-1.pl
+++ challenge-158/james-smith/perl/ch-1.pl
@@ -8,6 +8,10 @@ use Math::Prime::Util qw(next_prime is_prime);
 use Benchmark qw(cmpthese);
 
 my $N = shift || 100;
+my $I = 5e6/$N;
+$I = 200 if $I<100;
+say join ', ', count_primes_div();
+say join ', ', count_primes_split();
 
 cmpthese( 5e6/$N, {
   'div'   => sub { count_primes_div();   },
@@ -16,7 +20,7 @@ cmpthese( 5e6/$N, {
 
 sub count_primes_div {
   my @res;
-  for( my $p = 2; (my $s=0) || (my $q=$p)<=$N; $p = next_prime $p ) {
+  for( my $p = 2; my $s=0, (my $q=$p)<=$N; $p = next_prime $p ) {
     do { $s += $q%10 } while $q = int $q/10;
     push @res, $p if is_prime $s;
   }
@@ -26,7 +30,7 @@ sub count_primes_div {
 
 sub count_primes_split {
   my @res;
-  for( my $p =2; (my $s=0) || $p <= $N ; $p = next_prime $p ) {
+  for( my $p =2; my $s=0, $p <= $N ; $p = next_prime $p ) {
     $s+=$_ for split //,$p;
     push @res, $p if is_prime $s;
   }
