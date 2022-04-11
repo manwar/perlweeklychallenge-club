4333f25cc511778c5c1e8633ea0d9facbe799073
drbaggy <js5@sanger.ac.uk>
2022-04-11 10:58:07 +0100

    soln to 1

--- challenge-160/james-smith/perl/ch-1.pl
+++ challenge-160/james-smith/perl/ch-1.pl
@@ -7,16 +7,22 @@ use feature qw(say);
 use Test::More;
 use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
+use Lingua::EN::Numbers qw(num2en);
 
 my @TESTS = (
-  [ 0, 1 ],
+  [ 5, 'Five is four, four is magic.' ],
+  [ 7, 'Seven is five, five is four, four is magic.' ],
+  [ 6, 'Six is three, three is five, five is four, four is magic.' ],
 );
 
-is( my_function($_->[0]), $_->[1] ) foreach @TESTS;
+say magic($_) for 0..1000;
+is( magic($_->[0]), $_->[1] ) foreach @TESTS;
 
 done_testing();
 
-sub my_function {
-  return 1;
+sub magic {
+  my $r = ucfirst num2en( my $n = shift ).' is ';
+  $r .= join num2en( $n = length num2en($n)=~s/\W//rg ), '', ', ', ' is ' until $n==4;
+  $r.'magic.';
 }
 
