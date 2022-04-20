afb9b741d5752b6c7b26d10fe6f6db300fba2248
Michael Manring <michael@manring>
2022-04-20 22:59:05 +0700

    pwc161 solution in go and minor fix on perl solution

--- challenge-161/pokgopun/go/ch-2.go
+++ challenge-161/pokgopun/go/ch-2.go
@@ -5,20 +5,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
 
-type bools []bool
+type byteSeen map[byte]struct{}
 
-func (b *bools) allTrue() bool {
-	for i := 0; i < len(*b); i++ {
-		if !(*b)[i] {
-			return false
+func (bs byteSeen) countUnseen(s string) (n int) {
+	b := []byte(s)
+	sort.Slice(b, func(i, j int) bool {
+		return b[i] < b[j]
+	})
+	var seen byte
+	for _, v := range b {
+		if seen == v {
+			continue
+		} else {
+			seen = v
+		}
+		_, ok := bs[v]
+		if !ok {
+			n++
 		}
 	}
-	return true
-}
-func (b *bools) newTruth(bs bools) int {
-	return 0
+	return n
 }
 func main() {
 	f, err := os.Open("../../../data/dictionary.txt")
@@ -27,20 +37,35 @@ func main() {
 	}
 	defer f.Close()
 	pngrm := []string{}
-	seen := make(bools, 26)
+	letters := byteSeen{}
+	var best string
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		for _, v := range scanner.Bytes() {
-			v -= 97
-			seen[v] = true
+		if len(best) == 0 {
+			best = "a"
 		}
-		pngrm = append(pngrm, scanner.Text())
-		if seen.allTrue() {
+		b := scanner.Text()
+		if b[:1] == best[:1] {
+			if letters.countUnseen(best) >= letters.countUnseen(b) {
+				continue
+			}
+			best = b
+			continue
+		}
+		b, best = best, b
+		//fmt.Println("b=", b, "best=", best)
+		for _, v := range []byte(b) {
+			letters[v] = struct{}{}
+		}
+		pngrm = append(pngrm, b)
+		//fmt.Println(len(letters), len(pngrm))
+		if len(letters) == 26 {
 			break
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
-	fmt.Println(pngrm, len(pngrm))
+	fmt.Println(pngrm)
+	fmt.Printf("=> %d words, %d letters and %d unique letters\n", len(pngrm), len(strings.Join(pngrm, "")), len(letters))
 }
