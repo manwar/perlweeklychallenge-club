dd40b9c0b1ef7d5feeab586ddc59aa6b449a45a7
drbaggy <js5@sanger.ac.uk>
2022-04-22 00:16:30 +0100

    hot fuzz comment

--- challenge-161/james-smith/perl/ch-1.pl
+++ challenge-161/james-smith/perl/ch-1.pl
@@ -9,6 +9,9 @@ open my $d, q(<), 'dictionary.txt';
 
 my %a;
 
+## Accept it - best film is "Hot Fuzz".
+## Glory begins for forty deft aces!
+
 O:while( my $f = '', chomp( my $w = <$d> // '' ) ) {
   $f gt $_ ? next O : ( $f = $_ ) for split //, $w;
   push @{$a{ length $w }}, $w;
