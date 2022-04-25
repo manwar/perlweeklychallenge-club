0ad9c7b387d8ec526875f80e0e7ed484152702c5
drbaggy <js5@sanger.ac.uk>
2022-04-25 22:22:39 +0100

    better comments

--- challenge-162/james-smith/perl/ch-2.pl
+++ challenge-162/james-smith/perl/ch-2.pl
@@ -17,18 +17,22 @@ sub encrypt { return _crypt( 1,@_); }
 sub decrypt { return _crypt(-1,@_); }
 
 sub _crypt {
-  my($off,$key,$p,$out,@r,%l) = (shift,shift,0,'');           ## Initialise variables and get mapping...
-  ($_ eq 'j' && ($_='i')), exists $l{$_} || ($l{$_}=[int $p/5,($p++)%5]) for grep { /[a-z]/ } split(//,$key),'a'..'i','j'..'z';
-  $r[$l{$_}[0]][$l{$_}[1]]=$_ for keys %l;
+  my($o,$key,$p,$out,@r,%l) = (shift,shift,0,'');                        ## Initialise variables and get mapping...
+  ($_ eq 'j' && ($_='i')), exists $l{$_} || ($l{$_}=[int $p/5,($p++)%5]) ## %l maps letter to position
+    for grep { /[a-z]/ } split(//,$key),'a'..'i','k'..'z';
+  $r[$l{$_}[0]][$l{$_}[1]]=$_ for keys %l;                               ## @r maps position to letter
 
-  my @seq = grep {/[a-z]/} split //, shift =~ s{j}{j}gr;      ## Prep sequence
+  my @seq = grep {/[a-z]/} split //, shift =~ s{j}{i}gr;                 ## Prep sequence
 
-  while(my($m,$n)=splice @seq,0,2) {                          ## Loop through letter pairs
-    unshift(@seq,$n), $n='x' if $n && $n eq $m and $n ne 'x'; ## Deal with case when both letters the same
-    $n ||= 'x';                                               ## Pad if required...
-    $out.= $l{$m}[0] eq $l{$n}[0] ? $r[ $l{$m}[0]        ][($l{$m}[1]+$off)%5] . $r[ $l{$n}[0]        ][($l{$n}[1]+$off)%5]
-         : $l{$m}[1] eq $l{$n}[1] ? $r[($l{$m}[0]+$off)%5][ $l{$m}[1]        ] . $r[($l{$n}[0]+$off)%5][ $l{$n}[1]        ]
-         :                          $r[ $l{$m}[0]        ][ $l{$n}[1]        ] . $r[ $l{$n}[0]        ][ $l{$m}[1]        ]
+  while(my($m,$n)=splice @seq,0,2) {                                     ## Loop through letter pairs
+    unshift(@seq,$n), $n='x' if $n && $n eq $m && $n ne 'x';             ## Deal with case when both letters the same
+    $n ||= 'x';                                                          ## Pad if required...
+    $out.= $l{$m}[0] eq $l{$n}[0] ? $r[ $l{$m}[0]      ][($l{$m}[1]+$o)%5].
+                                    $r[ $l{$n}[0]      ][($l{$n}[1]+$o)%5]
+         : $l{$m}[1] eq $l{$n}[1] ? $r[($l{$m}[0]+$o)%5][ $l{$m}[1]      ].
+                                    $r[($l{$n}[0]+$o)%5][ $l{$n}[1]      ]
+         :                          $r[ $l{$m}[0]      ][ $l{$n}[1]      ].
+                                    $r[ $l{$n}[0]      ][ $l{$m}[1]      ]
          ;
   }
   $out;
