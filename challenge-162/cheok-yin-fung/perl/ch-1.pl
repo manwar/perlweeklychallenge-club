b1f7bf280c5ff285012778f8226dc41bb606c866
E7-87-83 <fungcheokyin@gmail.com>
2022-05-01 10:59:43 +0800

    check digit cases for Task 1 Perl script; Task 2 Partial Julia solution

--- challenge-162/cheok-yin-fung/perl/ch-1.pl
+++ challenge-162/cheok-yin-fung/perl/ch-1.pl
@@ -34,9 +34,13 @@ sub valid {
 
 
 
-use Test::More tests => 5;
+use Test::More tests => 9;
 ok valid("978-0-306-40615-7"), "task example";
 ok valid("978-1-492-04503-8"), "Think Julia";
 ok valid("978-1-59327-666-9"), "How Software Works";
 ok valid("978-1-260-08450-4"), "Database System Concepts";
 ok !valid("123-4-567-89123-4"), "random strings of digits";
+ok 7==lookup("978-0-306-40615-?"), "(check digit) task example";
+ok 8==lookup("978-1-492-04503-?"), "(check digit) Think Julia";
+ok 9==lookup("978-1-59327-666-?"), "(check digit) How Software Works";
+ok 4==lookup("978-1-260-08450-?"), "(check digit) Database System Concepts";
