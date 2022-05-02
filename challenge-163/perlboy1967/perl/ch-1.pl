31257d68eb905e6d6dd88f5c37d66aa73cd0b31b
Niels van Dijke <perlboy@cpan.org>
2022-05-02 21:39:36 +0000

    Task 1 - Uncertainty about 'unique pairs'. Adding second variant

--- challenge-163/perlboy1967/perl/ch-1.pl
+++ challenge-163/perlboy1967/perl/ch-1.pl
@@ -19,21 +19,47 @@ Write script to calculate the sum of bitwise & operator for all unique pairs.
 use v5.16;
 
 use List::Util qw(sum);
-use List::MoreUtils qw(singleton duplicates);
+use List::MoreUtils qw(singleton duplicates uniq);
 use Algorithm::Combinatorics qw(combinations);
 use Test::More;
 
-is (andSum(1,2,3), 3);
-is (andSum(2,3,4), 2);
+is (andSum1(1,2,3), 3);
+is (andSum2(1,2,3), 3);
 
-is (andSum(2,2), 2);
-is (andSum(2,2,2), 2);
-is (andSum(1), 0);
+is (andSum1(2,3,4), 2);
+is (andSum2(2,3,4), 2);
+
+is (andSum1(2,2), 2);
+is (andSum2(2,2), 0);
+
+is (andSum1(2,2,2), 2);
+is (andSum2(2,2,2), 0);
+
+is (andSum1(1), 0);
+is (andSum2(1), 0);
+
+is (andSum1(1,1,3), 3);
+is (andSum2(1,1,3), 1);
 
 done_testing();
 
+# Assumption on andSum1:
+#
+# Unique combinations of (1,1,3) results in:
+#
+#    (1 & 1) + (1 & 3) + (3 & 1)
+# 
+# as left hand side and right hand side of the '&' is three time q unique combination
+#
+# Note: Add (0,0) to always have two or more (harmless) values in the list
+sub andSum1 {
+  return sum map{$_->[0]&$_->[1]}combinations([0,0,singleton(@_),map{($_)x2}duplicates(@_)],2);
+}
 
-sub andSum {
-  return 0 if @_ < 2;
-  return sum map{$_->[0]&$_->[1]}combinations([singleton(@_),map{($_)x2}duplicates(@_)],2);
+# andSum2 is a more simplistic algorithm:
+# Work on the unique numbers given (remove duplicates)
+#
+# Note: Add (0,0) to always have two or more (harmless) values in the list
+sub andSum2 {
+  return sum map{$_->[0]&$_->[1]}combinations([0,0,uniq@_],2);
 }
