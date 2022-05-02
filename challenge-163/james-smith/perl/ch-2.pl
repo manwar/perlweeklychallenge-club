9f5c66fdca78058def4f7ce68ba4ff98caea36ce
drbaggy <js5@sanger.ac.uk>
2022-05-02 10:50:25 +0100

    added additional examples and a pretty print function

--- challenge-163/james-smith/perl/ch-2.pl
+++ challenge-163/james-smith/perl/ch-2.pl
@@ -9,13 +9,17 @@ use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 
 my @TESTS = (
-  [ [1..5],      42 ],
-  [ [1,3,5,7,9], 70 ],
+  [ [1,3,5,7,9],            70 ],
+  [ [1..5],                 42 ],
+  [ [1..9],              4_862 ],
+  [ [1..15],         9_694_845 ],
+  [ [1..20],     6_564_120_420 ],
 );
 
 say '';
 is( summation(@{$_->[0]}), $_->[1] ),say'' for @TESTS;
 is( summation_with_table(@{$_->[0]}), $_->[1] ),say'' for @TESTS;
+is( summation_with_pretty_table(@{$_->[0]}), $_->[1] ),say'' for @TESTS;
 done_testing();
 say '';
 
@@ -23,6 +27,13 @@ sub summation_with_table {
   my $t;
   say "@_";
   shift, ($t=0), say join ' ', @_ = map { $t+=$_ } @_ while @_>1;
+  shift;
+}
+
+sub summation_with_pretty_table {
+  my ($in,$t)='';
+  say map { sprintf ' %11d', $_ } @_;
+  ($in.='            '),shift, ($t=0), say $in, map { sprintf ' %11d', $_ } @_ = map { $t+=$_ } @_ while @_>1;
   shift;
 }
 
