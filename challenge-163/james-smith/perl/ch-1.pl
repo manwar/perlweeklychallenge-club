632ee1d3e18fe9399b67bfe8b3eb472eefbf87df
drbaggy <js5@sanger.ac.uk>
2022-05-02 09:49:50 +0100

    soln for ch 1 tidied up with comments

--- challenge-163/james-smith/perl/ch-1.pl
+++ challenge-163/james-smith/perl/ch-1.pl
@@ -9,14 +9,27 @@ use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 
 my @TESTS = (
-  [ 0, 1 ],
+  [ [1..3], 3 ],
+  [ [2..4], 2 ],
+  [ [2..4,2..4], 2 ], ## Check uniquification...
 );
 
-is( my_function($_->[0]), $_->[1] ) foreach @TESTS;
-
+is( bit_sum(@{$_->[0]}), $_->[1] ) for @TESTS;
 done_testing();
 
-sub my_function {
-  return 1;
-}
 
+sub bit_sum {
+  my $t = 0;                     ## Initialize sum
+
+  my %hash = map { $_ => 1 } @_; ## quick uniquification - if worried
+  @_ = keys %hash;               ## that there may be duplicates and
+                                 ## question talks of unique pairs...
+
+  while(@_>1) {                  ## Loop through array until we have
+                                 ## no pairs left
+    my $a = shift;               ## we shift off the value;
+    $t+= $a&$_ for @_;           ## and and it with all remaining
+                                 ## elements for each element in list
+  }
+  $t;                            ## Return sum
+}
