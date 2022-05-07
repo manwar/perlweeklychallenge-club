6cb0362a21ee1e5eb7405493fc234aeb8eff49c4
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2022-05-07 10:14:57 -0700

    Ch163 (Clojure): Task 2

--- challenge-163/tyler-wardhaugh/clojure/src/c163/t2.clj
+++ challenge-163/tyler-wardhaugh/clojure/src/c163/t2.clj
@@ -1,11 +1,18 @@
 (ns c163.t2
   (:require [clojure.edn :as edn]))
 
-(def DEFAULT-INPUT [])
+(def DEFAULT-INPUT [[1 2 3 4 5]])
+
+(defn summations
+  [coll]
+  (loop [coll coll]
+    (if (< 1 (count coll))
+      (recur (reductions + (drop 1 coll)))
+      (first coll))))
 
 (defn -main
   "Run Task 2 with a given input N, defaulting to the first example from the
   task description."
   [& args]
   (let [[N] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    ))
+    (println (summations N))))
