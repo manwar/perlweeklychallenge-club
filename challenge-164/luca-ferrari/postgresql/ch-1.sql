e4b9baa7e409823ac38accd7a295e54f0e6432fb
Luca Ferrari <fluca1978@gmail.com>
2022-05-09 09:53:29 +0200

    Task 2 plpgsql done

--- challenge-164/luca-ferrari/postgresql/ch-1.sql
+++ challenge-164/luca-ferrari/postgresql/ch-1.sql
@@ -3,9 +3,41 @@
 
 CREATE SCHEMA IF NOT EXISTS pwc164;
 
+
 CREATE OR REPLACE FUNCTION
-pwc164.task1_plpgsql()
-RETURNS VOID
+pwc164.is_prime( n int )
+RETURNS boolean
 AS $CODE$
+DECLARE
+BEGIN
+FOR i IN 2 .. n - 1 LOOP
+IF n % i = 0 THEN
+RETURN false;
+END IF;
+END LOOP;
+
+RETURN true;
+END
+$CODE$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION
+pwc164.task1_plpgsql( l int DEFAULT 1000 )
+RETURNS SETOF INT
+AS $CODE$
+DECLARE
+BEGIN
+
+FOR n IN 10 .. l LOOP
+IF pwc164.is_prime( n ) AND n = reverse( n::text )::int THEN
+RETURN NEXT n;
+l := l - 1;
+END IF;
+
+EXIT WHEN l = 0;
+END LOOP;
+
+RETURN;
+END
 $CODE$
 LANGUAGE plpgsql;
