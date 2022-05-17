4d5447118da51f918c1a1f3347f676a83957039b
Michael Manring <michael@manring>
2022-05-17 18:55:24 +0700

    pwc165 solution

---
+++ challenge-165/pokgopun/go/ch-2.go
@@ -0,0 +1,66 @@
+/*
+### To pipe output to ch1.go to create svg file
+
+go run ch-2.go | go run ch-1.go ch-2.svg
+
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+)
+
+func main() {
+	//rawin := `2,4 3,5 5,7 7,10 9,15`
+	/**/
+	rawin := `333,129  39,189 140,156 292,134 393,52  160,166 362,122  13,193
+			341,104 320,113 109,177 203,152 343,100 225,110  23,186 282,102
+			284,98  205,133 297,114 292,126 339,112 327,79  253,136  61,169
+			128,176 346,72  316,103 124,162  65,181 159,137 212,116 337,86
+			215,136 153,137 390,104 100,180  76,188  77,181  69,195  92,186
+			275,96  250,147  34,174 213,134 186,129 189,154 361,82  363,89`
+	/**/
+	var ds struct{ count, x, y, sumx, sumy, sumxx, sumxy, m, b, x1, x2, y1, y2 float64 }
+	_, err := fmt.Sscanf(rawin, "%f,%f %f,%f", &ds.x1, &ds.y1, &ds.x2, &ds.y2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := bufio.NewWriter(os.Stdout)
+	for _, v := range regexp.MustCompile(`\d+,\d+`).FindAllString(rawin, -1) {
+		_, err := fmt.Sscanf(v, "%f,%f", &ds.x, &ds.y)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintln(w, v)
+		ds.count++
+		ds.sumx += ds.x
+		ds.sumy += ds.y
+		ds.sumxx += ds.x * ds.x
+		ds.sumxy += ds.x * ds.y
+		if ds.x1 > ds.x {
+			ds.x1 = ds.x
+		}
+		if ds.x2 < ds.x {
+			ds.x2 = ds.x
+		}
+		/*
+			if ds.y1 > ds.y {
+				ds.y1 = ds.y
+			}
+			if ds.y2 < ds.y {
+				ds.y2 = ds.y
+			}
+		*/
+	}
+	ds.m = (ds.count*ds.sumxy - ds.sumx*ds.sumy) / (ds.count*ds.sumxx - ds.sumx*ds.sumx)
+	ds.b = (ds.sumy - ds.m*ds.sumx) / ds.count
+	ds.y1 = ds.m*ds.x1 + ds.b
+	ds.y2 = ds.m*ds.x2 + ds.b
+	fmt.Fprintf(w, "%f,%f,%f,%f\n", ds.x1, ds.y1, ds.x2, ds.y2)
+	w.Flush()
+	//fmt.Printf("%+v\n", ds)
+}
