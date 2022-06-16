68f76f4aab3253674ac22ee3a0dd7cab3f41f139
drbaggy <js5@sanger.ac.uk>
2022-06-16 10:02:30 +0100

    tidied up

--- challenge-169/james-smith/perl/ch-1.pl
+++ challenge-169/james-smith/perl/ch-1.pl
@@ -13,9 +13,9 @@ my $time = time;
 # must have preciesely 2 prime factors
 # THEN each factor must be the same length;
 
-for( my( $n, $c, $MAX, @f ) = ( 0, 0, @ARGV ? $ARGV[0] : 1e2 ); $c<$MAX; $n++ ) {
-  say sprintf '%8d: %10d = %5d x %d', ++$c, $n, @f
-    if 2 == ( @f = factor $n ) && length $f[0] == length $f[1];
+for( my( $MAX, $c, $n, @f ) = ($ARGV[0] // 1e2,0); $c < $MAX; ) {
+  printf "%8d: %10d = %5d x %d\n", ++$c, $n, @f
+    if 2 == ( @f = factor ++$n ) && length $f[0] == length $f[1];
 }
 
 warn 'Time taken: ', time-$time, "\n";
