e3ed838e776b2217f5ecf7beb7064077864fe44d
drbaggy <js5@sanger.ac.uk>
2022-06-20 14:36:08 +0100

    add pretty print table

--- challenge-170/james-smith/perl/ch-2.pl
+++ challenge-170/james-smith/perl/ch-2.pl
@@ -4,19 +4,18 @@ use strict;
 
 use warnings;
 use feature qw(say);
-use Test::More;
-use Benchmark qw(cmpthese timethis);
-use Data::Dumper qw(Dumper);
 
-my $m1 = [ [1,2],[3,4] ];
-my $m2 = [ [5,6],[7,8] ];
+my $m1=[[1,2],[3,4]];
+my $m2=[[5,6],[7,8]];
 
-print Dumper( k_product( $m1, $m2 ) );
-print Dumper( k( $m1, $m2 ) );
+d($_) foreach $m1, $m2, k_product($m1,$m2), k($m1,$m2);
+
 sub k_product {
   [ map { my$r = $_; map { my$t = $_; [ map { my$s=$_; map { $s*$_ } @{$t} } @{$r} ] } @{$_[1]} } @{$_[0]} ]
 }
 #----|----#----|----#----|----#----|----#----|----#----|----#----|----#--
 #73 chars...
 sub k{[map{$b=$_;map{$a=$_;[map{//;map{$'*$_}@$a}@$b]}@{$_[1]}}@{$_[0]}]}
+
+sub d{say'|',(map{sprintf' %6d',$_}@{$_}),' |'for@{$_[0]};say''}
 
