d11ee4615400e46c8d5789e8b6566980f4ca423c
drbaggy <js5@sanger.ac.uk>
2022-06-28 06:38:23 +0100

    layout stuff

--- challenge-171/james-smith/perl/ch-2.pl
+++ challenge-171/james-smith/perl/ch-2.pl
@@ -15,12 +15,16 @@ sub compose {
   @_ && ($f = pop) ? compose( @_, sub { $f->($g->(@_)) } ) : $g
 }
 
-my $x_r = compose( sub { $_[0]*2 }, sub { $_[0]-20 }  );
-my $x_s = comp_sm( sub { $_[0]*2 }, sub { $_[0]-20 }  );
+my $f1 = sub { $_[0]-20 };
+my $f2 = sub { $_[0]*2  };
+my $f3 = sub { $_[0]+10 };
+my $f4 = sub { $_[0]/7  };
+my $x_s = comp_sm( $f2, $f1 );
+my $x_r = compose( $f2, $f1 );
+my $y_s = comp_sm( $f4, comp_sm( $f3, comp_sm( $f2, $f1 ) ) );
+my $y_r = compose( $f4, $f3, $f2, $f1 );
 
-my $y_r = compose( sub { $_[0]/7 }, sub { $_[0]+10 },
-                 sub { $_[0]*2 }, sub { $_[0]-20 }  );
-my $y_s = comp_sm( sub { $_[0]/7 }, comp_sm( sub { $_[0]+10 }, $x_s ) );
+## 22 - 20 = 2 .. * 2 = 4 .. + 10 = 14 .. / 7 = 2
 
 say '';
 say 'Compose simple';
