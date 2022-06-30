6c0b3cf0497fb856402a85d3af24135e5c76180d
Kjetil S <kjetilskotheim@gmail.com>
2022-06-30 16:12:01 +0200

    del unnecessary

--- challenge-171/kjetillll/perl/ch-1-faster.pl
+++ challenge-171/kjetillll/perl/ch-1-faster.pl
@@ -2,7 +2,7 @@
 use Math::Prime::Util 'divisor_sum';
 my $want = shift // 20;
 my($have,$n)=(0,1);
-Math::Prime::Util::divisor_sum($n)-$n > $n
+divisor_sum($n)-$n > $n
     and print "$n\n"
     and $have++
     while $have < $want
