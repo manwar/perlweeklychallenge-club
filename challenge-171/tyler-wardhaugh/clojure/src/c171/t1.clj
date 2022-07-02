2b1e1400a8f58294ba10bb2ea8b474cb69d7686e
Tyler Wardhaugh <tyler.wardhaugh@gmail.com>
2022-07-02 13:03:33 -0700

    Ch171 (Clojure): Task 1

--- challenge-171/tyler-wardhaugh/clojure/src/c171/t1.clj
+++ challenge-171/tyler-wardhaugh/clojure/src/c171/t1.clj
@@ -1,11 +1,22 @@
 (ns c171.t1
   (:require [clojure.edn :as edn]))
 
-(def DEFAULT-INPUT [])
+(def DEFAULT-INPUT [20])
+
+(defn proper-divisors
+  [n]
+  (->> (range 2 n) (filter #(zero? (rem n %)))))
+
+(defn abundant?
+  [n]
+  (< n (inc (reduce + (proper-divisors n)))))
+
+(def generate-odd-abundants
+  (eduction (filter abundant?) (range 1 ##Inf 2)))
 
 (defn -main
   "Run Task 1 with a given input N, defaulting to the first example from the
   task description."
   [& args]
   (let [[N] (or (some->> args (map edn/read-string)) DEFAULT-INPUT)]
-    N))
+    (println (take N generate-odd-abundants))))
--- challenge-171/tyler-wardhaugh/clojure/test/c171/t1_test.clj
+++ challenge-171/tyler-wardhaugh/clojure/test/c171/t1_test.clj
@@ -1,6 +1,9 @@
 (ns c171.t1-test
   (:require [clojure.test :refer [deftest is testing]]
-            [c171.t1 :refer []]))
+            [c171.t1 :refer [generate-odd-abundants]]))
 
 (deftest task-1
-  (testing "Task 1 produces the correct result"))
+  (testing "Task 1 produces the correct result"
+    (is (= (take 20 generate-odd-abundants)
+           [945 1575 2205 2835 3465 4095 4725 5355 5775 5985 6435 6615 6825
+            7245 7425 7875 8085 8415 8505 8925]))))
