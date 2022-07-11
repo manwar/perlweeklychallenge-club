272eb489dfbdfb7153404d7c50cdca971132203d
Matthew Neleigh <matthew.neleigh@gmail.com>
2022-07-11 03:53:36 -0400

    new file:   challenge-173/mattneleigh/perl/ch-1.pl 	new file:   challenge-173/mattneleigh/perl/ch-2.pl

---
+++ challenge-173/mattneleigh/perl/ch-1.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use English;
+
+################################################################################
+# Begin main execution
+################################################################################
+
+my @numbers = ( 5456, 120, 6 );
+
+print("\n");
+foreach(@numbers){
+    print(
+        $_, 
+        " is",
+        is_esthetic_number($_) ? "" : " not",
+        " an esthetic number.\n"
+    );
+}
+print("\n");
+
+exit(0);
+################################################################################
+# End main execution; subroutines follow
+################################################################################
+
+
+
+################################################################################
+# Determine whether an integer N is an esthetic number (every adjacent digit
+# differs from its neighbour by 1)
+# Takes one argument:
+# * The integer N to examine
+# Returns on success:
+# * 1 if N is an esthetic number
+# * 0 if N is not an esthetic number
+# Returns on error:
+# * undef if N is less than 1 (not positive)
+# NOTE: Traditionally single-digit numbers are considered esthetic and will be
+# treated as such
+################################################################################
+sub is_esthetic_number{
+    my $n = int(shift());
+
+    return(undef)
+        if($n < 1);
+
+    my @digits = split("", $n);
+
+    for my $i (0 .. ($#digits - 1)){
+        return(0)
+            if(abs($digits[$i] - $digits[$i + 1]) == 1);
+    }
+
+    return(1);
+
+}
+
+
+
