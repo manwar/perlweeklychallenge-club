34b5062fc373e4e3ce33d868f08a8118d7aec1ee
drbaggy <js5@sanger.ac.uk>
2022-07-12 05:41:19 +0100

    updates including more golfy code

--- challenge-173/james-smith/perl/ch-1.pl
+++ challenge-173/james-smith/perl/ch-1.pl
@@ -14,12 +14,15 @@ my @TESTS = (
 );
 
 is( is_esthetic($_->[0]), $_->[1] ) foreach @TESTS;
+is( est($_->[0]), $_->[1] ) foreach @TESTS;
 
 done_testing();
 
-sub is_esthetic {
-  my($f,@n) = split //, shift;
-  ( $_==$f+1 || $_==$f-1 ) ? ($f=$_) : return 0 for @n;
-  1;
+sub is_esthetic{
+  my($f,@n)=split//,pop;
+  abs( $_-$f ) == 1 ? ($f=$_) : return 0 for@n;
+  1
 }
+#-68-|----#----|----#----|----#----|----#----|----#----|----#----|----
+sub est{my($f,@n)=split//,pop;abs($_-$f)==1?($f=$_):return 0for@n;1}
 
--- challenge-173/james-smith/perl/ch-2.pl
+++ challenge-173/james-smith/perl/ch-2.pl
@@ -9,9 +9,10 @@ use Benchmark qw(cmpthese timethis);
 use Data::Dumper qw(Dumper);
 use bigint;
 
-my $n =2;
-my @list = ( 2, map { $n = $n*($n-1) + 1 } 2..10 );
+my $n=2;
+say sprintf '%140s', th($_) for 2,map{$n*=$n-1;++$n}1..9;
 
-say sprintf '%144s', th($_) for @list;
+#-38-|----#----|----#----|----#----|----
+$a=2;say for 2,map{$a*=$a-1;++$a}1..9;
 
 sub th { scalar reverse( (reverse $_[0]) =~ s/(\d\d\d)(?=\d)(?!\d*\.)/$1,/gr ) }
