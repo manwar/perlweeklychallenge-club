21e207f6debfd4944cc504bfd05a6b3e81159898
Luca Ferrari <fluca1978@gmail.com>
2022-07-18 09:53:25 +0200

    Task 2 done plpgsql.
    Fix also plperl implementation

--- challenge-174/luca-ferrari/postgresql/ch-2.sql
+++ challenge-174/luca-ferrari/postgresql/ch-2.sql
@@ -4,8 +4,23 @@
 CREATE SCHEMA IF NOT EXISTS pwc174;
 
 CREATE OR REPLACE FUNCTION
-pwc174.task2_plpgsql()
-RETURNS VOID
+pwc174.task2_plpgsql_permutation2rank( input int[] )
+RETURNS int
 AS $CODE$
+BEGIN
+        RETURN pwc174.task2_permutation2rank( input );
+END
+$CODE$
+LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION
+pwc174.task2_plpgsql_rank2permutation( i int, input int[] )
+RETURNS int[]
+AS $CODE$
+BEGIN
+RETURN pwc174.task2_rank2permutation( i, input );
+END
 $CODE$
 LANGUAGE plpgsql;
