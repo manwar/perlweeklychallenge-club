f1fd329b7084bbded9262af04c5c6b912a537cd7
Dave Cross <dave@dave.org.uk>
2022-07-25 16:45:45 +0100

    Slight tweak. Add comments.

--- challenge-175/dave-cross/perl/ch-01.pl
+++ challenge-175/dave-cross/perl/ch-01.pl
@@ -20,8 +20,9 @@ for (2 .. 13) {
   my $date_str = sprintf '%4d-%02d-01', $year, $_;
   my $date = Time::Piece->strptime($date_str, '%Y-%m-%d');
 
-  $date -= ONE_DAY;
-  $date -= (ONE_DAY * $date->_wday) if $date->day ne 'Sun';
+  $date -= ONE_DAY; # Last day of previous month
+  # _wday() is 0 for Sun, 1 for Mon, etc...
+  $date -= (ONE_DAY * $date->_wday);
 
   say $date->ymd;
 }
