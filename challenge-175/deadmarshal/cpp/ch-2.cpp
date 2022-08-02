36494fe71a08eff9772a37892b032675eecd50b1
deadmarshal <adeadmarshal@gmail.com>
2022-08-02 12:10:49 +0430

    challenge175

---
+++ challenge-175/deadmarshal/cpp/ch-2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+int phi(int n)
+{
+  int i = 2, result = n;
+  while(i <= n)
+  {
+    if(n % i == 0)
+    {
+      while(n % i == 0) n /= i;
+      result -= (result / i);
+    }
+    if(i == 2) i = 1;
+    i += 2;
+  }
+  if(n > 1) result -= (result / n);
+  return result;
+}
+
+int phi_iter(int n)
+{
+  if(n == 2) return phi(n) + 0;
+  return phi(n) + phi_iter(phi(n));
+}
+
+int main()
+{
+  int i = 2, count = 0;
+  while(count != 20)
+  {
+    if(i == phi_iter(i))
+    {
+      std::cout << i << ' ';
+      count++;
+    }
+    i++;
+  }
+  return 0;
+}
+
---
+++ challenge-175/deadmarshal/pascal/ch2.pas
@@ -0,0 +1,48 @@
+program Ch2;
+
+{$mode objfpc}
+uses
+  SysUtils;
+
+var
+  I,Count:Integer;
+  
+function Phi(N:Integer):Integer;
+var
+  I:Integer;
+begin
+  Result := N;
+  I := 2;
+  while(I <= N) do
+  begin
+    if(N mod I = 0) then
+    begin
+      while(N mod I = 0) do N := N div I;
+      Result := Result - (Result div I);
+    end;
+    if(I = 2) then I := 1;
+    Inc(I, 2);
+  end;
+  if(N > 1) then Result := Result - (Result div N);
+end;
+
+function PhiIter(N:Integer):Integer;
+begin
+  if N = 2 then
+    Exit(Phi(N) + 0);
+  PhiIter := Phi(N) + PhiIter(Phi(N));
+end;
+
+begin
+  I := 2;
+  Count := 0;
+  while(Count <> 20) do
+  begin
+    if I = PhiIter(I) then
+    begin
+      Write(I, ' ');
+      Inc(Count);
+    end;
+    Inc(I);
+  end;
+end.
