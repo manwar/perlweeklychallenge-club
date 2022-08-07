f58ea86ace13f258fd2928783ccee14897a9bffe
Adam Russell <ac.russell@live.com>
2022-08-07 17:24:31 -0400

    updated Fortran solutions

--- challenge-176/adam-russell/fortran/ch-2.f
+++ challenge-176/adam-russell/fortran/ch-2.f
@@ -27,15 +27,27 @@ contains
         end do
     end function int2digits
 
+    function digits2int(digits) result(n)
+        integer :: x = 1
+        integer :: r = 0
+        integer :: n = 0
+        for(int i: v){
+            r = r + (i * x);
+            x = x * 10;
+        }
+    end function digits2int  
+
     function is_reversible(x) result(is_reversible)
         integer, intent(in) :: x
         logical :: is_reversible 
         integer :: number_digits_x
-        integer, allocatable :: x_digits(:)
+        integer, allocatable :: x_digits(:), x_digits_reversed(:)
         number_digits_x = floor(log10(real(x)) + 1)
         allocate(x_digits(number_digits_x))
+        allocate(x_digits_reversed(number_digits_x))
         x_digits = int2digits(x)
-        ! call sort(x_digits)
+        x_digits_reversed = int2digits(x)
+        x_digits_reversed = x_digits_reversed(ubound(x_digits_reversed,dim=1)::-1)
         is_permuted = x_digits == y_digits
     end function is_reversible
 
