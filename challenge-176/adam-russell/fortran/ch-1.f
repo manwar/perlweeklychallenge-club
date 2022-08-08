7209662f9d1be7ce8e2b2637e672a7dc181245fd
Adam Russell <ac.russell@live.com>
2022-08-08 12:30:18 -0400

    updated Fortran solutions

--- challenge-176/adam-russell/fortran/ch-1.f
+++ challenge-176/adam-russell/fortran/ch-1.f
@@ -1,15 +1,15 @@
 program permuted_multiple
-use stdlib_sorting, only: sort
 implicit none
 
+integer :: i
+logical :: l
 do i = 1, 1000000
-    if(is_permuted(i, i*2) .and. is_permuted(i*2, i*3) .and.
-    is_permuted(i*3, i*4) .and.  is_permuted(i*4, i*5) .and.
-    is_permuted(i*5, i*6)) then
+    if(is_permuted(i, i*2) .and. is_permuted(i*2, i*3) .and. &
+       is_permuted(i*3, i*4) .and.  is_permuted(i*4, i*5) .and. &
+       is_permuted(i*5, i*6)) then
           print *, i
     end if 
 end do
-
 
 contains
 
@@ -19,7 +19,7 @@ contains
         integer :: i, d, m, number_digits
         number_digits = floor(log10(real(n)) + 1)
         allocate(digits(number_digits))      
-        i = 0
+        i = 1
         m = n
         do while(m /= 0) 
             d = mod(m, 10)
@@ -29,9 +29,10 @@ contains
         end do 
     end function int2digits
 
-    function is_permuted(x, y) result(is_permuted)
+    function is_permuted(x, y) result(permuted)
         integer, intent(in) :: x, y 
-        logical :: is_permuted 
+        integer :: i
+        logical :: permuted 
         integer :: number_digits_x
         integer :: number_digits_y  
         integer, allocatable :: x_digits(:)
@@ -42,9 +43,41 @@ contains
         allocate(y_digits(number_digits_y))      
         x_digits = int2digits(x)
         y_digits = int2digits(y)
-        call sort(x_digits)   
-        call sort(y_digits)   
-        is_permuted = x_digits == y_digits 
+        call insertion_sort(size(x_digits), x_digits)   
+        call insertion_sort(size(y_digits), y_digits)
+        permuted = .true.
+        if(size(x_digits) /= size(y_digits)) then
+            permuted = .false.
+        else
+            do i = 1, size(x_digits)
+                if(x_digits(i) /= y_digits(i)) then
+                    permuted = .false.
+                end if
+            end do
+        end if
     end function is_permuted  
+    
+    ! sort code from https://github.com/yet-another-alex/basic-fortran-examples/blob/master/sorting.f90
+	subroutine insertion_sort(msize, array)
+		implicit none
+		! variables
+		integer, intent(in) :: msize
+		integer, intent(inout) :: array(msize)
+		integer :: prev_index, i, insert
+
+		do i = 1, msize
+			! current item to insert
+			insert = array(i)
+			! previous index
+			prev_index = i - 1
+			! if the item is larger, move everything forward
+			do while ((prev_index > 0) .and. (array(prev_index) > insert))
+				array(prev_index + 1) = array(prev_index)
+				prev_index = prev_index - 1
+			end do
+			! insert the item
+			array(prev_index + 1) = insert
+		end do
+	end subroutine insertion_sort
 
 end program permuted_multiple
