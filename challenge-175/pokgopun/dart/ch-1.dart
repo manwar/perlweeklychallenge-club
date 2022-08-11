1b33767df1f2dc3031db3f56b0b412c2f8891e64
Michael Manring <michael@manring>
2022-08-11 09:58:27 +0700

    pwc175 solution in dart

---
+++ challenge-175/pokgopun/dart/ch-1.dart
@@ -0,0 +1,23 @@
+void main() {
+  print(LastWeekdayOfMonth(DateTime.sunday, 2022));
+}
+
+class LastWeekdayOfMonth {
+  final int weekday, year, month;
+  final data = <DateTime>[];
+  LastWeekdayOfMonth(this.weekday, this.year, [this.month = 0]) {
+    final mdc = _monthdayCount(year);
+    for (var i = 0; i < mdc.length; i++) {
+      var t = DateTime.utc(year, i + 1, mdc[i]);
+      var o = weekday - t.weekday;
+      data.add(t.add(Duration(days: o > 0 ? o - 7 : o)));
+    }
+  }
+  List<int> _monthdayCount(int year) =>
+      [31, year % 4 == 0 ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  @override
+  String toString() => [
+        if (month == 0) ...data,
+        if (month > 0 && month <= 12) data[month - 1]
+      ].map((dt) => dt.toString().split(" ")[0]).join("\n");
+}
