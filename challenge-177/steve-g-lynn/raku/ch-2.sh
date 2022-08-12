9e5cb67ab6491c1efad9dfad054839759c41090d
Stephen Lynn <bizlsg@localhost.localdomain>
2022-08-13 07:55:14 +0800

    faster raku ch-2.sh

--- challenge-177/steve-g-lynn/raku/ch-2.sh
+++ challenge-177/steve-g-lynn/raku/ch-2.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#real	1m27.365s
-#user	1m25.656s
-#sys	0m1.164s
+#real	0m2.907s
+#user	0m3.050s
+#sys	0m0.047s
 
-raku -e '(101 ... 999,  slip(10001 ... 99999),slip(1000001 ... 9999999)).grep(*.is-prime).grep({($_.comb.Bag){"0"}==1}).grep({substr($_,$_.chars/2,1)==0}).grep({substr($_,0,(($_.chars)/2).floor) == substr($_.flip,0,(($_.chars)/2).floor) }).head(20).say'
+raku -e '(^Inf).grep(*.is-prime).grep(*.chars !%% 2 ).grep({$_.comb.sort.head(2).sum > 0}).grep({substr($_,$_.chars/2,1)==0}).grep({substr($_,0,(($_.chars)/2).floor) == substr($_.flip,0,(($_.chars)/2).floor) }).head(20).say'
 
 #(101 16061 31013 35053 38083 73037 74047 91019 94049 1120211 1150511
 # 1160611 1180811 1190911 1250521 1280821 1360631 1390931 1490941 1520251)
