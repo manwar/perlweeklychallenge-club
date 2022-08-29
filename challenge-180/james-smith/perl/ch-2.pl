43f0640e44b0f5a1c1ff2bc2aa7efb9e1d9d416e
drbaggy <js5@sanger.ac.uk>
2022-08-29 12:11:48 +0100

    add compact version of code

--- challenge-180/james-smith/perl/ch-2.pl
+++ challenge-180/james-smith/perl/ch-2.pl
@@ -5,8 +5,6 @@ use strict;
 use warnings;
 use feature qw(say);
 use Test::More;
-use Benchmark qw(cmpthese timethis);
-use Data::Dumper qw(Dumper);
 
 my @TESTS = (
   [ 3, [1,4,2,3,5], "4 5" ],
@@ -14,10 +12,13 @@ my @TESTS = (
 );
 
 is( "@{[ filter($_->[0],$_->[1]) ]}", $_->[2] ) foreach @TESTS;
+is( "@{[ f($_->[0],$_->[1])      ]}", $_->[2] ) foreach @TESTS;
 
 done_testing();
 
 sub filter {
-  grep { $_ > $_[0] } @{$_[1]};
+  grep { $_ > $_[0] } @{ $_[1] }
 }
+
+sub f{grep{$_>$_[0]}@{$_[1]}}
 
