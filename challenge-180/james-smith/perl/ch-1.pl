ecbc609030e30806045e98cacd9da2d3df2f4551
drbaggy <js5@sanger.ac.uk>
2022-08-30 08:10:50 +0100

    use frequency & added examples

--- challenge-180/james-smith/perl/ch-1.pl
+++ challenge-180/james-smith/perl/ch-1.pl
@@ -5,13 +5,18 @@ use strict;
 use warnings;
 use feature qw(say);
 use Test::More;
-use List::MoreUtils qw(firstidx);
+use List::MoreUtils qw(firstidx frequency);
 
 my @TESTS = (
   [ 'Perl Weekly Challenge',  0 ],
   [ 'Long Live Perl',         1 ],
   [ 'P.O.O.P',               -1 ],
   [ 'tractors',               2 ],
+  [ 'aabbccddeeffg',         12 ],
+  [ 'taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu',-1 ],
+  [ 'LLANFAIRPWLLGWYNGYLLGOGERYCHWYRNDROBWLLLLANTYSILIOGOGOGOCH', 4 ],
+  [ 'Krung Thep Mahanakhon Amon Rattanakosin Mahinthara Ayuthaya Mahadilok Phop Noppharat Ratchathani Burirom Udomratchaniwet Mahasathan Amon Piman Awatan Sathit Sakkathattiya Witsanukam Prasit', 0 ],
+  [ 'krung thep mahanakhon amon rattanakosin mahinthara ayuthaya mahadilok phop noppharat ratchathani burirom udomratchaniwet mahasathan amon piman awatan sathit sakkathattiya witsanukam prasit', 4 ],
 );
 
 is( first_unique($_->[0]), $_->[1] ) foreach @TESTS;
@@ -20,8 +25,7 @@ is( u($_->[0]),            $_->[1] ) foreach @TESTS;
 done_testing();
 
 sub first_unique {
-  my %counts;
-  $counts{$_}++ for my @p = split //, pop;
+  my %counts = frequency my @p = split //, pop;
   firstidx { $counts{$_} < 2 } @p;
 }
 
