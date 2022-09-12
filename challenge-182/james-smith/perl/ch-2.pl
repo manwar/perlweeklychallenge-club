e266dcdc46e5640ad62adc8473f73b2eac9a25ec
drbaggy <js5@sanger.ac.uk>
2022-09-12 07:02:42 +0100

    minor tweak to cope with relative directory paths (and a belt and braces of directories and filenames "matching")

--- challenge-182/james-smith/perl/ch-2.pl
+++ challenge-182/james-smith/perl/ch-2.pl
@@ -11,6 +11,11 @@ use Data::Dumper qw(Dumper);
 my @TESTS = (
   [ [qw(/a/b/c/1/x.pl /a/b/c/d/e/2/x.pl /a/b/c/d/3/x.pl /a/b/c/4/x.pl /a/b/c/d/5/x.pl)], '/a/b/c' ],
   [ [qw(/a/b/a.txt /a/b/c/d/e/q.txt /a/b/c.txt)], '/a/b' ],
+  [ [qw(a/b/a.txt a/b/c/d/e/q.txt a/b/c.txt)], 'a/b' ],
+  [ [qw(ab.txt ac.txt)], '' ],
+  [ [qw(ab ab/q.txt)], '' ],
+  [ [qw(/ab /ab/q.txt)], '' ],
+  [ [qw(ab/ ab/q.txt)], 'ab' ],
   [ [qw(/a/b.txt /c/d.txt)], '' ],
   [ [qw(/a/q.txt)], '/a' ],
   [ [qw(/aa/q.txt /ab/q.txt)], '' ],
@@ -26,6 +31,6 @@ sub common_path {
   ## Compute the common string....
   $l = substr $l, 0, length( (($_^$l) =~ m{^(\0+)})[0]) for @_;
   ## Remove the trailing "/"s
-  substr $l, 0, rindex $l, '/';
+  $l=~m{/} ? substr $l, 0, rindex $l, '/' : ''
 }
 
