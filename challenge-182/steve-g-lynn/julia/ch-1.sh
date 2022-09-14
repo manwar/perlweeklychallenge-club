56ef60a1b539bbcb13352a01de25c7a05f18c59c
Stephen Lynn <bizlsg@localhost.localdomain>
2022-09-15 04:16:48 +0800

    improved julia solution

--- challenge-182/steve-g-lynn/julia/ch-1.sh
+++ challenge-182/steve-g-lynn/julia/ch-1.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-julia -e 'for i in 1:length(ARGS); if parse(Float64,ARGS[i])==maximum(parse.(Float64,ARGS)); println(i-1); end; end;' 5 2 9 1 7 6
+julia -e 'println(argmax(ARGS)-1)' 5 2 9 1 7 6
 
 #2
 
-#julia -e 'for i in 1:length(ARGS); if parse(Float64,ARGS[i])==maximum(parse.(Float64,ARGS)); println(i-1); end; end;' 4 2 3 1 5 0
+#julia -e 'println(argmax(ARGS)-1)' 4 2 3 1 5 0
 
 #4
 
