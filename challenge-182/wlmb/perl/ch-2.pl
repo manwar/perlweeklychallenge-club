670cacc4cf2eb32a884e4de3ae9e419eb1cf1911
Luis Mochan <mochan@fis.unam.mx>
2022-09-16 12:06:35 -0500

    Canonicalize paths

--- challenge-182/wlmb/perl/ch-2.pl
+++ challenge-182/wlmb/perl/ch-2.pl
@@ -6,14 +6,26 @@
 use v5.36;
 use List::Util qw(all);
 use List::MoreUtils qw(zip6);
+use Cwd qw(getcwd);
 my $dirs;
 while(<>){
-    die "Not an absolute directory: $_" unless m(^\s*/);
+    canonical($_);
     my @parts=split '/';
     pop @parts;   # remove non-directory or empty at end
     $dirs=trim($dirs, @parts);
 }
 say join("/", @$dirs), "/";
+
+sub canonical{
+    for($_[0]){                         # localize $_
+        s{^\s*}{};                      # remove leading space
+        s{^([^/])}{getcwd . "/$1"}e;    # convert to absolute if relative
+	s{/\./}{/}g;                    # /a/./->/a/
+	1 while s{/[^/]*/\.\.(/|$)}{/}; # /a/b/../->/a/
+        s{/\.\.(/|$)}{/}g;              # /..->/ at beginning
+        1 while s{//}{/};               # //->/
+    }
+}
 
 sub trim($previous, @current){
     $previous=\@current unless defined $previous;
