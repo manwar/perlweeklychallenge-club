0ecdf995027b32199ecd525ecc8a86bfce077556
Kjetil S <kjetilskotheim@gmail.com>
2022-09-17 23:58:54 +0200

    https://theweeklychallenge.org/blog/perl-weekly-challenge-182/

--- challenge-182/kjetillll/perl/ch-2.pl
+++ challenge-182/kjetillll/perl/ch-2.pl
@@ -9,3 +9,18 @@ sub compath {
     my $l = max(map length,@_)-1;
     uniq(@_)==1 ? pop : compath( map s/(.{$l}).$/$1/r, @_ )
 }
+
+
+__END__
+
+Example commands:
+
+cat <<. > input.txt
+/a/b/c/1/x.pl
+/a/b/c/d/e/2/x.pl
+/a/b/c/d/3/x.pl
+/a/b/c/4/x.pl
+/a/b/c/d/5/x.pl
+.
+
+perl ch-2.pl input.txt            #prints /a/b/c/
