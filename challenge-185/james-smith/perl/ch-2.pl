3519104504da1a186f155fcff72b7103579538ac
drbaggy <js5@sanger.ac.uk>
2022-10-04 02:08:43 +0100

    renamed methods

--- challenge-185/james-smith/perl/ch-2.pl
+++ challenge-185/james-smith/perl/ch-2.pl
@@ -15,18 +15,18 @@ my @TESTS = (
   [ ['1234567.a',  'a-1234-bc',   'a.b.c.d.e.f' ], [ 'xxxx567.a',  'x-xxx4-bc',   'x.x.x.x.e.f' ] ],
 );
 
-is( "@{[ solreg(  @{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
-is( "@{[ solreghc(@{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
-is( "@{[ solsplit(@{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
+is( "@{[ mask_reg(  @{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
+is( "@{[ mask_reghc(@{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
+is( "@{[ mask_split(@{$_->[0]}) ]}", "@{$_->[1]}" ) for @TESTS;
 
 done_testing();
 cmpthese( 100_000, {
-  'reg'    => sub { solreg(   @{$_->[0]} ) for @TESTS; },
-  'reghc'  => sub { solreghc( @{$_->[0]} ) for @TESTS; },
-  'split'  => sub { solsplit( @{$_->[0]} ) for @TESTS; },
+  'reg'    => sub { mask_reg(   @{$_->[0]} ) for @TESTS; },
+  'reghc'  => sub { mask_reghc( @{$_->[0]} ) for @TESTS; },
+  'split'  => sub { mask_split( @{$_->[0]} ) for @TESTS; },
 });
 
-sub solreg   { map { s{$RE}{x$1x$2x$3x}or                                                                 } @_ }
-sub solreghc { map { s{[0-9a-z]([^0-9a-z]*)[0-9a-z]([^0-9a-z]*)[0-9a-z]([^0-9a-z]*)[0-9a-z]}{x$1x$2x$3x}r } @_ }
-sub solsplit { map { join 'x', split /[0-9a-z]/, $_, 5;                                                   } @_ }
+sub mask_reg   { map { s{$RE}{x$1x$2x$3x}or                                                                 } @_ }
+sub mask_reghc { map { s{[0-9a-z]([^0-9a-z]*)[0-9a-z]([^0-9a-z]*)[0-9a-z]([^0-9a-z]*)[0-9a-z]}{x$1x$2x$3x}r } @_ }
+sub mask_split { map { join 'x', split /[0-9a-z]/, $_, 5;                                                   } @_ }
 
