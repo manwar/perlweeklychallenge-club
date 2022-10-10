7fdb42e14a43c8cf62e5437607428eff037c31c0
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2022-10-10 19:17:23 +0100

    - Added guest contribution by Mohammad Meraj Zia.

---
+++ challenge-186/ziameraj16/java/ZipList.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ZipList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Mac address");
+        String macAddress = scanner.nextLine();
+        System.out.println(String.format("%s:%s:%s:%s:%s:%s", macAddress.substring(0, 2)
+                , macAddress.substring(2, 4), macAddress.substring(5, 7), macAddress.substring(7, 9)
+                , macAddress.substring(10, 12), macAddress.substring(12)));
+    }
+}
