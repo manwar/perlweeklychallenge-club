dd175ce8fbc9b484508b85b106f7806ba06450d6
Andrew Grangaard <andrew@malachiarts.com>
2022-10-30 15:26:56 -0700

    Tidy q1

--- challenge-188/spazm/perl/q1.pl
+++ challenge-188/spazm/perl/q1.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-
 use v5.30.0;
 
 =pod
@@ -21,17 +20,21 @@ This space could be saved by overwriting the input array.
 
 =cut
 
-sub count_matching_pairs {
-    my ($input, $size_n, $div_k) = @_;
+sub count_matching_pairs
+{
+    my ( $input, $size_n, $div_k ) = @_;
 
-    my @mods = map {$_ % $div_k} @$input;
+    my @mods = map { $_ % $div_k } @$input;
 
     my $count = 0;
 
-    for my $i (0..@$input -1) {
-        for my $j ($i + 1..@$input -1) {
+    for my $i ( 0 .. @$input - 1 )
+    {
+        for my $j ( $i + 1 .. @$input - 1 )
+        {
             my $test = $mods[$i] + $mods[$j];
-            if ($test == 0 or $test == $div_k ) {
+            if ( $test == 0 or $test == $div_k )
+            {
                 $count++;
             }
         }
@@ -39,17 +42,17 @@ sub count_matching_pairs {
     return $count;
 }
 
-
 use Test::More;
 my $test_data = [
-    [[4,5,1,6], 2 => 2],
-    [[1,2,3,4], 2 => 2],
-    [[1,3,4,5], 3 => 2],
-    [[5,1,2,3], 4 => 2],
-    [[7,2,4,5], 4 => 1],
+    [ [ 4, 5, 1, 6 ], 2 => 2 ],
+    [ [ 1, 2, 3, 4 ], 2 => 2 ],
+    [ [ 1, 3, 4, 5 ], 3 => 2 ],
+    [ [ 5, 1, 2, 3 ], 4 => 2 ],
+    [ [ 7, 2, 4, 5 ], 4 => 1 ],
 ];
-for my $test ( @$test_data ) {
-    my($in, $k, $expected) = @$test;
-    is count_matching_pairs($in, length(@$in), $k), $expected;
+for my $test (@$test_data)
+{
+    my ( $in, $k, $expected ) = @$test;
+    is count_matching_pairs( $in, length(@$in), $k ), $expected;
 }
 done_testing;
