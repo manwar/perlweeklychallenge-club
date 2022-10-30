d057a0c0a9ff1aefe3b08ce3486c7ca3dc6db0b6
Andrew Grangaard <andrew@malachiarts.com>
2022-10-30 15:24:45 -0700

    Week 187: Add perl solution for tast 2

---
+++ challenge-188/spazm/perl/q2.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/env perl
+
+use v5.30.0;
+
+=pod
+You are given two positive integers $x and $y.
+
+Write a script to find out the number of operations needed to make both ZERO. Each operation is made up either of the followings:
+
+$x = $x - $y if $x >= $y
+
+or
+
+$y = $y - $x if $y >= $x (using the original value of $x)
+
+=cut
+
+sub total_zero
+{
+    my ( $x, $y ) = @_;
+
+    my $count = 0;
+    while ( $x > 0 or $y > 0 )
+    {
+        $count++;
+        if ( $x == $y )
+        {
+            return $count;
+        }
+        elsif ( $x > $y )
+        {
+            $x -= $y;
+        }
+        else
+        {
+            $y -= $x;
+        }
+    }
+    return;
+}
+
+sub total_zero_recursive
+{
+    my ( $x, $y, $count ) = @_;
+    return 1 + (
+          $x > $y ? total_zero_recursive( $x - $y, $y )
+        : $x < $y ? total_zero_recursive( $x,      $y - $x )
+        : 0
+    );
+}
+
+use Test::More;
+my $test_data = [
+    [ ( 5, 4 ) => 5 ],
+    [ ( 4, 6 ) => 3 ],
+    [ ( 2, 5 ) => 4 ],
+    [ ( 3, 1 ) => 3 ],
+    [ ( 7, 4 ) => 5 ],
+];
+for my $test (@$test_data)
+{
+    my ( $x, $y, $expected ) = @$test;
+    say "# $x, $y => $expected";
+    my $count = total_zero( $x, $y );
+    is $count, $expected, "got $count expected $expected";
+    my $count = total_zero_recursive( $x, $y );
+    is $count, $expected, "got $count expected $expected";
+}
+done_testing;
