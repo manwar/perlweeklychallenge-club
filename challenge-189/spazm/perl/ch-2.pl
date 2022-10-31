801c2100bf461b4cd38b0f34dd1a83a65d25036f
Andrew Grangaard <granny-github@ofb.net>
2022-10-31 13:43:41 -0700

    Remove dead code and describe remaining

--- challenge-189/spazm/perl/ch-2.pl
+++ challenge-189/spazm/perl/ch-2.pl
@@ -6,85 +6,49 @@ use experimental qw/signatures/;
 
 sub array_degree (@input)
 {
-    say @input;
     my %cnt;
-    my @max;
-    my $len = 0;
     my %loc;
     my $min_len = scalar @input;
-    my ( $max, $max_c ) = 0, undef;
+    my $max     = 0;
     for my $i ( 0 .. $#input )
     {
         my $c = $input[$i];
+
+        # build a mapping of input element to count
         $cnt{$c}++;
-        if ( $cnt{$c} > $max )
-        {
-            $max = $cnt{$c};
-        }
+        $max = $cnt{$c} if ( $cnt{$c} > $max );
+
+# build a mapping of input element to array of indexes where that element is found
+# this could be tightened up by keeping only the first and last element
         push @{ $loc{$c} }, $i;
-        if ( $max_c == $c )
-        {
-            $max++;
-            $len++;
-        }
-        elsif ( $cnt{$c} > $max )
-        {
-            $max   = $cnt{$c};
-            $max_c = $c;
-            $len   = 1;
-        }
-        else
-        {
-            $len++;
-        }
-        $max[$i] = [ $max, [ $max_c, $c ], $len ];
     }
-    my @maxes;
     my @sol_index;
     while ( my ( $key, $value ) = each %cnt )
     {
         if ( $value == $max )
         {
+         # For all input elements appearing max times,
+         # find the shortest delta between first and last appearance.
+         # Minimal solutions will extend from first to last of a given element
             my $loc = $loc{$key};
             my ( $first, $last ) = ( $loc->[0], $loc->[-1] );
             my $len = $last - $first;
-            say Dumper {
-                loc     => $loc,
-                first   => $first,
-                last    => $last,
-                len     => $len,
-                min_len => $min_len
-            };
             if ( $len < $min_len )
             {
-                say "new min len: $len";
                 $min_len   = $len;
                 @sol_index = ( $first, $last );
             }
-            say Dumper { min_len => $min_len, sol => \@sol_index };
         }
-        push @maxes, $key if $value == $max;
     }
-    say @input;
-
-    #say Dumper \@max;
-    say Dumper \%loc;
-    say Dumper \%cnt;
-    say $max;
-    say Dumper @maxes;
-    my @answer = @input[@sol_index];
-    say Dumper { min_len => $min_len, indices => \@sol_index,
-        answer => \@answer };
-    return @answer;
+    @input[ $sol_index[0] .. $sol_index[1] ];
 }
 
-array_degree( 1, 3, 3, 2, 1 );
 {
     use Test::More;
-    is_deeply \array_degree( 1, 3, 3, 2 ), [ 3, 3 ];
-    is_deeply \array_degree( 1, 2, 1, 3 ), [ 1, 2, 1 ];
-    is_deeply \array_degree( 1, 3, 2, 1, 2 ), [ 3, 3 ];
-    is_deeply \array_degree( 1, 1, 2, 3, 2 ), [ 3, 3 ];
-    is_deeply \array_degree( 2, 1, 2, 1, 2 ), [ 1, 2, 1, 1 ];
+    is_deeply [ array_degree( 1, 3, 3, 2 ) ], [ 3, 3 ];
+    is_deeply [ array_degree( 1, 2, 1, 3 ) ], [ 1, 2, 1 ];
+    is_deeply [ array_degree( 1, 3, 2, 1, 2 ) ], [ 2, 1, 2 ];
+    is_deeply [ array_degree( 1, 1, 2, 3, 2 ) ], [ 1, 1 ];
+    is_deeply [ array_degree( 2, 1, 2, 1, 1 ) ], [ 1, 2, 1, 1 ];
     done_testing;
 }
