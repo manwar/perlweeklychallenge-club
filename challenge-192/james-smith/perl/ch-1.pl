c168e00d304e7ae57f59012beb6a94ef43bce7eb
James Smith <js5@sanger.ac.uk>
2022-11-21 10:06:53 +0000

    Update ch-1.pl

--- challenge-192/james-smith/perl/ch-1.pl
+++ challenge-192/james-smith/perl/ch-1.pl
@@ -11,7 +11,12 @@ use Data::Dumper qw(Dumper);
 my @TESTS = (  [5,2],[4,3],[6,1] );
 
 is( binary_flip( $_->[0] ), $_->[1] ) for @TESTS;
+is( string_flip( $_->[0] ), $_->[1] ) for @TESTS;
 done_testing();
+
+sub string_flip {
+  oct '0b'.sprintf('%b',$_[0])=~tr/01/10/r;
+}
 
 sub binary_flip {
   my($r,$k,$n) = (0,1,shift);
