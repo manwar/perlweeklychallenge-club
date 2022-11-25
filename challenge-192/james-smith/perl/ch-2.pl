0b7b82018ea2351ea9e141ffcbc74595f277a83e
James Smith <js5@sanger.ac.uk>
2022-11-25 23:18:04 +0000

    Update ch-2.pl

--- challenge-192/james-smith/perl/ch-2.pl
+++ challenge-192/james-smith/perl/ch-2.pl
@@ -11,6 +11,7 @@ use Data::Dumper qw(Dumper);
 my @TESTS = ( [ [1,0,5], 4 ], [ [0,2,0],-1], [ [0,3,0], 2 ] );
 
 is( equal_dis(  @{$_->[0]} ), $_->[1] ) for @TESTS;
+is( equal_dis2(  @{$_->[0]} ), $_->[1] ) for @TESTS;
 done_testing();
 
 sub equal_dis {
@@ -19,5 +20,14 @@ sub equal_dis {
   return -1 if $av%@_;
   $av/=@_;
   $k+=abs($av-$_[0]),$_[1]-=$av-shift while @_>1;
+  $k;
+}
+
+sub equal_dis2 {
+  my($av,$k,$f) = (0,0,$_[0]);
+  $av+=$_ for @_;
+  return -1 if $av%@_;
+  $av/=@_;
+  $k+=abs($av-$f),$f+=$_[$_]-$av,warn ">$_,$f,$k" for 1..$#_;
   $k;
 }
