9dfe91c69289ef2a9e954f325b7cda8c4fbc9e26
Stephen Lynn <bizlsg@localhost.localdomain>
2022-11-29 05:53:19 +0800

    minor edits

--- challenge-193/steve-g-lynn/julia/ch-1.sh
+++ challenge-193/steve-g-lynn/julia/ch-1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-julia -e 'using Printf;for i in (parse(Int64,"0b"*repeat("0",parse(Int64,ARGS[1]))):parse(Int64,"0b"*repeat("1",parse(Int64,ARGS[1])))); println(SubString(bitstring(i),65-parse(Int64,ARGS[1]))); end;' $@
+julia -e 'for i in (parse(Int64,"0b"*repeat("0",parse(Int64,ARGS[1]))):parse(Int64,"0b"*repeat("1",parse(Int64,ARGS[1])))); println(SubString(bitstring(i),65-parse(Int64,ARGS[1]))); end;' $@
 
