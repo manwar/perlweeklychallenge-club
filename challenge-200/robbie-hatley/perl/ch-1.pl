f1249991c0a493ec1340df80d1e5b4b1dc4db05a
LoneWolfiNTj <Hatley.Software@gmail.com>
2023-01-22 11:28:08 -0800

    Robbie Hatley's Perl solutions to Weekly Challenge #200.

---
+++ challenge-200/robbie-hatley/perl/ch-1.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/perl
+# Robbie Hatley's Perl solution to The Weekly Challenge #200-1
+
+=pod
+
+Task 1: Arithmetic Slices
+Submitted by: Mohammad S Anwar
+You are given an array of integers. Write a script to find out all Arithmetic
+Slices for the given array of integers. An integer array is called "arithmetic"
+if it has at least 3 elements and the differences between any three
+consecutive elements are the same.
+
+Example 1:  Input: @array = (1,2,3,4)  Output: (1,2,3), (2,3,4), (1,2,3,4)
+Example 2:  Input: @array = (2)        Output: () as no slice found.
+
+=cut
+
+# NOTE: Input is by either built-in array of arrays, or by @ARGV. If using
+#       @ARGV, input should be a space-separated list of integers, which will
+#       be interpreted as a single array.
+
+# NOTE: Output is to stdout and will be a list of all arithmetic slice for
+#       each input array.
+
+# PRELIMINARIES:
+use v5.36;
+
+say "Sorry, this is just a stub; ran out of time.";
+say "\"Task 2\", however, is fully functional.";
