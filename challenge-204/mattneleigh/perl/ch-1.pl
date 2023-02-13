af7c882a4b596eb5cfad61f6195d76d1faca4cce
Matthew Neleigh <matthew.neleigh@gmail.com>
2023-02-13 16:16:07 -0500

    new file:   challenge-204/mattneleigh/perl/ch-1.pl 	new file:   challenge-204/mattneleigh/perl/ch-2.pl

---
+++ challenge-204/mattneleigh/perl/ch-1.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use English;
+
+################################################################################
+# Begin main execution
+################################################################################
+
+my @arrays = (
+    # Given cases
+    [ 1, 2, 2, 3 ],
+    [ 1, 3, 2 ],
+    [ 6, 5, 5, 4 ],
+
+    # Additional test cases
+    [ 2, 2, 2, 4 ],
+    [ 2, 2, 2, 0 ],
+    [ 4, 2, 2, 2 ],
+    [ 0, 2, 2, 2 ]
+);
+
+print("\n");
+foreach my $array (@arrays){
+    printf(
+        "Input: \@nums = (%s)\nOutput: %d\n\n",
+        join(", ", @{$array}),
+        array_is_monotonic(@{$array})
+    );
+}
+
+exit(0);
+################################################################################
+# End main execution; subroutines follow
+################################################################################
+
+
+
+################################################################################
+# Determine whether an array of numbers is monotonic- that is to say:
+# for every i <= j, array[i] <= array[j] (monotone increasing)
+# - OR -
+# for every i <= j, array[i] >= array[j] (monotone decreasing)
+# Takes one argument:
+# * The list to examine
+# Returns:
+# * 1 if the array is monotonic, as defined above
+# * 0 if the array is not monotonic
+# NOTE: this function does not differentiate between the two kinds of
+# monotonicity
+################################################################################
+sub array_is_monotonic{
+
+    my $i = 0;
+    my $ascending = 0;
+
+    # Loop over the array, examining each pair
+    # of values- pairs of equal values don't
+    # preclude monotonicity of either type so
+    # they're effectively ignored
+    for my $i (1 .. $#ARG){
+        if($ARG[$i - 1] < $ARG[$i]){
+            # An ascending pair
+            if($ascending < 0){
+                # $ascending was less than zero-
+                # we'd seen descending values before
+                return(0);
+            }
+            $ascending = 1;
+        }
+        if($ARG[$i - 1] > $ARG[$i]){
+            # A descending pair
+            if($ascending > 0){
+                # $ascending was greater than zero-
+                # we'd seen ascending values before
+                return(0);
+            }
+            $ascending = -1;
+        }
+    }
+
+    return(1);
+    
+}
+
+
+
---
+++ challenge-204/mattneleigh/perl/ch-2.pl
@@ -0,0 +1,160 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use English;
+
+################################################################################
+# Begin main execution
+################################################################################
+
+my @matrix_data = (
+    {
+        matrix => [
+            [ 1, 2 ],
+            [ 3, 4 ]
+        ],
+        r => 1,
+        c => 4
+    },
+    {
+        matrix => [
+            [ 1, 2, 3 ],
+            [ 4, 5, 6 ]
+        ],
+        r => 3,
+        c => 2
+    },
+    {
+        matrix => [
+            [ 1, 2 ]
+        ],
+        r => 3,
+        c => 2
+    }
+);
+
+print("\n");
+foreach my $matrix_instance (@matrix_data){
+    my $rearranged_matrix = reshape_matrix(
+        $matrix_instance->{matrix},
+        $matrix_instance->{r},
+        $matrix_instance->{c}
+    );
+
+    printf(
+        "\$matrix = %s\n\$r = %d\n\$c = %d\nOutput: %s\n\n",
+        matrix_to_string($matrix_instance->{matrix}),
+        $matrix_instance->{r},
+        $matrix_instance->{c},
+        defined($rearranged_matrix) ?
+            matrix_to_string($rearranged_matrix)
+            :
+            "0"
+    );
+}
+
+exit(0);
+################################################################################
+# End main execution; subroutines follow
+################################################################################
+
+
+
+################################################################################
+# Reshape a matrix into a new rectangular matrix (i.e. all rows have the same
+# number of columns) of a specified set of dimensions
+# Takes three arguments:
+# * A reference to a matrix, which must consist of an array reference to a two
+#   dimensional array, e.g.
+#
+#   [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]
+#
+#   This must be the case even for a matrix with a single row, e.g.
+#
+#   [ [ 1, 2, 3, 4 ] ]
+#
+# * The number of rows the reshaped matrix is to have
+# * The number of columns the reshaped matrix is to have
+# Returns on success:
+# * A ref to a reshaped matrix into which data from the original has been
+#   copied, now arranged in the specified number of rows and columns; the
+#   internal arrangement of this matrix will meet the requirements specified
+#   above
+# Returns on error:
+# * undef if the quantity of elements in the original matrix does not permit
+#   rearrangement into a rectangular matrix of the specified dimensions
+# NOTE: The original matrix will NOT be altered
+################################################################################
+sub reshape_matrix{
+    my $matrix = shift();
+    my $r = shift();
+    my $c = shift();
+
+    my @elements;
+
+    # Copy the contents of the matrix into a
+    # flat list
+    foreach my $row (@{$matrix}){
+        push(@elements, @{$row});
+    }
+
+    # Now that we know how many elements there
+    # are, make sure we can produce a rectangular
+    # matrix of the specified dimensions
+    return(undef)
+        unless((scalar(@elements) / $r) == $c);
+
+    # Re-use $matrix to build the output matrix,
+    # transferring the contents from the flat
+    # list row by row
+    $matrix = [];
+    for(1 .. $r){
+        push(@{$matrix}, [ splice(@elements, 0, $c) ]);
+    }
+
+    return($matrix);
+
+}
+
+
+
+################################################################################
+# Produce a string representation of a matrix.
+# Takes one argument:
+# * A reference to a matrix, which must consist of an array reference to a two
+#   dimensional array, e.g.
+#
+#   [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ] ]
+#
+#   This must be the case even for a matrix with a single row, e.g.
+#
+#   [ [ 1, 2, 3, 4 ] ]
+#
+# Returns:
+# * A string representation of the specified matrix, which matches the format
+#   of the examples presented above
+################################################################################
+sub matrix_to_string{
+    my $matrix = shift();
+
+    return(
+        sprintf(
+            "[ %s ]",
+            join(
+                ", ",
+                map(
+                    sprintf(
+                        "[ %s ]",
+                        join(", ", @{$_})
+                    ),
+                    @{$matrix}
+                )
+            )
+        )
+    );
+
+}
+
+
+
