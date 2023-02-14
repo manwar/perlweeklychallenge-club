6e03a5fd3a9c0b8f9846d50e1a2e4c96cf3a931c
James Smith <js5@sanger.ac.uk>
2023-02-14 18:53:20 +0000

    Update ch-2.pl

--- challenge-204/james-smith/perl/ch-2.pl
+++ challenge-204/james-smith/perl/ch-2.pl
@@ -7,23 +7,29 @@ use Test::More;
 use Benchmark qw(cmpthese timethis);
 use List::Util qw(sum0);
 
-my @TESTS = (
-  [ [ 1,2,2,3       ], 1 ],
-  [ [ 1,3,2         ], 0 ],
-  [ [ 6,5,5,4       ], 1 ],
-  [ [ 1,1,1,1,1,1,1 ], 1 ],
-  [ [ 1,1,1,1,2,2,2 ], 1 ],
-  [ [ 1,1,1,1,2,1,1 ], 0 ],
-  [ [ 1,1,1,1,0,0,0 ], 1 ],
-  [ [ 1,1,1,1,0,1,1 ], 0 ],
-);
-
-is( monotonic( @{$_->[0]} ), $_->[1] ) for @TESTS;
-
-done_testing();
-
-sub monotonic {
-  my ( $delta, $tmp ) = ( shift ) <=> ( my $next = shift );
-  ( $tmp = $next<=>$_[0] ) && ( $delta||=$tmp ) != $tmp ? ( return 0 ) : ( $next = shift ) while @_;
-  1;
+sub reshape {
+  my($m,$r,$c) = @_;
+  return 0 unless @{$m}*@{$m->[0]} == $r * $c;
+  my @t = map { @{$_} } @{$m};
+  [ map { [ splice @t, 0, $c ] } 1..$r ];
 }
+
+sub reshape2 {
+  my($m,$r,$c) = @_;
+  my $R=@{$m}; my $C = @{$m->[0]};
+  return 0 unless (my $R=@{$m}) * (my $C=@{$m->[0]}) == $r * $c;
+  my($x,$y,$res)=(0,0);
+  for my $i (1..$r) {
+    my $row = [];
+    my $to_push = $c;
+    while( $to_push ) {
+      my $t = $to_push+$x<$C?$to_push+$x:$C;
+      push @{$row},@{$m->[$y]}[$x..$t-1];
+      $to_push -= $t-$x;
+      $t==$C ? ($y++,$x=0) : ($x=$t);
+    }
+    push @{$res},$row;
+  }
+  $res;
+}
+
