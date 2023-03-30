eef40563a50c1ebba58b404f2f9089c726e4aa0b
Paulo Custodio <pauloscustodio@gmail.com>
2023-03-30 16:06:27 +0100

    Add Perl solution

---
+++ challenge-171/paulo-custodio/perl/ch-2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# Challenge 171
+#
+# Task 2: First-class Function
+# Submitted by: Mohammad S Anwar
+#
+# Create sub compose($f, $g) which takes in two parameters $f and $g as
+# subroutine refs and returns subroutine ref i.e. compose($f, $g)->($x) =
+# $f->($g->($x))
+#
+# e.g.
+#
+# $f = (one or more parameters function)
+# $g = (one or more parameters function)
+#
+# $h = compose($f, $g)
+# $f->($g->($x,$y, ..)) == $h->($x, $y, ..) for any $x, $y, ...
+
+use Modern::Perl;
+
+sub compose {
+    my($f, $g) = @_;
+    return sub { return $f->($g->(@_)); };
+}
+
+sub times3 { return 3*$_[0]; }
+sub times5 { return 5*$_[0]; }
+my $h = compose(\&times3, \&times5);
+say $h->(shift);
