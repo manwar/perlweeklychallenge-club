8fbeb9ce09a58562610026884b60e8489243b897
Paulo Custodio <pauloscustodio@gmail.com>
2023-03-31 21:52:24 +0100

    Add Perl solution

---
+++ challenge-184/paulo-custodio/perl/ch-1.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+
+# Challenge 184
+#
+# Task 1: Sequence Number
+# Submitted by: Mohammad S Anwar
+#
+# You are given list of strings in the format aa9999 i.e. first 2 characters can
+# be anything 'a-z' followed by 4 digits '0-9'.
+#
+# Write a script to replace the first two characters with sequence starting with
+# '00', '01', '02' etc.
+# Example 1
+#
+# Input: @list = ( 'ab1234', 'cd5678', 'ef1342')
+# Output: ('001234', '015678', '021342')
+#
+# Example 2
+#
+# Input: @list = ( 'pq1122', 'rs3334')
+# Output: ('001122', '013334')
+
+use Modern::Perl;
+
+sub sequence {
+    my(@in) = @_;
+    my %keys;
+    @in = map {/([a-z]+)(\d+)/ and [$1, $2]} @in;
+    for (@in) { $keys{$_->[0]}=0; }
+    my $id = 0; for (sort keys %keys) { $keys{$_} = $id++; }
+    @in = sort map {sprintf("%02d%s", $keys{$_->[0]}, $_->[1])} @in;
+    return @in;
+}
+
+@ARGV or die "usage: ch-1.pl list...\n";
+say join " ", sequence(@ARGV);
---
+++ challenge-184/paulo-custodio/perl/ch-2.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+# Challenge 184
+#
+# Task 2: Split Array
+# Submitted by: Mohammad S Anwar
+#
+# You are given list of strings containing 0-9 and a-z separated by space only.
+#
+# Write a script to split the data into two arrays, one for integers and one for
+# alphabets only.
+# Example 1
+#
+# Input: @list = ( 'a 1 2 b 0', '3 c 4 d')
+# Output: [[1,2,0], [3,4]] and [['a','b'], ['c','d']]
+#
+# Example 2
+#
+# Input: @list = ( '1 2', 'p q r', 's 3', '4 5 t')
+# Output: [[1,2], [3], [4,5]] and [['p','q','r'], ['s'], ['t']]
+
+use Modern::Perl;
+
+sub split_one {
+    my(@in) = @_;
+    my(@num, @letters);
+    for (@in) {
+        if (/^\d+$/) {
+            push @num, $_;
+        }
+        else {
+            push @letters, $_;
+        }
+    }
+    return \@num, \@letters;
+}
+
+sub split_array {
+    my(@a) = @_;
+    my(@num, @letters);
+    for (@a) {
+        my($this_num, $this_letters) = split_one(split ' ', $_);
+        push @num, $this_num if @$this_num;
+        push @letters, $this_letters if @$this_letters;
+    }
+    my @result = (\@num, \@letters);
+    return @result;
+}
+
+sub print_result {
+    my($num, $letters) = @_;
+    print "[", join(", ", map {"[".join(",", @$_)."]"} @$num), "]";
+    print " and ";
+    print "[", join(", ", map {"[".join(",", map {"'$_'"} @$_)."]"} @$letters), "]";
+    print "\n";
+}
+
+@ARGV or die "usage: ch-2.pl list...\n";
+print_result(split_array(@ARGV));
