f4adf3185025eae906e2c9b33e8e016e2393b6ee
Niels van Dijke <perlboy@cpan.org>
2023-05-01 12:08:27 +0000

    Task 2 - Minor optimization

--- challenge-215/perlboy1967/perl/ch2.pl
+++ challenge-215/perlboy1967/perl/ch2.pl
@@ -25,7 +25,7 @@ use Test::More;
 sub numberPlacement ($@) {
   my ($n,$s) = (shift @_,join '',@_);
 
-  $n-- while ($s =~ s#000#010#);
+  $n-- while ($n and $s =~ s#000#010#);
 
   $n == 0 ? 1 : 0;
 }
@@ -33,6 +33,7 @@ sub numberPlacement ($@) {
 is(numberPlacement(1,1,0,0,0,1),1);
 is(numberPlacement(2,1,0,0,0,1),0);
 is(numberPlacement(3,1,0,0,0,0,0,0,0,1),1);
+is(numberPlacement(0),1);
 is(numberPlacement(1),0);
 is(numberPlacement(1,0),0);
 is(numberPlacement(1,0,0),0);
