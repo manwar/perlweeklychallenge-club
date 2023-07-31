9a0c1c66436b9453f517c547d9ac9418f93336d9
Bob Lied <boblied+github@gmail.com>
2023-07-31 15:52:56 -0500

    Checkpoint

---
+++ challenge-174/bob-lied/perl/ch-2.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# ch-2.pl Perl Weekly Challenge Task 2 Permutation Ranking 
+#=============================================================================
+# Copyright (c) 2023, Bob Lied
+#=============================================================================
+# You are given a list of integers with no duplicates, e.g. [0, 1, 2].
+# Write two functions, permutation2rank() which will take the list and
+# determine its rank (starting at 0) in the set of possible permutations
+# arranged in lexicographic order, and rank2permutation() which will take
+# the list and a rank number and produce just that permutation.
+# Please checkout this post for more informations and algorithm.
+# https://tryalgo.org/en/permutations/2016/09/05/permutation-rank
+#
+# Example: Given the list [0, 1, 2] the ordered permutations are:
+#   0: [0, 1, 2]
+#   1: [0, 2, 1]
+#   2: [1, 0, 2]
+#   3: [1, 2, 0]
+#   4: [2, 0, 1]
+#   5: [2, 1, 0]
+# and therefore:
+#   permutation2rank([1, 0, 2]) = 2
+#   rank2permutation([0, 1, 2], 1) = [0, 2, 1]
+#=============================================================================
+
+use v5.36;
+
+use Getopt::Long;
+my $Verbose = 0;
+my $DoTest  = 0;
+
+GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
+exit(!runTest()) if $DoTest;
+
+say permutation2rank(\@ARGV);
+
+# How many CPU cycles have been wasted re-computing factorials?
+my @Factorial = ( 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800 );
+
+sub permutation2rank($list)
+{
+    my @sortedOrder = sort { $list->[$a] <=> $list->[$b] } 0 .. $list->$#*;
+
+    my %place = map { $list->[$sortedOrder[$_]] => $_ } 0 .. $list->$#*;
+
+    say "SORT: [@$list]->[@sortedOrder]" if $Verbose;
+    if ($Verbose) { say "PLACE: $_ => $place{$_}" for sort keys %place };
+}
+
+sub rank2permutation($list, $rank)
+{
+}
+
+sub runTest
+{
+    use Test2::V0;
+
+    is( permutation2rank( [ 1, 0, 2 ] ),    2,         "Example p2r");
+    is( rank2permutaiono( [ 0, 1, 2 ], 1 ), [0, 2, 1], "Example r2p");
+
+    done_testing;
+}
