edd8d867978d3a7afefe3c263ee82dba7297e6ed
robbie-hatley <Hatley.Software@gmail.com>
2023-08-01 13:47:44 -0700

    228-1

---
+++ challenge-228/robbie-hatley/perl/ch-1.pl
@@ -0,0 +1,96 @@
+#! /bin/perl -CSDA
+
+=pod
+
+--------------------------------------------------------------------------------------------------------------
+COLOPHON:
+This is a 110-character-wide Unicode UTF-8 Perl-source-code text file with hard Unix line breaks ("\x0A").
+¡Hablo Español! Говорю Русский. Björt skjöldur. ॐ नमो भगवते वासुदेवाय. 看的星星，知道你是爱。麦藁雪、富士川町、山梨県。
+
+--------------------------------------------------------------------------------------------------------------
+TITLE BLOCK:
+Solutions in Perl for The Weekly Challenge 228-1.
+Written by Robbie Hatley on Tue Aug 01, 2023.
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM DESCRIPTION:
+Task 1: Unique Sum
+Submitted by: Mohammad S Anwar
+
+You are given an array of integers.
+Write a script to find out the sum of unique elements in the given array.
+
+Example 1:  Input: @int = (2, 1, 3, 2)  Output: 4
+In the given array we have 2 unique elements (1, 3).
+
+Example 2:  Input: @int = (1, 1, 1, 1)  Output: 0
+In the given array no unique element found.
+
+Example 3:  Input: @int = (2, 1, 3, 4)  Output: 10
+In the given array every element is unique.
+
+--------------------------------------------------------------------------------------------------------------
+PROBLEM NOTES:
+To solve this problem, I'll make a hash of the number of instances of each element in an array, then map a sum
+of just those hash keys with values of exactly 1.
+
+--------------------------------------------------------------------------------------------------------------
+IO NOTES:
+Input is via either built-in variables or via @ARGV. If using @ARGV, provide one argument which must be a
+double-quoted array of arrays of integers, in proper Perl syntax, like so:
+./ch-1.pl "([1,-4,2,-2,-4,-5,7,-3],[17,3,76,3,7,3])"
+
+Output is to STDOUT and will be each input array followed by the corresponding output.
+
+=cut
+
+# ------------------------------------------------------------------------------------------------------------
+# PRELIMINARIES:
+
+use v5.36;
+use strict;
+use warnings;
+use utf8;
+use Sys::Binmode;
+use Time::HiRes 'time';
+$"=', ';
+
+# ------------------------------------------------------------------------------------------------------------
+# SUBROUTINES:
+
+sub sum_unique ($aref) {
+   my %ab;                                      # Hash to hold element abundances.
+   for ( @$aref ) {++$ab{$_}}                   # Record how many copies of each element exist.
+   my $sum = 0;                                 # Variable to hold sum of unique elements
+   map { $sum += $_ if 1 == $ab{$_} } keys %ab; # Sum of 1-of-a-kind elements.
+   return $sum;                                 # Return sum.
+}
+
+# ------------------------------------------------------------------------------------------------------------
+# MAIN BODY OF PROGRAM:
+
+# Start timer:
+my $t0 = time;
+
+# Default inputs:
+my @arrays =
+(
+   [2, 1, 3, 2],
+   [1, 1, 1, 1],
+   [2, 1, 3, 4],
+);
+
+# Non-default inputs:
+@arrays = eval($ARGV[0]) if @ARGV;
+
+# Main loop:
+for my $aref (@arrays) {
+   say '';
+   my $sum = sum_unique($aref);
+   say "Array = (@$aref)";
+   say "Sum of unique elements = $sum";
+}
+
+# Determine and print execution time:
+my $µs = 1000000 * (time - $t0);
+printf("\nExecution time was %.0fµs.\n", $µs);
