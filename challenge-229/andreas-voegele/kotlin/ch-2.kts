9a8015ea2f6f5ecf61d9edad9343388ad1b4fc59
Andreas Vögele <andreas@andreasvoegele.com>
2023-08-07 14:55:51 +0200

    Challenge 229 by Andreas Vögele

---
+++ challenge-229/andreas-voegele/kotlin/ch-2.kts
@@ -0,0 +1,28 @@
+#!/usr/bin/env kotlin
+
+/*
+ * You are given three arrays of integers.  Write a script to return all the
+ * elements that are present in at least 2 out of 3 given arrays.
+ */
+
+typealias BitSet = Int
+
+operator fun BitSet?.plus(other: BitSet) = this?.plus(other) ?: other
+
+fun twoOutOfThree(vararg arrays: IntArray): List<Int> {
+    val bitSetFor = mutableMapOf<Int, BitSet>()
+    arrays.forEachIndexed { i, array ->
+        array.forEach {
+            bitSetFor[it] += 1 shl i
+        }
+    }
+    return bitSetFor.filterValues { it.countOneBits() > 1 }.keys
+        .toList().sorted()
+}
+
+println(
+    twoOutOfThree(intArrayOf(1, 1, 2, 4), intArrayOf(2, 4), intArrayOf(4)),
+)
+println(
+    twoOutOfThree(intArrayOf(4, 1), intArrayOf(2, 4), intArrayOf(1, 2)),
+)
