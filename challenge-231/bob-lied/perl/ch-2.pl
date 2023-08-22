f77e2c4a29d5d739726749217612b06cd1df5ead
Bob Lied <boblied+github@gmail.com>
2023-08-21 21:46:12 -0500

    Task 2 with unpack

--- challenge-231/bob-lied/perl/ch-2.pl
+++ challenge-231/bob-lied/perl/ch-2.pl
@@ -31,12 +31,24 @@ sub seniorCitizen(@passenger)
     return scalar grep { $_ >= 60 } map { substr($_, 11, 2) } @passenger;
 }
 
+sub sc_pack(@passenger)
+{
+    return scalar grep {
+        #my ($phone, $sex, $age, $seat) = unpack("A10 A1 A2 A2", $_);
+        #$age >= 60;
+        (unpack("A10 A1 A2 A2", $_))[2] >= 60
+    } @passenger
+}
+
 sub runTest
 {
     use Test2::V0;
 
     is( seniorCitizen( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
     is( seniorCitizen( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
+
+    is( sc_pack( qw( 7868190130M7522 5303914400F9211 9273338290F4010) ), 2, "Example 1");
+    is( sc_pack( qw( 1313579440F2036 2921522980M5644) ), 0, "Example 2");
 
     done_testing;
 }
