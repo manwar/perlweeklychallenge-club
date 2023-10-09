5966a37da10a8d575ce14270e7c4372f8d613666
Steven Wilson <steven1170@zoho.eu>
2023-10-09 14:51:13 +0100

    another edge case fix

--- challenge-235/steven-wilson/python/ch-01.py
+++ challenge-235/steven-wilson/python/ch-01.py
@@ -11,12 +11,14 @@ def remove_one(elements):
     True
     >>> remove_one( [1, 2, 3, 1, 2, 3] )
     False
+    >>> remove_one( [1, 2, 3, 1])
+    True
     """
     remove = 0
     for i in range(len(elements) - 1):
         if(elements[i] > elements[i+1]):
             remove += 1
-            if (i > 0) and (elements[i-1] > elements[i+1]):
+            if (i > 0) and (i < len(elements) - 2) and (elements[i-1] > elements[i+1]):
                 remove += 1
             if remove > 1:
                 return False
