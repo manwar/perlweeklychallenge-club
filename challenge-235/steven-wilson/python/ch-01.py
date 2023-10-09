34a882b97947ad6bb1c13273e228e64a20a93d9b
Steven Wilson <steven1170@zoho.eu>
2023-10-09 13:00:06 +0100

    fix for week 235 task 1

--- challenge-235/steven-wilson/python/ch-01.py
+++ challenge-235/steven-wilson/python/ch-01.py
@@ -9,12 +9,16 @@ def remove_one(elements):
     False
     >>> remove_one( [2, 2, 3] )
     True
+    >>> remove_one( [1, 2, 3, 1, 2, 3] )
+    False
     """
     remove = 0
     for i in range(len(elements) - 1):
         if(elements[i] > elements[i+1]):
             remove += 1
-            if remove == 2:
+            if (i > 0) and (elements[i-1] > elements[i+1]):
+                remove += 1
+            if remove > 1:
                 return False
     return True
 
