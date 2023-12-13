cead54566ca58e2838a17609bae24938dfc3057f
Stephen Lynn <bizlsg@localhost.localdomain>
2023-12-13 21:30:03 +0800

    ch-2 bugfix

--- challenge-247/steve-g-lynn/perl/ch-2.pl
+++ challenge-247/steve-g-lynn/perl/ch-2.pl
@@ -6,7 +6,7 @@ local *most_frequent_letter_pair = sub {
     foreach (0 .. (length($s)-2)) {
         $s{substr($s,$_,2)}++;
     }
-    return (sort { ($s{$b} <=> $s{$a}) && ($a cmp $b)} keys %s) [0];
+    return (sort { ($s{$b} <=> $s{$a}) || ($a cmp $b)} keys %s) [0];
 };
 
 print &most_frequent_letter_pair( 'abcdbca' ); #bc
