ec50c3e92128e742a65383cf511effd9ec3c5824
Matthew Neleigh <matthew.neleigh@gmail.com>
2024-02-13 02:17:57 -0500

    modified:   challenge-256/mattneleigh/perl/ch-2.pl

--- challenge-256/mattneleigh/perl/ch-2.pl
+++ challenge-256/mattneleigh/perl/ch-2.pl
@@ -45,7 +45,7 @@ sub merge_from_alternate_strings{
 
     my $length1 = length($ARG[0]);
     my $length2 = length($ARG[1]);
-    my $minimum_common_length = $length1 < $length2 ?
+    my $common_length = $length1 < $length2 ?
         $length1
         :
         $length2;
@@ -53,7 +53,7 @@ sub merge_from_alternate_strings{
 
     # Over the length the strings have in common,
     # concatenate a pair of characters from each
-    for my $pos (0 .. $minimum_common_length - 1){
+    for my $pos (0 .. $common_length - 1){
         $merged .=
             substr($ARG[0], $pos, 1)
             .
@@ -61,12 +61,12 @@ sub merge_from_alternate_strings{
     }
 
     # At least one string has been used up; if one
-    # is longer than the minimum common length,
+    # is longer than their common length,
     # concatenate the remaining characters from it
-    if($length1 > $minimum_common_length){
-        $merged .= substr($ARG[0], $minimum_common_length);
-    } elsif($length2 > $minimum_common_length){
-        $merged .= substr($ARG[1], $minimum_common_length);
+    if($length1 > $common_length){
+        $merged .= substr($ARG[0], $common_length);
+    } elsif($length2 > $common_length){
+        $merged .= substr($ARG[1], $common_length);
     }
 
     return($merged);
