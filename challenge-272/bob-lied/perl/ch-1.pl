c2046bf41c4be9b7f8b67a0a05c01c8bea77e2c0
Bob Lied <boblied+github@gmail.com>
2024-06-09 07:52:59 -0500

    Add comments

--- challenge-272/bob-lied/perl/ch-1.pl
+++ challenge-272/bob-lied/perl/ch-1.pl
@@ -5,7 +5,11 @@
 #=============================================================================
 # ch-1.pl Perl Weekly Challenge 272 Task 1 Defang IP Address
 #=============================================================================
-# 
+# You are given a valid IPv4 address.
+# Write a script to return the defanged version of the given IP address.
+# A defanged IP address replaces every period “.” with “[.]".
+# Example 1 Input: $ip = "1.1.1.1" Output: "1[.]1[.]1[.]1"
+# Example 2 Input: $ip = "255.101.1.0" Output: "255[.]101[.]1[.]0"
 #=============================================================================
 
 use v5.38;
