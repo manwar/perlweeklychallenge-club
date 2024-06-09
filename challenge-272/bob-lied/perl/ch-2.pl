0cc8bffa7fecfbffcfaa85c0fe7b0580369ea70f
Bob Lied <boblied+github@gmail.com>
2024-06-09 07:53:19 -0500

    Change to for loop

--- challenge-272/bob-lied/perl/ch-2.pl
+++ challenge-272/bob-lied/perl/ch-2.pl
@@ -5,7 +5,19 @@
 #=============================================================================
 # ch-2.pl Perl Weekly Challenge 272 Task 2 String Score
 #=============================================================================
-# 
+# You are given a string, $str.
+# Write a script to return the score of the given string.
+# The score of a string is defined as the sum of the absolute difference
+# between the ASCII values of adjacent characters.
+# Example 1 Input: $str = "hello"
+#           Output: 13
+#   ASCII values of characters: h=104 e=101 l=108 l=108 o=111
+#   Score => |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111|
+#         => 3 + 7 + 0 + 3 => 13
+# Example 2 Input: "perl"
+#           Output: 30
+# #xample 3 Input: "raku"
+#           Output: 37
 #=============================================================================
 
 use v5.38;
@@ -26,12 +38,10 @@ sub stringScore($str)
     my @v = map { ord($_) } split(//, $str);
 
     my $score = 0;
-    my $first = shift @v;
-    while ( @v )
+
+    for ( my $first = shift @v ; defined(my $second = shift @v) ; $first = $second )
     {
-        my $second = shift @v;
         $score += abs($first - $second);
-        $first = $second;
     }
 
     return $score;
