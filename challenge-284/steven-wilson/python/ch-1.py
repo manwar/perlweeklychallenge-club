8977c2782c87685be94fd43b452978c10f3614f7
Steven <steven1170@zoho.eu>
2024-08-26 19:56:55 +0100

    add solutions week 284 in python

---
+++ challenge-284/steven-wilson/python/ch-1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+
+def lucky_integer(*integers):
+    """ Given an array of integers, return the lucky integer if found otherwise
+    return -1. If there are more than one then return the largest.
+
+        A lucky integer is an integer that has a frequency in the array equal
+        to its value.
+
+    >>> lucky_integer(2, 2, 3, 4)
+    2
+    >>> lucky_integer(1, 2, 2, 3, 3, 3)
+    3
+    >>> lucky_integer(1, 1, 1, 3)
+    -1
+    """
+    counter = Counter(integers)
+    try:
+        return max(i for i in set(integers) if i == counter[i])
+    except ValueError:
+        return -1
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
