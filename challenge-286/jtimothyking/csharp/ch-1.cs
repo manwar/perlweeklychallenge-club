1f1c8e0a7954e5dd61dd9f12712e5213d9cfb582
Tim King <timk@theperlshop.com>
2024-09-15 10:30:39 -0400

    Solutions to challenge 286 by Tim King

---
+++ challenge-286/jtimothyking/csharp/ch-1.cs
@@ -0,0 +1,27 @@
+﻿namespace ch_1;
+
+public static class Ch1
+{
+    public static void Main(string[] args)
+    {
+        const string quineString = """
+           namespace ch_1;
+
+           public static class Ch1
+           {{
+               public static void Main(string[] args)
+               {{
+                   const string quineString = {0}{0}{0} {1} {0}{0}{0};
+                   var words = string.Format(quineString, (char)34, quineString)
+                       .Trim().Split([' ', '\n', '\r', '\t'], StringSplitOptions.RemoveEmptyEntries);
+                   var randomWord = words[Random.Shared.Next(words.Length)];
+                   Console.WriteLine(randomWord);
+               }}
+           }}
+       """;
+        var words = string.Format(quineString, (char)34, quineString)
+            .Trim().Split([' ', '\n', '\r', '\t'], StringSplitOptions.RemoveEmptyEntries);
+        var randomWord = words[Random.Shared.Next(words.Length)];
+        Console.WriteLine(randomWord);
+    }
+}
