4cec80ad6cca76ed64aa9b76f1a0ff47e6f684db
Paulo Custodio <pauloscustodio@gmail.com>
2024-09-17 11:10:54 +0100

    Add tests to Challenge 286

--- challenge-286/paulo-custodio/python/ch-1.py
+++ challenge-286/paulo-custodio/python/ch-1.py
@@ -29,8 +29,16 @@
 import sys
 from random import shuffle
 
+n = -1
+if len(sys.argv) == 2:
+    n = int(sys.argv[1])
+
 f = open(sys.argv[0], "r")
 text = f.read()
 words = text.split()
-shuffle(words)
-print(words[0])
+
+if n == -1:
+    shuffle(words)
+    print(words[0])
+else:
+    print(words[n])
