01aa2c3a77ad7501b7920be8ca1ba53e630857b7
Paulo Custodio <pauloscustodio@gmail.com>
2024-10-03 12:06:33 +0100

    Add Python solution to challenge 168

---
+++ challenge-168/paulo-custodio/python/ch-1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+
+# Challenge 168
+#
+# Task 1: Perrin Prime
+# Submitted by: Roger Bell_West
+#
+# The Perrin sequence is defined to start with [3, 0, 2]; after that, term N is
+# the sum of terms N-2 and N-3. (So it continues 3, 2, 5, 5, 7, ....)
+#
+#     A Perrin prime is a number in the Perrin sequence which is also a prime
+#     number.
+#
+# Calculate the first 13 Perrin Primes.
+#
+# f(13) = [2, 3, 5, 7, 17, 29, 277, 367, 853, 14197, 43721, 1442968193,
+#          792606555396977]
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def perrin_seq():
+    t = [3, 0, 2]
+    def next_perrin():
+        t.append(t[-2] + t[-3])
+        return t.pop(0)
+    return next_perrin
+
+def perrin_prime_seq():
+    it = perrin_seq()
+    seen = {}
+    def next_perrin_prime():
+        while True:
+            n = it()
+            if is_prime(n) and n not in seen:
+                seen[n] = True
+                return n
+    return next_perrin_prime
+
+import sys
+
+if len(sys.argv) != 2:
+    raise ValueError("usage: ch-1.py n")
+n = int(sys.argv[1])
+pp = []
+it = perrin_prime_seq()
+while len(pp) < n:
+    pp.append(it())
+print(", ".join(map(str, sorted(pp))))
---
+++ challenge-168/paulo-custodio/python/ch-2.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+
+# Challenge 168
+#
+# Task 2: Home Prime
+# Submitted by: Mohammad S Anwar
+#
+# You are given an integer greater than 1.
+#
+# Write a script to find the home prime of the given number.
+#
+# In number theory, the home prime HP(n) of an integer n greater than 1 is the
+# prime number obtained by repeatedly factoring the increasing concatenation of
+# prime factors including repetitions.
+#
+# Further information can be found on Wikipedia and OEIS.
+# Example
+#
+# As given in the Wikipedia page,
+#
+# HP(10) = 773, as 10 factors as 2x5 yielding HP10(1) = 25, 25 factors as 5x5
+# yielding HP10(2) = HP25(1) = 55, 55 = 5x11 implies HP10(3) = HP25(2) = HP55(1)
+# = 511, and 511 = 7x73 gives HP10(4) = HP25(3) = HP55(2) = HP511(1) = 773, a
+# prime number.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def prime_factors(n):
+    factors = []
+    p = 0
+    while n > 1:
+        p += 1
+        while not is_prime(p):
+            p += 1
+        while n % p == 0:
+            factors.append(p)
+            n //= p
+    return factors
+
+def home_prime(n):
+    if n <= 1:
+        raise ValueError("Input must be greater than 1")
+    while not is_prime(n):
+        factors = prime_factors(n)
+        n = int(''.join(map(str, factors)))
+    return n
+
+import sys
+
+if len(sys.argv) != 2:
+    raise ValueError("usage: script.py n")
+print(home_prime(int(sys.argv[1])))
