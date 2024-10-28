646a0e8830c9dbbbb893f4491074d587f571bb13
Peter Pentchev <roam@ringlet.net>
2024-10-28 11:16:26 +0200

    Add Peter Pentchev's Rust solutions to 293

---
+++ challenge-293/ppentchev/rust/src/lib.rs
@@ -0,0 +1,9 @@
+#![deny(missing_docs)]
+#![deny(clippy::missing_docs_in_private_items)]
+// SPDX-FileCopyrightText: Peter Pentchev <roam@ringlet.net>
+// SPDX-License-Identifier: BSD-2-Clause
+//! Peter Pentchev's solutions to the 293rd Perl Weekly Challenge.
+
+pub mod ch_1;
+pub mod ch_2;
+pub mod defs;
