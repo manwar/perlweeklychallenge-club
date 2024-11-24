8cbdc8062c1e070ad8b32f342dba5abdb2d0ac6f
DevSanti12 <santiagoleyva2013@gmail.com>
2024-11-23 23:42:21 -0500

    Added challenge descriptions

---
+++ challenge-296/santiago-leyva/perl/ch-01.pl
@@ -0,0 +1,19 @@
+=begin
+You are given a string of alphabetic characters, $chars.
+
+Write a script to compress the string with run-length encoding, as shown in the examples.
+
+A compressed unit can be either a single character or a count followed by a character.
+
+BONUS: Write a decompression function.
+
+Example 1
+Input: $chars = "abbc"
+Output: "a2bc"
+Example 2
+Input: $chars = "aaabccc"
+Output: "3ab3c"
+Example 3
+Input: $chars = "abcc"
+Output: "ab2c"
+=cut
