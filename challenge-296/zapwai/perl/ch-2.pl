de82238fa7bb177611672f700a8d33062f393b8e
David Ferrone <zapwai@gmail.com>
2024-11-24 14:13:50 -0500

    Week 296 Update

--- challenge-296/zapwai/perl/ch-2.pl
+++ challenge-296/zapwai/perl/ch-2.pl
@@ -1,11 +1,13 @@
 use v5.38;
 use List::Util qw( sum );
 
-sub sqsides($side_length, $sides_left, %h) {
+sub copout($side_length, $sides_left, %supply) {
+    # This function is incorrect but I'll use it for emergencies.
+    # Useful for weird cases where number of plausible moves is very large.
     my @nums;
-    for (sort keys %h) {
+    for (sort keys %supply) {
 	next if ($_ == $side_length);
-	for my $i (1 .. $h{$_}) {
+	for my $i (1 .. $supply{$_}) {
 	    push @nums, $_;
 	}
     }
@@ -24,10 +26,58 @@ sub sqsides($side_length, $sides_left, %h) {
 	    @taken = ();
 	    $index++;
 	}
-	
     } while ($index <= $#nums);
-    
     return 1 if ($cnt == $sides_left);
+    return 0;
+}
+
+sub sqsides($side_length, $sides_left, %supply) {
+    my @nums;
+    for (sort keys %supply) {
+	next if ($_ == $side_length);
+	for my $i (1 .. $supply{$_}) {
+	    push @nums, $_;
+	}
+    }
+    my @moves;
+    for my $i (1 .. 2**@nums - 1) {
+	my $n = @nums;
+	my $set = '%0'.$n.'b';
+	my $x = sprintf($set, $i);
+	my @d = split('', $x);
+	my @n;
+	for (0 .. $#nums) {
+	    push @n, $nums[$_] if ($d[$_] == 1);
+	}
+	my $tot = sum(@n);
+	push @moves, \@n if ($tot == $side_length);
+    }
+
+    if (@moves > 20) {
+	return 1 if copout($side_length, $sides_left, %supply);
+    } else {
+	unless (@moves == 0) {
+	    for my $i (1 .. 2**@moves - 1) {
+		my $n = @moves;
+		my $set = '%0'.$n.'b';
+		my $x = sprintf($set, $i);
+		my @d = split('', $x);
+		my $tally = sum(@d);
+		next if ($tally != $sides_left);
+		my @n;
+		for my $i (0 .. $#moves) {
+		    push @n, @{$moves[$i]} if ($d[$i] == 1);
+		}
+		my %demand;
+		$demand{$_}++ for (@n);
+		my $cnt = 0;
+		for my $key (sort keys %demand) {
+		    $cnt++ if ($demand{$key} <= $supply{$key});
+		}
+		return 1 if ($cnt == scalar keys %demand);
+	    }
+	}
+    }
     return 0;
 }
 
@@ -52,14 +102,12 @@ sub proc(@ints) {
 	}
 	say "\t(Side length $side_length)";
     } 
-    say "Output: Square $ans possible.";
+    say "Output: Square $ans possible.\n";
 }
 
 my @ints = (1, 2, 2, 2, 1);
 proc(@ints);
 @ints = (2, 2, 2, 4);
-proc(@ints);
-@ints = (2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 proc(@ints);
 @ints = (3, 1, 2, 4, 2, 4);
 proc(@ints);
@@ -68,4 +116,9 @@ proc(@ints);
 @ints = (3, 3, 2, 2, 2, 6, 6);
 proc(@ints);
 @ints = (3, 4, 1, 4, 3, 1);
+proc(@ints);
+# Thanks to Jörg Sommrey for testing :-)
+@ints = (571, 362, 67, 413, 215, 372, 16, 457, 527, 1000);
+proc(@ints);
+@ints = (2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
 proc(@ints);
