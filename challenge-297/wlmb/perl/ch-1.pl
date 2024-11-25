82ad2ab1700971968d1fe99ed2e67146db537de3
Luis Mochan <mochan@fis.unam.mx>
2024-11-25 11:24:08 -0600

    Change the error message

--- challenge-297/wlmb/perl/ch-1.pl
+++ challenge-297/wlmb/perl/ch-1.pl
@@ -8,15 +8,15 @@ use List::Util qw(sum0 max all);
 die <<~"FIN" unless @ARGV and all{$_==0 || $_==1}@ARGV;
     Usage: $0 N0 N1...
     to find the largest balanced contiguous sub array
-    from the array N0 N1...
+    from the binary (0's or 1's only) array N0 N1...
     FIN
 my $result = max(
     map {$_->[0]}
     grep {$_->[0]==2*$_->[1]}
     map{
-	my $f=$_;
+        my $f=$_;
         map {[$_+1-$f, sum0 @ARGV[$f .. $_]]}
-	    $f+1 .. @ARGV-1
+            $f+1 .. @ARGV-1
     } 0..@ARGV-2
     )//0;
 say "@ARGV -> $result";
