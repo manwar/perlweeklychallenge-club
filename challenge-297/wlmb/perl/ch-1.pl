e0793567c51a53121df6f03f0cf8e50ad0eff257
Luis Mochan <mochan@fis.unam.mx>
2024-11-25 11:55:39 -0600

    Filter on list lengths

--- challenge-297/wlmb/perl/ch-1.pl
+++ challenge-297/wlmb/perl/ch-1.pl
@@ -10,13 +10,14 @@ die <<~"FIN" unless @ARGV and all{$_==0 || $_==1}@ARGV;
     to find the largest balanced contiguous sub array
     from the binary (0's or 1's only) array N0 N1...
     FIN
-my $result = max(
-    map {$_->[0]}
-    grep {$_->[0]==2*$_->[1]}
+my $result = max(                               # get largest
+    map {$_->[0]}                               # get length
+    grep {$_->[0]==2*$_->[1]}                   # select sublist
     map{
-        my $f=$_;
-        map {[$_+1-$f, sum0 @ARGV[$f .. $_]]}
-            $f+1 .. @ARGV-1
-    } 0..@ARGV-2
-    )//0;
+        my $f=$_;                               # start of list
+        map {[$_+1-$f, sum0 @ARGV[$f .. $_]]}   # [start, length]
+	grep {($_-$f)%2}                        # filter even lengths only
+            $f+1 .. @ARGV-1                     # all possible remaining elements
+    } 0..@ARGV-2                                # all possible first elements.
+    )//0;                                       # default for empty case
 say "@ARGV -> $result";
