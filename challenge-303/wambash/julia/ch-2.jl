2ad6a71420b9dc63eb4dda6f106d55d94bec6fd0
Jan Krňávek <Jan.Krnavek@gmail.com>
2025-01-11 18:28:19 +0100

    solution week 303-2 -- Julia; JuMP; (Binary) linear programming

---
+++ challenge-303/wambash/julia/ch-2.jl
@@ -0,0 +1,31 @@
+using JuMP
+using GLPK
+using Lazy
+using Test
+
+function delete_and_earn(ints)
+    freq = frequencies(ints)
+    adepts = keys(freq)
+
+    model = Model(GLPK.Optimizer)
+    @variable(model, x[i in adepts], Bin)
+    @objective(model, Max, sum(i * freq[i] * x[i]  for i in keys(freq)) )
+
+    for i in adepts
+        if i+1 in adepts
+            @constraint(model, x[i] + x[i+1]  ≤ 1)
+        end
+        if i-1 in adepts
+            @constraint(model, x[i-1] + x[i]  ≤ 1)
+        end
+    end
+
+    optimize!(model)
+    #@show value.(x)
+    objective_value(model)
+end
+
+@testset "delete and earn" begin
+   @test delete_and_earn([3,4,2]) == 6
+   @test delete_and_earn([2, 2, 3, 3, 3, 4]) == 9
+end
