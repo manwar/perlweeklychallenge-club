352307760f63bc591f8f450a6edee2e33c3cc72e
Andreas Mahnke <andreas.mahnke@leuphana.de>
2025-01-27 17:27:10 +0100

    Challenge 306

---
+++ challenge-306/mahnkong/perl/ch-1.pl
@@ -0,0 +1,21 @@
+use strict;
+use feature 'signatures';
+use Test::More 'no_plan';
+
+sub run(@ints) {
+    my $sum = 0;
+    for (my $i = 1; $i <= scalar(@ints); $i +=2) {
+        my $start = 0;
+        while ($start + $i <= scalar(@ints)) {
+            my $end = $start + $i-1;
+            for my $e (@ints[$start..$end]) {
+                $sum += $e;
+            }
+            $start +=1;
+        }
+    }
+    return $sum;
+}
+
+is(run(2, 5, 3, 6, 4), 77, "Example 1");
+is(run(1, 3), 4, "Example 2");
