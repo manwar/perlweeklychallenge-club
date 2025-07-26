67f1c105a301c4ac6432e11515a9985732ed2508
Matthias Muth <matthias.muth@gmx.de>
2025-07-26 17:35:27 +0200

    Challenge 331 Task 1 and 2 solutions in Perl by Matthias Muth

---
+++ challenge-331/matthias-muth/perl/ch-1.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 331 Task 1: Last Word
+#
+#       Perl solution by Matthias Muth.
+#
+
+use v5.36;
+
+use Dsay;
+$debug{ALL} = 1;
+say pp split "", "";
+say pp scalar( ()[-1] );
+
+sub last_word_length( $str ) {
+    return length( ( split " ", $str )[-1] // "" );
+}
+
+use Test2::V0 qw( -no_srand );
+
+is last_word_length( "The Weekly Challenge" ), 9,
+    'Example 1: last_word_length( "The Weekly Challenge" ) == 9';
+is last_word_length( "   Hello   World    " ), 5,
+    'Example 2: last_word_length( "   Hello   World    " ) == 5';
+is last_word_length( "Let" ), 3,
+    'Example 3: last_word_length( "Let" ) == 3';
+is last_word_length( "" ), 0,
+    'Test 1: last_word_length( "" ) == 0';
+
+done_testing;
