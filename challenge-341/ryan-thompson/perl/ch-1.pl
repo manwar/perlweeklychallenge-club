fab447901fcd3def5ba9874f0a6028052f2a04bd
Ryan Thompson <i@ry.ca>
2025-10-05 15:35:14 -0600

    Remove unnecessary sub

--- challenge-341/ryan-thompson/perl/ch-1.pl
+++ challenge-341/ryan-thompson/perl/ch-1.pl
@@ -3,11 +3,6 @@
 use v5.38;
 
 # Upper/lowercase are usually on the same key, thus we ignore case
-sub apssible { grep !/[@_\0]/i, split /\s+/, shift }
-sub pssible {
-    #my $re = qr/[@_\0]/i;
-    #say STDERR $re;
-    grep !/[@_\0]/i, split /\s+/, shift 
-}
+sub pssible { grep !/[@_\0]/i, split /\s+/, shift }
 
 say join ' ', pssible(shift, @ARGV) if @ARGV
