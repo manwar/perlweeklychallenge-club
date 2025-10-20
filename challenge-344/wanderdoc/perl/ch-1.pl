0ca51e9de47a25326fe22e2121879ac3aea64dec
wanderdoc <wanderdoc@users.noreply.github.com>
2025-10-20 20:15:28 +0200

    Create ch-1.pl

---
+++ challenge-344/wanderdoc/perl/ch-1.pl
@@ -0,0 +1,56 @@
+#!perl
+use strict;
+use warnings FATAL => qw(all);
+
+=prompt
+You are given an array of integers, @ints and an integer, $x.
+
+Write a script to add $x to the integer in the array-form.
+
+    The array form of an integer is a digit-by-digit representation stored as an array, where the most significant digit is at the 0th index.
+
+Example 1
+
+Input: @ints = (1, 2, 3, 4), $x = 12
+Output: (1, 2, 4, 6)
+
+Example 2
+
+Input: @ints = (2, 7, 4), $x = 181
+Output: (4, 5, 5)
+
+Example 3
+
+Input: @ints = (9, 9, 9), $x = 1
+Output: (1, 0, 0, 0)
+
+Example 4
+
+Input: @ints = (1, 0, 0, 0, 0), $x = 9999
+Output: (1, 9, 9, 9, 9)
+
+Example 5
+
+Input: @ints = (0), $x = 1000
+Output: (1, 0, 0, 0)
+=cut
+
+
+
+
+use Test2::V0 -no_srand => 1;
+is([array_form([1, 2, 3, 4], 12)], [1, 2, 4, 6], 'Example 1');
+is([array_form([2, 7, 4], 181)], [4, 5, 5], 'Example 2');
+is([array_form([9, 9, 9], 1)], [1, 0, 0, 0], 'Example 3');
+is([array_form([1, 0, 0, 0, 0], 9999)], [1, 9, 9, 9, 9], 'Example 4');
+is([array_form([0], 1000)], [1, 0, 0, 0], 'Example 5');
+done_testing();
+
+sub array_form
+{
+     my ($ints, $x) = @_;
+     my $int_str = join('', @$ints); 
+     my $result = $int_str + $x; 
+     return split(//, $result);
+     
+}
