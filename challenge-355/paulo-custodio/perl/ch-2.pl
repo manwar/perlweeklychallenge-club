6587140bab75fabf3d7af929e00b5f40e0cff46a
Paulo Custodio <pauloscustodio@gmail.com>
2026-01-31 00:38:13 +0000

    Move documentation to README.txt

--- challenge-355/paulo-custodio/perl/ch-2.pl
+++ challenge-355/paulo-custodio/perl/ch-2.pl
@@ -1,39 +1,5 @@
 #!/usr/bin/env perl
 
-# Challenge 355
-#
-# Task 2: Mountain Array
-# Submitted by: Mohammad Sajid Anwar
-# You are given an array of integers, @ints.
-#
-# Write a script to return true if the given array is a valid mountain array.
-#
-# An array is mountain if and only if:
-# 1) arr.length >= 3
-# and
-# 2) There exists some i with 0 < i < arr.length - 1 such that:
-# arr[0] < arr[1]     < ... < arr[i - 1] < arr[i]
-# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
-# Example 1
-# Input: @ints = (1, 2, 3, 4, 5)
-# Output: false
-#
-# Example 2
-# Input: @ints = (0, 2, 4, 6, 4, 2, 0)
-# Output: true
-#
-# Example 3
-# Input: @ints = (5, 4, 3, 2, 1)
-# Output: false
-#
-# Example 4
-# Input: @ints = (1, 3, 5, 5, 4, 2)
-# Output: false
-#
-# Example 5
-# Input: @ints = (1, 3, 2)
-# Output: true
-
 use Modern::Perl;
 
 @ARGV or die "Usage: $0 nums...\n";
