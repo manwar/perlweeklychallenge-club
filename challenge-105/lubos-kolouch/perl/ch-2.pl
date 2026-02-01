373817f5fb3887c53992e2d3eb5c1c71ed5ace2d
Lubos Kolouch <lubos@kolouch.net>
2026-02-01 10:29:46 +0100

    Complete challenge 105 solutions

--- challenge-105/lubos-kolouch/perl/ch-2.pl
+++ challenge-105/lubos-kolouch/perl/ch-2.pl
@@ -1,14 +1,30 @@
 use strict;
 use warnings;
 
-sub name_game {
+sub name_game_lyrics {
     my $name         = shift;
     my $rest_of_name = substr $name, 1;
-    print "$name, $name, bo-b$rest_of_name,\n";
-    print "Bonana-fanna fo-f$rest_of_name\n";
-    print "Fee fi mo-m$rest_of_name\n";
-    print "$name!\n";
+    return join(
+        "\n",
+        "    $name, $name, bo-b$rest_of_name,",
+        "    Bonana-fanna fo-f$rest_of_name",
+        "    Fee fi mo-m$rest_of_name",
+        "    $name!",
+    );
 }
 
-name_game('Katie')
-  ; # Output: Katie, Katie, bo-batie, Bonana-fanna fo-fatie, Fee fi mo-matie, Katie!
+use Test::More;
+
+is(
+    name_game_lyrics('Katie'),
+    join(
+        "\n",
+        '    Katie, Katie, bo-batie,',
+        '    Bonana-fanna fo-fatie',
+        '    Fee fi mo-matie',
+        '    Katie!',
+    ),
+    'Example',
+);
+
+done_testing;
