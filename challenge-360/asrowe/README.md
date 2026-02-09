7dd2afd0a14830440b46d12740bba7d6cf0df8d0
Anthony Rowe <asrowe@gmail.com>
2026-02-09 19:13:01 +0000

    Solutions for Challenge 360

--- challenge-360/asrowe/README.md
+++ challenge-360/asrowe/README.md
@@ -5,5 +5,6 @@ Solutions by Anthony Rowe (asrowe)
 - 2026-23-01 Solutions for Challenge 357
 - 2026-30-01 Solutions for Challenge 358
     - [Blog](./Challenge-358.md)
-- 2026-08-01 Solutions for Challenge 359
+- 2026-08-02 Solutions for Challenge 359
+- 2026-09-02 Solutions for Challenge 360
 
--- challenge-360/asrowe/python/ch-2.py
+++ challenge-360/asrowe/python/ch-2.py
@@ -1,1 +1,29 @@
-"#TODO" 
+
+import unittest
+
+def orderWords(text: str) -> str:
+    out = sorted(text.split(), key=lambda x: x.lower())
+    return " ".join(out)
+
+
+
+class ChallengeRunner(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual(orderWords("The quick brown fox"), "brown fox quick The")
+
+    def test_case_2(self):
+        self.assertEqual(orderWords("Hello    World!   How   are you?"), "are Hello How World! you?")
+
+    def test_case_3(self):
+        self.assertEqual(orderWords("Hello"), "Hello")
+
+    def test_case_4(self):
+        self.assertEqual(orderWords("Hello, World! How are you?"), "are Hello, How World! you?")
+
+    def test_case_5(self):
+        self.assertEqual(orderWords("I have 2 apples and 3 bananas!"), "2 3 and apples bananas! have I")
+
+
+if __name__ == '__main__':
+    unittest.main()
