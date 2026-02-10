0387ec751d9fbe8dddc6c5dedf0aa1aadd625aba
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-02-10 17:16:10 +0000

    - Added solutions by Matt Martini.

--- challenge-360/ulrich-rieke/perl/ch-2.pl
+++ challenge-360/ulrich-rieke/perl/ch-2.pl
@@ -3,24 +3,9 @@ use strict ;
 use warnings ;
 use feature 'say' ;
 
-sub to_lower_case {
-   my $word = shift ;
-   my @revised ;
-   for my $letter( split( // , $word ) ) {
-      if ( ord( $letter ) > 64 && ord( $letter ) < 91 ) {
-         push( @revised , chr( ord( $letter ) + 32 ) ) ;
-      }
-      else {
-         push( @revised , $letter ) ;
-      }
-   }
-   return join( '' , @revised ) ;
-}
-
 say "Enter a sentence!" ;
 my $line = <STDIN> ;
 chomp $line ;
-my @sorted = sort { to_lower_case( $a ) cmp to_lower_case( $b ) } split(
-      /\s+/ , $line ) ;
+my @words = split( /\s+/ , $line ) ;
+my @sorted = sort { lc( $a ) cmp lc( $b ) } @words ;
 say join( ' ' , @sorted ) ;
-
