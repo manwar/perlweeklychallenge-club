6eaf9378e92df1a69f6d01d41ddf4d809b17b282
Paulo Custodio <pauloscustodio@gmail.com>
2026-02-16 18:18:52 +0000

    Add Perl and C solutions to challenge 361

---
+++ challenge-361/paulo-custodio/test.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Test::More;
+require "../../challenge-001/paulo-custodio/testlib.pl";
+
+test_line(1, 4,     "3, 1");
+test_line(1, 12,    "8, 3, 1");
+test_line(1, 20,    "13, 5, 2");
+test_line(1, 96,    "89, 5, 2");
+test_line(1, 100,   "89, 8, 3");
+
+test_block(2, <<IN, 4);
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 1 0
+0 0 0 0 0 0
+0 0 0 0 1 0
+IN
+test_block(2, <<IN, -1);
+0 1 0 0
+0 0 1 0
+0 0 0 1
+1 0 0 0
+IN
+test_block(2, <<IN, 0);
+0 0 0 0 0
+1 0 0 0 0
+1 0 0 0 0
+1 0 0 0 0
+1 0 0 0 0
+IN
+test_block(2, <<IN, 3);
+0 1 0 1 0 1
+1 0 1 1 0 0
+0 0 0 1 1 0
+0 0 0 0 0 0
+0 1 0 1 0 0
+1 0 1 1 0 0
+IN
+test_block(2, <<IN, -1);
+0 1 1 0
+1 0 1 0
+0 0 0 0
+0 0 0 0
+IN
+test_block(2, <<IN, -1);
+0 0 1 1
+1 0 0 0
+1 1 0 1
+1 1 0 0
+IN
+
+done_testing;
