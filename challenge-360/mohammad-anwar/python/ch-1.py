f6a0473e82537aff4e710a4bfab319d0432f2265
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-02-17 12:04:02 +0000

    - Added solutions by Mohammad Sajid Anwar.

--- challenge-360/mohammad-anwar/python/ch-1.py
+++ challenge-360/mohammad-anwar/python/ch-1.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 
-def justify_text(s: str, width: int) -> str:
-    pad   = width - len(s)
-    left  = pad // 2
-    right = pad - left
-    return "*" * left + s + "*" * right
+import unittest
 
-examples = [
-    (["Hi",    5], "*Hi**"),
-    (["Code", 10], "***Code***"),
-    (["Hello", 9], "**Hello**"),
-    (["Perl",  4], "Perl"),
-    (["A",     7], "***A***"),
-    (["",      5], "*****"),
-]
+examples = {
+    4: "3,1",
+    12: "8,3,1",
+    20: "13,5,2",
+    96: "89,5,2",
+    100: "89,8,3",
+}
 
-i = 1
-for (s, width), expected in examples:
-    result = justify_text(s, width)
-    if result == expected:
-        print("ok =", i)
-    i = i + 1
+def zeckendorf(n):
+    f = [1, 2]
+
+    while f[-1] <= n:
+        f.append(f[-1] + f[-2])
+    f.pop()
+    r = []
+    i = len(f) - 1
+    while i >= 0:
+        if f[i] <= n:
+            r.append(f[i])
+            n -= f[i]
+        i -= 1
+
+    return ",".join(str(x) for x in r)
+
+class TestZeckendorf(unittest.TestCase):
+    def test_examples(self):
+        for key, value in examples.items():
+            self.assertEqual(zeckendorf(key), value)
+
+if __name__ == "__main__":
+    unittest.main()
