c33c6d002b572d4bf1ce1c0bc310bc21b5142a95
Lubos Kolouch <lubos@kolouch.net>
2026-02-18 08:36:32 +0100

    Challenge 058 solutions (Perl & Python)

--- challenge-058/lubos-kolouch/perl/ch-1.pl
+++ challenge-058/lubos-kolouch/perl/ch-1.pl
@@ -31,10 +31,37 @@ use warnings;
 sub compare_versions {
     my ($ver1, $ver2) = @_;
 
-    my $v1 = version->new($ver1);
-    my $v2 = version->new($ver2);
+    my ( $nums1, $seps1 ) = _parse_version($ver1);
+    my ( $nums2, $seps2 ) = _parse_version($ver2);
 
-    return $v1 <=> $v2;
+    my $min = @$nums1 < @$nums2 ? scalar(@$nums1) : scalar(@$nums2);
+    for my $i ( 0 .. $min - 1 ) {
+        return -1 if $nums1->[$i] < $nums2->[$i];
+        return 1  if $nums1->[$i] > $nums2->[$i];
+
+        next if $i == 0;
+
+        my $s1 = $seps1->[ $i - 1 ];
+        my $s2 = $seps2->[ $i - 1 ];
+        next if $s1 eq $s2;
+
+        return 1  if $s1 eq '.';
+        return -1;
+    }
+
+    return -1 if @$nums1 < @$nums2;
+    return 1  if @$nums1 > @$nums2;
+    return 0;
+}
+
+sub _parse_version {
+    my ($ver) = @_;
+
+    die "Invalid version string\n" if $ver !~ /\A[0-9._]+\z/;
+
+    my @nums = ($ver =~ /(\d+)/g);
+    my @seps = ($ver =~ /([._])\d+/g);
+    return ( \@nums, \@seps );
 }
 
 # TESTS
@@ -49,3 +76,5 @@ is(compare_versions('1.2','1.2_5'),-1);
 # https://rt.cpan.org/Ticket/Display.html?id=132482
 is(compare_versions('1.2.1','1.2_1'),1);
 is(compare_versions('1.2.1','1.2.1'),0);
+
+done_testing;
--- challenge-058/lubos-kolouch/python/ch-1.py
+++ challenge-058/lubos-kolouch/python/ch-1.py
@@ -1,29 +1,88 @@
-#! /usr/bin/env python
-""" Perl weekly challenge 058
-    https://perlweeklychallenge.org/blog/perl-weekly-challenge-058/ """
+#!/usr/bin/env python3
+"""
+Perl Weekly Challenge 058 - Task 1: Compare Version
 
-from packaging import version
+Compare two version strings containing digits, '.' and '_' such that:
+- return 1  if v1 > v2
+- return -1 if v1 < v2
+- return 0  if equal
+
+Rule: '_' denotes an alpha/development separator and has lower precedence than '.'
+when numeric components are equal (see examples in the spec).
+"""
+
+from __future__ import annotations
+
+from typing import List, Sequence, Tuple
 
 
-def compare_versions(ver_1, ver_2):
-    """ Compare versions and return the result"""
+def _parse_version(ver: str) -> Tuple[List[int], List[str]]:
+    if any(ch not in "0123456789._" for ch in ver) or not ver:
+        raise ValueError("invalid version string")
 
-    v_1 = version.parse(ver_1)
-    v_2 = version.parse(ver_2)
+    nums: List[int] = []
+    seps: List[str] = []
+    current = ""
+    for ch in ver:
+        if ch.isdigit():
+            current += ch
+        else:
+            if current == "":
+                raise ValueError("invalid version string")
+            nums.append(int(current))
+            current = ""
+            seps.append(ch)
+    if current == "":
+        raise ValueError("invalid version string")
+    nums.append(int(current))
 
-    if v_1 > v_2:
-        return 1
+    return nums, seps
 
-    if v_1 < v_2:
-        return -1
 
+def compare_versions(v1: str, v2: str) -> int:
+    """Compare v1 and v2 according to the challenge rules."""
+    nums1, seps1 = _parse_version(v1)
+    nums2, seps2 = _parse_version(v2)
+
+    m = min(len(nums1), len(nums2))
+    for i in range(m):
+        if nums1[i] != nums2[i]:
+            return 1 if nums1[i] > nums2[i] else -1
+        if i > 0 and seps1[i - 1] != seps2[i - 1]:
+            return 1 if seps1[i - 1] == "." else -1
+
+    if len(nums1) != len(nums2):
+        return 1 if len(nums1) > len(nums2) else -1
     return 0
 
 
-assert compare_versions('0.1', '1.1') == -1
-assert compare_versions('2.0', '1.2') == 1
+def _run_cli(argv: Sequence[str]) -> None:
+    if not argv:
+        _run_tests()
+        return
+    if len(argv) != 2:
+        raise SystemExit(f"Usage: {__file__} <v1> <v2>")
+    print(compare_versions(argv[0], argv[1]))
 
-# This fails, reported : https://github.com/pypa/packaging/issues/299
-assert compare_versions('1.2', '1.2_5') == -1
-assert compare_versions('1.2.1', '1.2_1') == 1
-assert compare_versions('1.2.1', '1.2.1') == 0
+
+def _run_tests() -> None:
+    import unittest
+
+    class TestChallenge058Task1(unittest.TestCase):
+        def test_examples(self) -> None:
+            self.assertEqual(compare_versions("0.1", "1.1"), -1)
+            self.assertEqual(compare_versions("2.0", "1.2"), 1)
+            self.assertEqual(compare_versions("1.2", "1.2_5"), -1)
+            self.assertEqual(compare_versions("1.2.1", "1.2_1"), 1)
+            self.assertEqual(compare_versions("1.2.1", "1.2.1"), 0)
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestChallenge058Task1)
+    result = unittest.TextTestRunner().run(suite)
+    if not result.wasSuccessful():
+        raise SystemExit(1)
+
+
+if __name__ == "__main__":
+    import sys
+
+    _run_cli(sys.argv[1:])
