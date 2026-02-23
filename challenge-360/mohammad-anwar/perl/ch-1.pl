152108cefc039073078c7034d5c863aa608d64a2
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-02-23 08:57:14 +0000

    - Updated contributions to week 360 and 361.

--- challenge-360/mohammad-anwar/perl/ch-1.pl
+++ challenge-360/mohammad-anwar/perl/ch-1.pl
@@ -5,31 +5,22 @@ use warnings;
 
 use Test::More;
 
-my %examples = (
-    4   => "3,1"   ,
-    12  => "8,3,1" ,
-    20  => "13,5,2",
-    96  => "89,5,2",
-    100 => "89,8,3",
+my @examples = (
+    { in => ["Hi",    5], out => "*Hi**"      },
+    { in => ["Code", 10], out => "***Code***" },
+    { in => ["Hello", 9], out => "**Hello**"  },
+    { in => ["Perl",  4], out => "Perl"       },
+    { in => ["A",     7], out => "***A***"    },
+    { in => ["" ,     5], out => "*****"      },
 );
 
-is zeckendorf($_), $examples{$_} for keys %examples;
+is justify_text(@{$_->{in}}), $_->{out} for @examples;
 
 done_testing;
 
-sub zeckendorf {
-    my $n = shift;
-    my @f = (1, 2);
+sub justify_text {
+    my ($str, $width) = @_;
 
-    push(@f, $f[-1] + $f[-2]) while $f[-1] <= $n;
-    pop @f;
-    my @r;
-    for (my $i = $#f; $i >= 0; $i--) {
-        if ($f[$i] <= $n) {
-            push @r, $f[$i];
-            $n -= $f[$i];
-        }
-    }
-
-    return join ",", @r;
+    my $pad = $width - length $str;
+    return "*" x int($pad / 2) . $str . "*" x ($pad - int($pad / 2));
 }
