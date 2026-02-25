c2ada662aed702499a249dac6770cec6effc104d
pok <pok@noble>
2026-02-25 13:08:23 +0700

    pwc362 solution in python

---
+++ challenge-362/pokgopun/python/ch-2.py
@@ -0,0 +1,78 @@
+### https://theweeklychallenge.org/blog/perl-weekly-challenge-362/
+"""
+
+Task 2: Spellbound Sorting
+
+Submitted by: [46]Peter Campbell Smith
+     __________________________________________________________________
+
+   You are given an array of integers.
+
+   Write a script to return them in alphabetical order, in any language of
+   your choosing. Default language is English.
+
+Example 1
+
+Input: (6, 7, 8, 9 ,10)
+Output: (8, 9, 7, 6, 10)
+
+eight, nine, seven, six, ten
+
+Example 2
+
+Input: (-3, 0, 1000, 99)
+Output: (-3, 99, 1000, 0)
+
+minus three, ninety-nine, one thousand, zero
+
+Example 3
+
+Input: (1, 2, 3, 4, 5)
+
+Output: (5, 2, 4, 3, 1) for French language
+cinq, deux, quatre, trois, un
+
+Output: (5, 4, 1, 3, 2) for English language
+five, four, one, three, two
+
+Example 4
+
+Input: (0, -1, -2, -3, -4)
+Output: (-4, -1, -3, -2, 0)
+
+minus four, minus one, minus three, minus two, zero
+
+Example 5
+
+Input: (100, 101, 102)
+Output: (100, 101, 102)
+
+one hundred, one hundred and one, one hundred and two
+     __________________________________________________________________
+
+   Last date to submit the solution 23:59 (UK Time) Sunday 1st March 2026.
+     __________________________________________________________________
+
+SO WHAT DO YOU THINK ?
+"""
+### solution by pokgopun@gmail.com
+
+from num2words import num2words
+
+def spellboundSort(ints: tuple[int]) -> tuple[int]:
+    return tuple(e[1] for e in sorted((num2words(n), n) for n in ints))
+
+import unittest
+
+class TestSpellboundSorting(unittest.TestCase):
+    def test(self):
+        for inpt, otpt in {
+                (6, 7, 8, 9 ,10) : (8, 9, 7, 6, 10) ,
+                (-3, 0, 1000, 99) : (-3, 99, 1000, 0) ,
+                (1, 2, 3, 4, 5) : (5, 4, 1, 3, 2) ,
+                (0, -1, -2, -3, -4) : (-4, -1, -3, -2, 0) ,
+                (100, 101, 102) : (100, 101, 102),
+                }.items():
+            self.assertEqual(spellboundSort(inpt), otpt)
+
+unittest.main()
