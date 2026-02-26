81aca898939c7cbf8f59574fe86b19aff35109a1
Lubos Kolouch <lubos@kolouch.net>
2026-02-26 19:46:56 +0100

    Refine challenge 001 tests for pre-commit
    Replace Python assert checks with unittest TestCase and keep python3 shebang/typing updates; include formatting fixes required by pre-commit.

--- challenge-001/lubos-kolouch/perl5/ch-2.pl
+++ challenge-001/lubos-kolouch/perl5/ch-2.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 #===============================================================================
 #
 #         FILE: ch-1.pl
 #
-#        USAGE: ./ch-1.pl  
+#        USAGE: ./ch-1.pl
 #
 #  DESCRIPTION: Perl Weekly Challenge 001
 #               https://perlweeklychallenge.org/blog/perl-weekly-challenge-001/
@@ -20,7 +20,7 @@ sub fizz_buzz {
     my @output;
 
     for (1..20) {
-        
+
         my $line = '';
 
         $line = 'fizz' if $_ % 3 == 0;
