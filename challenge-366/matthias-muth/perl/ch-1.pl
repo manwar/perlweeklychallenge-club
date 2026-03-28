449cbc846fc5c5ccfe34f78e9f79de585d6245a2
Matthias Muth <matthias.muth@gmx.de>
2026-03-28 16:01:11 +0100

    Challenge 366 Task 1 and 2 solutions in Perl by Matthias Muth - Update

--- challenge-366/matthias-muth/perl/ch-1.pl
+++ challenge-366/matthias-muth/perl/ch-1.pl
@@ -10,15 +10,15 @@
 
 use v5.36;
 
-sub count_prefixes( $array, $str ) {
-    return scalar grep $_ eq substr( $str, 0, length( $_ ) ), $array->@*;
-}
-
 sub count_prefixes_regex( $array, $str ) {
     my $re = join " ",
         map( "(?:$_", split( "", $str ) ),
         map( ")?",    split( "", $str ) );
     return scalar grep /^ ${re} $/x, $array->@*;
+}
+
+sub count_prefixes( $array, $str ) {
+    return scalar grep $_ eq substr( $str, 0, length( $_ ) ), $array->@*;
 }
 
 use Test2::V0 qw( -no_srand );
