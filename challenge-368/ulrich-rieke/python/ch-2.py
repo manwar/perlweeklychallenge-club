d2bce7338027e3bba00c7a636c35cd1a9d6ad60a
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-04-08 16:35:15 +0100

    - Added solutions by Bob Lied. - Added solutions by Matthew Neleigh. - Added solutions by Ulrich Rieke. - Added solutions by Marton Polgar. - Added solutions by Lubos Kolouch.

--- challenge-368/ulrich-rieke/python/ch-2.py
+++ challenge-368/ulrich-rieke/python/ch-2.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-def findFactors( number , mode):
-   primefactors = []
-   divisor = 2
-   while number != 1:
-      if number % divisor == 0:
-         primefactors.append( divisor )
-         number //= divisor
-      else:
-         divisor += 1
-   if mode == 0:
-      uniques = [num for num in primefactors if primefactors.count( num ) == 1]
-      return len( uniques )
-   else:
-      return len( primefactors )
+def primecount( number , mode ):
+  primefactors = []
+  divisor = 2 
+  while number != 1:
+     if number % divisor == 0:
+        primefactors.append( divisor )
+        number //= divisor
+     else:
+       divisor += 1
+  if mode == 0:
+     uniques = {}
+     for n in primefactors:
+        uniques.add( n )
+     return len(uniques)
+  else:
+     return len( primefactors )
 
-number = input( "Enter a positive number!\n" ) 
-mode = input ("Enter a mode ( 0 or 1 )!\n" ) 
-print( findFactors( int( number ) , int( mode )))
+number = input ( "Enter a positive number!\n" )
+mode = input ( "Enter a mode ( 0 or 1 )!\n")
+print( primecount( int( number ) , int( mode ) ) )
