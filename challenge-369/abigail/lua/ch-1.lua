a339f89286c3f58451a01e47931a42cac89c7bf3
Abigail <abigail@abigail.freedom.nl>
2026-04-13 21:47:11 +0200

    Lua solution for week 369/1

---
+++ challenge-369/abigail/lua/ch-1.lua
@@ -0,0 +1,23 @@
+#!/opt/homebrew/bin/lua
+ 
+ 
+for line in io . lines () do
+    local input = line
+    input = input : gsub ("^%s+", "")     -- Remove leading white space
+    input = input : gsub ("[^%s%a]+", "") -- Remove non-letters, but keep space
+    input = input : gsub ("%s+$", "")     -- Remove trailing white space
+    input = input : lower ()              -- Lower case entire input
+
+    --
+    -- For each lower case letter, 'a' .. 'z', if we find one following
+    -- space, upper case the letter while deleting the space
+    --
+    for i = string . byte ("a"), string . byte ("z") do
+        local ch = string . char (i)
+        input = input : gsub ("%s+" .. ch, ch : upper ())
+    end
+
+    input = "#" .. input                  -- Add leading '#'
+
+    print (input : sub (0, 100))          -- Print at most 100 characters
+end
