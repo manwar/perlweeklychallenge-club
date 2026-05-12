4c2172a56bcf43e0a2660396f391069a306f55aa
KjetilS <kjetilskotheim@gmail.com>
2026-05-12 12:49:48 +0200

    https://theweeklychallenge.org/blog/perl-weekly-challenge-373

--- challenge-373/kjetillll/perl/ch-1.pl
+++ challenge-373/kjetillll/perl/ch-1.pl
@@ -1,9 +1,10 @@
-sub f { "@{[ map @$_, @_ ]}" =~ s/ //gr =~ /^(.*)\1$/ }
+sub f { join(",", map "@$_", @_) =~ s/ //gr =~ /^(.*),\1$/ }
 print pop(@$_) ^ f(@$_) ? "ERROR\n" : "ok\n" for
 [ ["a", "bc"],           ["ab", "c"]     => 1 ],
 [ ["a", "b", "c"],       ["a", "bc"]     => 1 ],
 [ ["a", "bc"],           ["a", "c", "b"] => 0 ],
 [ ["ab", "c", ""],       ["", "a", "bc"] => 1 ],
 [ ["p", "e", "r", "l"],  ["perl"]        => 1 ],
+[ ["p", "e", "p"],       ["e"]           => 0 ],
 
 # https://theweeklychallenge.org/blog/perl-weekly-challenge-373
