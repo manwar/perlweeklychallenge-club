29f23943cf0f445a486956c480ce7472b487b8eb
Bob Lied <boblied+github@gmail.com>
2026-05-18 16:27:08 -0500

    Week 374 solutions

---
+++ challenge-374/bob-lied/perl/ch-2.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# Copyright (c) 2026, Bob Lied
+#=============================================================================
+# ch-2.pl Perl Weekly Challenge 374 Task 2  Largest Same-Digits Number
+#=============================================================================
+# You are given a string containing 0-9 digits only.  Write a script to
+# return the largest number with all digits the same in the given string.
+# Example 1 Input: $str = "6777133339" Output: 3333
+# Example 2 Input: $str = "1200034"    Output: 4
+# Example 3 Input: $str = "44221155"   Output: 55
+# Example 4 Input: $str = "88888"      Output: 88888
+# Example 5 Input: $str = "11122233"   Output: 222
+#=============================================================================
+
+use v5.42;
+
+
+use Getopt::Long;
+my $Verbose = false;
+my $DoTest  = false;
+
+GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
+my $logger;
+{
+    use Log::Log4perl qw(:easy);
+    Log::Log4perl->easy_init({ level => ($Verbose ? $DEBUG : $INFO ),
+            layout => "%d{HH:mm:ss.SSS} %p{1} %m%n" });
+    $logger = Log::Log4perl->get_logger();
+}
+#=============================================================================
+
+exit(!runTest()) if $DoTest;
+
+say lsdn3($_) for @ARGV;
+
+#=============================================================================
+#
+# First solution: Extract repeating strings with a regular expression
+sub lsdn($str)
+{
+    use List::Util qw/max/;
+    return max ($str =~ m/((\d)\2*)/g);
+}
+
+#=============================================================================
+#
+# Second solution: Scan left to right as array, look at leading span of repeats
+sub lsdn2($str)
+{
+    use List::MoreUtils qw/before/;
+
+    my @n = split(//, $str);
+    my $biggest = $n[0];
+
+    while ( defined(my $digit = shift(@n)) )
+    {
+        my @span = before { $_ != $digit } @n;
+        my $val = 0+join("", $digit, @span);
+
+        $biggest = $val if $val > $biggest;
+
+        splice(@n, 0, scalar(@span));
+    }
+    return $biggest;
+}
+
+#=============================================================================
+#
+# Third solution: Similar to 2, but using substr instead of array manipulation
+sub lsdn3($str)
+{
+    my $biggest = substr($str,0,1);
+    
+    my $beg = 0;
+    while ( $beg < length($str) )
+    {
+        my $digit = substr($str, $beg, 1);
+        my $width = 1;
+        ++$width while ( substr($str, $beg+$width, 1) eq $digit );
+
+        my $val = 0+substr($str, $beg, $width);
+        $biggest = $val if $val > $biggest;
+        $beg += $width;
+    }
+    return $biggest;
+}
+
+sub runTest
+{
+    use Test2::V0;
+
+    is( lsdn3("6777133339"),  3333,  "Example 1");
+    is( lsdn3("1200034"   ),  4,     "Example 2");
+    is( lsdn3("44221155"  ),  55,    "Example 3");
+    is( lsdn3("88888"     ),  88888, "Example 4");
+    is( lsdn3("11122233"  ),  222,   "Example 5");
+
+    done_testing;
+}
