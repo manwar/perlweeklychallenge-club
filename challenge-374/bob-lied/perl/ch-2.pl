93ef64a5e6b250a4761b44af379ff3aec25b175b
Bob Lied <boblied+github@gmail.com>
2026-05-18 16:59:03 -0500

    Week 374 solutions, add benchmark for task 2

--- challenge-374/bob-lied/perl/ch-2.pl
+++ challenge-374/bob-lied/perl/ch-2.pl
@@ -16,12 +16,12 @@
 
 use v5.42;
 
-
 use Getopt::Long;
 my $Verbose = false;
 my $DoTest  = false;
+my $Benchmark = 0;
 
-GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
+GetOptions("test" => \$DoTest, "verbose" => \$Verbose, "benchmark:i" => \$Benchmark);
 my $logger;
 {
     use Log::Log4perl qw(:easy);
@@ -31,14 +31,24 @@ my $logger;
 }
 #=============================================================================
 
-exit(!runTest()) if $DoTest;
+my @solution = (
+    { func => \&lsdn_RE,   desc => "regex" },
+    { func => \&lsdn_LIST, desc => "list" },
+    { func => \&lsdn_STR,  desc => "substring" },
+);
 
-say lsdn3($_) for @ARGV;
+exit(!runTest()) if $DoTest;
+exit( runBenchmark($Benchmark) ) if $Benchmark;
+
+for my $s ( @solution )
+{
+    say "$s->{desc}: ", $s->{func}->($_) for @ARGV;
+}
 
 #=============================================================================
 #
 # First solution: Extract repeating strings with a regular expression
-sub lsdn($str)
+sub lsdn_RE($str)
 {
     use List::Util qw/max/;
     return max ($str =~ m/((\d)\2*)/g);
@@ -47,21 +57,17 @@ sub lsdn($str)
 #=============================================================================
 #
 # Second solution: Scan left to right as array, look at leading span of repeats
-sub lsdn2($str)
+sub lsdn_LIST($str)
 {
-    use List::MoreUtils qw/before/;
-
     my @n = split(//, $str);
     my $biggest = $n[0];
 
     while ( defined(my $digit = shift(@n)) )
     {
-        my @span = before { $_ != $digit } @n;
-        my $val = 0+join("", $digit, @span);
+        my $val = $digit;
+        $val .= shift(@n) while ( @n && $n[0] eq $digit );
 
         $biggest = $val if $val > $biggest;
-
-        splice(@n, 0, scalar(@span));
     }
     return $biggest;
 }
@@ -69,7 +75,7 @@ sub lsdn2($str)
 #=============================================================================
 #
 # Third solution: Similar to 2, but using substr instead of array manipulation
-sub lsdn3($str)
+sub lsdn_STR($str)
 {
     my $biggest = substr($str,0,1);
     
@@ -82,20 +88,47 @@ sub lsdn3($str)
 
         my $val = 0+substr($str, $beg, $width);
         $biggest = $val if $val > $biggest;
+
         $beg += $width;
     }
     return $biggest;
 }
 
+#=============================================================================
 sub runTest
 {
     use Test2::V0;
 
-    is( lsdn3("6777133339"),  3333,  "Example 1");
-    is( lsdn3("1200034"   ),  4,     "Example 2");
-    is( lsdn3("44221155"  ),  55,    "Example 3");
-    is( lsdn3("88888"     ),  88888, "Example 4");
-    is( lsdn3("11122233"  ),  222,   "Example 5");
+    my @test = (
+        { input => "6777133339", expect =>  3333, desc => "Example 1" },
+        { input => "1200034"   , expect =>     4, desc => "Example 2" },
+        { input => "44221155"  , expect =>    55, desc => "Example 3" },
+        { input => "88888"     , expect => 88888, desc => "Example 4" },
+        { input => "11122233"  , expect =>   222, desc => "Example 5" },
+    );
+
+    for my $s ( @solution )
+    {
+        for my $tc ( @test )
+        {
+            is( $s->{func}->($tc->{input}),  $tc->{expect},  "$s->{desc}, $tc->{desc}");
+        }
+    }
 
     done_testing;
+}
+
+#=============================================================================
+sub runBenchmark($repeat)
+{
+    use Benchmark qw/cmpthese/;
+
+    use List::Util qw/shuffle/;
+    my $input = join("", shuffle ( ( 0 .. 9 ) x 50 ) );
+
+    cmpthese($repeat, {
+            regex  => sub { lsdn_RE($input) },
+            list   => sub { lsdn_LIST($input) },
+            substr => sub { lsdn_STR($input) },
+        });
 }
