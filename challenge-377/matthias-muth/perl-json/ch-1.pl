1da75a5fa8ab3794df4a338ad41826160d3622bc
Matthias Muth <muthm@gmx.de>
2026-06-08 09:05:20 +0200

    Challenge 377 .json files and solution skeletons

---
+++ challenge-377/matthias-muth/perl-json/ch-1.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+#
+#       The Weekly Challenge - Perl & Raku
+#       (https://theweeklychallenge.org)
+#
+#       Challenge 377 Task 1: Reverse Existence
+#
+#       Perl solution by Matthias Muth.
+#
+
+use v5.36;
+
+sub reverse_existence( $str ) {
+    my @results;
+    return @results;
+}
+
+# Read test data from the accompanying 'ch-?.json' file and run the tests.
+# (Fixed boilerplate code, no changes needed.)
+
+use Test2::V0 qw( -no_srand );
+# Avoid messages about table width in case of test failures.
+# (Term::Table::Util::term_size comes with Test2::V0.)
+$ENV{TABLE_TERM_SIZE} //= Term::Table::Util::term_size() // 80;
+
+# Read the test data from the JSON file.
+use JSON::PP qw( decode_json );
+( my $json_file = $0 ) =~ s/\.pl$/.json/;
+note "reading tests from $json_file";
+my $json_text = -f $json_file
+    && do { local ( @ARGV, $/ ) = $json_file; <> }
+    or die "could not read test data from $json_file\n";
+my $json_data = decode_json( $json_text );
+
+# Convert any expected output strings "true" and "false"
+# as well as JSON Boolean constants true and false
+# into Test2::V0::Compare T and F conditions. 
+for ( @{ $json_data->{examples} } ) {
+    for ( @{ $_->{out} } ) {
+        if ( /^(?:(true)|(false))$/i || ref $_ eq "JSON::PP::Boolean" ) {
+            $_ = $1 ? T : $2 ? F : $_ ? T : F;
+        }
+    }
+}
+
+# Run the tests, calling the subroutine by its generated name.
+( my $sub_name = lc $json_data->{challenge}{name} ) =~ s/[^_a-z]+/_/g;
+no strict 'refs';
+is [ "::$sub_name"->( @{ $_->{in} } ) ], $_->{out}, $_->{name}
+    for @{ $json_data->{examples} };
+done_testing;
