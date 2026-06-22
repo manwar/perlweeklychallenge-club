413fd780cf7301c4719e28610b6e03b4b554963b
Matt Martini <matt.martini@gmail.com>
2026-06-22 12:36:57 -0400

    refactored to use PWC module

--- challenge-376/matt-martini/perl/ch-1.pl
+++ challenge-376/matt-martini/perl/ch-1.pl
@@ -14,29 +14,16 @@
 # 1 B W B W B W B W
 #   a b c d e f g h
 
-use 5.018;
-use strict;
-use warnings;
-use File::JSON::Slurper qw(read_json);
-use boolean             qw(true false);
-use Data::Printer;
-
+use Dev::Util::Syntax;
 use Test2::V0;
 plan tests => 5;
 
-my $json_ref = read_json( __FILE__ =~ s/pl$/json/r )
-    or die "ERROR: could not read test data from json file\n";
-my $challenge_ref  = $json_ref->{ challenge };
-my $input_vars_ref = $json_ref->{ input_vars };
-my $examples_ref   = $json_ref->{ examples };
-
-printf qq{Challenge: %s Task %s: %s\n\n},
-    $challenge_ref->{ week },
-    $challenge_ref->{ task },
-    $challenge_ref->{ name };
+use lib '.';
+use PWC;
+my $json_data = get_json_data();
 
 is( chessboard_squares( $_->{ in } ), $_->{ out }->[0], $_->{ name } )
-    for @{ $examples_ref };
+    for $json_data->{examples}->@*;
 
 sub alpha_2_num {
     my $alpha = shift;
@@ -52,16 +39,14 @@ sub sq_col {
 sub chessboard_squares {
     my $c1 = $_[0]->[0];
     my $c2 = $_[0]->[1];
-    printf qq{Input: %s = "%s, %s = "%s"\n},
-        $input_vars_ref->[0], $c1,
-        $input_vars_ref->[1], $c2;
+    print_inputs($json_data->{input_vars}, @_);
     my $result = false;
 
     if ( sq_col($c1) == sq_col($c2) ) {
         $result = true;
     }
 
-    printf qq{Output: %s\n}, $result;
+    print_outputs($result);
     return $result;
 }
 
