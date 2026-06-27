9d0f3b19d8acacbe6504b59fdd44d938a807731e
Mohammad Sajid Anwar <mohammad.anwar@yahoo.com>
2026-06-27 22:17:36 +0100

    - Added solutions by Ali Moradi. - Added solutions by Matthias Muth. - Added solutions by Wanderdoc. - Added solutions by Lubos Kolouch. - Added solutions by Eric Cheung.

---
+++ challenge-379/eric-cheung/python/ch-1.py
@@ -0,0 +1,8 @@
+
+## strInput = ""  ## Example 1
+## strInput = "reverse the given string"  ## Example 2
+## strInput = "Perl is Awesome"  ## Example 3
+## strInput = "v1.0.0-Beta!"  ## Example 4
+strInput = "racecar"  ## Example 5
+
+print (strInput[::-1])
