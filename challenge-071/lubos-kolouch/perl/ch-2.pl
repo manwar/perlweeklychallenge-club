a2babf392d1ae0c88d129daee91c037037455cc8
Lubos Kolouch <lubos@kolouch.net>
2026-08-20 09:09:47 +0200

    Add blog.txt and update solutions for Challenge 071

--- challenge-071/lubos-kolouch/perl/ch-2.pl
+++ challenge-071/lubos-kolouch/perl/ch-2.pl
@@ -1,77 +1,106 @@
-#!/usr/bin/perl
-use strict;
+#!/usr/bin/env perl
+use v5.38;
 use warnings;
+use experimental 'signatures';
 
-package Node;
-
-sub new {
-    my ( $class, $data ) = @_;
-    my $self = {
-        data => $data,
-        next => undef
-    };
-    return bless $self, $class;
-}
-
-package LinkedList;
-
-sub new {
-    my ($class) = @_;
-    my $self = { head => undef };
-    return bless $self, $class;
-}
-
-sub append {
-    my ( $self, $data ) = @_;
-    my $new_node = Node->new($data);
-    if ( !$self->{head} ) {
-        $self->{head} = $new_node;
-    }
-    else {
-        my $cur = $self->{head};
-        $cur = $cur->{next} while $cur->{next};
-        $cur->{next} = $new_node;
+package Node {
+    sub new ( $class, $data ) {
+        my $self = {
+            data => $data,
+            next => undef,
+        };
+        return bless $self, $class;
     }
 }
 
-sub remove_from_end {
-    my ( $self, $n ) = @_;
-    my $size = 0;
-    my $cur  = $self->{head};
-    while ($cur) {
-        $size++;
-        $cur = $cur->{next};
+package LinkedList {
+    sub new ($class) {
+        my $self = { head => undef };
+        return bless $self, $class;
     }
-    if ( $n >= $size ) {
-        $self->{head} = $self->{head}->{next};
+
+    sub append ( $self, $data ) {
+        my $new_node = Node->new($data);
+        if ( !$self->{head} ) {
+            $self->{head} = $new_node;
+        }
+        else {
+            my $cur = $self->{head};
+            $cur = $cur->{next} while $cur->{next};
+            $cur->{next} = $new_node;
+        }
     }
-    else {
-        $cur = $self->{head};
-        for ( 1 .. $size - $n - 1 ) {
+
+    sub remove_from_end ( $self, $n ) {
+        return if !$self->{head} || $n <= 0;
+
+        my $size = 0;
+        my $cur  = $self->{head};
+        while ($cur) {
+            $size++;
             $cur = $cur->{next};
         }
-        $cur->{next} = $cur->{next}->{next} if $cur->{next};
+
+        if ( $n >= $size ) {
+            $self->{head} = $self->{head}->{next};
+        }
+        else {
+            $cur = $self->{head};
+            for ( 1 .. $size - $n - 1 ) {
+                $cur = $cur->{next};
+            }
+            $cur->{next} = $cur->{next}->{next} if $cur->{next};
+        }
+    }
+
+    sub to_array ($self) {
+        my @values;
+        my $cur = $self->{head};
+        while ($cur) {
+            push @values, $cur->{data};
+            $cur = $cur->{next};
+        }
+        return \@values;
+    }
+
+    sub print ($self) {
+        my $arr = $self->to_array;
+        say join( " -> ", @$arr );
     }
 }
 
-sub print {
-    my ($self) = @_;
-    my @values;
-    my $cur = $self->{head};
-    while ($cur) {
-        push @values, $cur->{data};
-        $cur = $cur->{next};
+# Embedded tests
+if ( !@ARGV ) {
+    require Test::More;
+    Test::More->import();
+
+    {
+        my $ll = LinkedList->new();
+        $ll->append($_) for 1 .. 5;
+        $ll->remove_from_end(1);
+        is_deeply( $ll->to_array(), [ 1, 2, 3, 4 ], 'Remove last element (N=1)' );
     }
-    print join( " -> ", @values ), "\n";
-}
 
-# Tests
-my $ll = LinkedList->new();
-for my $i ( 1 .. 5 ) {
-    $ll->append($i);
-}
+    {
+        my $ll = LinkedList->new();
+        $ll->append($_) for 1 .. 5;
+        $ll->remove_from_end(2);
+        is_deeply( $ll->to_array(), [ 1, 2, 3, 5 ], 'Remove 2nd from end (N=2)' );
+    }
 
-for my $i ( 1 .. 6 ) {
-    $ll->remove_from_end($i);
-    $ll->print();
+    {
+        my $ll = LinkedList->new();
+        $ll->append($_) for 1 .. 5;
+        $ll->remove_from_end(5);
+        is_deeply( $ll->to_array(), [ 2, 3, 4, 5 ], 'Remove head (N=5)' );
+    }
+
+    {
+        my $ll = LinkedList->new();
+        $ll->append($_) for 1 .. 5;
+        $ll->remove_from_end(10);
+        is_deeply( $ll->to_array(), [ 2, 3, 4, 5 ], 'Remove out-of-range (N=10)' );
+    }
+
+    done_testing();
 }
--- challenge-071/lubos-kolouch/python/ch-1.py
+++ challenge-071/lubos-kolouch/python/ch-1.py
@@ -1,26 +1,56 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+"""
+Perl Weekly Challenge 071 - Task 1: Peak Elements
 
-from typing import List
+Write a script that prints all the peak elements in the given array.
+A peak element is an element that is greater than its neighbours.
+"""
+
+import unittest
 
 
-def find_peaks(arr: List[int]) -> List[int]:
+def find_peaks(arr: list[int]) -> list[int]:
+    """Find all peak elements in the array."""
     n = len(arr)
-    # Initialize peaks list with the first element if it's bigger than the second
-    peaks = [arr[0]] if arr[0] > arr[1] else []
+    if n == 0:
+        return []
+    if n == 1:
+        return [arr[0]]
 
-    # Iterate over the array checking for peaks
-    for i in range(1, n-1):
-        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
+    peaks: list[int] = []
+
+    # First element
+    if arr[0] > arr[1]:
+        peaks.append(arr[0])
+
+    # Middle elements
+    for i in range(1, n - 1):
+        if arr[i] > arr[i - 1] and arr[i] > arr[i + 1]:
             peaks.append(arr[i])
 
-    # Add the last element if it's bigger than the penultimate
+    # Last element
     if arr[-1] > arr[-2]:
         peaks.append(arr[-1])
 
     return peaks
 
 
-# Tests
-print(find_peaks([18, 45, 38, 25, 10, 7, 21, 6, 28, 48]))  # [48, 45, 21]
-print(find_peaks([47, 11, 32, 8, 1, 9, 39, 14, 36, 23]))  # [47, 32, 39, 36]
+class TestFindPeaks(unittest.TestCase):
+    def test_example_1(self) -> None:
+        self.assertEqual(
+            find_peaks([18, 45, 38, 25, 10, 7, 21, 6, 28, 48]),
+            [45, 21, 48],
+        )
+
+    def test_example_2(self) -> None:
+        self.assertEqual(
+            find_peaks([47, 11, 32, 8, 1, 9, 39, 14, 36, 23]),
+            [47, 32, 39, 36],
+        )
+
+    def test_single_element(self) -> None:
+        self.assertEqual(find_peaks([5]), [5])
+
+
+if __name__ == "__main__":
+    unittest.main()
